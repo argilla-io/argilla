c92952642369d8d35260cccb3aa4c37bc8de40d7
Francisco Aranda <francisco@recogn.ai>
2021-04-13 15:06:07 +0200

    build with additional docker tags

--- scripts/publish_gitlab_docker.sh
+++ scripts/publish_gitlab_docker.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-TAG=$(git rev-parse --short HEAD)
-
+IMAGE_NAME="registry.gitlab.com/recognai-team/biome/rubrix"
+COMMIT=$(git rev-parse --short HEAD)
+TAG=$(git describe --exact-match ${COMMIT})
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 DISTRIBUTION="$( $BASEDIR/build_distribution.sh )"
 
-docker build -t registry.gitlab.com/recognai-team/biome/rubrix:${TAG} .
-docker push registry.gitlab.com/recognai-team/biome/rubrix:${TAG}
+BUILD_TAGS="-t ${IMAGE_NAME}:${COMMIT} -t ${IMAGE_NAME}:latest "
+if [ ! -z "${TAG}" ]
+then
+  BUILD_TAGS="${BUILD_TAGS} -t ${IMAGE_NAME}:${TAG}"
+fi
+
+docker build ${BUILD_TAGS}  .
+docker push ${IMAGE_NAME} --all-tags
