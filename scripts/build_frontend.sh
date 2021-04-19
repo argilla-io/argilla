6903d6a4fe040c1a52f1c74cad7f3ad3fdc834dc
Francisco Aranda <frascuchon@gmail.com>
2021-04-19 16:02:45 +0200

    include basic workflow (#1)
    * testing ci workflows
    
    * fix: install build package
    
    * prepare docker image generation
    
    * only for releases

--- scripts/build_frontend.sh
+++ scripts/build_frontend.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TAG=$(git rev-parse --short HEAD)
-
-DIST_BUILD=$(cd frontend && npm install && ENABLE_SECURITY=1 DIST_FOLDER=../src/rubrix/server/static/secured npm run-script build)
-DIST_BUILD=$(cd frontend && npm install && ENABLE_SECURITY=0 DIST_FOLDER=../src/rubrix/server/static/unsecured npm run-script build)
+cd frontend \
+&& npm install \
+&& ENABLE_SECURITY=1 DIST_FOLDER=../src/rubrix/server/static/secured npm run-script build \
+&& ENABLE_SECURITY=0 DIST_FOLDER=../src/rubrix/server/static/unsecured npm run-script build
