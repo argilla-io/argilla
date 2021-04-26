d6fbd29aadd4e3e169fd1fb34ff605ce3e17e78d
Francisco Aranda <francisco@recogn.ai>
2021-04-26 23:32:44 +0200

    fix: build tests

--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -290,12 +290,12 @@ def test_token_classification_spans(monkeypatch):
         prediction=[("test", 1, 2)],  # Inicio y fin son consecutivos
         prediction_agent="test",
     )
-    with pytest.raises(Exception, match=r"Defined offset \[s\] is token misaligned"):
+    with pytest.raises(Exception, match=r"Defined offset \[s\] is a misaligned entity mention"):
         rubrix.log(item, name=dataset)
 
     item.prediction = [("test", 0, 6)]
     with pytest.raises(
-        Exception, match=r"Defined offset \[Esto e\] is token misaligned"
+        Exception, match=r"Defined offset \[Esto e\] is a misaligned entity mention"
     ):
         rubrix.log(item, name=dataset)
 
