713852260e3a4e52cd3cb3835f84a53f96be5575
David Fidalgo <david@recogn.ai>
2021-05-05 11:44:30 +0200

    Add tutorials to docs (#13)
    * Move tests&docs dependencies to condas environment_dev.yml file
    
    * include nbshpinx and docs

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,3 +54,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+nbsphinx_execute = 'never'
