5cadc1c60c10c160a9bd48eae659629fd474ab65
Francisco Aranda <francisco@recogn.ai>
2021-06-24 11:41:58 +0200

    refactor(security): auth provider factory and better configuration (#160)
    * feat(security): manage auth provider using factory
    
    * chore(security): mock auth provider when no security
    
    * chore(security): move security settings to local auth settings

---
+++ src/rubrix/server/security/auth_provider/__init__.py
@@ -0,0 +1,2 @@
+from .base import AuthProvider, MockAuthProvider
+from .local import LocalAuthProvider, create_local_auth_provider
--- src/rubrix/server/security/auth_provider/base.py
+++ src/rubrix/server/security/auth_provider/base.py
@@ -1,12 +1,14 @@
-from typing import Optional
-
-from fastapi.security import SecurityScopes
-from rubrix.server.users.model import User
-
+from fastapi.security import (
+    SecurityScopes,
+)
+from rubrix.server.users.model import MOCK_USER, User
 
 class AuthProvider:
-    async def get_user(
-        self, security_scopes: Optional[SecurityScopes], **kwargs
-    ) -> User:
+    """ Base class for auth provider"""
+    async def get_user(self, security_scopes: SecurityScopes, **kwargs) -> User:
         raise NotImplementedError()
 
+
+class MockAuthProvider(AuthProvider):
+    async def get_user(self, security_scopes: SecurityScopes) -> User:
+        return MOCK_USER
