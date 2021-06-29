a6c4e87b2bca8e63a9cdbff43e444148399f3d20
Ignacio Talavera <ignaciotalaveracepeda@gmail.com>
2021-06-29 18:13:56 +0200

    Fix/readme alignment (#171)
    * descriptions, keywords and license added
    
    * README alignment
    
    * description added readme
    
    * Update setup.py
    
    Co-authored-by: Daniel Vila Suero <daniel@recogn.ai>
    
    Co-authored-by: Daniel Vila Suero <daniel@recogn.ai>

--- setup.py
+++ setup.py
@@ -1,2 +1,24 @@
-import setuptools
-setuptools.setup(use_scm_version=True)
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="rubrix",
+    # other arguments omitted
+    description="Open-source tool for tracking, exploring and labelling data for AI projects.",
+    long_description=long_description,
+    author="recognai",
+    author_email="contact@recogn.ai",
+    maintainer="recognai",
+    maintainer_email="contact@recogn.ai",
+    url="https://recogn.ai",
+    license="Apache-2.0",
+    keywords="data-science natural-language-processing artificial-intelligence knowledged-graph developers-tools human-in-the-loop mlops",
+    long_description_content_type="text/markdown",
+    use_scm_version=True,
+)
