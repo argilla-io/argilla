c842246f42754b82bf0a1f3ba9163cc8d5f282da
Francisco Aranda <francisco@recogn.ai>
2021-07-07 17:43:27 +0200

    build only one statics folder

--- scripts/build_frontend.sh
+++ scripts/build_frontend.sh
@@ -2,5 +2,4 @@
 
 cd frontend \
 && npm install \
-&& ENABLE_SECURITY=1 DIST_FOLDER=../src/rubrix/server/static/secured npm run-script build \
-&& ENABLE_SECURITY=0 DIST_FOLDER=../src/rubrix/server/static/unsecured npm run-script build
+&& DIST_FOLDER=../src/rubrix/server/static npm run-script build
