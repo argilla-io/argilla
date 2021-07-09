6aed5ee524c0f8c135f2e9980d386f19f5bd4cda
Francisco Aranda <francisco@recogn.ai>
2021-07-09 12:31:17 +0200

    fix(build): set eslint prettier messages to warning (#203)
    * add nuxtjs/eslint-module
    
    * build: set eslint prettier messages to warning
    
    * remove nuxtjs/eslint-module

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     "vue/component-name-in-template-casing": ["error", "PascalCase"],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": "warn",
   },
   globals: {
     $nuxt: true,
