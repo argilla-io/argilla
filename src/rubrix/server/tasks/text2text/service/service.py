0ee3575786567ea24c13d0eaea46ae750629a71c
Francisco Aranda <francisco@recogn.ai>
2021-08-18 22:39:18 +0200

    feat(task): text2text task definition

---
+++ src/rubrix/server/tasks/text2text/service/service.py
@@ -0,0 +1,217 @@
+import datetime
+from typing import Any, Dict, Iterable, List, Optional
+
+from fastapi import Depends
+from rubrix.server.datasets.service import DatasetsService, create_dataset_service
+from rubrix.server.tasks.commons import BulkResponse
+from rubrix.server.tasks.commons.dao import extends_index_properties
+from rubrix.server.tasks.commons.dao.dao import DatasetRecordsDAO, dataset_records_dao
+from rubrix.server.tasks.commons.dao.model import RecordSearch
+from rubrix.server.tasks.commons.es_helpers import aggregations, filters
+from rubrix.server.tasks.text2text.api.model import (
+    CreationText2TextRecord,
+    ExtendedEsRecordDataFieldNames,
+    Text2TextQuery,
+    Text2TextRecord, Text2TextRecordDB,
+    Text2TextSearchAggregations,
+    Text2TextSearchResults,
+)
+
+extends_index_properties(
+    {
+        ExtendedEsRecordDataFieldNames.text_predicted: {
+            "type": "text",
+            "fielddata": True,
+            "analyzer": "multilingual_stop_analyzer",
+            "fields": {"extended": {"type": "text", "analyzer": "extended_analyzer"}},
+        },
+        ExtendedEsRecordDataFieldNames.text_annotated: {
+            "type": "text",
+            "fielddata": True,
+            "analyzer": "multilingual_stop_analyzer",
+            "fields": {"extended": {"type": "text", "analyzer": "extended_analyzer"}},
+        },
+    }
+)
+
+
+def as_elasticsearch(search: Text2TextQuery) -> Dict[str, Any]:
+    """Build an elasticsearch query part from search query"""
+
+    if search.ids:
+        return {"ids": {"values": search.ids}}
+
+    all_filters = filters.metadata(search.metadata)
+    query_filters = [
+        query_filter
+        for query_filter in [
+            filters.predicted_by(search.predicted_by),
+            filters.annotated_by(search.annotated_by),
+            filters.status(search.status),
+            filters.predicted(search.predicted),
+            filters.score(search.score),
+        ]
+        if query_filter
+    ]
+    query_text = filters.text_query(search.query_text)
+    all_filters.extend(query_filters)
+
+    return {
+        "bool": {
+            "must": query_text or {"match_all": {}},
+            "filter": {
+                "bool": {
+                    "should": all_filters,
+                    "minimum_should_match": len(all_filters),
+                }
+            },
+        }
+    }
+
+
+class Text2TextService:
+    """
+    Text2text service
+
+    """
+
+    def __init__(
+        self,
+        datasets: DatasetsService,
+        dao: DatasetRecordsDAO,
+    ):
+        self.__datasets__ = datasets
+        self.__dao__ = dao
+
+    def add_records(
+        self,
+        dataset: str,
+        owner: Optional[str],
+        records: List[CreationText2TextRecord],
+    ):
+        dataset = self.__datasets__.find_by_name(dataset, owner=owner)
+
+        db_records = []
+        now = datetime.datetime.now()
+        for record in records:
+            db_record = Text2TextRecordDB.parse_obj(record)
+            db_record.last_updated = now
+            db_records.append(db_record.dict(exclude_none=True))
+
+        failed = self.__dao__.add_records(
+            dataset=dataset,
+            records=db_records,
+        )
+        return BulkResponse(dataset=dataset.name, processed=len(records), failed=failed)
+
+    def search(
+        self,
+        dataset: str,
+        owner: Optional[str],
+        search: Text2TextQuery,
+        record_from: int = 0,
+        size: int = 100,
+    ) -> Text2TextSearchResults:
+        """
+        Run a search in a dataset
+
+        Parameters
+        ----------
+        dataset:
+            The dataset name
+        owner:
+            The dataset owner
+        search:
+            The search parameters
+        record_from:
+            The record from return results
+        size:
+            The max number of records to return
+
+        Returns
+        -------
+            The matched records with aggregation info for specified task_meta.py
+
+        """
+        dataset = self.__datasets__.find_by_name(dataset, owner=owner)
+
+        results = self.__dao__.search_records(
+            dataset,
+            search=RecordSearch(
+                query=as_elasticsearch(search),
+                aggregations={
+                    **aggregations.terms_aggregation(
+                        ExtendedEsRecordDataFieldNames.text_predicted
+                    ),
+                    **aggregations.terms_aggregation(
+                        ExtendedEsRecordDataFieldNames.text_predicted
+                    ),
+                },
+            ),
+            size=size,
+            record_from=record_from,
+        )
+        return Text2TextSearchResults(
+            total=results.total,
+            records=[Text2TextRecord.parse_obj(r) for r in results.records],
+            aggregations=Text2TextSearchAggregations(
+                **results.aggregations,
+                words=results.words,
+                metadata=results.metadata or {},
+            )
+            if results.aggregations
+            else None,
+        )
+
+    def read_dataset(
+        self,
+        dataset: str,
+        owner: Optional[str],
+        query: Optional[Text2TextQuery] = None,
+    ) -> Iterable[Text2TextRecord]:
+        """
+        Scan a dataset records
+
+        Parameters
+        ----------
+        dataset:
+            The dataset name
+        owner:
+            The dataset owner. Optional
+        query:
+            If provided, scan will retrieve only records matching
+            the provided query filters. Optional
+
+        """
+        dataset = self.__datasets__.find_by_name(dataset, owner=owner)
+        for db_record in self.__dao__.scan_dataset(
+            dataset, search=RecordSearch(query=as_elasticsearch(query))
+        ):
+            yield Text2TextRecord.parse_obj(db_record)
+
+
+_instance = None
+
+
+def text2text_service(
+    datasets: DatasetsService = Depends(create_dataset_service),
+    dao: DatasetRecordsDAO = Depends(dataset_records_dao),
+) -> Text2TextService:
+    """
+    Creates a dataset record service instance
+
+    Parameters
+    ----------
+    datasets:
+        The datasets service dependency
+    dao:
+        The dataset records dao dependency
+
+    Returns
+    -------
+        A dataset records service instance
+    """
+    global _instance
+    if not _instance:
+        _instance = Text2TextService(datasets=datasets, dao=dao)
+    return _instance
