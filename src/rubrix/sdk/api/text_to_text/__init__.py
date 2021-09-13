e5572d9c784809a4909d0fc08d9b0939f8d680ab
dcfidalgo <david@recogn.ai>
2021-09-13 18:44:22 +0200

    add copyright to 'src' and 'tests' files

--- src/rubrix/sdk/api/text_to_text/__init__.py
+++ src/rubrix/sdk/api/text_to_text/__init__.py
@@ -12,12 +12,3 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
-from rubrix.server.security.model import User
-
-
-class UserInDB(User):
-    """Internal user model"""
-
-    hashed_password: str
-    api_key: str
