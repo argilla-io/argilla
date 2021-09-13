bf61db1859d9628b96d275c4c226621f1c00cdbc
dcfidalgo <david@recogn.ai>
2021-09-13 18:49:44 +0200

    move module doc string below copyright

--- src/rubrix/sdk/__init__.py
+++ src/rubrix/sdk/__init__.py
@@ -1,4 +1,3 @@
-""" A client library for accessing Rubrix """
 #  coding=utf-8
 #  Copyright 2021-present, the Recognai S.L. team.
 #
@@ -13,5 +12,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+""" A client library for accessing Rubrix """
 
 from .client import AuthenticatedClient, Client
