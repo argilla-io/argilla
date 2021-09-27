6eea71791d4a1e3e7c23324eede75148964771b6
Francisco Aranda <frascuchon@gmail.com>
2021-09-27 06:39:23 +0200

    fix(ui): show metadata filters (#382)

--- frontend/components/commons/header/filters/FiltersList.vue
+++ frontend/components/commons/header/filters/FiltersList.vue
@@ -188,7 +188,9 @@ export default {
             group: "Metadata",
             placeholder: "Select options",
             id: key,
-            options: typeof filterContent === "object" ? undefined : filterContent,
+            options: Object.keys(filterContent).includes("rubrix:stats")
+              ? undefined
+              : filterContent,
             selected: (this.dataset.query.metadata || {})[key] || [],
           };
         });
