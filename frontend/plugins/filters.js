09e3356b0b1dfddc9d30b852a7696e8e4621fcda
LeireA <leire@recogn.ai>
2021-09-28 13:08:10 +0200

    format numbers based on locale

--- frontend/plugins/filters.js
+++ frontend/plugins/filters.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 
-var numeral = require("numeral");
-
 Vue.filter("formatNumber", function (value) {
-  return numeral(value).format("0,0"); // displaying other groupings/separators is possible, look at the docs
+  const locale = [
+    ...(navigator.languages || []),
+    navigator.language,
+    navigator.browserLanguage,
+    navigator.userLanguage,
+    navigator.systemLanguage
+  ].filter(Boolean)
+  return new Intl.NumberFormat(locale.length ? locale[0] : 'en').format(value);
 });
