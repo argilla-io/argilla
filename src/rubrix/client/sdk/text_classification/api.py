2d11b38f0d71387f511f7576c84560f2c8aa7053
dcfidalgo <david@recogn.ai>
2021-10-04 17:46:40 +0200

    add more detailed return type annotation

--- src/rubrix/client/sdk/text_classification/api.py
+++ src/rubrix/client/sdk/text_classification/api.py
@@ -12,12 +12,15 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from typing import Union
 
 import httpx
 from httpx import Response
-
 from rubrix.client.sdk._helpers import build_bulk_response
 from rubrix.client.sdk.client import AuthenticatedClient
+from rubrix.client.sdk.commons.models import BulkResponse
+from rubrix.client.sdk.commons.models import ErrorMessage
+from rubrix.client.sdk.commons.models import HTTPValidationError
 from rubrix.client.sdk.text_classification.models import TextClassificationBulkData
 
 
@@ -25,7 +28,7 @@ def bulk(
     client: AuthenticatedClient,
     name: str,
     json_body: TextClassificationBulkData,
-) -> Response:
+) -> Response[Union[BulkResponse, ErrorMessage, HTTPValidationError]]:
     url = "{}/api/datasets/{name}/TextClassification:bulk".format(
         client.base_url, name=name
     )
