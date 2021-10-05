db3b2e23a5c771a5c91abd0da17dc1869d9616ba
dcfidalgo <david@recogn.ai>
2021-10-05 12:30:12 +0200

    add textclassification from client tests

--- tests/client/sdk/text_classification/test_models.py
+++ tests/client/sdk/text_classification/test_models.py
@@ -12,10 +12,22 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import socket
+from datetime import datetime
 
-from rubrix.client.sdk.text_classification.models import TextClassificationBulkData
+import pytest
+from rubrix.client.models import TextClassificationRecord
+from rubrix.client.models import TokenAttributions
+from rubrix.client.sdk.text_classification.models import (
+    TextClassificationBulkData,
+    CreationTextClassificationRecord,
+)
+from rubrix.client.sdk.text_classification.models import TextClassificationQuery
 from rubrix.server.tasks.text_classification.api.model import (
     TextClassificationBulkData as ServerTextClassificationBulkData,
+)
+from rubrix.server.tasks.text_classification.api.model import (
+    TextClassificationQuery as ServerTextClassificationQuery,
 )
 
 
@@ -28,8 +40,60 @@ def test_bulk_data_schema(helpers):
     )
 
 
-def test_from_client():
-    raise NotImplementedError
+def test_query_schema(helpers):
+    client_schema = TextClassificationQuery.schema()
+    server_schema = ServerTextClassificationQuery.schema()
+
+    assert helpers.remove_description(client_schema) == helpers.remove_description(
+        server_schema
+    )
+
+
+def test_from_client_explanation():
+    token_attributions = [
+        TokenAttributions(token="test", attributions={"label1": 1.0, "label2": 2.0})
+    ]
+    record = TextClassificationRecord(
+        inputs={"text": "test"},
+        prediction=[("label1", 0.5), ("label2", 0.5)],
+        annotation="label1",
+        explanation={"text": token_attributions},
+        event_timestamp=datetime(2000, 1, 1),
+        id=1,
+    )
+    sdk_record = CreationTextClassificationRecord.from_client(record)
+
+    assert sdk_record.explanation["text"] == token_attributions
+
+
+@pytest.mark.parametrize(
+    "annotation,expected", [("label1", 1), (["label1", "label2"], 2)]
+)
+def test_from_client_annotation(annotation, expected):
+    record = TextClassificationRecord(
+        inputs={"text": "test"},
+        annotation=annotation,
+        multi_label=True,
+    )
+    sdk_record = CreationTextClassificationRecord.from_client(record)
+
+    assert len(sdk_record.annotation.labels) == expected
+
+
+@pytest.mark.parametrize(
+    "agent,expected", [(None, socket.gethostname()), ("agent", "agent")]
+)
+def test_from_client_agent(agent, expected):
+    record = TextClassificationRecord(
+        inputs="test",
+        prediction=[("label1", 0.5), ("label2", 0.5)],
+        annotation="label1",
+        prediction_agent=agent,
+        annotation_agent=agent,
+    )
+    sdk_record = CreationTextClassificationRecord.from_client(record)
+
+    assert sdk_record.annotation.agent == expected
 
 
 def test_to_client():
