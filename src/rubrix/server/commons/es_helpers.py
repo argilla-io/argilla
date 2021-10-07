2c9f64a3459dabc8630c4a49eca42f20002f3423
Francisco Aranda <francisco@recogn.ai>
2021-10-07 11:20:20 +0200

    hotfix(server): parse metric aggregation results properly

--- src/rubrix/server/commons/es_helpers.py
+++ src/rubrix/server/commons/es_helpers.py
@@ -142,17 +142,30 @@ def parse_aggregations(
     def parse_buckets(buckets: List[Dict[str, Any]]) -> Dict[str, Any]:
         parsed = {}
         for bucket in buckets:
-            key, doc_count, meta = (
+            key, doc_count, meta, _from, _to = (
                 bucket.pop("key", None),
                 bucket.pop("doc_count", 0),
                 bucket.pop("meta", None),
+                bucket.pop("from", None),
+                bucket.pop("to", None),
             )
-            if len(bucket) == 1 and not ("from" in bucket or "to" in bucket):
-                k = [k for k in bucket if k not in ["key", "doc_count"]][0]
+            if len(bucket) == 1 and not (_from or _to):
+                k = [k for k in bucket][0]
                 parsed.update({key or k: parse_buckets(bucket[k].get("buckets", []))})
+            elif len(bucket) > 1:
+                key_metrics = {}
+                for metric_key, metric in list(bucket.items()):
+                    if "buckets" in metric:
+                        key_metrics.update(
+                            {metric_key: parse_buckets(metric.get("buckets", []))}
+                        )
+                    else:
+                        metric_values = list(metric.values())
+                        value = metric_values[0] if len(metric_values) == 1 else metric
+                        key_metrics[metric_key] = value
+                parsed.update({key: key_metrics})
             elif key:
                 parsed.update({key: doc_count})
-
         return parsed
 
     result = {}
@@ -162,7 +175,7 @@ def parse_aggregations(
         elif is_extended_stats_aggregation(values):
             result[key] = {"rubrix:stats": values}  # statistical aggregations
         else:
-            result.update(parse_buckets([values]))
+            result[key] = list(parse_buckets([values]).values())[0]
     return result
 
 
