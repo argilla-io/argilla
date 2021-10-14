72979585502830adb516b1fedcf2ab81ced80bfc
Francisco Aranda <francisco@recogn.ai>
2021-10-14 13:32:08 +0200

    refresh anntations only if persisted

--- frontend/components/token-classifier/results/RecordTokenClassification.vue
+++ frontend/components/token-classifier/results/RecordTokenClassification.vue
@@ -153,7 +153,6 @@ export default {
     updateRecordEntities(entities) {
       this.updateRecords({
         dataset: this.dataset,
-        updateMetrics: false,
         records: [
           {
             ...this.record,
