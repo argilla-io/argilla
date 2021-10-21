9c2877d3100a4a151c8b00df4686a561bb0bbfc0
Francisco Aranda <francisco@recogn.ai>
2021-10-21 11:49:22 +0200

    register task metrics

--- src/rubrix/server/tasks/commons/task_factory.py
+++ src/rubrix/server/tasks/commons/task_factory.py
@@ -1,13 +1,15 @@
-from typing import Any, Optional, Type
+from typing import Any, List, Optional, Type
 
 from pydantic import BaseModel
 
 from rubrix.server.tasks.commons import TaskType
+from rubrix.server.tasks.commons.metrics.model.base import BaseTaskMetrics
 
 
-class _TaskConfig(BaseModel):
-
+class TaskConfig(BaseModel):
+    task: TaskType
     query: Any
+    metrics: Optional[Type[BaseTaskMetrics]]
 
 
 class TaskFactory:
@@ -19,9 +21,25 @@ class TaskFactory:
         cls,
         task_type: TaskType,
         query_request: Type[Any],
+        metrics: Optional[Type[BaseTaskMetrics]] = None,
     ):
-        cls._REGISTERED_TASKS[task_type] = _TaskConfig(query=query_request)
+        if metrics:
+            metrics.configure_es_index()
+
+        cls._REGISTERED_TASKS[task_type] = TaskConfig(
+            task=task_type, query=query_request, metrics=metrics
+        )
 
     @classmethod
-    def get_task_by_task_type(cls, task_type: TaskType) -> Optional[_TaskConfig]:
+    def get_all(cls) -> List[TaskConfig]:
+        return [cfg for cfg in cls._REGISTERED_TASKS.values()]
+
+    @classmethod
+    def get_task_by_task_type(cls, task_type: TaskType) -> Optional[TaskConfig]:
         return cls._REGISTERED_TASKS.get(task_type)
+
+    @classmethod
+    def get_task_metrics(cls, task: TaskType) -> Optional[Type[BaseTaskMetrics]]:
+        config = cls.get_task_by_task_type(task)
+        if config:
+            return config.metrics
