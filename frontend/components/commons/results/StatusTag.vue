2f7abad3baab17c6ab43054317b20e8fdc7133a2
LeireA <leire@recogn.ai>
2021-10-27 13:54:49 +0200

    Status tag

---
+++ frontend/components/commons/results/StatusTag.vue
@@ -0,0 +1,43 @@
+<template>
+    <span class="status-tag">
+      <svgicon
+        name="check"
+        width="12"
+        height="12"
+        color="#ffffff"
+      ></svgicon>
+      {{title}}
+    </span>
+</template>
+
+<script>
+import "assets/icons/check";
+export default {
+  props: {
+    title: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.status-tag {
+  position: absolute;
+  top: 4em;
+  right: 2em;
+  display: flex;
+  align-items: center;
+  background: $secondary-color;
+  color: palette(white);
+  border-radius: 50px;
+  padding: 0.7em 1em;
+  @include font-size(12px);
+  font-weight: 600;
+  z-index: 0;
+  .svg-icon {
+    margin-right: 0.5em;
+  }
+}
+</style>
+
