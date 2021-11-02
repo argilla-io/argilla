1bcfd46d80d070378a8fcfbc75e3dba7a56b1f50
Francisco Aranda <francisco@recogn.ai>
2021-11-02 18:33:54 +0100

    add zero-shot classifier monitor

--- src/rubrix/monitoring/model_monitor.py
+++ src/rubrix/monitoring/model_monitor.py
@@ -3,7 +3,7 @@ import warnings
 from typing import Any
 
 from ._spacy import Language, ner_monitor
-from ._transformers import TextClassificationPipeline, classifier_monitor
+from ._transformers import Pipeline, huggingface_monitor
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -11,8 +11,8 @@ _LOGGER = logging.getLogger(__name__)
 def monitor(task_model: Any, dataset: str, sample_rate: float = 0.3):
     if isinstance(task_model, Language):
         return ner_monitor(task_model, dataset=dataset, sample_rate=sample_rate)
-    if isinstance(task_model, TextClassificationPipeline):
-        return classifier_monitor(task_model, dataset=dataset, sample_rate=sample_rate)
+    if isinstance(task_model, Pipeline):
+        return huggingface_monitor(task_model, dataset=dataset, sample_rate=sample_rate)
 
     warnings.warn(
         "The provided task model is not supported by monitoring module. "
