ae95dd63b0f85d9b62bd41372fd34fe92fdba5ac
Francisco Aranda <francisco@recogn.ai>
2021-11-04 10:06:28 +0100

    simplify unsupported models flow

--- src/rubrix/monitoring/model_monitor.py
+++ src/rubrix/monitoring/model_monitor.py
@@ -10,24 +10,20 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def monitor(task_model: Any, dataset: str, sample_rate: float = 0.3):
-    try:
-        model_monitor = None
-        if isinstance(task_model, Language):
-            model_monitor = ner_monitor(
-                task_model, dataset=dataset, sample_rate=sample_rate
-            )
-        elif isinstance(task_model, Pipeline):
-            model_monitor = huggingface_monitor(
-                task_model, dataset=dataset, sample_rate=sample_rate
-            )
-        if model_monitor is None:
-            raise ModelNotSupportedError()
-        return model_monitor
-    except ModelNotSupportedError:
-        warnings.warn(
-            "The provided task model is not supported by monitoring module. "
-            "Predictions won't be logged into rubrix"
+    model_monitor = None
+    if isinstance(task_model, Language):
+        model_monitor = ner_monitor(
+            task_model, dataset=dataset, sample_rate=sample_rate
         )
+    elif isinstance(task_model, Pipeline):
+        model_monitor = huggingface_monitor(
+            task_model, dataset=dataset, sample_rate=sample_rate
+        )
+    if model_monitor:
+        return model_monitor
 
+    warnings.warn(
+        "The provided task model is not supported by monitoring module. "
+        "Predictions won't be logged into rubrix"
+    )
     return task_model
-
