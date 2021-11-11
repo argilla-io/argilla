51f79c8a96ed37b26c3d78f783485b1a2d782192
Francisco Aranda <frascuchon@gmail.com>
2021-11-11 12:29:58 +0100

    bugfixes(metrics): prevent index out of range for tokenclass metrics (#608)
    * fix(metrics): prevent tokens index out of range
    
    * add tests

--- tests/functional_tests/test_log_for_token_classification.py
+++ tests/functional_tests/test_log_for_token_classification.py
@@ -32,3 +32,59 @@ def test_log_with_empty_tokens_list(monkeypatch):
             TokenClassificationRecord(id=0, text=text, tokens=[]),
             name=dataset,
         )
+
+
+def test_log_record_that_makes_me_cry(monkeypatch):
+    mocking_client(monkeypatch, client)
+    dataset = "test_log_record_that_makes_me_cry"
+    record = TokenClassificationRecord(
+        text="'Secret Story : Última hora' debuta con un pobre 8.7% en el access de Telecinco.. . PROGRAMAS CON MEJOR CUOTA DEL LUNES (POR CADENAS). . ",
+        tokens=[
+            "'",
+            "Secret",
+            "Story",
+            ":",
+            "Última",
+            "hora",
+            "'",
+            "debuta",
+            "con",
+            "un",
+            "pobre",
+            "8.7%",
+            "en",
+            "el",
+            "access",
+            "de",
+            "Telecinco",
+            "..",
+            ".",
+            "PROGRAMAS",
+            "CON",
+            "MEJOR",
+            "CUOTA",
+            "DEL",
+            "LUNES",
+            "(",
+            "POR",
+            "CADENAS",
+            ")",
+            ".",
+            ".",
+        ],
+        prediction=[("ENG", 60, 66)],
+        annotation=None,
+        prediction_agent=None,
+        annotation_agent=None,
+        id=None,
+        metadata={"section": "television", "newspaper": "eldiario"},
+        status="Default",
+        event_timestamp=None,
+    )
+    rubrix.delete(dataset)
+    rubrix.log(record, name=dataset)
+
+    records = rubrix.load(dataset,as_pandas=False)
+    assert len(records) == 1
+    assert records[0].text == record.text
+    assert records[0].tokens == record.tokens
