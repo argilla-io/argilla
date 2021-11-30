f25c8b62325ef34edd476e1a3d29374dd7d72998
Francisco Aranda <francisco@recogn.ai>
2021-11-30 16:31:55 +0100

    feat(monitoring): auto-monitor for flair ner models

--- src/rubrix/monitoring/model_monitor.py
+++ src/rubrix/monitoring/model_monitor.py
@@ -1,14 +1,17 @@
 import logging
 import warnings
-from typing import Any
+from typing import Any, Optional
 
 from ._spacy import Language, ner_monitor
 from ._transformers import Pipeline, huggingface_monitor
+from ._flair import SequenceTagger, flair_monitor
 
 _LOGGER = logging.getLogger(__name__)
 
 
-def monitor(task_model: Any, dataset: str, sample_rate: float = 0.3):
+def monitor(
+    task_model: Any, dataset: str, sample_rate: float = 0.3, agent: Optional[str] = None
+):
     model_monitor = None
     if isinstance(task_model, Language):
         model_monitor = ner_monitor(
@@ -18,7 +21,12 @@ def monitor(task_model: Any, dataset: str, sample_rate: float = 0.3):
         model_monitor = huggingface_monitor(
             task_model, dataset=dataset, sample_rate=sample_rate
         )
+    elif isinstance(task_model, SequenceTagger):
+        model_monitor = flair_monitor(
+            task_model, dataset=dataset, sample_rate=sample_rate
+        )
     if model_monitor:
+        model_monitor.agent = agent
         return model_monitor
 
     warnings.warn(
