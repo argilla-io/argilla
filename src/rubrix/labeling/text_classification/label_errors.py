54adc31016e66d5126eb7e9fdeecc0128a3358bc
dcfidalgo <david@recogn.ai>
2021-12-09 17:29:53 +0100

    fix import

--- src/rubrix/labeling/text_classification/label_errors.py
+++ src/rubrix/labeling/text_classification/label_errors.py
@@ -12,12 +12,12 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import logging
 from enum import Enum
 from typing import Dict, List, Tuple, Union
 
 import numpy as np
 
-from rubrix import logging
 from rubrix.client.models import TextClassificationRecord
 
 _LOGGER = logging.getLogger(__name__)
