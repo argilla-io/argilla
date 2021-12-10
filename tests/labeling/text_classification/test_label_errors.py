67e0f71e3bffa515183c16e8e004a4fbfa66d2b4
dcfidalgo <david@recogn.ai>
2021-12-10 17:59:35 +0100

    add missing tests

--- tests/labeling/text_classification/test_label_errors.py
+++ tests/labeling/text_classification/test_label_errors.py
@@ -18,11 +18,44 @@ import pytest
 
 import rubrix as rb
 from rubrix.labeling.text_classification import find_label_errors
-from rubrix.labeling.text_classification.label_errors import NoRecordsError, SortBy
+from rubrix.labeling.text_classification.label_errors import (
+    MissingPredictionError,
+    NoRecordsError,
+    SortBy,
+    _construct_s_and_psx,
+)
 from tests.server.test_helpers import client, mocking_client
 
 
-def test_sort_by():
+@pytest.fixture(
+    params=[False, True], ids=["single_label", "multi_label"], scope="module"
+)
+def records(request):
+    if request.param:
+        return [
+            rb.TextClassificationRecord(
+                inputs="test", annotation=anot, prediction=pred, multi_label=True, id=i
+            )
+            for i, anot, pred in zip(
+                range(2 * 6),
+                [["bad"], ["bad", "good"]] * 6,
+                [[("bad", 0.1), ("good", 0.9)], [("good", 0.9), ("bad", 0.01)]] * 6,
+            )
+        ]
+
+    return [
+        rb.TextClassificationRecord(
+            inputs="test", annotation=anot, prediction=pred, id=i
+        )
+        for i, anot, pred in zip(
+            range(2 * 6),
+            ["good", "bad"] * 6,
+            [[("bad", 0.9), ("good", 0.1)], [("good", 0.8), ("bad", 0.2)]] * 6,
+        )
+    ]
+
+
+def test_sort_by_enum():
     with pytest.raises(ValueError, match="mock is not a valid SortBy"):
         SortBy("mock")
 
@@ -79,42 +112,94 @@ def test_sort_by(monkeypatch, sort_by, expected):
     )
     find_label_errors(records=[record], sort_by=sort_by)
 
+
+def test_kwargs(monkeypatch, records):
+    is_multi_label = records[0].multi_label
+
+    def mock_get_noise_indices(s, psx, **kwargs):
+        assert kwargs == {
+            "multi_label": is_multi_label,
+            "sorted_index_method": "normalized_margin",
+            "mock": "mock",
+        }
+        return []
+
+    monkeypatch.setattr(
+        "cleanlab.pruning.get_noise_indices",
+        mock_get_noise_indices,
+    )
+
     with pytest.raises(
         ValueError, match="'sorted_index_method' kwarg is not supported"
     ):
-        find_label_errors(records=[record], sorted_index_method="mock")
+        find_label_errors(records=records, sorted_index_method="mock")
+
+    find_label_errors(records=records, mock="mock")
 
 
-@pytest.fixture(params=[False, True], ids=["single_label", "multi_label"])
-def dataset(request, monkeypatch):
+def test_construct_s_and_psx(records):
+    import numpy as np
+
+    s, psx = _construct_s_and_psx(records[:2])
+
+    if records[0].multi_label:
+        s_expected = np.array(
+            [
+                list([0]),
+                list([0, 1]),
+            ]
+        )
+        psx_expected = np.array(
+            [
+                [0.1, 0.9],
+                [0.01, 0.9],
+            ]
+        )
+    else:
+        s_expected = np.array([1, 0])
+        psx_expected = np.array(
+            [
+                [0.9, 0.1],
+                [0.2, 0.8],
+            ]
+        )
+
+    assert (s == s_expected).all()
+    assert (psx == psx_expected).all()
+
+
+def test_missing_predictions():
+    records = [
+        rb.TextClassificationRecord(
+            inputs="test", annotation="mock", prediction=[("mock2", 0.1)]
+        )
+    ]
+    with pytest.raises(
+        MissingPredictionError,
+        match="It seems predictions are missing for the label 'mock'",
+    ):
+        _construct_s_and_psx(records)
+
+    records.append(
+        rb.TextClassificationRecord(
+            inputs="test", annotation="mock", prediction=[("mock", 0.1)]
+        )
+    )
+    with pytest.raises(
+        MissingPredictionError,
+        match="It seems a prediction for 'mock' is missing in the following record",
+    ):
+        _construct_s_and_psx(records)
+
+
+@pytest.fixture
+def dataset(monkeypatch, records):
     mocking_client(monkeypatch, client)
 
     dataset = "dataset_for_label_errors"
 
-    if not request.param:
-        records = [
-            rb.TextClassificationRecord(
-                inputs="test", annotation=anot, prediction=pred, id=i
-            )
-            for i, anot, pred in zip(
-                range(2 * 6),
-                ["good", "bad"] * 6,
-                [[("bad", 0.9), ("good", 0.1)], [("good", 0.8), ("bad", 0.2)]] * 6,
-            )
-        ]
-    else:
-        records = [
-            rb.TextClassificationRecord(
-                inputs="test", annotation=anot, prediction=pred, multi_label=True, id=i
-            )
-            for i, anot, pred in zip(
-                range(2 * 6),
-                [["bad"], ["bad", "good"]] * 6,
-                [[("bad", 0.1), ("good", 0.9)], [("good", 0.6), ("bad", 0.05)]] * 6,
-            )
-        ]
-
     rb.log(records, name=dataset)
+
     yield dataset
     rb.delete(dataset)
 
