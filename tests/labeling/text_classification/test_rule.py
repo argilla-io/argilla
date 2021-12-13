7923600f7ba2f65ab6589f1f51e66c04a12fcb6d
Francisco Aranda <francisco@recogn.ai>
2021-12-13 16:27:56 +0100

    add missing tests

--- tests/labeling/text_classification/test_rule.py
+++ tests/labeling/text_classification/test_rule.py
@@ -21,8 +21,14 @@ from rubrix.client.sdk.text_classification.models import (
     CreationTextClassificationRecord,
     TextClassificationBulkData,
 )
-from rubrix.labeling.text_classification.rule import Rule, RuleNotAppliedError
-from tests.server.test_helpers import client
+from rubrix.labeling.text_classification.rule import (
+    Rule,
+    RuleMetrics,
+    RuleNotAppliedError,
+    rule_metrics,
+    rules_from_dataset,
+)
+from tests.server.test_helpers import client, mocking_client
 
 
 @pytest.fixture(scope="module")
@@ -84,3 +90,42 @@ def test_call(monkeypatch, log_dataset):
     records = load(log_dataset, as_pandas=False)
     assert rule(records[0]) == "negative"
     assert rule(records[1]) is None
+
+
+def test_dataset_rules(monkeypatch, log_dataset):
+    mocking_client(monkeypatch, client)
+
+    client.post(
+        f"/api/datasets/TextClassification/{log_dataset}/labeling/rules",
+        json={"query": "a query", "label": "LALA"},
+    )
+
+    rules = rules_from_dataset(log_dataset)
+    assert len(rules) == 1
+    assert rules[0].query == "a query"
+    assert rules[0].label == "LALA"
+
+
+@pytest.mark.parametrize(
+    ["rule", "expected_metrics"],
+    [
+        (
+            Rule(query="neg*", label="LALA"),
+            RuleMetrics(correct=0, incorrect=1, coverage=0.5, precision=0),
+        ),
+        (
+            Rule(query="neg*", label="negative"),
+            RuleMetrics(correct=1, incorrect=0, coverage=0.5, precision=1),
+        ),
+    ],
+)
+def test_dataset_rule_metrics(monkeypatch, log_dataset, rule, expected_metrics):
+    mocking_client(monkeypatch, client)
+
+    client.post(
+        f"/api/datasets/TextClassification/{log_dataset}/labeling/rules",
+        json={"query": rule.query, "label": rule.label},
+    )
+
+    metrics = rule_metrics(log_dataset, rule=rule)
+    assert metrics == expected_metrics
