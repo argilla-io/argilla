856da52f57b91ed3bf1632e1a5fe3bbb4bee20fc
leiyre <leire@recogn.ai>
2021-12-17 11:40:25 +0100

    fix(ui): align records and global action layouts #825

--- frontend/components/commons/results/ResultsList.vue
+++ frontend/components/commons/results/ResultsList.vue
@@ -178,9 +178,6 @@ export default {
     .fixed-header & {
       padding-bottom: 200px;
     }
-    &::-webkit-scrollbar {
-      -webkit-appearance: none;
-    }
     .--metrics & {
       @include media(">desktop") {
         width: 100%;
