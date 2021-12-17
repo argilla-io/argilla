5359e9f5f467bd9e2a4ed615b9015c8d5c8ec79e
LeireA <leire@recogn.ai>
2021-12-17 15:14:10 +0100

    move actions to text_classification

--- frontend/database/modules/text_classification.js
+++ frontend/database/modules/text_classification.js
@@ -26,6 +26,64 @@ const actions = {
       data: { labels },
     });
   },
+  async getRules(_,{ dataset }) {
+    const { response } = await ObservationDataset.api().get(
+      `/datasets/${dataset.task}/${dataset.name}/labeling/rules`,
+    );
+    return response.data;
+  },
+
+  async defineRule(_,{ dataset, label }) {
+    await ObservationDataset.api().post(
+      `/datasets/${dataset.task}/${dataset.name}/labeling/rules`,
+      {
+        query: dataset.query.text,
+        label: label,
+        description: dataset.query.text
+      }
+    );
+  },
+
+  async updateRule(_,{ dataset, label }) {
+    await ObservationDataset.api().patch(
+      `/datasets/${dataset.task}/${dataset.name}/labeling/rules/${dataset.query.text}`,
+      {
+        label: label,
+        description: dataset.query.text
+      }
+    );
+  },
+
+  async getRule(_,{ dataset, query}) {
+    try {
+      const { response } = await ObservationDataset.api().get(
+        `/datasets/${dataset.task}/${dataset.name}/labeling/rules/${query}`,
+      )
+      return response.data;
+    } catch {
+      return undefined;
+    }
+  },
+
+  async deleteRule(_,{ dataset, query }) {
+    await ObservationDataset.api().delete(
+      `/datasets/${dataset.task}/${dataset.name}/labeling/rules/${query}`,
+    );
+  },
+
+  async getRulesMetrics(_,{ dataset }) {
+    const { response } = await ObservationDataset.api().get(
+      `/datasets/${dataset.task}/${dataset.name}/labeling/rules/metrics`,
+    );
+    return response.data;
+  },
+
+  async getRuleMetricsByLabel(_,{ dataset, query, label }) {
+    const { response } = await ObservationDataset.api().get(
+      `/datasets/${dataset.task}/${dataset.name}/labeling/rules/${query}/metrics?label=${label}`,
+    );
+    return response.data;
+  }
 };
 
 export default {
