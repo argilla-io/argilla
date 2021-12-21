4cef6fb61f812015c0cd28fcb3f93645c5eab696
LeireA <leire@recogn.ai>
2021-12-21 14:59:51 +0100

    empty-rules icon

---
+++ frontend/assets/icons/empty-rules.js
@@ -0,0 +1,27 @@
+/*
+ * coding=utf-8
+ * Copyright 2021-present, the Recognai S.L. team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'empty-rules': {
+    width: 42,
+    height: 33,
+    viewBox: '0 0 42 33',
+    data: '<g _fill="none" fill-rule="evenodd"><g _stroke="#6B6B6C" stroke-width=".5"><path pid="0" d="M4 20v5M30 20v5M4.5 19.5h25M17 25V15" stroke-linecap="square"/><path pid="1" d="M.25 25.25h7.5v7.5H.25zM26.25 25.25h7.5v7.5h-7.5zM13.25 25.25h7.5v7.5h-7.5zM13.25 7.25h7.5v7.5h-7.5z"/></g><path pid="2" d="M39.288 2.112l-.002-.001A7.175 7.175 0 0034.2 0C30.23 0 27 3.23 27 7.2c0 1.978.803 3.772 2.1 5.075.004.004.005.01.01.014.004.005.01.006.014.01a7.176 7.176 0 005.075 2.099c3.97 0 7.199-3.23 7.199-7.199a7.177 7.177 0 00-2.11-5.087zM34.2.692c1.67 0 3.19.638 4.343 1.676l-9.174 9.173A6.474 6.474 0 0127.692 7.2 6.514 6.514 0 0134.2.692zm0 13.013a6.472 6.472 0 01-4.342-1.675l9.174-9.173a6.473 6.473 0 011.675 4.342 6.514 6.514 0 01-6.507 6.506z" _fill="#6B6B6C" fill-rule="nonzero"/></g>'
+  }
+})
