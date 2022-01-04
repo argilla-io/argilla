d6a32dcb7dc4eb4f218b9c796ef3b9083b847b6e
leiyre <leire@recogn.ai>
2022-01-04 11:42:43 +0100

    UI: New rule metrics layout (#861)
    * remove old rule metrics
    
    * remove old overall metrics
    
    * new metrics component
    
    * restructure metrics
    
    * styles
    
    * button position
    
    * metrics min height
    
    * metrics loading placeholder
    
    * fix metric item height
    
    * show loading only in records area when refresh
    
    * fix padding and font-size
    
    * fix update total metrics
    
    * fix lint
    
    * show all non-reactive buttons
    
    * fix styles
    
    * rules props
    
    * text style
    
    * change copy
    
    * labels as array
    
    * prevent empty rules
    
    * fix empty search for data filter
    
    * nw rule empty query component
    
    * refresh only general metrics when updating the rule
    
    * overall metrics transition

--- frontend/components/text-classifier/labeling-rules/RuleDefinition.vue
+++ frontend/components/text-classifier/labeling-rules/RuleDefinition.vue
@@ -2,24 +2,37 @@
   <div>
     <div class="rule__area">
       <div :class="[query ? 'active' : null, 'rule__container']">
+        <rule-empty-query :dataset="dataset" v-if="!query"/>
         <rule-labels-definition
+          v-else
           :current-rule="currentRule"
           :dataset="dataset"
           @update-rule="updateRule"
-        />
+          @update-labels="updateLabels"
+        >
+          <template v-if="recordsMetric" #records-metric>
+            Records <strong>{{ recordsMetric.value | formatNumber }}</strong>
+          </template>
+        </rule-labels-definition>
       </div>
-      <div class="rule__global-metrics">
-        <p class="global-metrics__title">Overall Metrics</p>
-        <rules-overall-metrics
+      <div class="rule__metrics">
+        <rules-metrics
+          @records-metric="onUpdateRecordsMetric"
+          title="Rule Metrics"
+          metrics-type="all"
+          :activeLabel="activeLabel"
           :key="refresh"
           :rules="rules"
           :dataset="dataset"
-        />
-        <re-button
-          class="rule__button button-quaternary--outline"
-          @click="showRulesList"
-          >Manage rules</re-button
         >
+          <template #button-bottom>
+            <re-button
+              class="rule__button button-quaternary--outline"
+              @click="showRulesList"
+              >Manage rules</re-button
+            >
+          </template>
+        </rules-metrics>
       </div>
     </div>
     <p class="rule__records" v-if="dataset.results.total > 0">
@@ -39,12 +52,14 @@ export default {
   data: () => {
     return {
       rules: [],
+      selectedLabels: [],
       currentRule: undefined,
+      recordsMetric: undefined,
       refresh: 0,
     };
   },
   async fetch() {
-    this.rules = await this.getRules({ dataset: this.dataset });
+    await this.getAllRules();
     this.currentRule = await this.getRule({
       dataset: this.dataset,
       query: this.query,
@@ -54,30 +69,41 @@ export default {
     query() {
       return this.dataset.query.text;
     },
+    activeLabel() {
+      return this.selectedLabels.length
+        ? this.selectedLabels[0]
+        : this.currentRule
+        ? this.currentRule.label
+        : undefined;
+    },
   },
   watch: {
     async query(n, o) {
       if (o !== n) {
-        await this.$fetch();
-      }
-    },
-    async currentRule(n, o) {
-      if (o !== n) {
         this.refresh++;
+        await this.$fetch();
       }
     },
   },
   methods: {
+    async getAllRules() {
+      this.rules = await this.getRules({ dataset: this.dataset });
+    },
     async showRulesList() {
       await this.dataset.viewSettings.enableRulesSummary();
     },
     async updateRule() {
-      this.refresh++;
       await this.$fetch();
     },
+    updateLabels(labels) {
+      this.selectedLabels = labels;
+    },
+    onUpdateRecordsMetric(met) {
+      this.recordsMetric = met;
+    },
     ...mapActions({
-      getRule: "entities/text_classification/getRule",
       getRules: "entities/text_classification/getRules",
+      getRule: "entities/text_classification/getRule",
     }),
   },
 };
@@ -86,6 +112,7 @@ export default {
 .rule {
   &__area {
     display: flex;
+    margin-bottom: 2em;
   }
   &__container {
     padding: 20px;
@@ -93,51 +120,8 @@ export default {
     border: 1px solid $lighter-color;
     width: 100%;
     border-radius: 5px;
-    margin-bottom: 2em;
     &.active {
       box-shadow: 0 1px 4px 0 rgba(185, 185, 185, 0.5);
-    }
-  }
-  &__global-metrics {
-    width: 290px;
-    min-width: 290px;
-    max-height: 410px;
-    background: $primary-color;
-    margin-left: 1em;
-    color: $lighter-color;
-    border-radius: 5px;
-    margin-bottom: 2em;
-    padding: 20px;
-    display: flex;
-    flex-flow: column;
-    .global-metrics {
-      &__title {
-        padding-bottom: 0;
-        color: $lighter-color;
-        @include font-size(22);
-        font-weight: bold;
-        margin-top: 0;
-      }
-    }
-    &::v-deep {
-      .rules-global__metrics p {
-        width: 49%;
-        margin-right: 0;
-        color: $lighter-color;
-        padding-bottom: 2em;
-        @include font-size(14px);
-        &[data-title] {
-          &:after,
-          &:before {
-            display: none;
-          }
-        }
-        span {
-          color: $lighter-color;
-          @include font-size(20px);
-          margin-top: 0.3em;
-        }
-      }
     }
   }
   &__records {
@@ -148,6 +132,23 @@ export default {
     align-self: flex-start;
     margin-bottom: 0 !important;
     margin-top: auto;
+    clear: both;
+  }
+  &__metrics {
+    min-width: 350px;
+    @include media(">desktopLarge") {
+      min-width: 33%;
+    }
+    &::v-deep {
+      .rule-metrics__container {
+        flex-wrap: wrap;
+        height: 100%;
+      }
+      .rule-metrics__item {
+        width: 49%;
+        display: inline-block;
+      }
+    }
   }
 }
 </style>
