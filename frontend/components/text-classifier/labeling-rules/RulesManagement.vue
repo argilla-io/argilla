3670d689ecf780b5ce99ae79e71f55bff489a519
LeireA <leire@recogn.ai>
2022-01-13 14:26:56 +0100

    clean up

--- frontend/components/text-classifier/labeling-rules/RulesManagement.vue
+++ frontend/components/text-classifier/labeling-rules/RulesManagement.vue
@@ -236,7 +236,6 @@ export default {
     async updateSummary() {
       this.isLoading = true;
       try {
-        await this.dataset.refreshRules();
         await this.dataset.refreshRulesMetrics();
       } finally {
         this.isLoading = false;
