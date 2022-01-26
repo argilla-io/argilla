b6fc8b4b7d7a776ed4f602b7a040bc739071a1dc
Francisco Aranda <francisco@recogn.ai>
2022-01-26 18:29:12 +0100

    docs: generate api doc for error responses

--- src/rubrix/server/routes.py
+++ src/rubrix/server/routes.py
@@ -19,18 +19,15 @@ set the required security dependencies if api security is enabled
 """
 
 from fastapi import APIRouter
-from rubrix.server.commons.errors import ErrorMessage
 
+from .commons.errors.base_errors import __ALL__
 from .datasets import api as datasets
 from .info import api as info
 from .tasks import api as tasks
 from .users import api as users
 
 api_router = APIRouter(
-    responses={
-        "404": {"model": ErrorMessage, "description": "Item not found"},
-        "500": {"model": ErrorMessage, "description": "Generic error"},
-    }
+    responses={error.HTTP_STATUS: error.api_documentation() for error in __ALL__}
 )
 
 
