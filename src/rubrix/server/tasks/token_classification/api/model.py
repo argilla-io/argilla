5d14700a44683500bc86c70d48567bc21081bfdf
Francisco Aranda <francisco@recogn.ai>
2022-02-03 23:22:03 +0100

    fix: remove computed record fields returned in API results

--- src/rubrix/server/tasks/token_classification/api/model.py
+++ src/rubrix/server/tasks/token_classification/api/model.py
@@ -289,7 +289,7 @@ class CreationTokenClassificationRecord(BaseRecord[TokenClassificationAnnotation
         underscore_attrs_are_private = True
 
 
-class TokenClassificationRecord(CreationTokenClassificationRecord):
+class TokenClassificationRecordDB(CreationTokenClassificationRecord):
     """
     The main token classification task record
 
@@ -306,14 +306,6 @@ class TokenClassificationRecord(CreationTokenClassificationRecord):
     _predicted: Optional[PredictionStatus] = Field(alias="predicted")
 
     def extended_fields(self) -> Dict[str, Any]:
-        return {
-            **super().extended_fields(),
-            "raw_text": self.text,  # Maintain results compatibility
-        }
-
-
-class TokenClassificationRecordDB(TokenClassificationRecord):
-    def extended_fields(self) -> Dict[str, Any]:
         parent_fields = super().extended_fields()
         parent_fields.pop("raw_text")
 
@@ -329,6 +321,13 @@ class TokenClassificationRecordDB(TokenClassificationRecord):
                 for mention, entity in self.annotated_mentions()
             ],
             "words": self.all_text(),
+        }
+
+
+class TokenClassificationRecord(TokenClassificationRecordDB):
+    def extended_fields(self) -> Dict[str, Any]:
+        return {
+            "raw_text": self.text,  # Maintain results compatibility
         }
 
 
