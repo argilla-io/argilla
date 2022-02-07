4e2817df6c6ad540c96bf18f650200652d8b9a7f
LeireA <leire@recogn.ai>
2022-02-07 14:47:50 +0100

    set record collapsable height

--- frontend/components/text-classifier/results/RecordInputs.vue
+++ frontend/components/text-classifier/results/RecordInputs.vue
@@ -69,7 +69,7 @@ export default {
   }),
   computed: {
     visibleRecordHeight() {
-      return this.$mq === "lg" ? 480 : 146;
+      return this.$mq === "lg" ? 438 : 144;
     },
   },
   updated() {
@@ -101,10 +101,10 @@ export default {
   }
   &__collapsed {
     .record__content {
-      max-height: 146px;
+      max-height: 144px;
       overflow: hidden;
       @include media(">xxl") {
-        max-height: 480px;
+        max-height: 438px;
       }
     }
   }
