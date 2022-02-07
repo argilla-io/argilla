02c9ec825e26ce2bb921e317d670e17906a982d9
dcfidalgo <david@recogn.ai>
2022-02-07 18:16:18 +0100

    feat: add to from datasest for text classification

--- src/rubrix/client/dataset.py
+++ src/rubrix/client/dataset.py
@@ -12,9 +12,22 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from typing import List, Optional
+import logging
+from typing import Dict, List, Optional, Union
 
-from rubrix.client.models import Record
+import pandas as pd
+from pkg_resources import parse_version
+
+from rubrix.client.models import (
+    Record,
+    Text2TextRecord,
+    TextClassificationRecord,
+    TokenAttributions,
+    TokenClassificationRecord,
+)
+from rubrix.client.sdk.datasets.models import TaskType
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class Dataset:
@@ -25,6 +38,31 @@ class Dataset:
 
     Raises:
         MixedRecordTypesError: When the provided list contains more than one type of record.
+
+    Examples:
+        Pass in a list of records:
+        >>> import rubrix as rb
+        >>> records = [
+        ...     rb.TextClassificationRecord(inputs="example"),
+        ...     rb.TextClassificationRecord(inputs="another example"),
+        ... ]
+        >>> dataset = rb.Dataset(records)
+
+        Append records to an empty dataset:
+        >>> import rubrix as rb
+        >>> dataset = Dataset()
+        >>> for text in ["example", "another example"]:
+        ...     datasets.append(rb.TextClassificationRecord(inputs=text))
+
+        Index into the dataset:
+        >>> dataset[0]
+        ... rb.TextClassificationRecord(inputs={"text": "example"})
+        >>> dataset[0] = rb.TextClassificationRecord(inputs="replaced example")
+
+        Loop over the dataset:
+        >>> assert len(dataset) == 2
+        >>> for record in dataset:
+        ...     print(record)
     """
 
     def __init__(self, records: Optional[List[Record]] = None):
@@ -70,6 +108,183 @@ class Dataset:
         self._record_type = type(record)
 
         self._records.append(record)
+
+    def to_datasets(self) -> Optional["datasets.Dataset"]:
+        """Exports your records to a `datasets.Dataset`.
+
+        Returns:
+            A `datasets.Dataset` containing your records.
+        """
+        try:
+            import datasets
+        except ModuleNotFoundError:
+            raise ModuleNotFoundError(
+                "'datasets' must be installed to use `to_datasets`! "
+                "You can install 'datasets' with the command: `pip install datasets>1.17.0`"
+            )
+        if not (parse_version(datasets.__version__) > parse_version("1.17.0")):
+            raise ImportError(
+                "Version >1.17.0 of 'datasets' must be installed to use `to_datasets`! "
+                "You can update 'datasets' with the command: `pip install -U datasets>1.17.0`"
+            )
+
+        if self._record_type is TextClassificationRecord:
+            return self._textclassification_to_datasets()
+        if self._record_type is TokenClassificationRecord:
+            return self._tokenclassification_to_datasets()
+        if self._record_type is Text2TextRecord:
+            return self._text2text_to_datasets()
+
+    @classmethod
+    def from_datasets(
+        cls,
+        dataset: "datasets.Dataset",
+        task: Union[str, TaskType] = "TextClassification",
+    ) -> "Dataset":
+        """Imports records from a `datasets.Dataset`.
+
+        Args:
+            dataset: A datasets Dataset from which to import the records.
+            task: The task defines the record type.
+
+        Returns:
+            The imported records in a Rubrix Dataset.
+        """
+        if isinstance(task, str):
+            task = TaskType(task)
+
+        if task is TaskType.text_classification:
+            return cls._datasets_to_textclassification(dataset)
+        if task is TaskType.token_classification:
+            raise NotImplementedError(f"{task} is still not implemented.")
+        if task is TaskType.text2text:
+            raise NotImplementedError(f"{task} is still not implemented.")
+        raise ValueError(f"Provided task {task} is not supported!")
+
+    def _textclassification_to_datasets(self) -> "datasets.Dataset":
+        """Helper method to put text classification records in a `datasets.Dataset`"""
+        # create a dict first, where we make the necessary transformations
+        ds_dict = {}
+        for key in self._records[0].__fields__:
+            if key == "prediction":
+                ds_dict[key] = [
+                    [{"label": pred[0], "score": pred[1]} for pred in rec.prediction]
+                    if rec.prediction is not None
+                    else None
+                    for rec in self._records
+                ]
+            elif key == "explanation":
+                ds_dict[key] = [
+                    {
+                        key: list(map(dict, tokattrs))
+                        for key, tokattrs in rec.explanation.items()
+                    }
+                    if rec.explanation is not None
+                    else None
+                    for rec in self._records
+                ]
+            elif key == "id":
+                ds_dict[key] = [str(rec.id) for rec in self._records]
+            else:
+                ds_dict[key] = [getattr(rec, key) for rec in self._records]
+
+        return self._dict_to_datasets(ds_dict)
+
+    @classmethod
+    def _datasets_to_textclassification(
+        cls,
+        dataset: "datasets.Dataset",
+    ) -> "Dataset":
+        """Helper method to import text classification records from a `datasets.Dataset`.
+
+        Args:
+            dataset: A datasets Dataset
+
+        Returns:
+            A Dataset.
+        """
+        records = []
+        for row in dataset:
+            row["inputs"] = {
+                key: val for key, val in row["inputs"].items() if val is not None
+            }
+            row["prediction"] = (
+                [
+                    (
+                        pred["label"],
+                        pred["score"],
+                    )
+                    for pred in row["prediction"]
+                ]
+                if row["prediction"] is not None
+                else None
+            )
+            row["explanation"] = (
+                {
+                    key: [TokenAttributions(**tokattr_kwargs) for tokattr_kwargs in val]
+                    for key, val in row["explanation"].items()
+                }
+                if row["explanation"] is not None
+                else None
+            )
+
+            records.append(TextClassificationRecord(**row))
+
+        return cls(records)
+
+    def _tokenclassification_to_datasets(self):
+        raise NotImplementedError
+
+    def _text2text_to_datasets(self):
+        raise NotImplementedError
+
+    def _dict_to_datasets(self, records_dict: Dict) -> "datasets.Dataset":
+        """Helper method to create a datasets Dataset from a dict, removing the 'metadata' key from the dict if necessary.
+
+        Args:
+            records_dict: A dict containing dataset records.
+
+        Returns:
+            A datasets Dataset object, without the metadata, if it is incompatible with the Dataset format.
+        """
+        from datasets import Dataset
+
+        try:
+            dataset = Dataset.from_dict(records_dict)
+        # try without metadata
+        except Exception:
+            del records_dict["metadata"]
+            dataset = Dataset.from_dict(records_dict)
+            _LOGGER.warning(
+                "The 'metadata' of the records were removed, since it was incompatible with the 'datasets' format."
+            )
+
+        return dataset
+
+    def to_pandas(self) -> pd.DataFrame:
+        """Exports your records to a `pandas.DataFrame`.
+
+        Returns:
+            A `datasets.Dataset` containing your records.
+        """
+        return pd.DataFrame(map(dict, self._records))
+
+    def from_pandas(
+        cls, dataframe: pd.DataFrame, task: Union[str, TaskType] = "TextClassification"
+    ) -> "Dataset":
+        if isinstance(task, str):
+            task = TaskType(task)
+
+        if task is TaskType.text_classification:
+            return cls(
+                [TextClassificationRecord(**row) for row in dataframe.itertuples()]
+            )
+        if task is TaskType.token_classification:
+            raise NotImplementedError(f"{task} is still not implemented.")
+        if task is TaskType.text2text:
+            raise NotImplementedError(f"{task} is still not implemented.")
+
+        raise ValueError(f"Provided task {task} is not supported!")
 
 
 class DatasetError(Exception):
--- tests/client/test_dataset.py
+++ tests/client/test_dataset.py
@@ -14,6 +14,7 @@
 #  limitations under the License.
 from typing import List
 
+import datasets
 import pytest
 
 import rubrix as rb
@@ -87,3 +88,30 @@ def test_append(records):
         match="You are only allowed to append a record of type .*TextClassificationRecord.* but you provided .*Text2TextRecord.*",
     ):
         dataset.append(rb.Text2TextRecord(text="mock"))
+
+
+@pytest.mark.parametrize(
+    "records",
+    ["singlelabel_textclassification_records", "multilabel_textclassification_records"],
+)
+def test_to_from_datasets_textclassification(records, request):
+    records = request.getfixturevalue(records)
+    expected_dataset = rb.Dataset(records)
+
+    ds_dataset = expected_dataset.to_datasets()
+
+    assert isinstance(ds_dataset, datasets.Dataset)
+    assert ds_dataset.column_names == list(expected_dataset[0].__fields__.keys())
+    assert ds_dataset.features["prediction"] == [
+        {"label": datasets.Value("string"), "score": datasets.Value("float64")}
+    ]
+
+    dataset = rb.Dataset.from_datasets(ds_dataset)
+
+    assert isinstance(dataset, rb.Dataset)
+    for rec, expected in zip(dataset, expected_dataset):
+        for col in expected.__fields__.keys():
+            # TODO: have to think about how we deal with `None`s
+            if col in ["metadata", "metrics"]:
+                continue
+            assert getattr(rec, col) == getattr(expected, col)
