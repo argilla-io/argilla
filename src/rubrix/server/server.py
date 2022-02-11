d48c220db1d1ef68d14f8569c81d43ba9c933bda
Francisco Aranda <francisco@recogn.ai>
2022-02-11 13:49:49 +0100

    fix: index records when logging (#1133)
    * fix(wrapper): wraps index not found error
    
    * fix(server): configure startup datasets dao properly
    
    * test: add missing tests

--- src/rubrix/server/server.py
+++ src/rubrix/server/server.py
@@ -93,7 +93,7 @@ def configure_app_startup(app: FastAPI):
             es_wrapper = create_es_wrapper()
             dataset_records: DatasetRecordsDAO = DatasetRecordsDAO(es_wrapper)
             datasets: DatasetsDAO = DatasetsDAO.get_instance(
-                es_wrapper, dataset_records
+                es_wrapper, records_dao=dataset_records
             )
             datasets.init()
             dataset_records.init()
