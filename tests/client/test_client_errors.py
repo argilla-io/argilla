3737e8d6f531403cbc6022a92bbad3f72c5d6b21
Francisco Aranda <francisco@recogn.ai>
2022-02-17 14:24:23 +0100

    test: fix tests

--- tests/client/test_client_errors.py
+++ tests/client/test_client_errors.py
@@ -1,9 +1,11 @@
 import pytest
 
 from rubrix.client.sdk.commons.errors import UnauthorizedApiError
+from tests.server.test_helpers import client, mocking_client
 
 
-def test_unauthorized_response_error(mocked_client):
+def test_unauthorized_response_error(monkeypatch):
+    mocking_client(monkeypatch, client)
 
     with pytest.raises(UnauthorizedApiError, match="Could not validate credentials"):
         import rubrix as rb
