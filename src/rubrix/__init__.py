169ec5a2569c81d0425c690cff92de12ec511afe
dcfidalgo <david@recogn.ai>
2022-02-28 10:32:14 +0100

    feat: add LazyRubrixModule

--- src/rubrix/__init__.py
+++ src/rubrix/__init__.py
@@ -15,48 +15,72 @@
 
 """This file reflects the user facing API."""
 
-from rubrix.client.api import (
-    copy,
-    delete,
-    get_workspace,
-    init,
-    load,
-    log,
-    set_workspace,
-)
-from rubrix.client.datasets import (
-    DatasetForText2Text,
-    DatasetForTextClassification,
-    DatasetForTokenClassification,
-    read_datasets,
-    read_pandas,
-)
-from rubrix.client.models import (
-    Text2TextRecord,
-    TextClassificationRecord,
-    TokenAttributions,
-    TokenClassificationRecord,
-)
+from typing import TYPE_CHECKING
+
 from rubrix.logging import configure_logging
-from rubrix.monitoring.model_monitor import monitor
 
 from . import _version
 
 __version__ = _version.version
 
-try:
+if TYPE_CHECKING:
+    from rubrix.client.api import (
+        copy,
+        delete,
+        get_workspace,
+        init,
+        load,
+        log,
+        set_workspace,
+    )
+    from rubrix.client.datasets import (
+        DatasetForText2Text,
+        DatasetForTextClassification,
+        DatasetForTokenClassification,
+        read_datasets,
+        read_pandas,
+    )
+    from rubrix.client.models import (
+        Text2TextRecord,
+        TextClassificationRecord,
+        TokenAttributions,
+        TokenClassificationRecord,
+    )
+    from rubrix.monitoring.model_monitor import monitor
     from rubrix.server.server import app
-except ModuleNotFoundError as ex:
-    _module_name = ex.name
 
-    def fallback_app(*args, **kwargs):
-        raise RuntimeError(
-            "\n"
-            f"Cannot start rubrix server. Some dependencies was not found:[{_module_name}].\n"
-            "Please, install missing modules or reinstall rubrix with server extra deps:\n"
-            "pip install rubrix[server]"
-        )
+# try:
+#     from rubrix.server.server import app
+# except ModuleNotFoundError as ex:
+#     _module_name = ex.name
+#
+#     def fallback_app(*args, **kwargs):
+#         raise RuntimeError(
+#             "\n"
+#             f"Cannot start rubrix server. Some dependencies was not found:[{_module_name}].\n"
+#             "Please, install missing modules or reinstall rubrix with server extra deps:\n"
+#             "pip install rubrix[server]"
+#         )
+#
+#     app = fallback_app
 
-    app = fallback_app
+_import_structure = {
+    "client.models": ["TextClassificationRecord"],
+    "server.server": ["app"],
+}
+_deprecated_import_structure = {"client.models": ["Record"]}
+
+import sys as _sys
+
+from .utils import _LazyRubrixModule
+
+_sys.modules[__name__] = _LazyRubrixModule(
+    __name__,
+    globals()["__file__"],
+    _import_structure,
+    deprecated_import_structure=_deprecated_import_structure,
+    module_spec=__spec__,
+    extra_objects={"__version__": __version__},
+)
 
 configure_logging()
