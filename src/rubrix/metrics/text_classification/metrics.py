b43e68375ffd9a263692fa0ad43e87461a5a6628
ruanchaves <ruanchaves93@gmail.com>
2022-03-02 11:27:47 +0100

    feat: add cautious_classification_report

--- src/rubrix/metrics/text_classification/metrics.py
+++ src/rubrix/metrics/text_classification/metrics.py
@@ -5,6 +5,60 @@ from rubrix.metrics import helpers
 from rubrix.metrics.models import MetricSummary
 
 
+def cautious_classification_report(
+    y_true,
+    y_pred,
+    labels=None,
+    target_names=None,
+    sample_weight=None,
+    digits=2,
+    output_dict=False,
+    zero_division="warn",
+    is_tie=None,
+):
+    try:
+        import sklearn
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError(
+            "'sklearn' must be installed to compute the metrics! "
+            "You can install 'sklearn' with the command: `pip install scikit-learn`"
+        )
+    from sklearn.metrics import classification_report
+
+    if not output_dict:
+        raise NotImplementedError(
+            "Formatted string output is not implemented for cautious_classification_report."
+        )
+
+    y_true_partial = y_true[~is_tie]
+    y_pred_partial = y_pred[~is_tie]
+
+    coverage = len(y_true_partial) / len(y_true)
+
+    report_partial = classification_report(
+        y_true_partial,
+        y_pred_partial,
+        labels=labels,
+        target_names=target_names,
+        sample_weight=sample_weight,
+        digits=digits,
+        output_dict=True,
+        zero_division=zero_division,
+    )
+
+    report_final = {}
+
+    accuracy = report_partial["accuracy"]
+
+    report_final["efficacy"] = (accuracy + coverage) / 2
+
+    report_final["fscore_cautious"] = 2 * (accuracy * coverage) / (accuracy + coverage)
+
+    report_final.update(report_partial)
+
+    return report_final
+
+
 def f1(name: str, query: Optional[str] = None) -> MetricSummary:
     """Computes the single label f1 metric for a dataset
 
