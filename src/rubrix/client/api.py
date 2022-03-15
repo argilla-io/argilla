3c8d2a5f9ecf1b8c3b4af30e4ffd7465166c0327
David Fidalgo <david@recogn.ai>
2022-03-15 15:50:32 +0100

    docs(#1242, #1240, #1221): Add env var section + other minor fixes (#1251)
    * docs: add dependency to prevent failing builds for docs
    
    * docs: improve doc string
    
    * docs: fix link
    
    * docs: add env vars section
    
    * docs: add quotation marks for pip install rubrix[server]
    
    * docs: update supported tasks list
    
    * docs: add first complete list of environment variables

--- src/rubrix/client/api.py
+++ src/rubrix/client/api.py
@@ -84,8 +84,8 @@ class Api:
     ):
         """Init the Python client.
 
-        Passing an api_url disables environment variable reading, which will provide
-        default values.
+        We will automatically init a default client for you when calling other client methods.
+        The arguments provided here will overwrite your corresponding environment variables.
 
         Args:
             api_url: Address of the REST API. If `None` (default) and the env variable ``RUBRIX_API_URL`` is not set,
