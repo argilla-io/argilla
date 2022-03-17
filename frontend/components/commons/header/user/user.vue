1697c0da7e65b0e24d7e080104cab5c5cda68415
LeireA <leire@recogn.ai>
2022-03-17 10:23:00 +0100

    new user dropdown

--- frontend/components/commons/header/user/user.vue
+++ frontend/components/commons/header/user/user.vue
@@ -1,51 +1,19 @@
 <template>
   <div v-if="$auth.loggedIn" v-click-outside="close" class="user">
     <a
-      v-if="currentWorkspace"
       class="user__button"
       href="#"
       @click.prevent="showSelector"
     >
-      {{ firstChar(currentWorkspace) }}
+      {{ firstChar(user.username) }}
     </a>
     <div v-if="visibleSelector && user" class="user__content">
+      <p class="user__name">{{ user.username }}</p>
       <p class="user__mail">{{ user.email }}</p>
-      <a
-        href="#"
-        class="user__workspace"
-        @click="selectWorkspace(user.username)"
-      >
-        <div
-          :class="[
-            currentWorkspace === user.username ? 'active' : null,
-            'user__workspace__circle',
-          ]"
-        >
-          {{ firstChar(user.username) }}
-        </div>
-        <p class="user__workspace__name">
-          {{ user.username }}<span>Private Workspace</span>
-        </p>
-      </a>
-      <p v-if="userWorkspaces">Team workspaces</p>
-      <a
-        v-for="workspace in userWorkspaces"
-        :key="workspace"
-        href="#"
-        class="user__workspace"
-        @click="selectWorkspace(workspace)"
-      >
-        <div
-          :class="[
-            currentWorkspace === workspace ? 'active' : null,
-            'user__workspace__circle',
-          ]"
-        >
-          {{ firstChar(workspace) }}
-        </div>
-        <p class="user__workspace__name">{{ workspace }}</p>
-      </a>
-      <a class="user__logout" href="#" @click.prevent="logout"> Log out </a>
+      <a class="user__link" href="https://docs.rubrix.ml/en/stable/" target="_blank">
+        <svgicon name="docs"></svgicon> View docs </a>
+      <a class="user__link" href="#" @click.prevent="logout">
+        <svgicon name="logout"></svgicon> Log out </a>
       <span class="copyright">© 2022 Rubrix ({{ rubrixVersion }})</span>
     </div>
   </div>
@@ -53,11 +21,8 @@
 
 <script>
 import { mapActions } from "vuex";
-import {
-  setWorkspace,
-  currentWorkspace,
-  NO_WORKSPACE,
-} from "@/models/Workspace";
+import "assets/icons/docs";
+import "assets/icons/logout";
 export default {
   data: () => {
     return {
@@ -68,19 +33,6 @@ export default {
   computed: {
     user() {
       return this.$auth.user;
-    },
-    userWorkspaces() {
-      return (this.user.workspaces || [])
-        .map((ws) => {
-          if (ws === "") {
-            return NO_WORKSPACE;
-          }
-          return ws;
-        })
-        .filter((ws) => ws !== this.user.username);
-    },
-    currentWorkspace() {
-      return currentWorkspace(this.$route);
     },
   },
   async fetch() {
@@ -102,12 +54,6 @@ export default {
     async logout() {
       await this.$auth.logout();
       await this.$auth.strategy.token.reset();
-    },
-    async selectWorkspace(workspace) {
-      if (this.currentWorkspace !== workspace) {
-        return await setWorkspace(this.$router, workspace);
-      }
-      this.close();
     },
   },
 };
@@ -144,61 +90,40 @@ $buttonSize: 30px;
   }
   &__content {
     position: absolute;
-    top: 3em;
+    top: 3.5em;
     right: 0;
+    padding-top: 1em;
     background: $lighter-color;
     border-radius: 3px;
-    @include font-size(12px);
-    font-weight: 600;
+    @include font-size(14px);
+    font-weight: 400;
     color: palette(grey, medium);
-    padding: 1.2em 1.2em 0.8em 1.2em;
     box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.5);
     min-width: 200px;
+    text-align: center;
     a {
       text-decoration: none;
     }
+  }
+  &__name {
+    color: palette(grey, dark);
+    @include font-size(16px);
+    margin-bottom: 0.5em;
+    margin-top: 0;
   }
   &__mail {
     margin-bottom: 1em;
     margin-top: 0;
   }
-  &__logout {
-    display: block;
-    @include font-size(14px);
-    margin-top: 0.5em;
-  }
-  &__workspace {
+  &__link {
     display: flex;
     align-items: center;
-    outline: none !important;
-    padding: 0.7em;
-    margin: 0 -0.5em 0 -0.5em;
-    transition: background-color 0.3s ease-in-out;
-    border-radius: 3px;
-    &:hover {
-      background: #f5f5f5;
-      transition: background-color 0.3s ease-in-out;
-    }
-    &__circle {
-      @extend %circle;
-      margin-right: 0.7em;
-      background: palette(grey, dark);
-      color: $lighter-color;
-      &.active {
-        background: $primary-color;
-      }
-    }
-    &__name {
-      color: palette(grey, dark);
-      margin-top: 0;
-      margin-bottom: 0;
-      line-height: 1.4em;
-      span {
-        display: block;
-        margin-top: 0;
-        margin-bottom: 0;
-        color: palette(grey, medium);
-      }
+    margin-top: 0.5em;
+    text-align: left;
+    color: palette(grey, medium);
+    margin-left: 1em;
+    .svg-icon {
+      margin-right: 0.5em;
     }
   }
 }
@@ -209,6 +134,8 @@ $buttonSize: 30px;
   font-weight: 400;
   color: palette(grey, dark);
   line-height: 1em;
-  margin-top: 1em;
+  margin-top: 1.5em;
+  padding: 1em;
+  background: #fcfcfc;
 }
 </style>
