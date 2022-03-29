d384713fb3f2bd0f525b079ce591f614b9e24790
leiyre <leire@recogn.ai>
2022-03-29 12:36:52 +0200

    fix(#1337): number of records in the overall annotated coverage (#1338)
    This PR fixes weak supervision overall annotated coverage metric
    
    Closes #1337
    
    (cherry picked from commit 6024639e994f91774d3683fe99f93e7622468c0a)

--- frontend/components/text-classifier/labeling-rules/RulesMetrics.vue
+++ frontend/components/text-classifier/labeling-rules/RulesMetrics.vue
@@ -120,7 +120,7 @@ export default {
             )} <span class="records-number">(${this.$options.filters.formatNumber(
               Math.round(
                 this.metricsTotal.coverage_annotated *
-                  this.dataset.globalResults.total
+                  this.metricsTotal.annotated_records
               ) || 0
             )} records)</span>`,
           },
