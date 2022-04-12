cd65334a01528b54c57401d0013487a1de9ac1f8
LeireA <leire@recogn.ai>
2022-04-12 12:13:29 +0200

    force break word in selectors

--- frontend/components/core/ReCheckbox.vue
+++ frontend/components/core/ReCheckbox.vue
@@ -130,6 +130,8 @@ $checkbox-color-dark: $primary-color;
     text-align: center;
     vertical-align: middle;
     text-align: center;
+    margin-right: 0;
+    margin-left: auto;
     .svg-icon {
       fill: $lighter-color;
       transform: scale(0);
@@ -147,9 +149,9 @@ $checkbox-color-dark: $primary-color;
     }
   }
   .checkbox-label {
-    height: $checkbox-size;
+    word-break: break-all;
     line-height: $checkbox-size;
-    margin-right: auto;
+    margin-right: 0.5em;
   }
   &--dark {
     &.checked {
@@ -169,7 +171,6 @@ $checkbox-color-dark: $primary-color;
 .checkbox-label {
   .dropdown--filter & {
     height: auto;
-    padding-right: 2em;
     white-space: normal;
     text-transform: none;
     word-break: break-all;
