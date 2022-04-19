d769a3de582e78db2be8f526b393d1e38619af22
Francisco Aranda <francisco@recogn.ai>
2022-04-19 12:04:27 +0200

    chore: add script to generate api spec for documentation

---
+++ scripts/update_api_docs_spec.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rm -rf docs/reference/api/v1/spec.json*
+
+python -m rubrix &
+RUBRIX_PID=$!
+
+sleep 5 && wget http://localhost:6900/api/v1/openapi.json -P docs/reference/api/v1/
+
+kill $RUBRIX_PID
