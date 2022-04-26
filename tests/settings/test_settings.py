247c1b1776e95500c00421d2b36d66ba0bd5a5d9
Francisco Aranda <francisco@recogn.ai>
2022-04-26 14:52:21 +0200

    test: add more tests

--- tests/settings/test_settings.py
+++ tests/settings/test_settings.py
@@ -30,3 +30,14 @@ def test_settings_workflow(mocked_client, settings_, wrong_settings):
         ValueError, match="Provided settings are not compatible with dataset task."
     ):
         settings.save_settings(dataset, wrong_settings)
+
+
+def test_settings_with_a_created_dataset(mocked_client):
+    dataset = "dataset-name"
+    rb.delete(dataset)
+    rb.log(rb.TextClassificationRecord(text="The input text"), name=dataset)
+
+    classification_settings = TextClassificationSettings(labels_schema={"L1", "L2"})
+    rb.settings.save_settings(dataset, classification_settings)
+    settings_ = rb.settings.load_settings(dataset)
+    assert settings_ == classification_settings
