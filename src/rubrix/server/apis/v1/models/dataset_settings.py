96f015627897116ac436eece32a86dba31f63da4
Francisco Aranda <francisco@recogn.ai>
2022-04-29 13:26:23 +0200

    refactor: accept a list string as labels schema

--- src/rubrix/server/apis/v1/models/dataset_settings.py
+++ src/rubrix/server/apis/v1/models/dataset_settings.py
@@ -1,6 +1,6 @@
-from typing import List, Optional, TypeVar
+from typing import List, Optional, TypeVar, Union
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class AbstractDatasetSettings(BaseModel):
@@ -16,7 +16,19 @@ class LabelsSchema(BaseModel):
         name: str = Field(description="The label name")
         description: Optional[str] = Field(None, description="The label description")
 
-    labels: List[Schema] = Field(description="A set of labels")
+    labels: Union[List[str], List[Schema]] = Field(description="A set of labels")
+
+    @validator("labels", pre=True)
+    def normalize_labels(cls, labels):
+        """
+        Labels schema accept a list of strings. Those string will be converted
+        into ``LabelsSchema.Schema`` objects
+        """
+        if not labels:
+            return labels
+        if isinstance(labels[0], str):
+            return [LabelsSchema.Schema(id=label, name=label) for label in labels]
+        return labels
 
 
 class WithLabelsSchemaSettings(AbstractDatasetSettings):
