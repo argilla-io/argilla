126e6f4d3ba53ce2bef5908c82174fa20b20551e
leiyre <leire@recogn.ai>
2022-05-12 11:17:43 +0200

    fix(#1447): change agent when validating records with annotation but default status (#1480)
    (cherry picked from commit eadb3df68044d36e71d6f1fe6bc5b20e72797544)

--- frontend/components/text-classifier/results/RecordTextClassification.vue
+++ frontend/components/text-classifier/results/RecordTextClassification.vue
@@ -86,11 +86,8 @@ export default {
       return this.dataset.viewSettings.viewMode === "labelling-rules";
     },
     allowValidate() {
-      const isBinary =
-        this.dataset.labels.length === 2 && !this.dataset.isMultiLabel;
       return (
         this.record.status !== "Validated" &&
-        !isBinary &&
         (this.record.annotation ||
           this.record.prediction ||
           this.dataset.isMultiLabel)
