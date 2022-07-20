670c3b0a20146b34750797d88b89eea7b3998d8b
leiyre <leire@recogn.ai>
2022-07-20 11:47:28 +0200

    fix: Show predicted OK/KO when predictions exist (#1620)
    This PR prevents the OK/KO from being displayed when there is any prediction
    
    Closes #1619

--- frontend/specs/components/text-classification/results/RecordTextClassification.spec.js
+++ frontend/specs/components/text-classification/results/RecordTextClassification.spec.js
@@ -7,34 +7,41 @@ const $route = {
   query: {},
 };
 
+const props = {
+  record: new TextClassificationRecord({
+    annotation: {
+      agent: "recognai",
+      labels: [{ class: "Test", score: 1 }],
+    },
+    inputs: {
+      text: "My text",
+    },
+    multi_label: false,
+    prediction: {
+      agent: "test",
+      labels: [
+        { class: "Test", score: 0.6 },
+        { class: "A", score: 0.4 },
+        { class: "B", score: 0.2 },
+      ],
+    },
+  }),
+  dataset: {
+    task: "TextClassification",
+    query: {
+      text: "mock test",
+    },
+    isMultiLabel: false,
+    viewSettings: {
+      viewMode: "explore",
+    },
+    labels: ["Test", "A", "B"],
+  },
+};
+
 function mountComponent() {
   return mount(Component, {
-    propsData: {
-      record: new TextClassificationRecord({
-        inputs: {
-          text: "My text",
-          multi_label: true,
-          prediction: {
-            agent: "test",
-            labels: [
-              { class: "Test", score: 0.6 },
-              { class: "A", score: 0.4 },
-              { class: "B", score: 0.2 },
-            ],
-          },
-        },
-      }),
-      dataset: {
-        task: "TextClassification",
-        query: {
-          text: "mock test",
-        },
-        viewSettings: {
-          annotationEnabled: false,
-        },
-        labels: ["Test", "A", "B"],
-      },
-    },
+    propsData: props,
     mocks: {
       $route,
     },
@@ -56,6 +63,18 @@ describe("RecordTextClassification", () => {
 
   test("renders properly", () => {
     const wrapper = mountComponent();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("renders with empty prediction correctly", () => {
+    const wrapper = mount(Component, {
+      propsData: {
+        ...props,
+        record: {
+          prediction: {},
+        },
+      },
+    });
     expect(wrapper).toMatchSnapshot();
   });
 });
