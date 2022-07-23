9e965fce5ed011063d1ef046baae3f756ab281a7
Ankush-Chander <ankush.watchtower@gmail.com>
2022-07-23 15:26:18 +0530

    fix(#1559): handle api connection err gracefully

--- src/rubrix/client/sdk/users/api.py
+++ src/rubrix/client/sdk/users/api.py
@@ -7,15 +7,17 @@ from rubrix.client.sdk.users.models import User
 
 def whoami(client: AuthenticatedClient) -> User:
     url = "{}/api/me".format(client.base_url)
-
-    response = httpx.get(
+    try:
+        response = httpx.get(
         url=url,
         headers=client.get_headers(),
         cookies=client.get_cookies(),
         timeout=client.get_timeout(),
-    )
+        )
 
-    if response.status_code == 200:
-        return User(**response.json())
+        if response.status_code == 200:
+            return User(**response.json())
 
-    handle_response_error(response, msg="Invalid credentials")
+        handle_response_error(response, msg="Invalid credentials")
+    except httpx.ConnectError as err:
+        raise Exception(f"Could not connect to api_url:{client.base_url}") from None
