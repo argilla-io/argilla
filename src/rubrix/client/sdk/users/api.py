854a972e3ad0e3beac9bdc62d79fdf8d53e15f84
Francisco Aranda <francisco@recogn.ai>
2022-08-02 15:39:17 +0200

    fix(Client): reusing the inner `httpx` client (#1640)
    Closes #1646
    
    (cherry picked from commit f9a80cc363b91bb29194f50e3e5129b1e0e36c27)

--- src/rubrix/client/sdk/users/api.py
+++ src/rubrix/client/sdk/users/api.py
@@ -6,16 +6,5 @@ from rubrix.client.sdk.users.models import User
 
 
 def whoami(client: AuthenticatedClient) -> User:
-    url = "{}/api/me".format(client.base_url)
-
-    response = httpx.get(
-        url=url,
-        headers=client.get_headers(),
-        cookies=client.get_cookies(),
-        timeout=client.get_timeout(),
-    )
-
-    if response.status_code == 200:
-        return User(**response.json())
-
-    handle_response_error(response, msg="Invalid credentials")
+    response = client.get("/api/me")
+    return User(**response)
