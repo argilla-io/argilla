a3e56694a64fde614068ebf07a0aa2d2bf9f0ff9
Tom Aarsen <Cubiegamedev@gmail.com>
2022-09-04 15:53:49 +0200

    test: Speed up slow test
    On my device, api.init() takes 2.5 seconds per execution. Running it 1000 times does not make the test any more robust than e.g. 20, so I've made this change to speed up the total test suite execution.\n\nIt is however noteworthy that this test is so slow on my device...

--- tests/client/test_init.py
+++ tests/client/test_init.py
@@ -5,7 +5,7 @@ def test_resource_leaking_with_several_inits(mocked_client):
     dataset = "test_resource_leaking_with_several_inits"
     api.delete(dataset)
 
-    for i in range(0, 1000):
+    for i in range(0, 20):
         api.init()
 
     for i in range(0, 10):
