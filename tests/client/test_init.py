357c49c8ec1b8a116acd2030007eb769e4dfac9b
Francisco Aranda <francisco@recogn.ai>
2022-09-06 15:39:36 +0200

    Update tests/client/test_init.py

--- tests/client/test_init.py
+++ tests/client/test_init.py
@@ -5,6 +5,7 @@ def test_resource_leaking_with_several_inits(mocked_client):
     dataset = "test_resource_leaking_with_several_inits"
     api.delete(dataset)
 
+    # TODO: review performance in Windows. See https://github.com/recognai/rubrix/pull/1702
     for i in range(0, 20):
         api.init()
 
