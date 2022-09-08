f947dfefb96e79965b3b1ba5e362da1c5a8e7caa
Francisco Aranda <francisco@recogn.ai>
2022-09-08 11:37:34 +0200

    ci: add type checking step (#1704)
    This PR adds a separate type checking workflow and includes several type fixes.
    Refs: #1675

--- src/rubrix/client/apis/base.py
+++ src/rubrix/client/apis/base.py
@@ -1,6 +1,6 @@
 from rubrix.client.sdk.client import AuthenticatedClient
 
 
-class AbstractApi:
+class AbstractApi(object):
     def __init__(self, client: AuthenticatedClient):
         self.__client__ = client
