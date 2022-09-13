0199aaf534a34530bcfd593eea92116ca3406801
Francisco Aranda <francisco@recogn.ai>
2022-09-13 16:57:49 +0200

    tests: adding missing tests

--- tests/client/test_init.py
+++ tests/client/test_init.py
@@ -1,8 +1,8 @@
 from rubrix.client import api
 
 
-def test_resource_leaking_with_several_inits(mocked_client):
-    dataset = "test_resource_leaking_with_several_inits"
+def test_resource_leaking_with_several_init(mocked_client):
+    dataset = "test_resource_leaking_with_several_init"
     api.delete(dataset)
 
     # TODO: review performance in Windows. See https://github.com/recognai/rubrix/pull/1702
@@ -16,3 +16,17 @@ def test_resource_leaking_with_several_inits(mocked_client):
         )
 
     assert len(api.load(dataset)) == 10
+
+
+def test_init_with_extra_headers(mocked_client):
+    expected_headers = {
+        "X-Custom-Header": "Mocking rules!",
+        "Other-header": "Header value",
+    }
+    api.init(extra_headers=expected_headers)
+    active_api = api.active_api()
+
+    for key, value in expected_headers.items():
+        assert (
+            active_api.client.headers[key] == value
+        ), f"{key}:{value} not in client headers"
