6be0b6887006a14c161364d68a99796827fbbe7a
Francisco Aranda <francisco@recogn.ai>
2022-09-30 10:39:46 +0200

    fix: using httpx.request instead of delete to pass request body

--- src/rubrix/client/sdk/client.py
+++ src/rubrix/client/sdk/client.py
@@ -123,7 +123,8 @@ class Client(_ClientCommonDefaults, _Client):
     @with_httpx_error_handler
     def delete(self, path: str, *args, **kwargs):
         path = self._normalize_path(path)
-        response = self.__httpx__.delete(
+        response = self.__httpx__.request(
+            method="DELETE",
             url=path,
             headers=self.get_headers(),
             *args,
