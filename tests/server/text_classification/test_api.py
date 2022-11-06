be7f877110e0a15da5a211a029385004b48f0ac5
ufukhurriyet <ufkabak@gmail.com>
2022-11-06 22:21:30 +0300

    test: add vectpr search test for text_classification server api tests

--- tests/server/text_classification/test_api.py
+++ tests/server/text_classification/test_api.py
@@ -181,6 +181,111 @@ def test_create_records_for_text_classification(mocked_client, telemetry_track_d
     telemetry_track_data.assert_called_once()
 
 
+def test_create_records_for_text_classification_vector_search(
+    mocked_client, telemetry_track_data
+):
+    dataset = "test_create_records_for_text_classification_vector_search"
+    assert mocked_client.delete(f"/api/datasets/{dataset}").status_code == 200
+    tags = {"env": "test", "class": "text classification"}
+    metadata = {"config": {"the": "config"}}
+    classification_bulk = TextClassificationBulkRequest(
+        tags=tags,
+        metadata=metadata,
+        records=[
+            TextClassificationRecord(**a)
+            for a in [
+                {
+                    "id": 0,
+                    "inputs": {"data": "my data"},
+                    "prediction": {
+                        "agent": "test",
+                        "labels": [
+                            {"class": "Test", "score": 0.3},
+                            {"class": "Mocking", "score": 0.7},
+                        ],
+                    },
+                    "embeddings": {"my_bert": {"vector": [10, 11, 12, 13]}},
+                },
+                {
+                    "id": 1,
+                    "inputs": {"data": "your data"},
+                    "prediction": {
+                        "agent": "test",
+                        "labels": [
+                            {"class": "Test", "score": 0.35},
+                            {"class": "Mocking", "score": 0.65},
+                        ],
+                    },
+                    "embeddings": {"my_bert": {"vector": [14, 15, 16, 17]}},
+                },
+                {
+                    "id": 2,
+                    "inputs": {"data": "their data"},
+                    "prediction": {
+                        "agent": "test",
+                        "labels": [
+                            {"class": "Test", "score": 0.4},
+                            {"class": "Mocking", "score": 0.6},
+                        ],
+                    },
+                    "embeddings": {"my_bert": {"vector": [14, 15, 16, 18]}},
+                },
+            ]
+        ],
+    )
+    response = mocked_client.post(
+        f"/api/datasets/{dataset}/TextClassification:bulk",
+        json=classification_bulk.dict(by_alias=True),
+    )
+
+    assert response.status_code == 200
+    bulk_response = BulkResponse.parse_obj(response.json())
+    assert bulk_response.dataset == dataset
+    assert bulk_response.failed == 0
+    assert bulk_response.processed == 3
+
+    response = mocked_client.get(f"/api/datasets/{dataset}")
+    assert response.status_code == 200
+    created_dataset = Dataset.parse_obj(response.json())
+    assert created_dataset.tags == tags
+    assert created_dataset.metadata == metadata
+
+    response = mocked_client.post(
+        f"/api/datasets/{dataset}/TextClassification:search", json={}
+    )
+
+    assert response.status_code == 200
+    results = TextClassificationSearchResults.parse_obj(response.json())
+    assert results.total == 3
+    assert all(hasattr(record, "embeddings") for record in results.records)
+    assert results.aggregations.dict(exclude={"score"}) == {
+        "annotated_as": {},
+        "annotated_by": {},
+        "metadata": {},
+        "predicted": {},
+        "predicted_as": {"Mocking": 3},
+        "predicted_by": {"test": 3},
+        "status": {"Default": 3},
+        "words": {"data": 3},
+    }
+
+    response = mocked_client.post(
+        f"/api/datasets/{dataset}/TextClassification:search",
+        json={
+            "query": {"embedding_name": "my_bert", "embedding_vector": [14, 15, 16, 17]}
+        },
+    )
+    assert response.status_code == 200
+    results = TextClassificationSearchResults.parse_obj(response.json())
+    assert results.total == 3
+    assert all(hasattr(record, "embeddings") for record in results.records)
+    assert [record.id for record in results.records] == [
+        1,
+        2,
+        0,
+    ]  ## similarity ordered records
+
+
 def test_partial_record_update(mocked_client):
     name = "test_partial_record_update"
     assert mocked_client.delete(f"/api/datasets/{name}").status_code == 200
