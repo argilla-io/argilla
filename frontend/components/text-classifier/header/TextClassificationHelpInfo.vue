60ccbdc59a65f327ebd76aca0bf56e5f8e702620
leire <leire@recogn.ai>
2022-11-15 16:36:12 +0100

    update test

--- frontend/components/text-classifier/header/TextClassificationHelpInfo.vue
+++ frontend/components/text-classifier/header/TextClassificationHelpInfo.vue
@@ -63,6 +63,7 @@ export default {
     visible: {
       type: Boolean,
       default: false,
+      required: true,
     },
   },
   methods: {
---
+++ frontend/specs/components/text-classification/header/TextClassificationHelpInfo.spec.js
@@ -0,0 +1,28 @@
+import { shallowMount } from "@vue/test-utils";
+import ComponentTextClassifierHelpInfo from "@/components/text-classifier/header/TextClassificationhelpInfo";
+
+const props = {
+  visible: true
+};
+
+let wrapper = null;
+beforeEach(() => {
+  wrapper = shallowMount(ComponentTextClassifierHelpInfo, {
+    propsData: props,
+  });
+});
+afterEach(() => {
+  wrapper.destroy();
+});
+describe("TextClassificationhelpInfo", () => {
+  let spy = jest.spyOn(console, "error");
+  afterEach(() => spy.mockReset());
+
+  test("Required property", () => {
+    expect(ComponentTextClassifierHelpInfo.props.visible.required).toBe(true);
+  });
+
+  test("renders properly", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
