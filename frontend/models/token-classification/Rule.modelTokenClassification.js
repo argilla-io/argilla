10759fcc14c5aa1548973d180017800f8daac43c
keithCuniah <keithcuniah@gmail.comh>
2022-11-18 16:01:28 +0100

    feat(Client): add param in Rule to know if it came from backend dataset

--- frontend/models/token-classification/Rule.modelTokenClassification.js
+++ frontend/models/token-classification/Rule.modelTokenClassification.js
@@ -11,12 +11,12 @@ class Rule extends Model {
       id: this.uid(),
       dataset_id: this.attr(null),
       label: this.string(null),
-      labeling_function: this.string(null),
+      labeling_function: this.attr(null),
       description: this.string(null),
       query: this.string(null),
       author: this.string(null),
       created_at: this.string(null),
-
+      is_saved_in_dataset: this.boolean(true),
       //name have been added to be able to reach out the specific rule from a dataset primarykey
       owner: this.string(null),
       name: this.string(null),
