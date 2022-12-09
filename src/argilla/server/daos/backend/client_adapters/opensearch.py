d6c7eec5f26afed8fa1a668a66416208ac4600b1
Francisco Aranda <francis@argilla.io>
2022-12-09 01:14:20 +0100

    ci: Using opensearch (#2013)
    Co-authored-by: Francisco Aranda <francisco@recogn.ai>

--- src/argilla/server/daos/backend/client_adapters/opensearch.py
+++ src/argilla/server/daos/backend/client_adapters/opensearch.py
@@ -70,7 +70,7 @@ class OpenSearchClient(IClientAdapter):
             settings={
                 "index": {
                     "knn": True,
-                    "knn.algo_param.ef_search": 100,
+                    "knn.algo_param.ef_search": 128,  # Ignored when engine=lucene
                 }
             },
         )
@@ -84,8 +84,8 @@ class OpenSearchClient(IClientAdapter):
                     "space_type": "l2",
                     "engine": "nmslib",
                     "parameters": {
+                        "m": 16,
                         "ef_construction": 128,
-                        "m": 24,
                     },
                 },
             }
