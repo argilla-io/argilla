84af2fddb9dcb92590806df2d68ed1fc55b0dd77
leire <leire@recogn.ai>
2022-12-14 13:18:08 +0100

    clean code

--- frontend/components/commons/results/similarity/SimilaritySearchContent.spec.js
+++ frontend/components/commons/results/similarity/SimilaritySearchContent.spec.js
@@ -38,34 +38,4 @@ describe("SimilaritySearchContentComponent", () => {
   it("expect to exist similaritySelectedVector", async () => {
     expect(wrapper.vm.similaritySelectedVector).toBeTruthy();
   });
-  // it("expect visible dropdown if multiple vectors and hidden direct button", async () => {
-  //   await wrapper.setProps({
-  //     vectors: [
-  //       {
-  //         id: "text_vector",
-  //         name: "text_vector",
-  //         value: [
-  //           -0.019986379891633987, 0.05246466398239136, 0.023972749710083008,
-  //         ],
-  //       },
-  //       {
-  //         id: "text_vector",
-  //         name: "text_vector",
-  //         value: [
-  //           -0.019986379891633987, 0.05246466398239136, 0.023972749710083008,
-  //         ],
-  //       },
-  //     ],
-  //   });
-  //   const dropdown = wrapper.find("#dropdown");
-  //   const directButton = wrapper.find("#find-similar-button");
-  //   expect(dropdown.exists()).toBe(true);
-  //   expect(directButton.exists()).toBe(false);
-  // });
-  // it("expect hidden dropdown if single vectors and visible direct button", async () => {
-  //   const dropdown = wrapper.find("#dropdown");
-  //   const directButton = wrapper.find("#find-similar-button");
-  //   expect(dropdown.exists()).toBe(false);
-  //   expect(directButton.exists()).toBe(true);
-  // });
 });
