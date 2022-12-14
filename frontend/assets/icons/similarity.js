70e0b9ee7d2ea9659e976f1612340fc912199e7b
leire <leire@recogn.ai>
2022-12-14 13:10:19 +0100

    include similarity icon

---
+++ frontend/assets/icons/similarity.js
@@ -0,0 +1,27 @@
+/*
+ * coding=utf-8
+ * Copyright 2021-present, the Recognai S.L. team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'similarity': {
+    width: 24,
+    height: 16,
+    viewBox: '0 0 24 16',
+    data: '<path pid="0" fill-rule="evenodd" clip-rule="evenodd" d="M11.352 12.267c.23.244.476.465.734.664a5.84 5.84 0 00.787-.74l-1.572.022.051.054zm-1.262-2.014l3.918-.053c.123-.382.205-.775.245-1.173l-4.425.307c.062.31.15.618.262.92zm3.747-4.033c.113.274.205.554.275.837l-4.331.3c.054-.357.141-.71.263-1.052l3.793-.085zm-2.56-1.918l1.257-.028a6.195 6.195 0 00-.62-.547 5.824 5.824 0 00-.636.575zm2.461-.89c2.83 3 2.703 7.61-.276 10.317 2.105.883 4.605.564 6.365-1.017 2.408-2.163 2.519-5.868.247-8.277-1.8-1.906-4.552-2.428-6.816-1.49.165.147.326.303.48.467zm7.118 10.39c-2.422 2.175-5.965 2.44-8.736.892-2.95 1.65-6.828 1.11-9.284-1.492-2.84-3.01-2.702-7.643.308-10.346C5.566.68 9.108.417 11.88 1.964c2.95-1.649 6.829-1.11 9.284 1.492 2.84 3.01 2.702 7.643-.308 10.346zm-10.594-.556c.154.164.315.32.48.467-2.264.939-5.017.416-6.816-1.49-2.272-2.409-2.162-6.114.247-8.277 1.76-1.58 4.26-1.9 6.365-1.017-2.98 2.707-3.106 7.317-.276 10.317z" _fill="url(#paint0_linear_1158_16406)"/><defs><linearGradient id="paint0_linear_1158_16406" x1="19.45" y1="1.639" x2="4.888" y2="15.377" gradientUnits="userSpaceOnUse"><stop stop-color="#FFB097"/><stop offset="1" stop-color="#F73D3D"/></linearGradient></defs>'
+  }
+})
