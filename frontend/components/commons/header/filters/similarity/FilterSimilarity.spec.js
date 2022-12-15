073c01e809af76ab78d638ddbf30f8c9d710966a
leire <leire@recogn.ai>
2022-12-15 10:19:10 +0100

    feat: similarity search component for filters area

---
+++ frontend/components/commons/header/filters/similarity/FilterSimilarity.spec.js
@@ -0,0 +1,46 @@
+import { shallowMount } from "@vue/test-utils";
+import FilterSimilarity from "./FilterSimilarity";
+
+let wrapper = null;
+const options = {
+  propsData: {
+    filterIsActive: false,
+  },
+};
+beforeEach(() => {
+  wrapper = shallowMount(FilterSimilarity, options);
+});
+
+afterEach(() => {
+  wrapper.destroy();
+});
+
+describe("FilterSimilarityComponent", () => {
+  it("render the component", () => {
+    expect(wrapper.is(FilterSimilarity)).toBe(true);
+  });
+  it("expect active filtered status to be hidden", async () => {
+    testIfActiveClassIsVisible(false);
+  });
+  it("expect active filtered status to be visible", async () => {
+    await wrapper.setProps({ filterIsActive: true });
+    testIfActiveClassIsVisible(true);
+  });
+  it("expect active filtered status to be visible", async () => {
+    await wrapper.setProps({ filterIsActive: true });
+    testIfActiveClassIsVisible(true);
+  });
+  it("expect to emit 'search-records'", async () => {
+    await wrapper.setProps({ filterIsActive: true });
+    const query = { query: { vector: undefined } };
+    const removeButton = wrapper.find(".filter__similarity__button");
+    removeButton.trigger("click");
+    wrapper.vm.$nextTick();
+    expect(wrapper.emitted("search-records")).toStrictEqual([[query]]);
+  });
+});
+
+const testIfActiveClassIsVisible = async (value) => {
+  const activeComponent = wrapper.find("#filter-active");
+  expect(activeComponent.exists()).toBe(value);
+};
