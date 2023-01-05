0bf6da3ea7d28c2bd0eb5c9099f7453c0dbd628e
Francisco Aranda <francisco@recogn.ai>
2023-01-05 11:24:07 +0100

    fix: make manifests and icon links relative

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -38,15 +38,15 @@ export default {
       { hid: "description", name: "description", content: "" },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/x-icon", href: "favicon.ico" },
       {
         rel: "apple-touch-icon",
         sizes: "180x180",
-        href: "/apple-touch-icon.png",
+        href: "apple-touch-icon.png",
       },
-      { rel: "icon", sizes: "32x32", href: "/favicon-32x32.png" },
-      { rel: "icon", sizes: "16x16", href: "/favicon-16x16.png" },
-      { rel: "manifest", href: "/site.webmanifest" },
+      { rel: "icon", sizes: "32x32", href: "favicon-32x32.png" },
+      { rel: "icon", sizes: "16x16", href: "favicon-16x16.png" },
+      { rel: "manifest", href: "site.webmanifest" },
     ],
   },
 
