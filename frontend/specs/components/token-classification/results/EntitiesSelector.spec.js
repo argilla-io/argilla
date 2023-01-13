3d2db506cc4a82c48f1965b5797f0a3a4dedc439
keithCuniah <keithcuniah@gmail.comh>
2023-01-13 11:10:07 +0100

    feat(Client): update snapshot test

--- frontend/specs/components/token-classification/results/EntitiesSelector.spec.js
+++ frontend/specs/components/token-classification/results/EntitiesSelector.spec.js
@@ -5,17 +5,11 @@ const $entitiesMaxColors = 50;
 function mountEntitiesSelector() {
   return mount(EntitiesSelector, {
     propsData: {
-      dataset: {
-        task: "TextClassification",
-        lastSelectedEntity: {
-          colorId: 14,
-          shortCut: "1",
-          text: "Arreglo",
-        },
-        viewSettings: {
-          annotationEnabled: false,
-        },
-        labels: ["A", "B"],
+      datasetId: ["name", "owner"],
+      datasetLastSelectedEntity: {
+        colorId: 14,
+        shortCut: "1",
+        text: "Arreglo",
       },
       formattedEntities: [
         {
