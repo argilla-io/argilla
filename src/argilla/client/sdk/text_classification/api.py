e636187cbb876df129d5fb58fbe4eba36904b786
Tom Aarsen <Cubiegamedev@gmail.com>
2023-02-10 13:10:43 +0100

    Resolved error introduced in merge conflict

--- src/argilla/client/sdk/text_classification/api.py
+++ src/argilla/client/sdk/text_classification/api.py
@@ -12,13 +12,17 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from typing import List, Union
+from typing import List, Optional, Union
 
 import httpx
 
 from argilla.client.sdk._helpers import build_typed_response
 from argilla.client.sdk.client import AuthenticatedClient
-from argilla.client.sdk.commons.api import build_list_response
+from argilla.client.sdk.commons.api import (
+    build_data_response,
+    build_list_response,
+    build_param_dict,
+)
 from argilla.client.sdk.commons.models import (
     ErrorMessage,
     HTTPValidationError,
@@ -27,6 +31,8 @@ from argilla.client.sdk.commons.models import (
 from argilla.client.sdk.text_classification.models import (
     LabelingRule,
     LabelingRuleMetricsSummary,
+    TextClassificationQuery,
+    TextClassificationRecord,
 )
 
 
