f6c07771aff3c77a85de5f987699ea558d879dff
keithCuniah <keithcuniah@gmail.comh>
2023-02-22 18:02:48 +0100

    feat: init globalLabel model ORM

---
+++ frontend/models/GlobalLabel.model.js
@@ -0,0 +1,28 @@
+import { Model } from "@vuex-orm/core";
+
+class GlobalLabel extends Model {
+  static entity = "globalLabels";
+  static primaryKey = ["text", "color_id"];
+
+  static fields() {
+    return {
+      id: this.uid(),
+      color_id: this.attr(null),
+      text: this.string(null),
+      dataset_id: this.attr(null),
+      is_activate: this.attr(false),
+    };
+  }
+
+  static mutators() {
+    return {
+      text(value) {
+        return value.toUpperCase();
+      },
+    };
+  }
+}
+
+const formatDatasetIdForGlobalLabelModel = (dataset_id) => dataset_id.join(".");
+
+export { GlobalLabel, formatDatasetIdForGlobalLabelModel };
