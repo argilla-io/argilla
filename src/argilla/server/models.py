5c150d76d9e51cbbba54b319360431014107e42a
José Francisco Calvo <jose@argilla.io>
2023-02-24 10:11:55 +0100

    Set UserWorkspace model relationships as viewonly to avoid warnings

--- src/argilla/server/models.py
+++ src/argilla/server/models.py
@@ -43,8 +43,8 @@ class UserWorkspace(Base):
     inserted_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
     updated_at: Mapped[datetime] = mapped_column(default=default_inserted_at, onupdate=datetime.utcnow)
 
-    user: Mapped["User"] = relationship()
-    workspace: Mapped["Workspace"] = relationship()
+    user: Mapped["User"] = relationship(viewonly=True)
+    workspace: Mapped["Workspace"] = relationship(viewonly=True)
 
 
 class Workspace(Base):
