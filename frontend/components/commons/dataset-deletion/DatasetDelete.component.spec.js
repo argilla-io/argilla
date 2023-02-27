6ee475cbbf8e88f45aa9251e13632d76f415d752
leire <leire@recogn.ai>
2023-02-27 18:32:52 +0100

    include test

---
+++ frontend/components/commons/dataset-deletion/DatasetDelete.component.spec.js
@@ -0,0 +1,44 @@
+import { shallowMount } from "@vue/test-utils";
+import DatasetDelete from "./DatasetDelete.component";
+const $route = {
+  params: {
+    workspace: "argilla",
+  },
+};
+let wrapper = null;
+const options = {
+  stubs: ["base-button", "base-card", "base-modal"],
+  propsData: {
+    datasetName: "datasete_1",
+    sectionTitle: "Danger zone",
+    isDeleteModalVisible: false,
+  },
+  mocks: {
+    $route,
+  },
+};
+beforeEach(() => {
+  console.log(wrapper);
+  wrapper = shallowMount(DatasetDelete, options);
+});
+
+afterEach(() => {
+  wrapper.destroy();
+});
+
+describe("DatasetDeleteComponent", () => {
+  it("render the component", () => {
+    expect(wrapper.is(DatasetDelete)).toBe(true);
+  });
+
+  it("not render base-modal when isDeleteModalVisible is false", async () => {
+    expect(wrapper.find(".modal-wrapper").exists()).toBe(false);
+  });
+
+  it("render base-modal when isDeleteModalVisible is true", async () => {
+    await wrapper.setProps({
+      isDeleteModalVisible: true,
+    });
+    expect(wrapper.find(".delete-modal").exists()).toBe(true);
+  });
+});
