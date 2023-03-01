7332dad3c141f8e7a7e5f1bc7ef22826a117d043
leire <leire@recogn.ai>
2023-03-01 14:15:08 +0100

    fix current page legend for single record page

--- frontend/components/base/pagination/BasePagination.vue
+++ frontend/components/base/pagination/BasePagination.vue
@@ -92,11 +92,7 @@
     <div class="pagination__info">
       <template v-if="!onePage">
         <strong>
-          {{ paginationSize * currentPage - (paginationSize - 1) }}-{{
-            paginationSize * currentPage > totalItems
-              ? totalItems
-              : paginationSize * currentPage
-          }}
+          {{ currentPaginationPosition }}
         </strong>
         of
       </template>
@@ -158,8 +154,25 @@ export default {
         (p) => p !== this.paginationSize
       );
     },
+    currentPaginationPosition() {
+      return this.isSingleRecordPage
+        ? this.currentPage
+        : this.currentPaginationRange;
+    },
+    currentPaginationRange() {
+      const rangeFrom =
+        this.paginationSize * this.currentPage - (this.paginationSize - 1);
+      const rangeTo =
+        this.paginationSize * this.currentPage > this.totalItems
+          ? this.totalItems
+          : this.paginationSize * this.currentPage;
+      return `${rangeFrom} - ${rangeTo}`;
+    },
     currentPage() {
       return this.paginationSettings.page;
+    },
+    isSingleRecordPage() {
+      return this.paginationSize === 1;
     },
     pages() {
       const rangeOfPages = (this.visiblePagesRange - 1) / 2;
