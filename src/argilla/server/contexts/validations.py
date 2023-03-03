f1237014121e95c8fd0b35a43bb4eb29bd6f2be6
frascuchon <francis@argilla.io>
2023-03-03 18:16:59 +0100

    Define composable permission validations

--- src/argilla/server/contexts/validations.py
+++ src/argilla/server/contexts/validations.py
@@ -11,15 +11,42 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from enum import Enum
 
-from argilla.server.contexts import accounts
-from argilla.server.models import User, Workspace
+from argilla.server.models import User, UserRole, Workspace
 
 
-def can_user_access_to_workspace(user: User, workspace: Workspace):
-    if workspace in user.workspaces:
-        return True
-    elif accounts.is_admin_user(user):
-        return True
+class Permission(Enum):
+    create = 1
+    read = 2
+    update = 3
+    delete = 4
+
+
+def _allow_workspace_create(user: User, workspace: Workspace) -> bool:
+    return user.role == UserRole.admin
+
+
+def _is_allowed_read_workspace(user: User, workspace: Workspace) -> bool:
+    return user.role == UserRole.admin or workspace in user.workspaces
+
+
+def _is_allowed_update_workspace(user: User, workspace: Workspace) -> bool:
+    return _is_allowed_read_workspace(user, workspace)
+
+
+def _is_allowed_delete_workspace(user: User, workspace: Workspace) -> bool:
+    return _is_allowed_read_workspace(user, workspace)
+
+
+def is_authorized(user: User, permission: Permission, workspace: Workspace):
+    if permission == Permission.read:
+        return _is_allowed_read_workspace(user, workspace)
+    elif permission == Permission.create:
+        return _allow_workspace_create(user, workspace)
+    elif permission == Permission.update:
+        return _is_allowed_update_workspace(user, workspace)
+    elif permission == Permission.delete:
+        return _is_allowed_delete_workspace(user, workspace)
     else:
         return False
