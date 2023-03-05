e04f916aab257132c9d60d9e92f8c451767b6381
frascuchon <francis@argilla.io>
2023-03-06 00:08:48 +0100

    Set default event_timestamp sort for scan records in client

--- src/argilla/client/apis/datasets.py
+++ src/argilla/client/apis/datasets.py
@@ -188,6 +188,9 @@ class Datasets(AbstractApi):
             "query": query,
         }
 
+        if not sort and not id_from:
+            sort = [("event_timestamp", "desc")]  # Defaults
+
         if sort is not None:
             try:
                 if isinstance(sort, list):
@@ -196,8 +199,10 @@ class Datasets(AbstractApi):
                     raise Exception()
             except Exception:
                 raise ValueError("sort must be a dict formatted as List[Tuple[<field_name>, 'asc|desc']]")
+            request["sort_by"] = [{"id": item[0], "order": item[-1]} for item in sort]
 
-        if id_from:
+        elif id_from:
+            # TODO: Show message since sort + next_id is not compatible since fixes a sort by id
             request["next_idx"] = id_from
 
         with api_compatibility(self, min_version="1.2.0"):
@@ -213,13 +218,15 @@ class Datasets(AbstractApi):
                 if limit <= 0:
                     return
 
-                next_idx = response.get("next_idx")
-                if next_idx:
-                    request_limit = min(limit, batch_size)
-                    response = self.http_client.post(
-                        path=url.format(limit=request_limit),
-                        json={**request, "next_idx": next_idx},
-                    )
+                next_request_params = {k: response[k] for k in ["next_idx", "next_pagination_id"] if response.get(k)}
+                if not next_request_params:
+                    return
+
+                request_limit = min(limit, batch_size)
+                response = self.http_client.post(
+                    path=url.format(limit=request_limit),
+                    json={**request, **next_request_params},
+                )
 
     def update_record(
         self,
