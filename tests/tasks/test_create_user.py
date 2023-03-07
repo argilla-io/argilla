623c63e7a249725506501e8ac57906a4b759bd86
frascuchon <francis@argilla.io>
2023-03-07 14:32:14 +0100

    Add missing tests

---
+++ tests/tasks/test_create_user.py
@@ -0,0 +1,146 @@
+#  Copyright 2021-present, the Recognai S.L. team.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import pytest
+from argilla.server.contexts import accounts
+from argilla.server.database import SessionLocal
+from argilla.server.models import User, UserRole
+from argilla.tasks.create_user import create_user
+from click.testing import CliRunner
+from pydantic import ValidationError
+
+
+@pytest.fixture
+def db():
+    with SessionLocal() as db, db.begin():
+        yield db
+
+        db.query(User).delete()
+        db.commit()
+
+
+def test_create_user_passing_all_params(db):
+    username = "test-user"
+    first_name = "First"
+    role = "admin"
+    password = "12345678"
+
+    runner = CliRunner()
+    cli_params = f"--username {username} --password {password} --role {role} --first-name {first_name}"
+    runner.invoke(create_user, cli_params)
+
+    user = accounts.get_user_by_username(db, username)
+    assert user
+    assert user.role.value == role
+    assert user.first_name == first_name
+    assert accounts.authenticate_user(db, username=username, password=password) == user
+
+
+def test_create_user_with_input_role(db):
+    username = "test-user"
+    role = "admin"
+
+    runner = CliRunner()
+    cli_params = f"--username {username} --password 12345678 --first-name First"
+    runner.invoke(create_user, cli_params, input=f"{role}\n")
+
+    user = accounts.get_user_by_username(db, username)
+    assert user
+    assert user.role.value == role
+
+
+def test_create_user_with_input_password(db):
+    username = "test-user"
+    password = "12345678"
+
+    runner = CliRunner()
+    cli_params = f"--username {username} --role admin --first-name First"
+    runner.invoke(create_user, cli_params, input=f"{password}\n{password}\n")
+
+    user = accounts.get_user_by_username(db, username)
+    assert user
+    assert accounts.authenticate_user(db, username=username, password=password) == user
+
+
+def test_create_user_with_role_default(db):
+    username = "test-user"
+    password = "12345678"
+
+    runner = CliRunner()
+    cli_params = f"--username {username} --password {password} --first-name First"
+    runner.invoke(create_user, cli_params, input=f"\n")
+
+    user = accounts.get_user_by_username(db, username)
+    assert user
+    assert user.role == UserRole.annotator
+
+
+def test_create_user_with_input_username(db):
+    username = "test-user"
+    password = "12345678"
+
+    runner = CliRunner()
+    cli_params = f"--password {password} --first-name First"
+    runner.invoke(create_user, cli_params, input=f"{username}\n")
+
+    user = accounts.get_user_by_username(db, username)
+    assert user
+
+
+def test_create_user_with_last_name(db):
+    username = "test-user"
+    password = "12345678"
+    last_name = "Last"
+
+    runner = CliRunner()
+    cli_params = f"--username {username} --password {password} --role admin --first-name First --last-name {last_name}"
+    runner.invoke(create_user, cli_params)
+
+    user = accounts.get_user_by_username(db, username)
+    assert user
+    assert user.last_name == last_name
+
+
+def test_create_user_with_wrong_username(db):
+    username = "Wrong-username "
+    password = "12345678"
+
+    runner = CliRunner()
+    cli_params = f"--username {username} --password {password} --role admin --first-name First"
+    result = runner.invoke(create_user, cli_params)
+
+    assert accounts.get_user_by_username(db, username) is None
+    assert str(result.exception) == (
+        "1 validation error for UserCreate\n"
+        "username\n"
+        '  string does not match regex "^(?!-|_)[a-z0-9-_]+$" '
+        "(type=value_error.str.regex; pattern=^(?!-|_)[a-z0-9-_]+$)"
+    )
+
+
+def test_create_user_with_wrong_password(db):
+    username = "username"
+    password = "11"
+
+    runner = CliRunner()
+    cli_params = f"--username {username} --password {password} --role admin --first-name First"
+    result = runner.invoke(create_user, cli_params)
+
+    assert accounts.get_user_by_username(db, username) is None
+    assert str(result.exception) == (
+        "1 validation error for UserCreate\n"
+        "password\n"
+        "  ensure this value has at least 8 characters "
+        "(type=value_error.any_str.min_length; limit_value=8)"
+    )
