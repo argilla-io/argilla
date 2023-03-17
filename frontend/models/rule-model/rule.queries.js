c7eb6320e07de3d04571a7d863a885167c6b3e02
keithCuniah <keithcuniah@gmail.com>
2023-03-17 12:53:05 +0100

    add queries

--- frontend/models/rule-model/rule.queries.js
+++ frontend/models/rule-model/rule.queries.js
@@ -1,25 +1,49 @@
 import { Rule as RuleModel, formatDatasetIdForRuleModel } from "./Rule.model";
 import { getDatasetModelPrimaryKey } from "@/models/Dataset";
 
+// Getters
+const getAllRulesByDatasetId = (datasetId, savedInDataset = true) => {
+  const formattedDatasetId = formatDatasetIdForRuleModel(datasetId);
+  return RuleModel.query()
+    .where("dataset_id", formattedDatasetId)
+    .where("is_saved_in_dataset", savedInDataset)
+    .get();
+};
+
+const getQueryRuleArrayByDatasetNameAndWorkspace = ({
+  datasetName,
+  datasetWorkspace,
+}) => {
+  const datasetId = getDatasetModelPrimaryKey({
+    name: datasetName,
+    workspace: datasetWorkspace,
+  });
+  const rules = getAllRulesByDatasetId(datasetId);
+
+  return rules.map((rule) => rule.query);
+};
+
 // Exists
-const isAnyRuleByDatasetId = ({ datasetName, datasetWorkspace }) => {
+const isAnyRuleByDatasetNameAndWorkspace = ({
+  datasetName,
+  datasetWorkspace,
+}) => {
   const datasetId = formatDatasetId({ datasetName, datasetWorkspace });
   return RuleModel.query().where("dataset_id", datasetId).exists();
 };
 
-// insert and update
+// Insert and update
 const upsertRulesInRuleModel = async (rules) => {
   await RuleModel.insertOrUpdate({ data: rules });
 };
 
-// delete
+// Delete
 const deleteAllRuleModel = () => {
   RuleModel.deleteAll();
 };
 
-// utils
+// Utils
 const formatDatasetId = ({ datasetName, datasetWorkspace }) => {
-  console.log(datasetName);
   const datasetPrimaryKey = getDatasetModelPrimaryKey({
     name: datasetName,
     workspace: datasetWorkspace,
@@ -30,4 +54,10 @@ const formatDatasetId = ({ datasetName, datasetWorkspace }) => {
   return formattedDatasetId;
 };
 
-export { upsertRulesInRuleModel, deleteAllRuleModel, isAnyRuleByDatasetId };
+export {
+  getAllRulesByDatasetId,
+  getQueryRuleArrayByDatasetNameAndWorkspace,
+  upsertRulesInRuleModel,
+  deleteAllRuleModel,
+  isAnyRuleByDatasetNameAndWorkspace,
+};
