9f0407e5cca9ed11d1004884506ec390d1f15e35
leire <leire@recogn.ai>
2023-03-23 13:47:54 +0100

    prevent cant find module error and styles

--- frontend/components/commons/dataset-train/DatasetTrain.component.vue
+++ frontend/components/commons/dataset-train/DatasetTrain.component.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
+  <div class="snippet__container">
     <base-tabs
       class="train__tabs"
       :tabs="snippetsTabs"
       :active-tab="visibleTab"
-      @change-tab="getSelectedHelpComponent"
+      @change-tab="getSelectedLibrary"
     />
     <transition v-if="snippet" name="fade" mode="out-in" appear>
       <div class="snippet">
-        <h1 v-if="snippetAttributes.title" class="snippet__title --heading3">
+        <h1 v-if="snippetAttributes.title" class="snippet__title --heading4">
           {{ snippetAttributes.title }}
         </h1>
         <h2
@@ -17,10 +17,25 @@
         >
           {{ snippetAttributes.description }}
         </h2>
-        <base-code v-if="snippet.html" :code="snippet.html"></base-code>
+        <div class="snippet__code" v-if="snippet.html">
+          <div v-highlight v-html="snippet.html"></div>
+          <base-action-tooltip tooltip="Copied">
+            <base-button
+              class="secondary small"
+              @on-click="copyCode(snippet.html)"
+            >
+              <svgicon name="copy" width="16" height="16" />
+              Copy
+            </base-button>
+          </base-action-tooltip>
+        </div>
+        <base-code
+          v-if="snippet.html"
+          :code="parseHtml(snippet.html)"
+        ></base-code>
         <base-button
           v-if="snippetAttributes.buttonLink"
-          class="snippet__button primary"
+          class="snippet__button primary small"
           :href="snippetAttributes.buttonLink"
           target="_blank"
           >{{ snippetAttributes.buttonText }}</base-button
@@ -45,83 +60,50 @@ export default {
   },
   computed: {
     snippetsTabs() {
-      switch (this.datasetTask) {
-        case "TextClassification":
-          return [
-            {
-              id: "transformers",
-              name: "Transformers",
-            },
-            {
-              id: "spacy",
-              name: "Spacy",
-            },
-            {
-              id: "setfit",
-              name: "Setfit",
-            },
-            {
-              id: "spark-nlp",
-              name: "Spark-nlp",
-            },
-            {
-              id: "autotrain",
-              name: "AutoTrain",
-            },
-          ];
-        case "TokenClassification":
-          return [
-            {
-              id: "transformers",
-              name: "Transformers",
-            },
-            {
-              id: "spacy",
-              name: "Spacy",
-            },
-            {
-              id: "spark-nlp",
-              name: "Spark-nlp",
-            },
-            {
-              id: "autotrain",
-              name: "AutoTrain",
-            },
-          ];
-        case "Text2Text":
-          return [
-            {
-              id: "transformers",
-              name: "Transformers",
-            },
-            {
-              id: "spark-nlp",
-              name: "Spark-nlp",
-            },
-            {
-              id: "autotrain",
-              name: "AutoTrain",
-            },
-          ];
-        default:
-          throw Error("unknown dataset task");
-      }
+      return [
+        this.getLibrary("Transformers"),
+        ...((this.datasetTask !== "Text2Text" && [this.getLibrary("Spacy")]) ||
+          []),
+        ...((this.datasetTask === "TextClassification" && [
+          this.getLibrary("Setfit"),
+        ]) ||
+          []),
+        this.getLibrary("Spark-nlp"),
+        this.getLibrary("AutoTrain"),
+      ];
     },
     visibleTab() {
       return this.selectedComponent || this.snippetsTabs[0];
     },
     snippet() {
-      return require(`../../../../docs/_source/_common/snippets/training/TextClassification/${this.visibleTab.id}.md`);
+      try {
+        return require(`../../../../docs/_source/_common/snippets/training/TextClassification/${this.visibleTab.id}.md`);
+      } catch (e) {
+        return null;
+      }
     },
     snippetAttributes() {
       return this.snippet.attributes;
     },
   },
   methods: {
-    getSelectedHelpComponent(id) {
+    getSelectedLibrary(id) {
       this.selectedComponent = this.snippetsTabs.find(
         (snippet) => snippet.id === id
       );
+    },
+    parseHtml(snippet) {
+      const snippetCode = new DOMParser().parseFromString(snippet, "text/html");
+      return snippetCode.body.textContent;
+    },
+    copyCode(snippet) {
+      this.$copyToClipboard(this.parseHtml(snippet));
+    },
+    getLibrary(name) {
+      return {
+        id: name.toLowerCase(),
+        name,
+      };
     },
   },
 };
@@ -129,8 +111,21 @@ export default {
 
 <style lang="scss" scoped>
 .snippet {
-  margin-top: $base-space * 5;
-  max-width: 1000px;
+  margin-top: $base-space * 3;
+  :deep(pre) {
+    white-space: pre-line;
+  }
+  :deep(code) {
+    border-radius: $border-radius;
+  }
+  &__container {
+    width: 800px;
+  }
+  &__code {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
   &__description {
     font-weight: normal;
   }
