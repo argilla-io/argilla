0947953aadf9e369d1463630fb62b5dd22960d65
keithCuniah <keithcuniah@gmail.com>
2023-04-05 12:27:38 +0200

    single label component

---
+++ frontend/components/commons/forms/questions-form/fields/single-label/SingleLabel.component.vue
@@ -0,0 +1,33 @@
+<template>
+  <MonoSelectionComponent
+    :title="title"
+    :initialOutputs="initialOutputs"
+    :isRequired="isRequired"
+    :colorHighlight="colorHighlight"
+    @on-change="$emit('on-change-rating', $event)"
+  />
+</template>
+
+<script>
+export default {
+  name: "RatingComponent",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    initialOutputs: {
+      type: Array,
+      required: true,
+    },
+    isRequired: {
+      type: Boolean,
+      default: () => false,
+    },
+    colorHighlight: {
+      type: String,
+      default: () => "black",
+    },
+  },
+};
+</script>
