22764ed2b81259f87de4de0964872c5eeed67537
keithCuniah <keithcuniah@gmail.com>
2023-04-05 16:52:30 +0200

    css && reset/validate buttons

--- frontend/components/commons/forms/questions-form/QuestionsForm.component.vue
+++ frontend/components/commons/forms/questions-form/QuestionsForm.component.vue
@@ -1,12 +1,12 @@
 <template>
-  <form>
+  <form @submit.prevent="onSubmit" :key="renderForm">
     <div class="form-group" v-for="input in inputs" :key="input.key">
       <TextAreaComponent
         v-if="input.componentType === PROPERTIES.FREE_TEXT"
         :title="input.question"
         :initialOutputs="input.outputs"
         :isRequired="input.required"
-        :isIcon="input.tooltipMessage"
+        :isIcon="!!input.tooltipMessage"
         :tooltipMessage="input.tooltipMessage"
         colorHighlight="red"
         @on-change-text-area="
@@ -19,7 +19,7 @@
         :title="input.question"
         :initialOutputs="input.outputs"
         :isRequired="input.required"
-        :isIcon="input.tooltipMessage"
+        :isIcon="!!input.tooltipMessage"
         :tooltipMessage="input.tooltipMessage"
         colorHighlight="red"
         @on-change-rating="
@@ -32,7 +32,7 @@
         :title="input.question"
         :initialOutputs="input.outputs"
         :isRequired="input.required"
-        :isIcon="input.tooltipMessage"
+        :isIcon="!!input.tooltipMessage"
         :tooltipMessage="input.tooltipMessage"
         colorHighlight="red"
         @on-change-rating="
@@ -40,29 +40,54 @@
         "
       />
     </div>
+
+    <div class="buttons-area">
+      <BaseButton
+        type="reset"
+        class="primary outline small"
+        @on-click="onReset"
+        :disabled="isFormUntouched"
+      >
+        <span v-text="'Reset'" />
+      </BaseButton>
+      <BaseButton
+        type="submit"
+        class="primary small"
+        :disabled="isFormUntouched"
+      >
+        <span v-text="'Validate'" />
+      </BaseButton>
+    </div>
     {{ inputs }}
   </form>
 </template>
 
 <script>
-import { cloneDeep } from "lodash";
+import { isEqual, cloneDeep } from "lodash";
 import { PROPERTIES } from "./questionsForm.properties";
 
 export default {
   name: "QuestionsFormComponent",
   props: {
-    initialInput: {
+    initialInputs: {
       type: Array,
       required: true,
     },
   },
   data() {
     return {
-      inputs: cloneDeep(this.initialInput),
+      inputs: [],
+      renderForm: 0,
     };
   },
   created() {
     this.PROPERTIES = PROPERTIES;
+    this.onReset();
+  },
+  computed: {
+    isFormUntouched() {
+      return isEqual(this.initialInputs, this.inputs);
+    },
   },
   methods: {
     onChange({ newOutputs, idComponent }) {
@@ -73,6 +98,13 @@ export default {
         return input;
       });
     },
+    onSubmit() {
+      console.log(this.inputs);
+    },
+    onReset() {
+      this.inputs = cloneDeep(this.initialInputs);
+      this.renderForm++;
+    },
   },
 };
 </script>
@@ -81,6 +113,15 @@ export default {
 form {
   display: flex;
   flex-direction: column;
-  gap: $base-space * 2;
+  gap: $base-space * 4;
+  border: 2px solid #4c4ea3;
+  border-radius: 5px;
+  padding: $base-space * 4;
+}
+
+.buttons-area {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 </style>
