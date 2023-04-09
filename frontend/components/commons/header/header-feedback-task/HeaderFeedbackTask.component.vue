271529fd4c0b57fa354713a52a46522503190ead
keithCuniah <keithcuniah@gmail.com>
2023-04-09 20:16:41 +0200

    header for feedback task

---
+++ frontend/components/commons/header/header-feedback-task/HeaderFeedbackTask.component.vue
@@ -0,0 +1,69 @@
+<template>
+  <BaseTopbarBrand>
+    <BaseBreadcrumbs
+      v-if="breadcrumbs"
+      :breadcrumbs="breadcrumbs"
+      :copy-button="copyButton"
+      @breadcrumb-action="$emit('breadcrumb-action', $event)"
+    />
+    <DatasetSettingsIcon
+      v-if="datasetId && datasetName"
+      :datasetId="datasetId"
+      @click-settings-icon="goToSettings()"
+    />
+    <user />
+  </BaseTopbarBrand>
+</template>
+
+<script>
+import { getDatasetModelPrimaryKey } from "@/models/Dataset";
+
+export default {
+  name: "HeaderFeedbaskTaskComponent",
+  data() {
+    return {
+      copyButton: false,
+    };
+  },
+  computed: {
+    datasetId() {
+      return getDatasetModelPrimaryKey({
+        name: this.datasetName,
+        workspace: this.workspace,
+      });
+    },
+    workspace() {
+      return this.$route.params.workspace;
+    },
+    datasetName() {
+      return this.$route.params.dataset;
+    },
+    breadcrumbs() {
+      return [
+        { link: { name: "datasets" }, name: "Home" },
+        {
+          link: { path: `/datasets?workspace=${this.workspace}` },
+          name: this.workspace,
+        },
+        {
+          link: {
+            name: null,
+            params: { workspace: this.workspace, dataset: this.datasetName },
+          },
+          name: this.datasetName,
+        },
+      ];
+    },
+  },
+  methods: {
+    goToSettings() {
+      const currentRoute = this.$route.path;
+      const newRoute = `/datasets/${this.workspace}/${this.datasetName}/settings`;
+      const allowNavigate = currentRoute !== newRoute;
+      if (this.datasetName && allowNavigate) {
+        this.$router.push(newRoute);
+      }
+    },
+  },
+};
+</script>
