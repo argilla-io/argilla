bf2b11a5c40f5e17115195cf1aaf9afa7d7238ed
leire <leire@recogn.ai>
2023-04-13 12:58:44 +0200

    update test

--- frontend/specs/components/commons/TaskSidebar.spec.js
+++ frontend/specs/components/commons/TaskSidebar.spec.js
@@ -1,16 +1,42 @@
-import { mount } from "@vue/test-utils";
-import TaskSidebar from "@/components/commons/sidebar/TaskSidebar";
+import { shallowMount } from "@vue/test-utils";
+import TaskSidebar from "@/components/commons/sidebar/TaskSidebar.component";
+import { SIDEBAR_ITEMS } from "@/components/commons/sidebar/sidebarItems.config";
+
+jest.mock("@/models/dataset.utilities", () => ({
+  getDatasetFromORM: () => ({
+    $id: "settings_textclass_no_labels",
+    id: "settings_textclass_no_labels",
+    name: "settings_textclass_no_labels",
+  }),
+  getDatasetTaskById: () => "text-classification",
+}));
+jest.mock("@/models/viewSettings.queries", () => ({
+  getViewSettingsByDatasetName: () => ({
+    viewMode: "annotation",
+  }),
+}));
+jest.mock("@/models/Dataset", () => ({
+  getDatasetModelPrimaryKey: () => ({
+    name: "settings_textclass_no_labels",
+    workspace: "recognai",
+  }),
+}));
+const $route = {
+  params: {
+    workspace: "recognai",
+    dataset: "dataset_name",
+  },
+};
 
 function mountSidebar() {
-  return mount(TaskSidebar, {
-    stubs: ["TextClassificationSidebar"],
+  return shallowMount(TaskSidebar, {
+    stubs: ["SidebarMenu", "SidebarPanel"],
     propsData: {
-      dataset: {
-        task: "TextClassification",
-        viewSettings: {
-          annotationEnabled: false,
-        },
-      },
+      currentMetric: undefined,
+      sidebarItems: SIDEBAR_ITEMS["TEXT_CLASSIFICATION"],
+    },
+    mocks: {
+      $route,
     },
   });
 }
@@ -20,7 +46,7 @@ describe("TaskSidebar", () => {
   afterEach(() => spy.mockReset());
 
   test.skip("Required property", () => {
-    mount(TaskSidebar);
+    shallowMount(TaskSidebar);
     expect(spy).toBeCalledWith(
       expect.stringContaining('[Vue warn]: Missing required prop: "dataset"')
     );
