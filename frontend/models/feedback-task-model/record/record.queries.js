072cd84922e982401e3514807336208ed82c2239
keithCuniah <keithcuniah@gmail.comh>
2023-04-14 10:50:34 +0200

    renaming queries

--- frontend/models/feedback-task-model/record/record.queries.js
+++ frontend/models/feedback-task-model/record/record.queries.js
@@ -6,17 +6,16 @@ const upsertRecords = (records) => {
 };
 
 // GET
-const getRecordWithFieldsAndResponsesByDatasetId = (
+const getRecordWithFieldsByDatasetId = (
   datasetId,
   numberOfRecord = 1,
   fromRecord = 0
 ) => {
   return RecordModel.query()
     .with("record_fields")
-    .with("record_responses")
     .where("dataset_id", datasetId)
     .limit(numberOfRecord)
     .offset(fromRecord)
     .first();
 };
-export { upsertRecords, getRecordWithFieldsAndResponsesByDatasetId };
+export { upsertRecords, getRecordWithFieldsByDatasetId };
