455f14ac8cc21341eefa3bcadb3b0c1a165dbc10
keithCuniah <keithcuniah@gmail.comh>
2023-04-14 18:13:44 +0200

    layout : include footer-area  && grid size

--- frontend/layouts/HeaderAndTopAndOneColumn.vue
+++ frontend/layouts/HeaderAndTopAndOneColumn.vue
@@ -1,20 +1,22 @@
 <template>
-  <div>
-    <div class="header">
+  <div class="layout">
+    <div class="header-area">
       <slot name="header">here is the header</slot>
     </div>
-    <main class="layout">
-      <div class="empty-content-left"></div>
-      <div class="sidebar-content-right">
-        <slot name="sidebar-right">here is the sidebar content left</slot>
-      </div>
-      <div class="top">
-        <slot name="top">here is the top content</slot>
-      </div>
-      <div class="center">
-        <slot name="center">here is the center content</slot>
-      </div>
-    </main>
+    <div class="empty-content-left"></div>
+    <div class="footer-area">
+      <slot name="footer">here is the footer</slot>
+    </div>
+    <div class="sidebar-area">
+      <slot name="sidebar-right">here is the sidebar content left</slot>
+    </div>
+    <div class="empty-content-right"></div>
+    <div class="top-area">
+      <slot name="top">here is the top content</slot>
+    </div>
+    <div class="center-area">
+      <slot name="center">here is the center content</slot>
+    </div>
   </div>
 </template>
 
@@ -27,27 +29,33 @@ export default {
 <style lang="scss" scoped>
 .layout {
   display: grid;
-  grid-template-columns: 56px 1fr 1fr 1fr 56px;
-  grid-template-rows: 0 100px 1fr 1fr 1fr;
+  grid-template-columns: 56px 1fr 56px auto;
+  grid-template-rows: auto 100px 1fr auto;
   grid-column-gap: 0px;
   grid-row-gap: 0px;
-  height: calc(100vh - $topbarHeight);
-  background: palette(white);
+  height: 100vh;
 }
-.header {
+
+.header-area {
   grid-area: 1 / 1 / 2 / 6;
 }
 .empty-content-left {
-  grid-area: 2 / 1 / 6 / 2;
+  grid-area: 2 / 1 / 4 / 2;
 }
-.sidebar-content-right {
-  grid-area: 2 / 5 / 6 / 6;
+.footer-area {
+  grid-area: 4 / 1 / 5 / 4;
 }
-.top {
-  grid-area: 2 / 2 / 3 / 5;
+.sidebar-area {
+  grid-area: 2 / 4 / 5 / 5;
 }
-.center {
-  grid-area: 3 / 2 / 6 / 5;
+.empty-content-right {
+  grid-area: 2 / 3 / 4 / 4;
+}
+.top-area {
+  grid-area: 2 / 2 / 3 / 3;
+}
+.center-area {
+  grid-area: 3 / 2 / 4 / 3;
   overflow-x: hidden;
 }
 </style>
