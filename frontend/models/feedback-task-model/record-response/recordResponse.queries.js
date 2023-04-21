5aaef5551d7d471d695874eaa434f4a0988b39ff
keithCuniah <keithcuniah@gmail.comh>
2023-04-21 12:49:32 +0200

    update query to get response by recordId && userid

--- frontend/models/feedback-task-model/record-response/recordResponse.queries.js
+++ frontend/models/feedback-task-model/record-response/recordResponse.queries.js
@@ -1,8 +1,11 @@
 import { RecordResponse as RecordResponseModel } from "./RecordResponse.model";
 
 // GET
-const getRecordResponsesByRecordId = (recordId) => {
-  return RecordResponseModel.query().where("record_id", recordId).get();
+const getRecordResponsesByRecordId = ({ userId, recordId }) => {
+  return RecordResponseModel.query()
+    .where("record_id", recordId)
+    .where("user_id", userId)
+    .get();
 };
 const findRecordResponseByRecordIdByQuestionId = ({ recordId, questionId }) => {
   return RecordResponseModel.query().whereId([questionId, recordId]).first();
