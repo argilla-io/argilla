4589eb777600ba983c7c510887045f9ca1e9a2d6
keithCuniah <keithcuniah@gmail.comh>
2023-04-27 12:45:39 +0200

    delete feedback dataset component

--- frontend/components/commons/dataset-deletion-feedback-task/DatasetDeleteFeedbackTask.component.vue
+++ frontend/components/commons/dataset-deletion-feedback-task/DatasetDeleteFeedbackTask.component.vue
@@ -39,6 +39,7 @@ import {
   getFeedbackDatasetNameById,
   getFeedbackDatasetWorkspaceNameById,
 } from "@/models/feedback-task-model/feedback-dataset/feedbackDataset.queries";
+import { urlDeleteDatasetV1 } from "/utils/url.properties";
 
 const TYPE_OF_FEEDBACK = Object.freeze({
   NOT_ALLOWED_TO_DELETE_DATASET: "NOT_ALLOWED_TO_DELETE_DATASET",
@@ -89,7 +90,7 @@ export default {
       let typeOfNotification = "";
       let statusCall = null;
 
-      const url = `/v1/datasets/${this.datasetId}`;
+      const url = urlDeleteDatasetV1(this.datasetId);
       try {
         await this.$axios.delete(url);
         message = `${this.datasetName} has been deleted`;
