ecec09c293380a229dcc28746f96c9fb17db272c
david <david.m.berenstein@gmail.com>
2023-05-03 16:44:51 +0200

    docs: initial iteration updated readme
    # Conflicts:
    #	README.md

--- frontend/components/text2text/results/Text2TextContentEditable.vue
+++ frontend/components/text2text/results/Text2TextContentEditable.vue
@@ -3,14 +3,13 @@
     <div class="content__edition-area">
       <transition appear name="fade">
         <p
-          :key="editableText"
           ref="text"
           class="content__text"
           :class="textIsEdited ? '--edited-text' : null"
           :contenteditable="annotationEnabled"
           :placeholder="placeholder"
           @input="onInputText"
-          v-html="customEditableText"
+          v-html="editableText"
           @focus="setFocus(true)"
           @blur="setFocus(false)"
         ></p>
@@ -20,7 +19,6 @@
   </span>
 </template>
 <script>
-import { escapeHtmlChars } from "@/utils/escapeHtmlChars";
 export default {
   props: {
     annotationEnabled: {
@@ -54,11 +52,6 @@ export default {
         this.defaultText !== this.editableText ||
         this.defaultText === this.annotations[0]?.text
       );
-    },
-    customEditableText() {
-      return this.$checkValidHtml(this.editableText)
-        ? this.editableText
-        : escapeHtmlChars(this.editableText);
     },
   },
   mounted() {
