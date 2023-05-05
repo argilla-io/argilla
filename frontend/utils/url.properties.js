b54a88abfe10aadce7d91f988b80062d1c0509d1
Keith Cuniah <88380932+keithCuniah@users.noreply.github.com>
2023-05-05 13:29:26 +0200

    feat : front contextual endpoints using me prefix (#2847)
    # Description
    
    The following PR add changes to support new `/api/v1/me/datasets` and
    `/api/v1/datasets/:dataset_id/records` endpoints
    
    
    Refs: #2826
    
    ---------
    
    Co-authored-by: keithCuniah <keithcuniah@gmail.comh>

--- frontend/utils/url.properties.js
+++ frontend/utils/url.properties.js
@@ -1,6 +1,6 @@
 // GET
 const URL_GET_WORKSPACES = `/workspaces`;
-const URL_GET_V1_DATASETS = `/v1/datasets`;
+const URL_GET_V1_DATASETS = `/v1/me/datasets`;
 
 // DELETE
 const urlDeleteDatasetV1 = (datasetId) => `/v1/datasets/${datasetId}`;
