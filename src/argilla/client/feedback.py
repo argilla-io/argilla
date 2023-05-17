17e449e1dc84c5f1cc47ab32adc28d77e8cd1c96
Alvaro Bartolome <alvaro@argilla.io>
2023-05-17 15:10:23 +0200

    Remove `__future__.annotations` as no longer required
    Co-authored-by: frascuchon <francis@argilla.io>

--- src/argilla/client/feedback.py
+++ src/argilla/client/feedback.py
@@ -12,8 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from __future__ import annotations
-
 import warnings
 from typing import TYPE_CHECKING, Any, Dict, Iterator, List, Optional, Tuple, Union
 from uuid import UUID
