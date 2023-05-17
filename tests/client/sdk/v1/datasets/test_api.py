20f809635ac632eb0e89ce6da58230b99d4f012d
Alvaro Bartolome <alvaro@argilla.io>
2023-05-17 14:56:23 +0200

    Add every `/api/v1/datasets` unit test

--- tests/client/sdk/v1/datasets/test_api.py
+++ tests/client/sdk/v1/datasets/test_api.py
@@ -31,6 +31,7 @@ from argilla.client.sdk.v1.datasets.api import (
 from argilla.client.sdk.v1.datasets.models import (
     FeedbackDatasetModel,
     FeedbackFieldModel,
+    FeedbackItemModel,
     FeedbackQuestionModel,
     FeedbackRecordsModel,
 )
@@ -56,3 +57,268 @@ def test_list_datasets(mocked_client, sdk_client, monkeypatch) -> None:
     assert isinstance(response.parsed, list)
     assert len(response.parsed) > 0
     assert isinstance(response.parsed[0], FeedbackDatasetModel)
+
+
+def test_get_datasets(mocked_client, sdk_client, monkeypatch) -> None:
+    monkeypatch.setattr(sdk_client, "get", mocked_client.get)
+
+    workspace_name = "test_workspace"
+    mocked_response = mocked_client.post(f"/api/workspaces", json={"name": workspace_name})
+    workspace_id = mocked_response.json()["id"]
+
+    dataset_name = "test_dataset"
+    mocked_response = mocked_client.post(
+        f"/api/v1/datasets", json={"name": dataset_name, "guidelines": "", "workspace_id": workspace_id}
+    )
+    dataset_id = mocked_response.json()["id"]
+
+    response = get_dataset(client=sdk_client, id=dataset_id)
+    assert response.status_code == 200
+    assert isinstance(response.parsed, FeedbackDatasetModel)
+    assert response.parsed.name == dataset_name
+
+
+def test_create_dataset(mocked_client, sdk_client, monkeypatch) -> None:
+    monkeypatch.setattr(sdk_client, "post", mocked_client.post)
+
+    workspace_name = "test_workspace"
+    mocked_response = mocked_client.post(f"/api/workspaces", json={"name": workspace_name})
+    workspace_id = mocked_response.json()["id"]
+
+    dataset_name = "test_dataset"
+    response = create_dataset(client=sdk_client, name=dataset_name, workspace_id=workspace_id, guidelines="")
+    assert response.status_code == 201
+    assert isinstance(response.parsed, FeedbackDatasetModel)
+    assert response.parsed.name == dataset_name
+
+
+def test_delete_dataset(mocked_client, sdk_client, monkeypatch) -> None:
+    monkeypatch.setattr(sdk_client, "delete", mocked_client.delete)
+
+    workspace_name = "test_workspace"
+    mocked_response = mocked_client.post(f"/api/workspaces", json={"name": workspace_name})
+    workspace_id = mocked_response.json()["id"]
+
+    dataset_name = "test_dataset"
+    mocked_response = mocked_client.post(
+        f"/api/v1/datasets", json={"name": dataset_name, "guidelines": "", "workspace_id": workspace_id}
+    )
+    dataset_id = mocked_response.json()["id"]
+
+    response = delete_dataset(client=sdk_client, id=dataset_id)
+    assert response.status_code == 200
+
+    mocked_response = mocked_client.get("/api/v1/me/datasets")
+    assert mocked_response.status_code == 200
+    assert len(mocked_response.json()["items"]) < 1
+
+
+def test_publish_dataset(mocked_client, sdk_client, monkeypatch) -> None:
+    monkeypatch.setattr(sdk_client, "put", mocked_client.put)
+
+    workspace_name = "test_workspace"
+    mocked_response = mocked_client.post(f"/api/workspaces", json={"name": workspace_name})
+    workspace_id = mocked_response.json()["id"]
+
+    dataset_name = "test_dataset"
+    mocked_response = mocked_client.post(
+        f"/api/v1/datasets", json={"name": dataset_name, "guidelines": "", "workspace_id": workspace_id}
+    )
+    dataset_id = mocked_response.json()["id"]
+
+    mocked_client.post(
+        f"/api/v1/datasets/{dataset_id}/fields",
+        json={"name": "test_field", "title": "text_field", "required": True, "settings": {"type": "text"}},
+    )
+    mocked_client.post(
+        f"/api/v1/datasets/{dataset_id}/questions",
+        json={
+            "name": "test_question",
+            "title": "text_question",
+            "description": "test_description",
+            "required": True,
+            "settings": {"type": "text"},
+        },
+    )
+
+    response = publish_dataset(client=sdk_client, id=dataset_id)
+    assert response.status_code == 200
+    assert isinstance(response.parsed, FeedbackDatasetModel)
+    assert response.parsed.name == dataset_name
+    assert response.parsed.status == "ready"
+
+
+def test_add_field(mocked_client, sdk_client, monkeypatch) -> None:
+    monkeypatch.setattr(sdk_client, "post", mocked_client.post)
+
+    workspace_name = "test_workspace"
+    mocked_response = mocked_client.post(f"/api/workspaces", json={"name": workspace_name})
+    workspace_id = mocked_response.json()["id"]
+
+    dataset_name = "test_dataset"
+    mocked_response = mocked_client.post(
+        f"/api/v1/datasets", json={"name": dataset_name, "guidelines": "", "workspace_id": workspace_id}
+    )
+    dataset_id = mocked_response.json()["id"]
+
+    response = add_field(
+        client=sdk_client,
+        id=dataset_id,
+        field={"name": "test_field", "title": "text_field", "required": True, "settings": {"type": "text"}},
+    )
+    assert response.status_code == 201
+
+
+def test_get_fields(mocked_client, sdk_client, monkeypatch) -> None:
+    monkeypatch.setattr(sdk_client, "get", mocked_client.get)
+
+    workspace_name = "test_workspace"
+    mocked_response = mocked_client.post(f"/api/workspaces", json={"name": workspace_name})
+    workspace_id = mocked_response.json()["id"]
+
+    dataset_name = "test_dataset"
+    mocked_response = mocked_client.post(
+        f"/api/v1/datasets", json={"name": dataset_name, "guidelines": "", "workspace_id": workspace_id}
+    )
+    dataset_id = mocked_response.json()["id"]
+
+    mocked_client.post(
+        f"/api/v1/datasets/{dataset_id}/fields",
+        json={"name": "test_field", "title": "text_field", "required": True, "settings": {"type": "text"}},
+    )
+
+    response = get_fields(client=sdk_client, id=dataset_id)
+    assert response.status_code == 200
+    assert isinstance(response.parsed, list)
+    assert len(response.parsed) > 0
+    assert isinstance(response.parsed[0], FeedbackFieldModel)
+
+
+def test_add_question(mocked_client, sdk_client, monkeypatch) -> None:
+    monkeypatch.setattr(sdk_client, "post", mocked_client.post)
+
+    workspace_name = "test_workspace"
+    mocked_response = mocked_client.post(f"/api/workspaces", json={"name": workspace_name})
+    workspace_id = mocked_response.json()["id"]
+
+    dataset_name = "test_dataset"
+    mocked_response = mocked_client.post(
+        f"/api/v1/datasets", json={"name": dataset_name, "guidelines": "", "workspace_id": workspace_id}
+    )
+    dataset_id = mocked_response.json()["id"]
+
+    response = add_question(
+        client=sdk_client,
+        id=dataset_id,
+        question={
+            "name": "test_question",
+            "title": "text_question",
+            "description": "test_description",
+            "required": True,
+            "settings": {"type": "text"},
+        },
+    )
+    assert response.status_code == 201
+
+
+def test_get_questions(mocked_client, sdk_client, monkeypatch) -> None:
+    monkeypatch.setattr(sdk_client, "get", mocked_client.get)
+
+    workspace_name = "test_workspace"
+    mocked_response = mocked_client.post(f"/api/workspaces", json={"name": workspace_name})
+    workspace_id = mocked_response.json()["id"]
+
+    dataset_name = "test_dataset"
+    mocked_response = mocked_client.post(
+        f"/api/v1/datasets", json={"name": dataset_name, "guidelines": "", "workspace_id": workspace_id}
+    )
+    dataset_id = mocked_response.json()["id"]
+
+    mocked_client.post(
+        f"/api/v1/datasets/{dataset_id}/questions",
+        json={
+            "name": "test_question",
+            "title": "text_question",
+            "description": "test_description",
+            "required": True,
+            "settings": {"type": "text"},
+        },
+    )
+
+    response = get_questions(client=sdk_client, id=dataset_id)
+    assert response.status_code == 200
+    assert isinstance(response.parsed, list)
+    assert len(response.parsed) > 0
+    assert isinstance(response.parsed[0], FeedbackQuestionModel)
+
+
+def test_add_records(mocked_client, sdk_client, monkeypatch) -> None:
+    monkeypatch.setattr(sdk_client, "post", mocked_client.post)
+
+    workspace_name = "test_workspace"
+    mocked_response = mocked_client.post(f"/api/workspaces", json={"name": workspace_name})
+    workspace_id = mocked_response.json()["id"]
+
+    dataset_name = "test_dataset"
+    mocked_response = mocked_client.post(
+        f"/api/v1/datasets", json={"name": dataset_name, "guidelines": "", "workspace_id": workspace_id}
+    )
+    dataset_id = mocked_response.json()["id"]
+
+    mocked_client.post(
+        f"/api/v1/datasets/{dataset_id}/fields",
+        json={"name": "test_field", "title": "text_field", "required": True, "settings": {"type": "text"}},
+    )
+    mocked_client.post(
+        f"/api/v1/datasets/{dataset_id}/questions",
+        json={
+            "name": "test_question",
+            "title": "text_question",
+            "description": "test_description",
+            "required": True,
+            "settings": {"type": "text"},
+        },
+    )
+    mocked_client.put(f"/api/v1/datasets/{dataset_id}/publish")
+
+    response = add_records(client=sdk_client, id=dataset_id, records=[{"fields": {"test_field": "test_value"}}])
+    assert response.status_code == 204
+
+
+def test_get_records(mocked_client, sdk_client, monkeypatch) -> None:
+    monkeypatch.setattr(sdk_client, "get", mocked_client.get)
+
+    workspace_name = "test_workspace"
+    mocked_response = mocked_client.post(f"/api/workspaces", json={"name": workspace_name})
+    workspace_id = mocked_response.json()["id"]
+
+    dataset_name = "test_dataset"
+    mocked_response = mocked_client.post(
+        f"/api/v1/datasets", json={"name": dataset_name, "guidelines": "", "workspace_id": workspace_id}
+    )
+    dataset_id = mocked_response.json()["id"]
+
+    mocked_client.post(
+        f"/api/v1/datasets/{dataset_id}/fields",
+        json={"name": "test_field", "title": "text_field", "required": True, "settings": {"type": "text"}},
+    )
+    mocked_client.post(
+        f"/api/v1/datasets/{dataset_id}/questions",
+        json={
+            "name": "test_question",
+            "title": "text_question",
+            "description": "test_description",
+            "required": True,
+            "settings": {"type": "text"},
+        },
+    )
+    mocked_client.put(f"/api/v1/datasets/{dataset_id}/publish")
+    mocked_client.post(
+        f"/api/v1/datasets/{dataset_id}/records", json={"items": [{"fields": {"test_field": "test_value"}}]}
+    )
+
+    response = get_records(client=sdk_client, id=dataset_id)
+    assert response.status_code == 200
+    assert isinstance(response.parsed, FeedbackRecordsModel)
+    assert len(response.parsed.items) > 0
+    assert FeedbackItemModel(**response.parsed.items[0])
