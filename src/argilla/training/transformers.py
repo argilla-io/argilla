3bf4ab2ebfc85e7bec21a4db2b1016b8ce49d35f
davidberenstein1957 <david.m.berenstein@gmail.com>
2023-05-17 13:50:31 +0200

    chore: added tokenclassification support chore: added tokenclassification tests

--- src/argilla/training/transformers.py
+++ src/argilla/training/transformers.py
@@ -135,7 +135,7 @@ class ArgillaTransformersTrainer(ArgillaTrainerSkeleton):
         else:
             padding_side = "right"
         self._transformers_tokenizer = AutoTokenizer.from_pretrained(
-            self.model_kwargs.get("pretrained_model_name_or_path"), padding_side=padding_side
+            self.model_kwargs.get("pretrained_model_name_or_path"), padding_side=padding_side, add_prefix_space=True
         )
         if getattr(self._transformers_tokenizer, "pad_token_id") is None:
             self._transformers_tokenizer.pad_token_id = self._transformers_tokenizer.eos_token_id
@@ -215,18 +215,18 @@ class ArgillaTransformersTrainer(ArgillaTrainerSkeleton):
 
         def token_classification_preprocess_function(examples):
             tokenized_inputs = self._transformers_tokenizer(
-                examples["tokens"], max_length=None, is_split_into_words=True, truncation=True
+                examples["tokens"], truncation=True, is_split_into_words=True
             )
 
             labels = []
             for i, label in enumerate(examples["ner_tags"]):
-                word_ids = tokenized_inputs.word_ids(batch_index=i)  # Map tokens to their respective word.
+                word_ids = tokenized_inputs.word_ids(batch_index=i)
                 previous_word_idx = None
                 label_ids = []
-                for word_idx in word_ids:  # Set the special tokens to -100.
+                for word_idx in word_ids:
                     if word_idx is None:
                         label_ids.append(-100)
-                    elif word_idx != previous_word_idx:  # Only label the first token of a given word.
+                    elif word_idx != previous_word_idx:
                         label_ids.append(label[word_idx])
                     else:
                         label_ids.append(-100)
