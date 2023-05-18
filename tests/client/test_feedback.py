fad826596312d1bcd471c7d244a14b38de2aa079
Alvaro Bartolome <alvaro@argilla.io>
2023-05-18 18:57:14 +0200

    Remove `TestFeedbackDataset` as not required

--- tests/client/test_feedback.py
+++ tests/client/test_feedback.py
@@ -29,205 +29,207 @@ from argilla.client.feedback import (
 )
 
 
-class TestFeedbackDataset:
-    @pytest.mark.usefixtures("feedback_dataset_guidelines", "feedback_dataset_fields", "feedback_dataset_questions")
-    def test_init(
-        self, feedback_dataset_guidelines: str, feedback_dataset_fields: list, feedback_dataset_questions: list
-    ) -> None:
-        dataset = FeedbackDataset(
-            guidelines=feedback_dataset_guidelines,
+@pytest.mark.usefixtures("feedback_dataset_guidelines", "feedback_dataset_fields", "feedback_dataset_questions")
+def test_init(
+    feedback_dataset_guidelines: str, feedback_dataset_fields: list, feedback_dataset_questions: list
+) -> None:
+    dataset = FeedbackDataset(
+        guidelines=feedback_dataset_guidelines,
+        fields=feedback_dataset_fields,
+        questions=feedback_dataset_questions,
+    )
+
+    assert dataset.guidelines == feedback_dataset_guidelines
+    assert dataset.fields == feedback_dataset_fields
+    assert dataset.questions == feedback_dataset_questions
+
+
+@pytest.mark.usefixtures("feedback_dataset_fields", "feedback_dataset_questions")
+def test_init_wrong_guidelines(feedback_dataset_fields: list, feedback_dataset_questions: list) -> None:
+    with pytest.raises(TypeError, match="Expected `guidelines` to be a string"):
+        FeedbackDataset(
+            guidelines=None,
             fields=feedback_dataset_fields,
             questions=feedback_dataset_questions,
         )
 
-        assert dataset.guidelines == feedback_dataset_guidelines
-        assert dataset.fields == feedback_dataset_fields
-        assert dataset.questions == feedback_dataset_questions
 
-    @pytest.mark.usefixtures("feedback_dataset_fields", "feedback_dataset_questions")
-    def test_init_wrong_guidelines(self, feedback_dataset_fields: list, feedback_dataset_questions: list) -> None:
-        with pytest.raises(TypeError, match="Expected `guidelines` to be a string"):
-            FeedbackDataset(
-                guidelines=None,
-                fields=feedback_dataset_fields,
-                questions=feedback_dataset_questions,
-            )
-
-    @pytest.mark.usefixtures("feedback_dataset_guidelines", "feedback_dataset_questions")
-    def test_init_wrong_fields(self, feedback_dataset_guidelines: str, feedback_dataset_questions: list) -> None:
-        with pytest.raises(TypeError, match="Expected `fields` to be a list"):
-            FeedbackDataset(
-                guidelines=feedback_dataset_guidelines,
-                fields=None,
-                questions=feedback_dataset_questions,
-            )
-        with pytest.raises(TypeError, match="Expected `fields` to be a list of `FieldSchema`"):
-            FeedbackDataset(
-                guidelines=feedback_dataset_guidelines,
-                fields=[{"wrong": "field"}],
-                questions=feedback_dataset_questions,
-            )
-        with pytest.raises(ValueError, match="At least one `FieldSchema` in `fields` must be required"):
-            FeedbackDataset(
-                guidelines=feedback_dataset_guidelines,
-                fields=[TextField(name="test", required=False)],
-                questions=feedback_dataset_questions,
-            )
-
-    @pytest.mark.usefixtures("feedback_dataset_guidelines", "feedback_dataset_fields")
-    def test_init_wrong_questions(self, feedback_dataset_guidelines: str, feedback_dataset_fields: list) -> None:
-        with pytest.raises(TypeError, match="Expected `questions` to be a list"):
-            FeedbackDataset(
-                guidelines=feedback_dataset_guidelines,
-                fields=feedback_dataset_fields,
-                questions=None,
-            )
-        with pytest.raises(TypeError, match="Expected `questions` to be a list of `QuestionSchema`"):
-            FeedbackDataset(
-                guidelines=feedback_dataset_guidelines,
-                fields=feedback_dataset_fields,
-                questions=[{"wrong": "question"}],
-            )
-        with pytest.raises(ValueError, match="At least one `QuestionSchema` in `questions` must be required"):
-            FeedbackDataset(
-                guidelines=feedback_dataset_guidelines,
-                fields=feedback_dataset_fields,
-                questions=[
-                    TextQuestion(name="test", required=False),
-                    RatingQuestion(name="test", values=[0, 1], required=False),
-                ],
-            )
-
-    @pytest.mark.usefixtures("feedback_dataset_guidelines", "feedback_dataset_fields", "feedback_dataset_questions")
-    def test_records(
-        self,
-        feedback_dataset_guidelines: str,
-        feedback_dataset_fields: List["FieldSchema"],
-        feedback_dataset_questions: List["QuestionSchema"],
-    ) -> None:
-        dataset = FeedbackDataset(
+@pytest.mark.usefixtures("feedback_dataset_guidelines", "feedback_dataset_questions")
+def test_init_wrong_fields(feedback_dataset_guidelines: str, feedback_dataset_questions: list) -> None:
+    with pytest.raises(TypeError, match="Expected `fields` to be a list"):
+        FeedbackDataset(
             guidelines=feedback_dataset_guidelines,
-            fields=feedback_dataset_fields,
+            fields=None,
+            questions=feedback_dataset_questions,
+        )
+    with pytest.raises(TypeError, match="Expected `fields` to be a list of `FieldSchema`"):
+        FeedbackDataset(
+            guidelines=feedback_dataset_guidelines,
+            fields=[{"wrong": "field"}],
+            questions=feedback_dataset_questions,
+        )
+    with pytest.raises(ValueError, match="At least one `FieldSchema` in `fields` must be required"):
+        FeedbackDataset(
+            guidelines=feedback_dataset_guidelines,
+            fields=[TextField(name="test", required=False)],
             questions=feedback_dataset_questions,
         )
 
-        assert dataset.records == []
 
-        dataset.add_records(
-            [
-                FeedbackRecord(
-                    fields={
-                        "text": "A",
-                        "label": "B",
-                    },
-                ),
-            ]
+@pytest.mark.usefixtures("feedback_dataset_guidelines", "feedback_dataset_fields")
+def test_init_wrong_questions(feedback_dataset_guidelines: str, feedback_dataset_fields: list) -> None:
+    with pytest.raises(TypeError, match="Expected `questions` to be a list"):
+        FeedbackDataset(
+            guidelines=feedback_dataset_guidelines,
+            fields=feedback_dataset_fields,
+            questions=None,
+        )
+    with pytest.raises(TypeError, match="Expected `questions` to be a list of `QuestionSchema`"):
+        FeedbackDataset(
+            guidelines=feedback_dataset_guidelines,
+            fields=feedback_dataset_fields,
+            questions=[{"wrong": "question"}],
+        )
+    with pytest.raises(ValueError, match="At least one `QuestionSchema` in `questions` must be required"):
+        FeedbackDataset(
+            guidelines=feedback_dataset_guidelines,
+            fields=feedback_dataset_fields,
+            questions=[
+                TextQuestion(name="test", required=False),
+                RatingQuestion(name="test", values=[0, 1], required=False),
+            ],
         )
 
-        assert len(dataset.records) == 1
-        assert dataset.records[0]["fields"] == {
-            "text": "A",
-            "label": "B",
-        }
-        assert dataset.records[0]["responses"] == []
 
-        dataset.add_records(
-            [
-                FeedbackRecord(
-                    fields={
-                        "text": "C",
-                        "label": "D",
-                    },
-                    responses={
-                        "values": {
-                            "question-1": {"value": "answer"},
-                            "question-2": {"value": 0},
-                        },
-                        "status": "submitted",
-                    },
-                    external_id="test-id",
-                ),
-            ]
-        )
+@pytest.mark.usefixtures("feedback_dataset_guidelines", "feedback_dataset_fields", "feedback_dataset_questions")
+def test_records(
+    feedback_dataset_guidelines: str,
+    feedback_dataset_fields: List["FieldSchema"],
+    feedback_dataset_questions: List["QuestionSchema"],
+) -> None:
+    dataset = FeedbackDataset(
+        guidelines=feedback_dataset_guidelines,
+        fields=feedback_dataset_fields,
+        questions=feedback_dataset_questions,
+    )
 
-        assert len(dataset.records) == 2
-        assert dataset.records[1]["fields"] == {
-            "text": "C",
-            "label": "D",
-        }
-        assert dataset.records[1]["responses"] == [
-            {
-                "values": {
-                    "question-1": {"value": "answer"},
-                    "question-2": {"value": 0},
+    assert dataset.records == []
+
+    dataset.add_records(
+        [
+            FeedbackRecord(
+                fields={
+                    "text": "A",
+                    "label": "B",
                 },
-                "status": "submitted",
-            },
+            ),
         ]
+    )
 
-        with pytest.raises(ValueError, match="`rg.FeedbackRecord.fields` does not match the expected schema"):
-            dataset.add_records(
-                [
-                    FeedbackRecord(
-                        fields={
-                            "wrong": "field",
-                        },
-                    ),
-                ]
-            )
+    assert len(dataset.records) == 1
+    assert dataset.records[0]["fields"] == {
+        "text": "A",
+        "label": "B",
+    }
+    assert dataset.records[0]["responses"] == []
 
-        for record in dataset.records:
+    dataset.add_records(
+        [
+            FeedbackRecord(
+                fields={
+                    "text": "C",
+                    "label": "D",
+                },
+                responses={
+                    "values": {
+                        "question-1": {"value": "answer"},
+                        "question-2": {"value": 0},
+                    },
+                    "status": "submitted",
+                },
+                external_id="test-id",
+            ),
+        ]
+    )
+
+    assert len(dataset.records) == 2
+    assert dataset.records[1]["fields"] == {
+        "text": "C",
+        "label": "D",
+    }
+    assert dataset.records[1]["responses"] == [
+        {
+            "values": {
+                "question-1": {"value": "answer"},
+                "question-2": {"value": 0},
+            },
+            "status": "submitted",
+        },
+    ]
+
+    with pytest.raises(ValueError, match="`rg.FeedbackRecord.fields` does not match the expected schema"):
+        dataset.add_records(
+            [
+                FeedbackRecord(
+                    fields={
+                        "wrong": "field",
+                    },
+                ),
+            ]
+        )
+
+    for record in dataset.records:
+        assert all(
+            key in ["id", "fields", "external_id", "responses", "inserted_at", "updated_at"]
+            for key in list(record.keys())
+        )
+
+    for batch in dataset.iter(batch_size=1):
+        assert len(batch) == 1
+        for record in batch:
             assert all(
                 key in ["id", "fields", "external_id", "responses", "inserted_at", "updated_at"]
                 for key in list(record.keys())
             )
 
-        for batch in dataset.iter(batch_size=1):
-            assert len(batch) == 1
-            for record in batch:
-                assert all(
-                    key in ["id", "fields", "external_id", "responses", "inserted_at", "updated_at"]
-                    for key in list(record.keys())
-                )
+    assert len(dataset[:2]) == 2
+    assert len(dataset[1:2]) == 1
+    assert len(dataset) == len(dataset.records)
 
-        assert len(dataset[:2]) == 2
-        assert len(dataset[1:2]) == 1
-        assert len(dataset) == len(dataset.records)
 
-    @pytest.mark.usefixtures(
-        "feedback_dataset_guidelines",
-        "feedback_dataset_fields",
-        "feedback_dataset_questions",
-        "feedback_dataset_records",
+@pytest.mark.usefixtures(
+    "feedback_dataset_guidelines",
+    "feedback_dataset_fields",
+    "feedback_dataset_questions",
+    "feedback_dataset_records",
+)
+def test_push_to_argilla_and_from_argilla(
+    mocked_client,
+    feedback_dataset_guidelines: str,
+    feedback_dataset_fields: List["FieldSchema"],
+    feedback_dataset_questions: List["QuestionSchema"],
+    feedback_dataset_records: List[FeedbackRecord],
+) -> None:
+    api.active_api()
+    api.init(api_key="argilla.apikey")
+
+    dataset = FeedbackDataset(
+        guidelines=feedback_dataset_guidelines,
+        fields=feedback_dataset_fields,
+        questions=feedback_dataset_questions,
     )
-    def test_push_to_argilla_and_from_argilla(
-        self,
-        mocked_client,
-        feedback_dataset_guidelines: str,
-        feedback_dataset_fields: List["FieldSchema"],
-        feedback_dataset_questions: List["QuestionSchema"],
-        feedback_dataset_records: List[FeedbackRecord],
-    ) -> None:
-        api.active_api()
-        api.init(api_key="argilla.apikey")
+    dataset.add_records(records=feedback_dataset_records)
+    dataset.push_to_argilla(name="test-dataset")
 
-        dataset = FeedbackDataset(
-            guidelines=feedback_dataset_guidelines,
-            fields=feedback_dataset_fields,
-            questions=feedback_dataset_questions,
-        )
-        dataset.add_records(records=feedback_dataset_records)
-        dataset.push_to_argilla(name="test-dataset")
+    assert dataset.argilla_id is not None
 
-        assert dataset.argilla_id is not None
+    dataset_from_argilla = FeedbackDataset.from_argilla(id=dataset.argilla_id)
 
-        dataset_from_argilla = FeedbackDataset.from_argilla(id=dataset.argilla_id)
-
-        assert dataset_from_argilla.guidelines == dataset.guidelines
-        assert len(dataset_from_argilla.fields) == len(dataset.fields)
-        assert [field.name for field in dataset_from_argilla.fields] == [field.name for field in dataset.fields]
-        assert len(dataset_from_argilla.questions) == len(dataset.questions)
-        assert [question.name for question in dataset_from_argilla.questions] == [
-            question.name for question in dataset.questions
-        ]
-        assert len(dataset_from_argilla.records) == len(dataset.records)
+    assert dataset_from_argilla.guidelines == dataset.guidelines
+    assert len(dataset_from_argilla.fields) == len(dataset.fields)
+    assert [field.name for field in dataset_from_argilla.fields] == [field.name for field in dataset.fields]
+    assert len(dataset_from_argilla.questions) == len(dataset.questions)
+    assert [question.name for question in dataset_from_argilla.questions] == [
+        question.name for question in dataset.questions
+    ]
+    assert len(dataset_from_argilla.records) == len(dataset.records)
