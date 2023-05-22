8139f81e830dbf7d50df58c0aa4e6f18acde07b1
Alvaro Bartolome <alvaro@argilla.io>
2023-05-22 11:36:56 +0200

    Add `format_as`, `push_to_huggingface`, & `from_huggingface` in `FeedbackDataset` (#2949)
    # Description
    
    This PR adds some methods to the `rg.FeedbackDataset` as follows:
    
    * `format_as` to format the records of the existing dataset as
    🤗`datasets` format (i.e. `datasets.Dataset`) to be extended to support
    other formats like `pandas` or `numpy`
    * `push_to_huggingface` to push the existing records and dataset
    configuration to the HuggingFace Hub (including an auto-generated
    dataset card! 🎉)
    * `from_huggingface` is a classmethod to load a `rg.FeedbackDataset`
    from the HuggingFace Hub, pulling the dataset itself (records and
    annotations) and the configuration (guidelines, fields, and questions)
    
    Closes #2765
    
    **Type of change**
    
    - [X] New feature (non-breaking change which adds functionality)
    
    **How Has This Been Tested**
    
    - [X] Added integration tests for `push_to_huggingface` and
    `from_huggingface`
    - [x] Added integration tests for `format_as("datasets")`
    
    **Checklist**
    
    - [X] I have merged the original branch into my forked branch
    - [X] I added relevant documentation
    - [X] follows the style guidelines of this project
    - [X] I did a self-review of my code
    - [X] My changes generate no new warnings
    - [X] I have added tests that prove my fix is effective or that my
    feature works
    
    ---------
    
    Co-authored-by: Francisco Aranda <francis@argilla.io>

--- src/argilla/client/feedback.py
+++ src/argilla/client/feedback.py
@@ -12,6 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import tempfile
 import warnings
 from typing import TYPE_CHECKING, Any, Dict, Iterator, List, Optional, Tuple, Union
 from uuid import UUID
@@ -35,9 +36,11 @@ from tqdm import tqdm
 
 import argilla as rg
 from argilla.client.sdk.v1.datasets import api as datasets_api_v1
+from argilla.utils.dependency import requires_version
 
 if TYPE_CHECKING:
     import httpx
+    from datasets import Dataset
 
     from argilla.client.sdk.v1.datasets.models import (
         FeedbackDatasetModel,
@@ -150,6 +153,12 @@ class RatingQuestion(QuestionSchema):
 
     class Config:
         validate_assignment = True
+
+
+class FeedbackDatasetConfig(BaseModel):
+    fields: List[FieldSchema]
+    questions: List[QuestionSchema]
+    guidelines: Optional[str] = None
 
 
 class FeedbackDataset:
@@ -468,7 +477,7 @@ class FeedbackDataset:
             yield self.records[i : i + batch_size]
 
     def push_to_argilla(self, name: Optional[str] = None, workspace: Optional[Union[str, rg.Workspace]] = None) -> None:
-        """Pushes the dataset to Argilla. If the dataset has been previously pushed to Argilla, it will be updated
+        """Pushes the `FeedbackDataset` to Argilla. If the dataset has been previously pushed to Argilla, it will be updated
         with the new records.
 
         Note that you may need to `rg.init(...)` with your Argilla credentials before calling this function, otherwise
@@ -671,6 +680,227 @@ class FeedbackDataset:
         if with_records:
             self.fetch_records()
         return self
+
+    @requires_version("datasets")
+    def format_as(self, format: Literal["datasets"]) -> "Dataset":
+        """Formats the `FeedbackDataset` as a `datasets.Dataset` object.
+
+        Args:
+            format: the format to use to format the `FeedbackDataset`. Currently supported formats are:
+                `datasets`.
+
+        Returns:
+            The `FeedbackDataset.records` formatted as a `datasets.Dataset` object.
+
+        Raises:
+            ValueError: if the provided format is not supported.
+
+        Examples:
+            >>> import argilla as rg
+            >>> rg.init(...)
+            >>> dataset = rg.FeedbackDataset.from_argilla(name="my-dataset")
+            >>> huggingface_dataset = dataset.format_as("datasets")
+        """
+        if format == "datasets":
+            from datasets import Dataset, Features, Sequence, Value
+
+            dataset = {}
+            features = {}
+            for field in self.fields:
+                if field.settings["type"] not in FIELD_TYPE_TO_PYTHON_TYPE.keys():
+                    raise ValueError(
+                        f"Field {field.name} has an unsupported type: {field.settings['type']}, for the moment only the"
+                        f" following types are supported: {list(FIELD_TYPE_TO_PYTHON_TYPE.keys())}"
+                    )
+                features[field.name] = Value(dtype="string", id="field")
+                if field.name not in dataset:
+                    dataset[field.name] = []
+            for question in self.questions:
+                # TODO(alvarobartt): if we constraint ranges from 0 to N, then we can use `ClassLabel` for ratings
+                features[question.name] = Sequence(
+                    {
+                        "user_id": Value(dtype="string"),
+                        "value": Value(dtype="string" if question.settings["type"] == "text" else "int32"),
+                    },
+                    id="question",
+                )
+                if question.name not in dataset:
+                    dataset[question.name] = []
+            features["external_id"] = Value(dtype="string", id="external_id")
+            dataset["external_id"] = []
+
+            for record in self.records:
+                for field in self.fields:
+                    dataset[field.name].append(record["fields"][field.name])
+                for question in self.questions:
+                    dataset[question.name].append(
+                        [
+                            {"user_id": r["user_id"], "value": r["values"][question.name]["value"]}
+                            for r in record["responses"]
+                        ]
+                        or None
+                    )
+                dataset["external_id"].append(record["external_id"] or None)
+
+            return Dataset.from_dict(
+                dataset,
+                features=Features(features),
+            )
+        raise ValueError(f"Unsupported format '{format}'.")
+
+    @requires_version("huggingface_hub")
+    def push_to_huggingface(self, repo_id: str, generate_card: Optional[bool] = True, *args, **kwargs) -> None:
+        """Pushes the `FeedbackDataset` to the HuggingFace Hub. If the dataset has been previously pushed to the
+        HuggingFace Hub, it will be updated instead. Note that some params as `private` have no effect at all
+        when a dataset is previously uploaded to the HuggingFace Hub.
+
+        Args:
+            repo_id: the ID of the HuggingFace Hub repo to push the `FeedbackDataset` to.
+            generate_card: whether to generate a dataset card for the `FeedbackDataset` in the HuggingFace Hub. Defaults
+                to `True`.
+            *args: the args to pass to `datasets.Dataset.push_to_hub`.
+            **kwargs: the kwargs to pass to `datasets.Dataset.push_to_hub`.
+        """
+        import huggingface_hub
+        from huggingface_hub import DatasetCard, HfApi
+        from packaging.version import parse as parse_version
+
+        if parse_version(huggingface_hub.__version__) < parse_version("0.14.0"):
+            warnings.warn(
+                f"Recommended `huggingface_hub` version is 0.14.0 or higher, and you have {huggingface_hub.__version__}, so in case you have any issue when pushing the dataset to the HuggingFace Hub upgrade it as `pip install huggingface_hub --upgrade`."
+            )
+
+        if len(self) < 1:
+            raise ValueError(
+                "Cannot push an empty `rg.FeedbackDataset` to the HuggingFace Hub, please make sure to add at least one record, via the method `add_records`."
+            )
+
+        hfds = self.format_as("datasets")
+        hfds.push_to_hub(repo_id, *args, **kwargs)
+
+        with tempfile.NamedTemporaryFile(mode="w", suffix=".cfg", delete=False) as f:
+            f.write(
+                FeedbackDatasetConfig(fields=self.fields, questions=self.questions, guidelines=self.guidelines).json()
+            )
+            f.flush()
+
+            HfApi().upload_file(
+                path_or_fileobj=f.name,
+                path_in_repo="argilla.cfg",
+                repo_id=repo_id,
+                repo_type="dataset",
+                token=kwargs.get("token"),
+            )
+
+        if generate_card:
+            explained_fields = "## Fields\n\n" + "".join(
+                [
+                    f"* `{field.name}` is of type {FIELD_TYPE_TO_PYTHON_TYPE[field.settings['type']]}\n"
+                    for field in self.fields
+                ]
+            )
+            explained_questions = "## Questions\n\n" + "".join(
+                [
+                    f"* `{question.name}` {': ' + question.description if question.description else None}\n"
+                    for question in self.questions
+                ]
+            )
+            loading_guide = (
+                "## Load with Argilla\n\nTo load this dataset with Argilla, you'll just need to "
+                "install Argilla as `pip install argilla --upgrade` and then use the following code:\n\n"
+                "```python\n"
+                "import argilla as rg\n\n"
+                f"ds = rg.FeedbackDataset.from_huggingface({repo_id!r})\n"
+                "```\n\n"
+                "## Load with Datasets\n\nTo load this dataset with Datasets, you'll just need to "
+                "install Datasets as `pip install datasets --upgrade` and then use the following code:\n\n"
+                "```python\n"
+                "from datasets import load_dataset\n\n"
+                f"ds = load_dataset({repo_id!r})\n"
+                "```"
+            )
+            card = DatasetCard(
+                (
+                    f"## Guidelines\n\n{self.guidelines}\n\n"
+                    f"{explained_fields}\n\n"
+                    f"{explained_questions}\n\n"
+                    f"{loading_guide}\n\n"
+                )
+            )
+            card.push_to_hub(repo_id, repo_type="dataset", token=kwargs.get("token"))
+
+    @classmethod
+    @requires_version("datasets")
+    @requires_version("huggingface_hub")
+    def from_huggingface(cls, repo_id: str, *args, **kwargs) -> "FeedbackDataset":
+        """Loads a `FeedbackDataset` from the HuggingFace Hub.
+
+        Args:
+            repo_id: the ID of the HuggingFace Hub repo to load the `FeedbackDataset` from.
+            *args: the args to pass to `datasets.Dataset.load_from_hub`.
+            **kwargs: the kwargs to pass to `datasets.Dataset.load_from_hub`.
+
+        Returns:
+            A `FeedbackDataset` loaded from the HuggingFace Hub.
+        """
+        import huggingface_hub
+        from datasets import DatasetDict, load_dataset
+        from huggingface_hub import hf_hub_download
+        from packaging.version import parse as parse_version
+
+        if parse_version(huggingface_hub.__version__) < parse_version("0.14.0"):
+            warnings.warn(
+                f"Recommended `huggingface_hub` version is 0.14.0 or higher, and you have {huggingface_hub.__version__}, so in case you have any issue when pushing the dataset to the HuggingFace Hub upgrade it as `pip install huggingface_hub --upgrade`."
+            )
+
+        config_path = hf_hub_download(
+            repo_id=repo_id,
+            filename="argilla.cfg",
+            repo_type="dataset",
+            token=kwargs["token"] if "token" in kwargs else None,
+        )
+        with open(config_path, "rb") as f:
+            config = FeedbackDatasetConfig.parse_raw(f.read())
+
+        cls = cls(
+            fields=config.fields,
+            questions=config.questions,
+            guidelines=config.guidelines,
+        )
+
+        if "token" in kwargs:
+            kwargs["use_auth_token"] = kwargs["token"]
+            del kwargs["token"]
+
+        hfds = load_dataset(repo_id, *args, **kwargs)
+        if isinstance(hfds, DatasetDict) and "split" not in kwargs:
+            if len(hfds.keys()) > 1:
+                raise ValueError(
+                    f"Only one dataset can be loaded at a time, use `split` to select a split, available splits are: {', '.join(hfds.keys())}."
+                )
+
+        for index in range(len(hfds)):
+            responses = {}
+            for question in cls.questions:
+                if hfds[index][question.name] is None or len(hfds[index][question.name]) < 1:
+                    continue
+                for user_id, value in zip(hfds[index][question.name]["user_id"], hfds[index][question.name]["value"]):
+                    if user_id not in responses:
+                        responses[user_id] = {
+                            "user_id": user_id,
+                            "status": "submitted",
+                            "values": {},
+                        }
+                    responses[user_id]["values"].update({question.name: {"value": value}})
+            cls.__records.append(
+                FeedbackRecord(
+                    fields={field.name: hfds[index][field.name] for field in cls.fields},
+                    responses=list(responses.values()) or None,
+                    external_id=hfds[index]["external_id"],
+                )
+            )
+        del hfds
+        return cls
 
 
 def generate_pydantic_schema(fields: List[FieldSchema], name: Optional[str] = "FieldsSchema") -> BaseModel:
