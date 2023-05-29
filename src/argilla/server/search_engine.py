2c3bdeed28ee2d2e0262b717f432a3ef5172b55d
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2023-05-29 14:04:55 +0200

    Update src/argilla/server/search_engine.py
    Co-authored-by: Francisco Aranda <francis@argilla.io>

--- src/argilla/server/search_engine.py
+++ src/argilla/server/search_engine.py
@@ -103,6 +103,7 @@ class SearchEngine:
             # See https://www.elastic.co/guide/en/elasticsearch/reference/current/number.html
             return {"type": "integer"}
         elif settings.type in [QuestionType.text, QuestionType.label_selection]:
+            # TODO: Review mapping for label selection. Could make sense to use `keyword` mapping instead. See https://www.elastic.co/guide/en/elasticsearch/reference/current/keyword.html
             # See https://www.elastic.co/guide/en/elasticsearch/reference/current/text.html
             return {"type": "text", "index": False}
         else:
