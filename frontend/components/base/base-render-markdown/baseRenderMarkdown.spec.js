755a7d0eccf02f039eea7a9d5e5c882026575970
leire <leire@recogn.ai>
2023-05-30 14:46:37 +0200

    update test

--- frontend/components/base/base-render-markdown/baseRenderMarkdown.spec.js
+++ frontend/components/base/base-render-markdown/baseRenderMarkdown.spec.js
@@ -23,18 +23,18 @@ describe("BaseRenderMarkdown", () => {
   });
 
   it("render parsed html", () => {
-    expect(wrapper.contains("h1")).toBe(true);
+    expect(wrapper.html().includes("h1")).toBe(true);
   });
 
   it("prevent render not allowed tags", async () => {
     await wrapper.setProps({ markdown: "<script>" });
     await wrapper.vm.$nextTick();
-    expect(wrapper.contains("script")).toBe(false);
+    expect(wrapper.html().includes("<script>")).toBe(false);
   });
 
   it("prevent render unsanitized html", async () => {
     await wrapper.setProps({ markdown: "<TABLE>" });
     await wrapper.vm.$nextTick();
-    expect(wrapper.contains("TABLE")).toBe(true);
+    expect(wrapper.html().includes("<TABLE>")).toBe(false);
   });
 });
