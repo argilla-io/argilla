72ba1b1b0c5945862715d8408c461f5668174b87
gabrielmbmb <gmartinbdev@gmail.com>
2023-06-05 15:36:35 +0200

    feat: check that `offset` is greater than `0`

--- src/argilla/server/apis/v1/handlers/datasets.py
+++ src/argilla/server/apis/v1/handlers/datasets.py
@@ -310,7 +310,7 @@ async def search_dataset_records(
     query: SearchEngineQuery,
     include: List[RecordInclude] = Query([]),
     response_status: Optional[ResponseStatusFilter] = Query(None),
-    offset: int = Query(0),
+    offset: int = Query(0, ge=0),
     limit: int = Query(default=LIST_DATASET_RECORDS_LIMIT_DEFAULT, lte=LIST_DATASET_RECORDS_LIMIT_LTE),
     current_user: User = Security(auth.get_current_user),
 ):
