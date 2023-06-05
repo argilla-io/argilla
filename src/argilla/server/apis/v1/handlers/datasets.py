7d1605fdc5b1f0d54cca9b2f0b554b9fdbf68fb6
gabrielmbmb <gmartinbdev@gmail.com>
2023-06-05 10:54:13 +0200

    feat: remove `Dict` import

--- src/argilla/server/apis/v1/handlers/datasets.py
+++ src/argilla/server/apis/v1/handlers/datasets.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import TYPE_CHECKING, Dict, List, Optional
+from typing import TYPE_CHECKING, List, Optional
 from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException, Query, Security, status
