74ea3a0fadbfd084144f0adb3b803486e0502f17
Alvaro Bartolome <alvaro@argilla.io>
2023-06-06 16:56:22 +0200

    feat: add `SingleLabelQuestion` and `MultiLabelQuestion` in `__init__`

--- src/argilla/__init__.py
+++ src/argilla/__init__.py
@@ -69,7 +69,9 @@ if _TYPE_CHECKING:
     from argilla.client.feedback.dataset import FeedbackDataset
     from argilla.client.feedback.schemas import (
         FeedbackRecord,
+        MultiLabelQuestion,
         RatingQuestion,
+        SingleLabelQuestion,
         TextField,
         TextQuestion,
     )
@@ -127,6 +129,8 @@ _import_structure = {
         "RatingQuestion",
         "TextField",
         "TextQuestion",
+        "SingleLabelQuestion",
+        "MultiLabelQuestion",
     ],
     "client.workspaces": ["Workspace"],
     "monitoring.model_monitor": ["monitor"],
