5bcf71cbeecc22cd6a6fd2b65e77f604eec2d5a9
Keith Cuniah <keith@argilla.io>
2023-06-09 09:28:47 +0200

    :speech_balloon: update messagewhen no result after search

--- frontend/components/page-contents/feedback-task-content/RecordFeedbackTaskAndQuestionnaire.content.vue
+++ frontend/components/page-contents/feedback-task-content/RecordFeedbackTaskAndQuestionnaire.content.vue
@@ -229,7 +229,12 @@ export default {
       });
     },
     noRecordsMessage() {
-      return `You have no ${this.recordStatusToFilterWith} records`;
+      if (
+        isNil(this.searchTextToFilterWith) ||
+        this.searchTextToFilterWith.length === 0
+      )
+        return `You have no ${this.recordStatusToFilterWith} records`;
+      return `You have no ${this.recordStatusToFilterWith} records matching the search input : ${this.searchTextToFilterWith}`;
     },
     statusClass() {
       return `--${this.record.record_status.toLowerCase()}`;
