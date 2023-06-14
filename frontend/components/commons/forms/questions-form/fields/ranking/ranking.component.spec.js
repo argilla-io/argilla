10295b414cde6596d28ea588c8ed96140fc937db
Keith Cuniah <keith@argilla.io>
2023-06-14 13:24:35 +0200

    :white_check_mark: test `Ranking` pass the good props to the QuestionHeaderComponent

--- frontend/components/commons/forms/questions-form/fields/ranking/ranking.component.spec.js
+++ frontend/components/commons/forms/questions-form/fields/ranking/ranking.component.spec.js
@@ -3,8 +3,14 @@ import RankingComponent from "./Ranking.component";
 
 let wrapper = null;
 
+const QuestionHeaderComponentStub = {
+  name: "child-component",
+  template: "<div />",
+  props: ["title", "isRequired", "tooltipMessage"],
+};
+
 const options = {
-  stubs: ["QuestionHeaderComponent"],
+  stubs: { QuestionHeaderComponent: QuestionHeaderComponentStub },
   propsData: {
     title: "This is the title",
     settings: {},
@@ -23,11 +29,14 @@ describe("RankingComponent", () => {
   it("render the component", () => {
     expect(wrapper.is(RankingComponent)).toBe(true);
 
-    const QuestionHeaderWrapper = wrapper.findComponent({
-      name: "QuestionHeaderComponent",
-    });
+    const QuestionHeaderWrapper = wrapper.findComponent(
+      QuestionHeaderComponentStub
+    );
 
     expect(QuestionHeaderWrapper.exists()).toBe(true);
+
+    expect(wrapper.vm.isRequired).toBe(false);
+    expect(wrapper.vm.description).toBe("");
   });
   it("has a title prop as required and must be a string", () => {
     expect(RankingComponent.props.title).toMatchObject({
@@ -35,17 +44,32 @@ describe("RankingComponent", () => {
       required: true,
     });
   });
+  it("pass the title props to the QuestionHeaderComponent", () => {
+    expect(
+      wrapper.findComponent(QuestionHeaderComponentStub).props("title")
+    ).toBe(wrapper.vm.title);
+  });
   it("has a isRequired prop with a default value and must be a boolean", () => {
     expect(RankingComponent.props.isRequired).toMatchObject({
       type: Boolean,
       default: false,
     });
   });
+  it("pass the isRequired props to the QuestionHeaderComponent", () => {
+    expect(
+      wrapper.findComponent(QuestionHeaderComponentStub).props("isRequired")
+    ).toBe(wrapper.vm.isRequired);
+  });
   it("has a description prop with a default value and must be a string", () => {
     expect(RankingComponent.props.description).toMatchObject({
       type: String,
       default: "",
     });
+  });
+  it("pass the description props to the QuestionHeaderComponent", () => {
+    expect(
+      wrapper.findComponent(QuestionHeaderComponentStub).props("tooltipMessage")
+    ).toBe(wrapper.vm.description);
   });
   it("has a settings prop with as required and must be an object", () => {
     expect(RankingComponent.props.settings).toMatchObject({
