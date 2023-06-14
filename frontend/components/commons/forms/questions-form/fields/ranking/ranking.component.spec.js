883e6b9fff2f2556840b2d128fc69995f5f64c3f
Keith Cuniah <keith@argilla.io>
2023-06-14 11:53:35 +0200

    :white_check_mark: unit test on props

--- frontend/components/commons/forms/questions-form/fields/ranking/ranking.component.spec.js
+++ frontend/components/commons/forms/questions-form/fields/ranking/ranking.component.spec.js
@@ -3,7 +3,13 @@ import RankingComponent from "./Ranking.component";
 
 let wrapper = null;
 
-const options = { stubs: ["QuestionHeaderComponent"] };
+const options = {
+  stubs: ["QuestionHeaderComponent"],
+  propsData: {
+    title: "This is the title",
+    settings: {},
+  },
+};
 
 beforeEach(() => {
   wrapper = shallowMount(RankingComponent, options);
@@ -22,5 +28,29 @@ describe("RankingComponent", () => {
     });
 
     expect(QuestionHeaderWrapper.exists()).toBe(true);
+  });
+  it("has a title prop as required and must be a string", () => {
+    expect(RankingComponent.props.title).toMatchObject({
+      type: String,
+      required: true,
+    });
+  });
+  it("has a isRequired prop with a default value and must be a boolean", () => {
+    expect(RankingComponent.props.isRequired).toMatchObject({
+      type: Boolean,
+      default: false,
+    });
+  });
+  it("has a description prop with a default value and must be a string", () => {
+    expect(RankingComponent.props.description).toMatchObject({
+      type: String,
+      default: "",
+    });
+  });
+  it("has a settings prop with as required and must be an object", () => {
+    expect(RankingComponent.props.settings).toMatchObject({
+      type: Object,
+      required: true,
+    });
   });
 });
