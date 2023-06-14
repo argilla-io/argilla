4f3a962ab459d6a82316e00b2c1785abe73db33c
Keith Cuniah <keith@argilla.io>
2023-06-14 16:32:28 +0200

    :white_check_mark: stub children && check that we pass the good props

--- frontend/components/commons/forms/questions-form/fields/multi-label/multiLabel.component.spec.js
+++ frontend/components/commons/forms/questions-form/fields/multi-label/multiLabel.component.spec.js
@@ -1,6 +1,17 @@
 import { shallowMount } from "@vue/test-utils";
 import MultiLabelComponent from "./MultiLabel.component";
 
+const QuestionHeaderComponentStub = {
+  name: "QuestionHeaderComponent",
+  template: "<div />",
+  props: ["title", "isRequired", "tooltipMessage"],
+};
+const LabelSelectionComponentStub = {
+  name: "LabelSelectionComponent",
+  template: "<div />",
+  props: ["multiple", "componentId", "maxOptionsToShowBeforeCollapse"],
+};
+
 describe("MultiLabelComponent", () => {
   it("render by default the QuestionHeader and the LabelSelectionComponent", () => {
     const options = initOptionsToMount({
@@ -66,6 +77,29 @@ describe("MultiLabelComponent", () => {
       },
     ]);
     expect(wrapper.vm.maxOptionsToShowBeforeCollapse).toBe(-1);
+
+    // test what we pass to children components
+    expect(
+      wrapper.findComponent(QuestionHeaderComponentStub).props("title")
+    ).toBe(wrapper.vm.title);
+    expect(
+      wrapper.findComponent(QuestionHeaderComponentStub).props("isRequired")
+    ).toBe(wrapper.vm.isRequired);
+    expect(
+      wrapper.findComponent(QuestionHeaderComponentStub).props("tooltipMessage")
+    ).toBe(wrapper.vm.description);
+
+    expect(
+      wrapper.findComponent(LabelSelectionComponentStub).props("multiple")
+    ).toBe(true);
+    expect(
+      wrapper.findComponent(LabelSelectionComponentStub).props("componentId")
+    ).toBe(wrapper.vm.questionId);
+    expect(
+      wrapper
+        .findComponent(LabelSelectionComponentStub)
+        .props("maxOptionsToShowBeforeCollapse")
+    ).toBe(wrapper.vm.maxOptionsToShowBeforeCollapse);
   });
   it("update the maxOptionsToShowBeforeCollapse depending of the value of the props visibleOptions", async () => {
     const options = initOptionsToMount({
@@ -111,6 +145,29 @@ describe("MultiLabelComponent", () => {
       name: "LabelSelectionComponent",
     });
     expect(labelSelectionWrapper.exists()).toBe(true);
+
+    // test what we pass to children components
+    expect(
+      wrapper.findComponent(QuestionHeaderComponentStub).props("title")
+    ).toBe(wrapper.vm.title);
+    expect(
+      wrapper.findComponent(QuestionHeaderComponentStub).props("isRequired")
+    ).toBe(wrapper.vm.isRequired);
+    expect(
+      wrapper.findComponent(QuestionHeaderComponentStub).props("tooltipMessage")
+    ).toBe(wrapper.vm.description);
+
+    expect(
+      wrapper.findComponent(LabelSelectionComponentStub).props("multiple")
+    ).toBe(true);
+    expect(
+      wrapper.findComponent(LabelSelectionComponentStub).props("componentId")
+    ).toBe(wrapper.vm.questionId);
+    expect(
+      wrapper
+        .findComponent(LabelSelectionComponentStub)
+        .props("maxOptionsToShowBeforeCollapse")
+    ).toBe(wrapper.vm.maxOptionsToShowBeforeCollapse);
   });
   it("remove the duplicate values before passing the props uniqueOptions to LabelSelection component", async () => {
     const options = initOptionsToMount({
@@ -152,16 +209,38 @@ describe("MultiLabelComponent", () => {
         is_selected: false,
       },
     ]);
+
+    // test what we pass to children components
+    expect(
+      wrapper.findComponent(QuestionHeaderComponentStub).props("title")
+    ).toBe(wrapper.vm.title);
+    expect(
+      wrapper.findComponent(QuestionHeaderComponentStub).props("isRequired")
+    ).toBe(wrapper.vm.isRequired);
+    expect(
+      wrapper.findComponent(QuestionHeaderComponentStub).props("tooltipMessage")
+    ).toBe(wrapper.vm.description);
+
+    expect(
+      wrapper.findComponent(LabelSelectionComponentStub).props("multiple")
+    ).toBe(true);
+    expect(
+      wrapper.findComponent(LabelSelectionComponentStub).props("componentId")
+    ).toBe(wrapper.vm.questionId);
+    expect(
+      wrapper
+        .findComponent(LabelSelectionComponentStub)
+        .props("maxOptionsToShowBeforeCollapse")
+    ).toBe(wrapper.vm.maxOptionsToShowBeforeCollapse);
   });
 });
 
 const initOptionsToMount = ({ propsOptions }) => {
   const options = {
-    stubs: [
-      "QuestionHeaderComponent",
-      "LabelSelectionComponent",
-      "BaseIconWithBadge",
-    ],
+    stubs: {
+      LabelSelectionComponent: LabelSelectionComponentStub,
+      QuestionHeaderComponent: QuestionHeaderComponentStub,
+    },
     directives: {
       "optional-field"() {
         // this directive is used to show '(optional)' at the end of a question optional
