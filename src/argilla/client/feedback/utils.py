ce757014d8a5db9d1f0bcac12f0191e5b1cc32c7
Alvaro Bartolome <alvaro@argilla.io>
2023-06-20 11:33:42 +0200

    fix: remove unused import

--- src/argilla/client/feedback/utils.py
+++ src/argilla/client/feedback/utils.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import TYPE_CHECKING, List, Optional, Tuple, Union
+from typing import TYPE_CHECKING, List, Optional, Union
 
 from pydantic import (
     BaseModel,
