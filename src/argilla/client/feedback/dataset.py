b68278db465e6a31441c783a4fd89c6453a9d4e8
Alvaro Bartolome <alvaro@argilla.io>
2023-06-20 12:14:36 +0200

    fix: `push_to_huggingface` parsing of UUIDs (#3231)
    # Description
    
    Due to the recent constraint for the `FeedbackRecord.user_id`s to be
    `UUID`s instead of `str`s, the conversion to `dict()` was failing, as
    the `UUID`s are not JSON-serializable, which was leading to some issues
    when trying to add the example record to the `DatasetCard` via the
    `dict()` conversion.
    
    **Type of change**
    
    - [X] Bug fix (non-breaking change which fixes an issue)
    
    **How Has This Been Tested**
    
    - [X] Re-run unit tests to pass when generating the `DatasetCard` for
    `FeedbackDataset`s
    
    **Checklist**
    
    - [X] I have merged the original branch into my forked branch
    - [ ] I added relevant documentation
    - [X] follows the style guidelines of this project
    - [X] I did a self-review of my code
    - [ ] I made corresponding changes to the documentation
    - [X] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have added relevant notes to the CHANGELOG.md file (See
    https://keepachangelog.com/)

--- src/argilla/client/feedback/dataset.py
+++ src/argilla/client/feedback/dataset.py
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
 import json
 import logging
 import tempfile
@@ -844,7 +845,7 @@ class FeedbackDataset:
                 argilla_fields=self.fields,
                 argilla_questions=self.questions,
                 argilla_guidelines=self.guidelines,
-                argilla_record=self.records[0].dict(),
+                argilla_record=json.loads(self.records[0].json()),
                 huggingface_record=hfds[0],
             )
             card.push_to_hub(repo_id, repo_type="dataset", token=kwargs.get("token"))
@@ -893,7 +894,7 @@ class FeedbackDataset:
             repo_type="dataset",
             **hub_auth,
         )
-        with open(config_path, "rb") as f:
+        with open(config_path, "r") as f:
             config = FeedbackDatasetConfig.parse_raw(f.read())
 
         cls = cls(
