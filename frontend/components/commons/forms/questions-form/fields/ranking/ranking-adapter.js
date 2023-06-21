bad034232e1de803d687804d5e75413130b7a2dd
keithCuniah <keithcuniah@gmail.com>
2023-06-21 12:18:37 +0200

    :sparkles: update util function to prefilled slots

--- frontend/components/commons/forms/questions-form/fields/ranking/ranking-adapter.js
+++ frontend/components/commons/forms/questions-form/fields/ranking/ranking-adapter.js
@@ -1,18 +1,22 @@
+import { isNil } from "lodash";
+
 export const adaptQuestionsToSlots = ({ options }) => {
   const slots = options.map((_, index) => {
     const id = index + 1;
+    const items = options.filter((option) => option.rank == id);
+
     return {
-      ranking: id,
-      items: [],
+      rank: id,
+      items,
     };
   });
 
-  const questions = options.map((o) => o);
+  const questions = options.filter((o) => isNil(o.rank));
 
   const getRanking = (option) => {
     return slots.find((slot) =>
       slot.items.some((item) => item.value === option.value)
-    )?.ranking;
+    )?.rank;
   };
 
   return {
