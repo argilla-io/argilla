3ce5cb7d44e6fec4f74429b82ab50c150f6483e3
Mark Krasner <markkrasner@Marks-MacBook.local>
2023-06-28 17:31:12 -0400

    Adding @babel/plugin-proposal-private-methods to package.json and changes to build_frontend script to use yarn

--- scripts/build_frontend.sh
+++ scripts/build_frontend.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 cd frontend \
-&& npm install \
-&& npm run-script lint \
-&& npm run-script test \
-&& BASE_URL=@@baseUrl@@ DIST_FOLDER=../src/argilla/server/static npm run-script build \
+&& yarn install \
+&& yarn lint \
+&& yarn test \
+&& BASE_URL=@@baseUrl@@ DIST_FOLDER=../src/argilla/server/static yarn build \
 
