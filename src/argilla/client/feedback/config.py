ceae4c2a708ba98056d781e13008f860ac84be5e
Alvaro Bartolome <alvaro@argilla.io>
2023-07-03 15:55:40 +0200

    refactor: `FeedbackDatasetConfig`->`DatasetConfig`

--- src/argilla/client/feedback/config.py
+++ src/argilla/client/feedback/config.py
@@ -12,63 +12,22 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import List, Optional
+from typing import Annotated, List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from yaml import SafeLoader, dump, load
 
 from argilla.client.feedback.typing import AllowedFieldTypes, AllowedQuestionTypes
 
 
-class FeedbackDatasetConfig(BaseModel):
-    """`FeedbackDatasetConfig`
-
-    Args:
-        fields (List[AllowedFieldTypes]): The fields of the feedback dataset.
-        questions (List[AllowedQuestionTypes]): The questions of the feedback dataset.
-        guidelines (Optional[str]): the guidelines of the feedback dataset. Defaults to None.
-
-    Examples:
-        >>> import argilla as rg
-        >>> config = rg.FeedbackDatasetConfig(
-        ...     fields=[
-        ...         rg.TextField(name="text", title="Human prompt"),
-        ...     ],
-        ...     questions =[
-        ...         rg.TextQuestion(
-        ...             name="question-1",
-        ...             description="This is the first question",
-        ...             required=True,
-        ...         ),
-        ...         rg.RatingQuestion(
-        ...             name="question-2",
-        ...             description="This is the second question",
-        ...             required=True,
-        ...             values=[1, 2, 3, 4, 5],
-        ...         ),
-        ...         rg.LabelQuestion(
-        ...             name="relevant",
-        ...             title="Is the response relevant for the given prompt?",
-        ...             labels=["Yes","No"],
-        ...             required=True,
-        ...             visible_labels=None
-        ...         ),
-        ...         rg.MultiLabelQuestion(
-        ...             name="content_class",
-        ...             title="Does the response include any of the following?",
-        ...             description="Select all that apply",
-        ...             labels={"cat-1": "Category 1" , "cat-2": "Category 2"},
-        ...             required=False,
-        ...             visible_labels=4
-        ...         ),
-        ...     ],
-        ...     guidelines="Add some guidelines for the annotation team here."
-        ... )
-
-    """
-
+class DatasetConfig(BaseModel):
     fields: List[AllowedFieldTypes]
-    questions: List[AllowedQuestionTypes]
+    questions: List[Annotated[AllowedQuestionTypes, Field(..., discriminator="type")]]
     guidelines: Optional[str] = None
 
-    class Config:
-        smart_union = True
+    def to_yaml(self):
+        return dump(self.dict())
+
+    @classmethod
+    def from_yaml(cls, yaml):
+        return cls(**load(yaml, Loader=SafeLoader))
