7745b06845ac1855b674aec4888cc441d5955cb4
leiyre <leire@argilla.io>
2023-07-05 16:20:07 +0200

    feat: feedback task helpbox (#3305)
    <!-- Thanks for your contribution! As part of our Community Growers
    initiative 🌱, we're donating Justdiggit bunds in your name to reforest
    sub-Saharan Africa. To claim your Community Growers certificate, please
    contact David Berenstein in our Slack community or fill in this form
    https://tally.so/r/n9XrxK once your PR has been merged. -->
    
    # Description
    
    Add shortcuts section to the help box
    
    Closes #2438
    
    **Type of change**
    
    (Please delete options that are not relevant. Remember to title the PR
    according to the type of change)
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Refactor (change restructuring the codebase without changing
    functionality)
    - [ ] Improvement (change adding some improvement to an existing
    functionality)
    - [ ] Documentation update
    
    
    **Checklist**
    
    - [ ] I added relevant documentation
    - [x] follows the style guidelines of this project
    - [x] I did a self-review of my code
    - [ ] I made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I filled out [the contributor form](https://tally.so/r/n9XrxK)
    (see text above)
    - [ ] I have added relevant notes to the CHANGELOG.md file (See
    https://keepachangelog.com/)
    
    ---------
    
    Co-authored-by: nataliaElv <natalia@argilla.io>
    Co-authored-by: Damián Pumar <damianpumar@gmail.com>

---
+++ docs/_source/_common/shortcuts.md
@@ -0,0 +1,16 @@
+You can speed up your annotation by using these keyboard shortcuts:
+
+|Action|Keys|
+|------|----|
+|Go to next interactive element|⇥ `Tab`|
+|Go to previous interactive element|⇥ `Tab` + ⇧ `Shift`|
+|Go to next question|⇧ `Shift` + ↓ `Down arrow`|
+|Go to previous question|⇧ `Shift` + ↑ `Up arrow`|
+|Go to next page|&rarr; `Right arrow`|
+|Go to previous page|&larr; `Left arrow`|
+|Trigger action of the focused element|↵ `Enter`|
+|Select/unselect value </br>(*Rating*, *Label* and *MultiLabel* questions)|␣ `Space`|
+|Rank focused value </br>(*Ranking* question)|`1`-`9`, ⇧ `Shift` + `0`-`2`|
+|Clear|&#8679; `Shift` + &blank; `Space`|
+|Discard|&#8679; `Shift` + &#x232B; `Backspace`|
+|Submit|&#8679; `Shift` + &crarr; `Enter`|
---
+++ frontend/components/feedback-task/help-info/DatasetHelpInfoContent.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <base-spinner v-if="$fetchState.pending" />
+    <documentation-viewer
+      class="help-info__content"
+      v-else
+      :content="content"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: {
+        tabs: [],
+      },
+    };
+  },
+  async fetch() {
+    try {
+      const folderContent = require.context(
+        `../../../../docs/_source/_common/`,
+        false,
+        /^[^_]+\.md$/,
+        "lazy"
+      );
+
+      const helpContent = await folderContent("./shortcuts.md");
+
+      this.content.tabs.push({
+        id: "shortcuts",
+        name: "Shortcuts",
+        html: helpContent.html,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.help-info__content {
+  width: auto;
+}
+:deep(.snippet) {
+  max-height: calc(100vh - 400px);
+  overflow: auto;
+  margin-bottom: 2em;
+}
+:deep(table) {
+  width: 100%;
+  border-collapse: collapse;
+  border-radius: $border-radius;
+  border-spacing: 0;
+  box-shadow: 0 0.2rem 0.5rem rgba(0, 0, 0, 0.05),
+    0 0 0.0625rem rgba(0, 0, 0, 0.1);
+  td,
+  th {
+    border-bottom: 1px solid $black-4;
+    border-left: 1px solid $black-4;
+    border-right: 1px solid $black-4;
+    padding: 0 0.25rem;
+    height: $base-space * 5;
+  }
+  th {
+    background: $black-4;
+    border: none;
+  }
+  thead tr:last-child th:last-child {
+    border-top-right-radius: $border-radius;
+  }
+  thead tr:first-child th:first-child {
+    border-top-left-radius: $border-radius;
+  }
+  td:last-child,
+  th:last-child {
+    border-left: none;
+    border-right: none;
+  }
+  td:first-child,
+  th:first-child {
+    border-left: none;
+  }
+  code {
+    padding: calc($base-space / 2);
+    border: 1px solid $black-10;
+    border-radius: $border-radius;
+    background: $black-4;
+  }
+}
+</style>
