d1f29c412d03dd872b13c8a40337e4a2670d57ba
Alvaro Bartolome <alvaro@argilla.io>
2023-07-10 15:22:35 +0200

    feat: add `suggestions` to `HuggingFaceDatasetMixIn`

--- src/argilla/client/feedback/integrations/huggingface/dataset.py
+++ src/argilla/client/feedback/integrations/huggingface/dataset.py
@@ -93,6 +93,19 @@ class HuggingFaceDatasetMixIn:
             if question.name not in hf_dataset:
                 hf_dataset[question.name] = []
 
+            value.id = "suggestion"
+            hf_features[f"{question.name}-suggestion"] = value
+            if f"{question.name}-suggestion" not in hf_dataset:
+                hf_dataset[f"{question.name}-suggestion"] = []
+
+            hf_features[f"{question.name}-suggestion-metadata"] = {
+                "type": Value(dtype="string", id="suggestion-metadata"),
+                "score": Value(dtype="float32", id="suggestion-metadata"),
+                "agent": Value(dtype="string", id="suggestion-metadata"),
+            }
+            if f"{question.name}-suggestion-metadata" not in hf_dataset:
+                hf_dataset[f"{question.name}-suggestion-metadata"] = []
+
         hf_features["external_id"] = Value(dtype="string", id="external_id")
         hf_dataset["external_id"] = []
 
@@ -121,6 +134,27 @@ class HuggingFaceDatasetMixIn:
                         "status": [r.status for r in record.responses],
                     }
                 )
+
+                suggestion = next(filter(lambda s: s.question_name == question.name, record.suggestions), None)
+                if not record.suggestions or not suggestion:
+                    hf_dataset[f"{question.name}-suggestion"].append(None)
+                    hf_dataset[f"{question.name}-suggestion-metadata"].append(
+                        {
+                            "type": None,
+                            "score": None,
+                            "agent": None,
+                        }
+                    )
+                else:
+                    hf_dataset[f"{question.name}-suggestion"].append(suggestion.value)
+                    hf_dataset[f"{question.name}-suggestion-metadata"].append(
+                        {
+                            "type": suggestion.type,
+                            "score": suggestion.score,
+                            "agent": suggestion.agent,
+                        }
+                    )
+
             hf_dataset["metadata"].append(json.dumps(record.metadata) if record.metadata else None)
             hf_dataset["external_id"].append(record.external_id or None)
 
@@ -281,23 +315,29 @@ class HuggingFaceDatasetMixIn:
         records = []
         for index in range(len(hfds)):
             responses = {}
+            suggestions = []
             for question in config.questions:
-                if hfds[index][question.name] is None or len(hfds[index][question.name]) < 1:
-                    continue
-                for user_id, value, status in zip(
-                    hfds[index][question.name]["user_id"],
-                    hfds[index][question.name]["value"],
-                    hfds[index][question.name]["status"],
-                ):
-                    if user_id not in responses:
-                        responses[user_id] = {
-                            "user_id": user_id,
-                            "status": status,
-                            "values": {},
-                        }
-                    if question.settings["type"] == "ranking":
-                        value = [{"rank": r, "value": v} for r, v in zip(value["rank"], value["value"])]
-                    responses[user_id]["values"].update({question.name: {"value": value}})
+                if hfds[index][question.name] is not None or len(hfds[index][question.name]) > 0:
+                    for user_id, value, status in zip(
+                        hfds[index][question.name]["user_id"],
+                        hfds[index][question.name]["value"],
+                        hfds[index][question.name]["status"],
+                    ):
+                        if user_id not in responses:
+                            responses[user_id] = {
+                                "user_id": user_id,
+                                "status": status,
+                                "values": {},
+                            }
+                        if question.settings["type"] == "ranking":
+                            value = [{"rank": r, "value": v} for r, v in zip(value["rank"], value["value"])]
+                        if value is not None:
+                            responses[user_id]["values"].update({question.name: {"value": value}})
+                if f"{question.name}-suggestion" in hfds[index]:
+                    suggestion = {"question_name": question.name, "value": hfds[index][f"{question.name}-suggestion"]}
+                    if hfds[index][f"{question.name}-suggestion-metadata"] is not None:
+                        suggestion.update(hfds[index][f"{question.name}-suggestion-metadata"])
+                    suggestions.append(suggestion)
 
             metadata = None
             if "metadata" in hfds[index] and hfds[index]["metadata"] is not None:
@@ -308,6 +348,7 @@ class HuggingFaceDatasetMixIn:
                     fields={field.name: hfds[index][field.name] for field in config.fields},
                     metadata=metadata,
                     responses=list(responses.values()) or None,
+                    suggestions=[suggestion for suggestion in suggestions if suggestion["value"] is not None] or None,
                     external_id=hfds[index]["external_id"],
                 )
             )
