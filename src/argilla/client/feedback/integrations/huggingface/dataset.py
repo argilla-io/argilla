8abdca2d6e050c786b45223f13ec5f32b5b60f2e
Alvaro Bartolome <alvaro@argilla.io>
2023-07-11 16:43:26 +0200

    fix: use `requires_version` decorator
    Co-authored-by: Gabriel Martin <gabriel@argilla.io>

--- src/argilla/client/feedback/integrations/huggingface/dataset.py
+++ src/argilla/client/feedback/integrations/huggingface/dataset.py
@@ -22,9 +22,9 @@ from packaging.version import parse as parse_version
 
 from argilla.client.feedback.config import DatasetConfig
 from argilla.client.feedback.constants import FIELD_TYPE_TO_PYTHON_TYPE
-from argilla.client.feedback.integrations.config import CONFIG
 from argilla.client.feedback.schemas import FeedbackRecord
 from argilla.client.feedback.typing import AllowedQuestionTypes
+from argilla.utils.dependency import requires_version
 
 if TYPE_CHECKING:
     from datasets import Dataset
@@ -36,6 +36,7 @@ _LOGGER = logging.getLogger(__name__)
 
 class HuggingFaceDatasetMixIn:
     @staticmethod
+    @requires_version("datasets")
     def _huggingface_format(dataset: "FeedbackDataset") -> "Dataset":
         """Formats a `FeedbackDataset` as a `datasets.Dataset` object.
 
@@ -53,12 +54,6 @@ class HuggingFaceDatasetMixIn:
             >>> dataset = rg.FeedbackDataset.from_argilla(name="my-dataset")
             >>> huggingface_dataset = rg.HuggingFaceDatasetMixIn.set_format(dataset)
         """
-        if not CONFIG.datasets_installed:
-            raise ImportError(
-                "The `datasets` library is required to use the `HuggingFaceDatasetMixIn`."
-                " You can install it as `pip install datasets`."
-            )
-
         from datasets import Dataset, Features, Sequence, Value
 
         hf_dataset, hf_features = {}, {}
@@ -139,6 +134,8 @@ class HuggingFaceDatasetMixIn:
             features=Features(hf_features),
         )
 
+    @requires_version("huggingface_hub")
+    @requires_version("datasets")
     def push_to_huggingface(
         self: "FeedbackDataset", repo_id: str, generate_card: Optional[bool] = True, *args, **kwargs
     ) -> None:
@@ -154,18 +151,6 @@ class HuggingFaceDatasetMixIn:
             *args: the args to pass to `datasets.Dataset.push_to_hub`.
             **kwargs: the kwargs to pass to `datasets.Dataset.push_to_hub`.
         """
-        if not CONFIG.datasets_installed:
-            raise ImportError(
-                "The `datasets` library is required to use the `HuggingFaceDatasetMixIn`."
-                " You can install it as `pip install datasets`."
-            )
-        if not CONFIG.huggingface_hub_installed:
-            raise ImportError(
-                "The `huggingface_hub` library is required to use the `HuggingFaceDatasetMixIn`."
-                " You can install it as `pip install huggingface_hub`. Make sure to upgrade it"
-                " to 0.14.0 or higher to avoid potential issues with older versions."
-            )
-
         import huggingface_hub
         from huggingface_hub import DatasetCardData, HfApi
 
@@ -224,6 +209,8 @@ class HuggingFaceDatasetMixIn:
             card.push_to_hub(repo_id, repo_type="dataset", token=kwargs.get("token"))
 
     @classmethod
+    @requires_version("huggingface_hub")
+    @requires_version("datasets")
     def from_huggingface(cls: Type["FeedbackDataset"], repo_id: str, *args: Any, **kwargs: Any) -> "FeedbackDataset":
         """Loads a `FeedbackDataset` from the HuggingFace Hub.
 
@@ -235,18 +222,6 @@ class HuggingFaceDatasetMixIn:
         Returns:
             A `FeedbackDataset` loaded from the HuggingFace Hub.
         """
-        if not CONFIG.datasets_installed:
-            raise ImportError(
-                "The `datasets` library is required to use the `HuggingFaceDatasetMixIn`."
-                " You can install it as `pip install datasets`."
-            )
-        if not CONFIG.huggingface_hub_installed:
-            raise ImportError(
-                "The `huggingface_hub` library is required to use the `HuggingFaceDatasetMixIn`."
-                " You can install it as `pip install huggingface_hub`. Make sure to upgrade it"
-                " to 0.14.0 or higher to avoid potential issues with older versions."
-            )
-
         import huggingface_hub
         from datasets import DatasetDict, load_dataset
         from huggingface_hub import hf_hub_download
