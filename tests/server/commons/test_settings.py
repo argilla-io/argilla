249e618f38da0d9b67b02a8ffd80d725b2b9cfae
frascuchon <francis@argilla.io>
2023-07-13 09:10:34 +0200

    remove support to non-prefixed env vars

--- tests/server/commons/test_settings.py
+++ tests/server/commons/test_settings.py
@@ -33,3 +33,16 @@ def test_settings_namespace():
     assert settings.namespace == "namespace"
     assert settings.dataset_index_name == "namespace.ar.datasets"
     assert settings.dataset_records_index_name == "namespace.ar.dataset.{}"
+
+
+def test_settings_index_replicas_with_shards_defined():
+    os.environ["ARGILLA_ES_RECORDS_INDEX_SHARDS"] = "100"
+    os.environ["ARGILLA_ES_RECORDS_INDEX_REPLICAS"] = "2"
+    settings = Settings()
+    assert settings.es_records_index_replicas == 2
+
+
+def test_settings_default_index_replicas_with_shards_defined():
+    os.environ["ARGILLA_ES_RECORDS_INDEX_SHARDS"] = "100"
+    settings = Settings()
+    assert settings.es_records_index_replicas == 0
