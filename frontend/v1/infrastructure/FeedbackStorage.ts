7c8d608071d9ee02ffc1c4671f6de8a32b31b62c
Damián Pumar <damianpumar@gmail.com>
2023-07-24 16:32:05 +0200

    💄 Remove unused

--- frontend/v1/infrastructure/FeedbackStorage.ts
+++
@@ -1,7 +0,0 @@
-import { Feedback } from "../domain/entities/Feedback";
-import { IDatasetsStorage } from "../domain/services/IDatasetsStorage";
-import { useStoreFor } from "../store/create";
-
-const useStoreForFeedback = useStoreFor<Feedback, IDatasetsStorage>(Feedback);
-
-export const useFeedback = () => useStoreForFeedback();
