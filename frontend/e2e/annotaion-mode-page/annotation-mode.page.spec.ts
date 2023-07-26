4117032272cd659fdffc69913afd068fa3bff9c2
Damián Pumar <damianpumar@gmail.com>
2023-07-26 16:50:55 +0200

    🚑  New test for shortcuts

--- frontend/e2e/annotaion-mode-page/annotation-mode.page.spec.ts
+++ frontend/e2e/annotaion-mode-page/annotation-mode.page.spec.ts
@@ -73,3 +73,445 @@ test.describe("Annotate page", () => {
     await expect(page).toHaveScreenshot();
   });
 });
+
+test.describe("Annotation page shortcuts", () => {
+  test.describe("Single component", () => {
+    test("user press just letter V and go automatically to search bar and filter data", async ({
+      page,
+    }) => {
+      await goToAnnotationPage(page);
+
+      await page.getByPlaceholder("Search labels").first().fill("v");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("after the user filter labels can move to other option and select one pressing Space key", async ({
+      page,
+    }) => {
+      await goToAnnotationPage(page);
+
+      await page.getByPlaceholder("Search labels").first().fill("v");
+
+      await page.getByPlaceholder("Search labels").first().press("Tab");
+      await page.locator("#sentiment_positive").press("Tab");
+      await page.locator("#sentiment_very_positive").press("Tab");
+
+      await page.keyboard.type(" ");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("after the user filter labels can move to other option and select one pressing Space key and go to next question when user click in other option", async ({
+      page,
+    }) => {
+      await goToAnnotationPage(page);
+
+      await page.getByPlaceholder("Search labels").first().fill("v");
+      await page.getByPlaceholder("Search labels").first().press("Tab");
+      await page.locator("#sentiment_positive").press("Tab");
+      await page.locator("#sentiment_very_positive").press("Tab");
+      await page.keyboard.type(" ");
+
+      await page.getByText("Loving").click();
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("expand collapsable label", async ({ page }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Tab");
+      await page.locator("#sentiment_very_positive").press("Tab");
+      await page.locator("#sentiment_negative").press("Tab");
+      await page.locator("#sentiment_neutral").press("Tab");
+      await page.locator("#sentiment_happy").press("Tab");
+      await page.locator("#sentiment_sad").press("Tab");
+      await page.locator("#sentiment_angry").press("Tab");
+      await page.locator("#sentiment_excited").press("Tab");
+      await page.locator("#sentiment_disappointed").press("Tab");
+      await page.locator("#sentiment_surprised").press("Tab");
+      await page.getByLabel("Grateful").press("Tab");
+      await page.getByLabel("Loving").press("Tab");
+      await page.getByLabel("Optimistic").press("Tab");
+      await page.getByLabel("Proud").press("Tab");
+      await page.getByLabel("Worried").press("Tab");
+      await page.getByLabel("Hopeful").press("Tab");
+      await page.getByLabel("Bored").press("Tab");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("go to next question with shift and arrow down", async ({ page }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("go to previous question with shift and arrow up", async ({
+      page,
+    }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowUp");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("the user move as a loop with tab key", async ({ page }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Tab");
+      await page.locator("#sentiment_very_positive").press("Tab");
+      await page.locator("#sentiment_negative").press("Tab");
+      await page.locator("#sentiment_neutral").press("Tab");
+      await page.locator("#sentiment_happy").press("Tab");
+      await page.locator("#sentiment_sad").press("Tab");
+      await page.locator("#sentiment_angry").press("Tab");
+      await page.locator("#sentiment_excited").press("Tab");
+      await page.locator("#sentiment_disappointed").press("Tab");
+      await page.locator("#sentiment_surprised").press("Tab");
+      await page.getByLabel("Grateful").press("Tab");
+      await page.getByLabel("Loving").press("Tab");
+      await page.getByLabel("Optimistic").press("Tab");
+      await page.getByLabel("Proud").press("Tab");
+      await page.getByLabel("Worried").press("Tab");
+      await page.getByLabel("Hopeful").press("Tab");
+      await page.getByLabel("Bored").press("Tab");
+      await page.getByLabel("Amused").press("Tab");
+      await page.getByLabel("Confused").press("Tab");
+      await page.getByLabel("Frustrated").press("Tab");
+      await page.getByLabel("Content").press("Tab");
+      await page.getByLabel("Over the Moon").press("Tab");
+      await page.getByLabel("Heartbroken").press("Tab");
+      await page.getByLabel("Furious").press("Tab");
+      await page.getByLabel("Ecstatic").press("Tab");
+      await page.getByLabel("Devastated").press("Tab");
+      await page.getByLabel("Anxious").press("Tab");
+      await page.getByLabel("Terrified").press("Tab");
+      await page.getByLabel("Delighted").press("Tab");
+      await page.getByLabel("Disgusted").press("Tab");
+      await page.getByLabel("Jealous").press("Tab");
+      await page.getByLabel("Lonely").press("Tab");
+      await page.getByLabel("Shocked").press("Tab");
+      await page.getByLabel("Satisfied").press("Tab");
+      await page.getByLabel("Relaxed").press("Tab");
+      await page.getByLabel("Thankful").press("Tab");
+      await page.getByLabel("Annoyed").press("Tab");
+      await page.getByLabel("Guilty").press("Tab");
+      await page.getByLabel("Embarrassed").press("Tab");
+      await page.getByLabel("Ashamed").press("Tab");
+      await page.getByLabel("Pessimistic").press("Tab");
+      await page.getByLabel("Nostalgic").press("Tab");
+      await page.getByLabel("Sympathetic").press("Tab");
+      await page.getByLabel("Fearful").press("Tab");
+      await page.getByLabel("Hurt").press("Tab");
+      await page.locator("#sentiment_positive").press("Tab");
+      await page.locator("#sentiment_very_positive").press("Tab");
+      await page.locator("#sentiment_negative").press("Tab");
+
+      await expect(page).toHaveScreenshot();
+    });
+  });
+
+  test.describe("Multi component", () => {
+    test("go to multilabel component", async ({ page }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+      await page.locator("#sentiment-multi-label_positive").press("v");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("expand collapsable when the user try to find other label", async ({
+      page,
+    }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+      await page.locator("#sentiment-multi-label_positive").press("Tab");
+      await page.locator("#sentiment-multi-label_very_positive").press("Tab");
+      await page.locator("#sentiment-multi-label_negative").press("Tab");
+      await page.locator("#sentiment-multi-label_neutral").press("Tab");
+      await page.locator("#sentiment-multi-label_happy").press("Tab");
+      await page.locator("#sentiment-multi-label_sad").press("Tab");
+      await page.locator("#sentiment-multi-label_angry").press("Tab");
+      await page.locator("#sentiment-multi-label_excited").press("Tab");
+      await page.locator("#sentiment-multi-label_disappointed").press("Tab");
+      await page.locator("#sentiment-multi-label_surprised").press("Tab");
+      await page.getByLabel("Grateful").press("Tab");
+      await page.getByLabel("Loving").press("Tab");
+      await page.getByLabel("Optimistic").press("Tab");
+
+      await page.getByText("Negative").nth(1).click();
+      await page.getByText("Neutral").nth(1).click();
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("the user move as a loop inside multilabel component", async ({
+      page,
+    }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment-multi-label_positive").press("Tab");
+      await page.locator("#sentiment-multi-label_very_positive").press("Tab");
+      await page.locator("#sentiment-multi-label_negative").press("Tab");
+      await page.locator("#sentiment-multi-label_neutral").press("Tab");
+      await page.locator("#sentiment-multi-label_happy").press("Tab");
+      await page.locator("#sentiment-multi-label_sad").press("Tab");
+      await page.locator("#sentiment-multi-label_angry").press("Tab");
+      await page.locator("#sentiment-multi-label_excited").press("Tab");
+      await page.locator("#sentiment-multi-label_disappointed").press("Tab");
+      await page.locator("#sentiment-multi-label_surprised").press("Tab");
+      await page.locator("#sentiment-multi-label_grateful").press("Tab");
+      await page.locator("#sentiment-multi-label_loving").press("Tab");
+      await page.locator("#sentiment-multi-label_optimistic").press("Tab");
+      await page.locator("#sentiment-multi-label_proud").press("Tab");
+      await page.locator("#sentiment-multi-label_worried").press("Tab");
+      await page.locator("#sentiment-multi-label_hopeful").press("Tab");
+      await page.locator("#sentiment-multi-label_bored").press("Tab");
+      await page.locator("#sentiment-multi-label_amused").press("Tab");
+      await page.locator("#sentiment-multi-label_confused").press("Tab");
+      await page.locator("#sentiment-multi-label_frustrated").press("Tab");
+      await page.locator("#sentiment-multi-label_content").press("Tab");
+      await page.locator("#sentiment-multi-label_over_the_moon").press("Tab");
+      await page.locator("#sentiment-multi-label_heartbroken").press("Tab");
+      await page.locator("#sentiment-multi-label_furious").press("Tab");
+      await page.locator("#sentiment-multi-label_ecstatic").press("Tab");
+      await page.locator("#sentiment-multi-label_devastated").press("Tab");
+      await page.locator("#sentiment-multi-label_anxious").press("Tab");
+      await page.locator("#sentiment-multi-label_terrified").press("Tab");
+      await page.locator("#sentiment-multi-label_delighted").press("Tab");
+      await page.locator("#sentiment-multi-label_disgusted").press("Tab");
+      await page.locator("#sentiment-multi-label_jealous").press("Tab");
+      await page.locator("#sentiment-multi-label_lonely").press("Tab");
+      await page.locator("#sentiment-multi-label_shocked").press("Tab");
+      await page.locator("#sentiment-multi-label_satisfied").press("Tab");
+      await page.locator("#sentiment-multi-label_relaxed").press("Tab");
+      await page.locator("#sentiment-multi-label_thankful").press("Tab");
+      await page.locator("#sentiment-multi-label_annoyed").press("Tab");
+      await page.locator("#sentiment-multi-label_guilty").press("Tab");
+      await page.locator("#sentiment-multi-label_embarrassed").press("Tab");
+      await page.locator("#sentiment-multi-label_ashamed").press("Tab");
+      await page.locator("#sentiment-multi-label_pessimistic").press("Tab");
+      await page.locator("#sentiment-multi-label_nostalgic").press("Tab");
+      await page.locator("#sentiment-multi-label_sympathetic").press("Tab");
+      await page.locator("#sentiment-multi-label_fearful").press("Tab");
+      await page.locator("#sentiment-multi-label_hurt").press("Tab");
+      await page.locator("#sentiment-multi-label_positive").press("Tab");
+      await page.locator("#sentiment-multi-label_very_positive").press("Tab");
+      await page.locator("#sentiment-multi-label_negative").press("Tab");
+      await page.locator("#sentiment-multi-label_neutral").press("Tab");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("go to next question with shift and arrow down", async ({ page }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+
+      await page
+        .locator("#sentiment-multi-label_positive")
+        .press("Shift+ArrowDown");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("go to previous question with shift and arrow up", async ({
+      page,
+    }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+      await page
+        .locator("#sentiment-multi-label_positive")
+        .press("Shift+ArrowDown");
+
+      await page.getByLabel("1").press("Shift+ArrowUp");
+
+      await expect(page).toHaveScreenshot();
+    });
+  });
+
+  test.describe("Rating component", () => {
+    test("go to rating component with keyboard", async ({ page }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+      await page
+        .locator("#sentiment-multi-label_positive")
+        .press("Shift+ArrowDown");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("when the user click in the option go automatically to next question", async ({
+      page,
+    }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+      await page
+        .locator("#sentiment-multi-label_positive")
+        .press("Shift+ArrowDown");
+
+      await page.locator("label").filter({ hasText: "2" }).click();
+
+      await page
+        .getByText("This is a review of the review")
+        .press("Meta+Shift+ArrowRight");
+      await page.getByText("This is a review of the review").fill("TEST");
+
+      await expect(page).toHaveScreenshot();
+    });
+  });
+
+  test.describe("Text component", () => {
+    test("go to text component with keyboard", async ({ page }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+      await page
+        .locator("#sentiment-multi-label_positive")
+        .press("Shift+ArrowDown");
+      await page.getByLabel("1").press("Shift+ArrowDown");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("go to previous question with keyboard", async ({ page }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+      await page
+        .locator("#sentiment-multi-label_positive")
+        .press("Shift+ArrowDown");
+      await page.getByLabel("1").press("Shift+ArrowDown");
+
+      await page
+        .getByText("This is a review of the review")
+        .press("Shift+ArrowUp");
+
+      await expect(page).toHaveScreenshot();
+    });
+  });
+
+  test.describe("Ranking component", () => {
+    test("go to ranking component with keyboard", async ({ page }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+      await page
+        .locator("#sentiment-multi-label_positive")
+        .press("Shift+ArrowDown");
+      await page.getByLabel("1").press("Shift+ArrowDown");
+      await page
+        .getByText("This is a review of the review")
+        .press("Shift+ArrowDown");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("go to previous question if the user is in ranking component with keyboard", async ({
+      page,
+    }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+      await page
+        .locator("#sentiment-multi-label_positive")
+        .press("Shift+ArrowDown");
+      await page.getByLabel("1").press("Shift+ArrowDown");
+      await page
+        .getByText("This is a review of the review")
+        .press("Shift+ArrowDown");
+
+      await page.getByTitle("Option A").press("Shift+ArrowUp");
+
+      await page
+        .getByText("This is a review of the review")
+        .press("Meta+Shift+ArrowRight");
+      await page.getByText("This is a review of the review").fill("TEST");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("order ranking question options", async ({ page }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+      await page
+        .locator("#sentiment-multi-label_positive")
+        .press("Shift+ArrowDown");
+      await page.getByLabel("1").press("Shift+ArrowDown");
+      await page
+        .getByText("This is a review of the review")
+        .press("Shift+ArrowDown");
+
+      await page.getByTitle("Option A").press("2");
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("reset order ranking question options", async ({ page }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+      await page
+        .locator("#sentiment-multi-label_positive")
+        .press("Shift+ArrowDown");
+      await page.getByLabel("1").press("Shift+ArrowDown");
+      await page
+        .getByText("This is a review of the review")
+        .press("Shift+ArrowDown");
+
+      await page
+        .getByTitle("Option A")
+        .dragTo(page.locator(".draggable__questions-container"));
+
+      await page
+        .getByTitle("Option B")
+        .dragTo(page.locator(".draggable__questions-container"));
+
+      await page
+        .getByTitle("Option C")
+        .dragTo(page.locator(".draggable__questions-container"));
+
+      await page
+        .getByTitle("Option D")
+        .dragTo(page.locator(".draggable__questions-container"));
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test("reorder ranking question options", async ({ page }) => {
+      await goToAnnotationPage(page);
+
+      await page.locator("#sentiment_positive").press("Shift+ArrowDown");
+      await page
+        .locator("#sentiment-multi-label_positive")
+        .press("Shift+ArrowDown");
+      await page.getByLabel("1").press("Shift+ArrowDown");
+      await page
+        .getByText("This is a review of the review")
+        .press("Shift+ArrowDown");
+
+      await page.getByTitle("Option A").press("2");
+
+      await page.getByTitle("Option B").press("1");
+
+      await page.getByTitle("Option C").press("4");
+
+      await page.getByTitle("Option D").press("3");
+
+      await expect(page).toHaveScreenshot();
+    });
+  });
+});
