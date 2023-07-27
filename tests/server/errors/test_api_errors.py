argilla-io/argilla

commit dca69cce39d94fbdea6f8224787971b8920d70c0

chore: added telemetry to ArgillaTrainer (#3336)
<!-- Thanks for your contribution! As part of our Community Growers
initiative 🌱, we're donating Justdiggit bunds in your name to reforest
sub-Saharan Africa. To claim your Community Growers certificate, please
contact David Berenstein in our Slack community or fill in this form
https://tally.so/r/n9XrxK once your PR has been merged. -->

# Description

Please include a summary of the changes and the related issue. Please
also include relevant motivation and context. List any dependencies that
are required for this change.

Closes #3325 

**Type of change**

- [X] New feature (non-breaking change which adds functionality)

**How Has This Been Tested**

N.A.

**Checklist**

N.A.

src/argilla/server/errors/api_errors.py
@@ -19,9 +19,9 @@ from fastapi import HTTPException, Request
 from fastapi.exception_handlers import http_exception_handler
 from pydantic import BaseModel
 
-from argilla.server.commons import telemetry
 from argilla.server.errors.adapter import exception_to_argilla_error
 from argilla.server.errors.base_errors import ServerError
+from argilla.utils import telemetry
 
 _LOGGER = logging.getLogger("argilla")
 

tests/server/commons/test_telemetry.py
@@ -16,9 +16,7 @@ import uuid
 from unittest.mock import MagicMock
 
 import pytest
-from argilla.server.commons import telemetry
 from argilla.server.commons.models import TaskType
-from argilla.server.commons.telemetry import TelemetryClient, get_telemetry_client
 from argilla.server.errors import (
     EntityAlreadyExistsError,
     EntityNotFoundError,
@@ -26,6 +24,8 @@ from argilla.server.errors import (
     ServerError,
 )
 from argilla.server.schemas.datasets import Dataset
+from argilla.utils import telemetry
+from argilla.utils.telemetry import TelemetryClient, get_telemetry_client
 from fastapi import Request
 
 mock_request = Request(scope={"type": "http", "headers": {}})

src/argilla/feedback/__init__.py
@@ -17,6 +17,7 @@ from argilla.client.feedback import (
     LabelQuestionStrategy,
     MultiLabelQuestionStrategy,
     RatingQuestionStrategy,
+    TrainingTaskMapping,
 )
 from argilla.client.feedback.dataset import FeedbackDataset
 from argilla.client.feedback.schemas import (
@@ -36,6 +37,7 @@ __all__ = [
     "LabelQuestionStrategy",
     "MultiLabelQuestionStrategy",
     "RatingQuestionStrategy",
+    "TrainingTaskMapping",
     "FeedbackDataset",
     "FeedbackRecord",
     "LabelQuestion",

src/argilla/client/feedback/training/base.py
@@ -181,6 +181,7 @@ class ArgillaTrainer(ArgillaTrainerV1):
             raise NotImplementedError(f"{framework} is not a valid framework.")
 
         self._logger.info(self)
+        self._track_trainer_usage(framework=framework, task=self._task_mapping.__class__.__name__)
 
     def __repr__(self) -> str:
         """

src/argilla/server/apis/v0/handlers/users.py
@@ -21,13 +21,13 @@ from pydantic import parse_obj_as
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from argilla.server import models
-from argilla.server.commons import telemetry
 from argilla.server.contexts import accounts
 from argilla.server.database import get_async_db
 from argilla.server.errors import EntityAlreadyExistsError, EntityNotFoundError
 from argilla.server.policies import UserPolicy, authorize
 from argilla.server.security import auth
 from argilla.server.security.model import User, UserCreate
+from argilla.utils import telemetry
 
 router = APIRouter(tags=["users"])
 

src/argilla/server/apis/v1/handlers/datasets.py
@@ -18,7 +18,6 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, Query, Security, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from argilla.server.commons.telemetry import TelemetryClient, get_telemetry_client
 from argilla.server.contexts import accounts, datasets
 from argilla.server.database import get_async_db
 from argilla.server.enums import ResponseStatusFilter
@@ -50,6 +49,7 @@ from argilla.server.search_engine import (
     get_search_engine,
 )
 from argilla.server.security import auth
+from argilla.utils.telemetry import TelemetryClient, get_telemetry_client
 
 LIST_DATASET_RECORDS_LIMIT_DEFAULT = 50
 LIST_DATASET_RECORDS_LIMIT_LTE = 1000

src/argilla/server/services/storage/service.py
@@ -17,7 +17,6 @@ from typing import List, Optional, Type
 
 from fastapi import Depends
 
-from argilla.server.commons import telemetry
 from argilla.server.commons.config import TasksFactory
 from argilla.server.commons.models import TaskStatus
 from argilla.server.daos.backend.base import WrongLogDataError
@@ -28,6 +27,7 @@ from argilla.server.policies import DatasetPolicy, is_authorized
 from argilla.server.services.datasets import ServiceDataset
 from argilla.server.services.search.model import ServiceBaseRecordsQuery
 from argilla.server.services.tasks.commons import ServiceRecord
+from argilla.utils import telemetry
 
 
 @dataclasses.dataclass

commit d37ea7ee1e44d51d526f9153876f01cb0a2fa486

fix: import errors when importing from `argilla.feedback` (#3471)
# Description

This PRs fixes the `ModuleNotFoundError` and `ImportError` that occurred
when trying to import something from `argilla.feedback` module.

The first error was caused because in #3336 the telemetry was included
in the `ArgillaTrainer`, but in the `argilla.utils.telemetry` module
some optional dependencies used by the server were being imported.

The second one was caused because the module in which
`HuggingFaceDatasetMixin` (and from which `FeedbackDataset` is
inheriting) class lives was importing classes from the
`argilla.client.feedback.config` module, which was importing `pyyaml` in
its root causing the `ImportError`.

Closes #3468 

**Type of change**

- [x] Bug fix (non-breaking change which fixes an issue)

**How Has This Been Tested**

I've created a wheel of this branch, installed in a new virtual
environment and I was able to import something `argilla.feedback` module
without errors.

**Checklist**

- [ ] I added relevant documentation
- [x] follows the style guidelines of this project
- [x] I did a self-review of my code
- [ ] I made corresponding changes to the documentation
- [x] My changes generate no new warnings
- [x] I have added tests that prove my fix is effective or that my
feature works
- [ ] I filled out [the contributor form](https://tally.so/r/n9XrxK)
(see text above)
- [x] I have added relevant notes to the CHANGELOG.md file (See
https://keepachangelog.com/)

---------

Co-authored-by: Francisco Aranda <francis@argilla.io>

src/argilla/client/feedback/integrations/huggingface/dataset.py
@@ -20,7 +20,6 @@ from typing import TYPE_CHECKING, Any, Optional, Type
 
 from packaging.version import parse as parse_version
 
-from argilla.client.feedback.config import DatasetConfig, DeprecatedDatasetConfig
 from argilla.client.feedback.constants import FIELD_TYPE_TO_PYTHON_TYPE
 from argilla.client.feedback.schemas import FeedbackRecord
 from argilla.client.feedback.types import AllowedQuestionTypes
@@ -188,6 +187,9 @@ class HuggingFaceDatasetMixin:
         import huggingface_hub
         from huggingface_hub import DatasetCardData, HfApi
 
+        # https://github.com/argilla-io/argilla/issues/3468
+        from argilla.client.feedback.config import DatasetConfig
+
         if parse_version(huggingface_hub.__version__) < parse_version("0.14.0"):
             _LOGGER.warning(
                 "Recommended `huggingface_hub` version is 0.14.0 or higher, and you have"
@@ -260,6 +262,12 @@ class HuggingFaceDatasetMixin:
         from datasets import DatasetDict, load_dataset
         from huggingface_hub import hf_hub_download
         from huggingface_hub.utils import EntryNotFoundError
+
+        # https://github.com/argilla-io/argilla/issues/3468
+        from argilla.client.feedback.config import (
+            DatasetConfig,
+            DeprecatedDatasetConfig,
+        )
 
         if parse_version(huggingface_hub.__version__) < parse_version("0.14.0"):
             _LOGGER.warning(

src/argilla/server/errors/api_errors.py
@@ -12,7 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import logging
 from typing import Any, Dict
 
 from fastapi import HTTPException, Request
@@ -20,10 +19,13 @@ from fastapi.exception_handlers import http_exception_handler
 from pydantic import BaseModel
 
 from argilla.server.errors.adapter import exception_to_argilla_error
-from argilla.server.errors.base_errors import ServerError
+from argilla.server.errors.base_errors import (
+    EntityAlreadyExistsError,
+    EntityNotFoundError,
+    GenericServerError,
+    ServerError,
+)
 from argilla.utils import telemetry
-
-_LOGGER = logging.getLogger("argilla")
 
 
 class ErrorDetail(BaseModel):
@@ -42,9 +44,21 @@ class ServerHTTPException(HTTPException):
 
 class APIErrorHandler:
     @staticmethod
+    async def track_error(error: ServerError, request: Request):
+        data = {
+            "code": error.code,
+            "user-agent": request.headers.get("user-agent"),
+            "accept-language": request.headers.get("accept-language"),
+        }
+        if isinstance(error, (GenericServerError, EntityNotFoundError, EntityAlreadyExistsError)):
+            data["type"] = error.type
+
+        telemetry.get_telemetry_client().track_data(action="ServerErrorFound", data=data)
+
+    @staticmethod
     async def common_exception_handler(request: Request, error: Exception):
         """Wraps errors as custom generic error"""
         argilla_error = exception_to_argilla_error(error)
-        await telemetry.track_error(argilla_error, request=request)
+        await APIErrorHandler.track_error(argilla_error, request=request)
 
         return await http_exception_handler(request, ServerHTTPException(argilla_error))

src/argilla/utils/telemetry.py
@@ -16,18 +16,13 @@ import dataclasses
 import logging
 import platform
 import uuid
-from typing import Any, Dict, Optional
-
-from fastapi import Request
+from typing import TYPE_CHECKING, Any, Dict, Optional
 
 from argilla.server.commons.models import TaskType
-from argilla.server.errors.base_errors import (
-    EntityAlreadyExistsError,
-    EntityNotFoundError,
-    GenericServerError,
-    ServerError,
-)
 from argilla.server.settings import settings
+
+if TYPE_CHECKING:
+    from fastapi import Request
 
 try:
     from analytics import Client  # This import works only for version 2.2.0
@@ -89,25 +84,15 @@ class TelemetryClient:
 _CLIENT = TelemetryClient()
 
 
-def _process_request_info(request: Request):
+def _process_request_info(request: "Request"):
     return {header: request.headers.get(header) for header in ["user-agent", "accept-language"]}
-
-
-async def track_error(error: ServerError, request: Request):
-    data = {"code": error.code}
-    if isinstance(error, (GenericServerError, EntityNotFoundError, EntityAlreadyExistsError)):
-        data["type"] = error.type
-
-    data.update(_process_request_info(request))
-
-    _CLIENT.track_data(action="ServerErrorFound", data=data)
 
 
 async def track_bulk(task: TaskType, records: int):
     _CLIENT.track_data(action="LogRecordsRequested", data={"task": task, "records": records})
 
 
-async def track_login(request: Request, username: str):
+async def track_login(request: "Request", username: str):
     _CLIENT.track_data(
         action="UserInfoRequested",
         data={

tests/server/commons/test_telemetry.py
@@ -17,13 +17,6 @@ from unittest.mock import MagicMock
 
 import pytest
 from argilla.server.commons.models import TaskType
-from argilla.server.errors import (
-    EntityAlreadyExistsError,
-    EntityNotFoundError,
-    GenericServerError,
-    ServerError,
-)
-from argilla.server.schemas.datasets import Dataset
 from argilla.utils import telemetry
 from argilla.utils.telemetry import TelemetryClient, get_telemetry_client
 from fastapi import Request
@@ -57,50 +50,3 @@ async def test_track_bulk(test_telemetry):
 
     await telemetry.track_bulk(task=task, records=records)
     test_telemetry.assert_called_once_with("LogRecordsRequested", {"task": task, "records": records})
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-    ["error", "expected_event"],
-    [
-        (
-            EntityNotFoundError(name="mock-name", type="MockType"),
-            {
-                "accept-language": None,
-                "code": "argilla.api.errors::EntityNotFoundError",
-                "type": "MockType",
-                "user-agent": None,
-            },
-        ),
-        (
-            EntityAlreadyExistsError(name="mock-name", type=Dataset, workspace="mock-workspace"),
-            {
-                "accept-language": None,
-                "code": "argilla.api.errors::EntityAlreadyExistsError",
-                "type": "Dataset",
-                "user-agent": None,
-            },
-        ),
-        (
-            GenericServerError(RuntimeError("This is a mock error")),
-            {
-                "accept-language": None,
-                "code": "argilla.api.errors::GenericServerError",
-                "type": "builtins.RuntimeError",
-                "user-agent": None,
-            },
-        ),
-        (
-            ServerError(),
-            {
-                "accept-language": None,
-                "code": "argilla.api.errors::ServerError",
-                "user-agent": None,
-            },
-        ),
-    ],
-)
-async def test_track_error(test_telemetry, error, expected_event):
-    await telemetry.track_error(error, request=mock_request)
-
-    test_telemetry.assert_called_once_with("ServerErrorFound", expected_event)

tests/server/errors/__init__.py
@@ -1,0 +1,13 @@
+#  Copyright 2021-present, the Recognai S.L. team.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.

tests/server/errors/test_api_errors.py
@@ -1,0 +1,75 @@
+#  Copyright 2021-present, the Recognai S.L. team.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import pytest
+from argilla.server.errors.api_errors import APIErrorHandler
+from argilla.server.errors.base_errors import (
+    EntityAlreadyExistsError,
+    EntityNotFoundError,
+    GenericServerError,
+    ServerError,
+)
+from argilla.server.schemas.datasets import Dataset
+from fastapi import Request
+
+mock_request = Request(scope={"type": "http", "headers": {}})
+
+
+@pytest.mark.asyncio
+class TestAPIErrorHandler:
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        ["error", "expected_event"],
+        [
+            (
+                EntityNotFoundError(name="mock-name", type="MockType"),
+                {
+                    "accept-language": None,
+                    "code": "argilla.api.errors::EntityNotFoundError",
+                    "type": "MockType",
+                    "user-agent": None,
+                },
+            ),
+            (
+                EntityAlreadyExistsError(name="mock-name", type=Dataset, workspace="mock-workspace"),
+                {
+                    "accept-language": None,
+                    "code": "argilla.api.errors::EntityAlreadyExistsError",
+                    "type": "Dataset",
+                    "user-agent": None,
+                },
+            ),
+            (
+                GenericServerError(RuntimeError("This is a mock error")),
+                {
+                    "accept-language": None,
+                    "code": "argilla.api.errors::GenericServerError",
+                    "type": "builtins.RuntimeError",
+                    "user-agent": None,
+                },
+            ),
+            (
+                ServerError(),
+                {
+                    "accept-language": None,
+                    "code": "argilla.api.errors::ServerError",
+                    "user-agent": None,
+                },
+            ),
+        ],
+    )
+    async def test_track_error(self, test_telemetry, error, expected_event):
+        await APIErrorHandler.track_error(error, request=mock_request)
+
+        test_telemetry.assert_called_once_with("ServerErrorFound", expected_event)