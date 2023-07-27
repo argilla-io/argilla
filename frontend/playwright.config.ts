a3b3d34cc3268ed3daaa3a0c0a6dba78ca5198cd
Damián Pumar <damianpumar@gmail.com>
2023-07-27 12:55:07 +0200

    🚀  Improve screenshots folder

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -11,6 +11,8 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./e2e",
+  snapshotPathTemplate:
+    "{testDir}/{testFileDir}/__screenshots__/{projectName}/{arg}{ext}",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
