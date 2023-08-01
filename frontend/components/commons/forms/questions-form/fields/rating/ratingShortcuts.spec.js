7588450476eed28c033e6a70859f3e919e4be1aa
keithCuniah <keithcuniah@gmail.com>
2023-08-01 12:41:04 +0200

    :white_check_mark:

--- frontend/components/commons/forms/questions-form/fields/rating/ratingShortcuts.spec.js
+++ frontend/components/commons/forms/questions-form/fields/rating/ratingShortcuts.spec.js
@@ -37,6 +37,11 @@ const options = {
   },
 };
 
+const spyOnIsValidKeyFor = jest.spyOn(
+  RatingShortcutsComponent.methods,
+  "isValidKeyFor"
+);
+
 beforeEach(() => {
   wrapper = shallowMount(RatingShortcutsComponent, options);
 });
@@ -48,6 +53,22 @@ afterEach(() => {
 describe("RatingMonoSelectionComponent", () => {
   it("render the component", () => {
     expect(wrapper.is(RatingShortcutsComponent)).toBe(true);
+  });
+  it("validate shortcut when pressing numpad number", () => {
+    for (let i = 1; i <= 10; i++) {
+      const code = `Numpad${i}`;
+      expect(spyOnIsValidKeyFor({ code })).toBe(true);
+    }
+  });
+  it("validate shortcut when pressing digit number", () => {
+    for (let i = 1; i <= 10; i++) {
+      const code = `Digit${i}`;
+      expect(spyOnIsValidKeyFor({ code })).toBe(true);
+    }
+  });
+  it("not validate shortcut when not pressing a number", () => {
+    const code = `KeyQ`;
+    expect(spyOnIsValidKeyFor({ code })).toBe(false);
   });
   it.skip("click on target corresponding to value from shortcut", () => {
     wrapper.trigger("keydown", { key: "1" });
