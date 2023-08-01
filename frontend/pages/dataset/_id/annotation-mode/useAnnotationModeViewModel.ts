4eafa24630c0634cb0b6a7552eb01bf0fb2eb8a0
Damián Pumar <damianpumar@gmail.com>
2023-08-01 17:14:56 +0200

    🚀 Improve breadcrumbs

--- frontend/pages/dataset/_id/annotation-mode/useAnnotationModeViewModel.ts
+++ frontend/pages/dataset/_id/annotation-mode/useAnnotationModeViewModel.ts
@@ -1,20 +1,53 @@
-import { onBeforeMount } from "vue-demi";
-import { useDatasetViewModel } from "../useDatasetViewModel";
 import {
-  UpdateMetricsEventHandler,
+  computed,
+  onBeforeMount,
+  ref,
+  useRoute,
+  useRouter,
+} from "@nuxtjs/composition-api";
+import { useResolve } from "ts-injecty";
+import { createBreadcrumbs } from "../breadcrumbs";
+import { useDatasetViewModel } from "../useDatasetViewModel";
+import { Notification } from "@/models/Notifications";
+import { GetDatasetByIdUseCase } from "@/v1/domain/usecases/get-dataset-by-id-use-case";
+import { DATASET_API_ERRORS } from "@/v1/infrastructure/repositories/DatasetRepository";
+import { useDataset } from "@/v1/infrastructure/storage/DatasetStorage";
+import {
   useEvents,
-} from "@/v1/infrastructure/events";
+  UpdateMetricsEventHandler,
+} from "~/v1/infrastructure/events";
 
 export const useAnnotationModeViewModel = () => {
-  const datasetViewModel = useDatasetViewModel();
+  const router = useRouter();
+  const { state: dataset } = useDataset();
+  const getDatasetUseCase = useResolve(GetDatasetByIdUseCase);
+
+  const { datasetId, isLoadingDataset, handleError, createRootBreadCrumbs } =
+    useDatasetViewModel();
+
+  const breadcrumbs = computed(() => createRootBreadCrumbs(dataset));
+
+  const loadDataset = async () => {
+    try {
+      isLoadingDataset.value = true;
+
+      await getDatasetUseCase.execute(datasetId);
+    } catch (error) {
+      handleError(error.response);
+
+      router.push("/");
+    } finally {
+      isLoadingDataset.value = false;
+    }
+  };
 
   onBeforeMount(() => {
     useEvents(() => {
       new UpdateMetricsEventHandler();
     });
 
-    datasetViewModel.loadDataset();
+    loadDataset();
   });
 
-  return { ...datasetViewModel };
+  return { dataset, datasetId, isLoadingDataset, loadDataset, breadcrumbs };
 };
--- frontend/pages/dataset/_id/useDatasetSettingViewModel.ts
+++ frontend/pages/dataset/_id/useDatasetSettingViewModel.ts
@@ -1,67 +1,17 @@
-import { computed, onBeforeMount, ref } from "vue-demi";
+import { computed, onBeforeMount } from "vue-demi";
 import { useResolve } from "ts-injecty";
-import { useRouter, useRoute } from "@nuxtjs/composition-api";
+import { useRouter } from "@nuxtjs/composition-api";
+import { useDatasetViewModel } from "./useDatasetViewModel";
 import { GetDatasetSettingsUseCase } from "~/v1/domain/usecases/get-dataset-settings-use-case";
 import { useDatasetSetting } from "~/v1/infrastructure/storage/DatasetSettingStorage";
-import { Dataset } from "~/v1/domain/entities/Dataset";
-import { DATASET_API_ERRORS } from "~/v1/infrastructure/repositories";
-import { Notification } from "@/models/Notifications";
 
 export const useDatasetSettingViewModel = () => {
-  const isLoadingDataset = ref(false);
   const router = useRouter();
-  const route = useRoute();
-  const datasetId = route.value.params.id;
-
   const { state: datasetSetting } = useDatasetSetting();
   const getDatasetSetting = useResolve(GetDatasetSettingsUseCase);
 
-  const breadcrumbs = computed(() =>
-    createBreadcrumbs(datasetSetting?.dataset)
-  );
-
-  const handleError = (response: string) => {
-    let message = "";
-    switch (response) {
-      case DATASET_API_ERRORS.ERROR_FETCHING_DATASET_INFO:
-        message = `Can't get dataset info for dataset_id: ${datasetId}`;
-        break;
-      case DATASET_API_ERRORS.ERROR_FETCHING_WORKSPACE_INFO:
-        message = `Can't get workspace info for dataset_id: ${datasetId}`;
-        break;
-      default:
-        message =
-          "There was an error on fetching dataset info and workspace info. Please try again";
-    }
-
-    const paramsForNotification = {
-      message,
-      numberOfChars: message.length,
-      type: "error",
-    };
-
-    Notification.dispatch("notify", paramsForNotification);
-  };
-
-  const createBreadcrumbs = (dataset: Dataset) => {
-    return [
-      { link: { name: "datasets" }, name: "Home" },
-      {
-        link: { path: `/datasets?workspace=${dataset.workspace}` },
-        name: dataset.workspace,
-      },
-      {
-        link: {
-          name: null,
-        },
-        name: dataset.name,
-      },
-      {
-        link: null,
-        name: "settings",
-      },
-    ];
-  };
+  const { datasetId, isLoadingDataset, handleError, createRootBreadCrumbs } =
+    useDatasetViewModel();
 
   const loadDatasetSetting = async () => {
     try {
@@ -76,6 +26,16 @@ export const useDatasetSettingViewModel = () => {
       isLoadingDataset.value = false;
     }
   };
+
+  const breadcrumbs = computed(() => {
+    return [
+      ...createRootBreadCrumbs(datasetSetting.dataset),
+      {
+        link: {},
+        name: "settings",
+      },
+    ];
+  });
 
   onBeforeMount(() => {
     loadDatasetSetting();
