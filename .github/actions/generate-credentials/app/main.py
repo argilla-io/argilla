52e33cd634ebe46985c6df08a17bbf88b88d9814
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2023-08-07 09:09:47 +0200

    feat: environment per PR (#3507)
    # Description
    
    This PR updates the current CI workflow to add an extra job that will
    automatically deploy the built quickstart image to Google Cloud Run so
    the changes made can be tested in an isolated environment.
    
    When the PR is closed or merged, the deployment in Google Cloud Run will
    be automatically removed.
    
    **Type of change**
    
    - [x] New feature (non-breaking change which adds functionality)
    
    **How Has This Been Tested**
    
    I've opened #3513 PR to test the deploy and teardown workflows:
    
    - Build package workflow:
    https://github.com/argilla-io/argilla/actions/runs/5776403769
      - [x] Branch deployed to Cloud Run
    - Close PR workflow:
    https://github.com/argilla-io/argilla/actions/runs/5776562105
      - [x] Environment for the PR was removed
    
    **Checklist**
    
    - [ ] I added relevant documentation
    - [x] follows the style guidelines of this project
    - [x] I did a self-review of my code
    - [ ] I made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I filled out [the contributor form](https://tally.so/r/n9XrxK)
    (see text above)
    - [ ] I have added relevant notes to the CHANGELOG.md file (See
    https://keepachangelog.com/)
    
    ---------
    
    Co-authored-by: Alvaro Bartolome <alvaro@argilla.io>

---
+++ .github/actions/generate-credentials/app/main.py
@@ -0,0 +1,53 @@
+#  Copyright 2021-present, the Recognai S.L. team.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import base64
+import hashlib
+import logging
+import os
+from typing import Any, Dict
+
+SECRET = os.environ.get("ENVIRONMENT_CREDENTIALS_SECRET")
+GITHUB_REF = os.environ.get("GITHUB_REF")
+
+
+def generate_password_from_secret(secret: str, salt: str, length: int = 16) -> str:
+    dk = hashlib.pbkdf2_hmac("sha256", secret.encode(), salt.encode(), 100000)
+    password = base64.urlsafe_b64encode(dk).decode("utf-8")
+    password = "".join(char for char in password if char.isalnum())
+    return password[:length]
+
+
+def generate_credentials() -> Dict[str, Any]:
+    credentials = {}
+    for user in ["owner", "admin", "annotator"]:
+        logging.info(f"Generating random credential for user '{user}'")
+        password = generate_password_from_secret(secret=SECRET, salt=f"{GITHUB_REF}/{user}", length=32)
+        credentials[user] = password
+    return credentials
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    if not SECRET:
+        logging.error("`ENVIRONMENT_CREDENTIALS_SECRET` secret is not set!")
+        raise KeyError("`ENVIRONMENT_CREDENTIALS_SECRET` secret is not set")
+
+    credentials = generate_credentials()
+
+    with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
+        for key, value in credentials.items():
+            print(f"::add-mask::{value}")
+            print(f"{key}={value}", file=fh)
