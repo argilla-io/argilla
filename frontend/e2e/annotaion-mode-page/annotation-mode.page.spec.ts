e22523b20cdf100b1abe53bccea9fc6b54720c79
Damián Pumar <damianpumar@gmail.com>
2023-08-08 09:56:35 +0200

    🔥 New e2e

--- frontend/e2e/annotaion-mode-page/annotation-mode.page.spec.ts
+++ frontend/e2e/annotaion-mode-page/annotation-mode.page.spec.ts
@@ -16,13 +16,13 @@ const goToAnnotationPage = async (page, shortAndLongQuestions = false) => {
   await mockAllDatasets(page);
   const record = shortAndLongQuestions
     ? await mockRecordForLongAndShortQuestion(page, {
-      datasetId: dataset.id,
-      workspaceId: dataset.workspace_id,
-    })
+        datasetId: dataset.id,
+        workspaceId: dataset.workspace_id,
+      })
     : await mockRecord(page, {
-      datasetId: dataset.id,
-      workspaceId: dataset.workspace_id,
-    });
+        datasetId: dataset.id,
+        workspaceId: dataset.workspace_id,
+      });
 
   await loginUserAndWaitFor(page, "datasets");
 
@@ -171,9 +171,11 @@ test.describe("Annotate page", () => {
     await expect(page).toHaveScreenshot();
   });
 
-  test("focus on first question when click on form (where there is no in question)", async ({ page }) => {
+  test("focus on first question when click on form (where there is no in question)", async ({
+    page,
+  }) => {
     await goToAnnotationPage(page);
-    const formBox = await page.getByText('Submit your feedback').boundingBox();
+    const formBox = await page.getByText("Submit your feedback").boundingBox();
 
     await page.mouse.click(formBox.x - 25, formBox.y - 25); // click outside form to loose focus
     await expect(page).toHaveScreenshot();
@@ -182,7 +184,22 @@ test.describe("Annotate page", () => {
     await page.waitForTimeout(400);
 
     await expect(page).toHaveScreenshot();
-  })
+  });
+
+  test("focus on question clicked when user click outside of form and click in a question again", async ({
+    page,
+  }) => {
+    await goToAnnotationPage(page);
+    const formBox = await page.getByText("Submit your feedback").boundingBox();
+
+    await page.mouse.click(formBox.x - 25, formBox.y - 25); // click outside form to loose focus
+    await expect(page).toHaveScreenshot();
+
+    await page.getByText("Negative").nth(1).click();
+    await page.waitForTimeout(400);
+
+    await expect(page).toHaveScreenshot();
+  });
 });
 
 test.describe("Annotation page shortcuts", () => {
