be751f7c6762f7a5bf722f3ceed48805cc70f0b9
agustin <agustin.piqueres@gmail.com>
2023-08-13 18:41:51 +0200

    refactor: updating requirement to requirements and the uses inside the functions

--- src/argilla/utils/dependency.py
+++ src/argilla/utils/dependency.py
@@ -63,13 +63,13 @@ def _compare_versions(
         )
 
 
-def require_dependencies(requirement: Union[str, List[str]], func_name: Optional[str] = None) -> None:
+def require_dependencies(requirements: Union[str, List[str]], func_name: Optional[str] = None) -> None:
     """
     Perform a runtime check of the dependency versions, using the exact same syntax used by pip.
     The installed module version comes from the *site-packages* dir via *importlib_metadata*.
 
     Args:
-        requirement (`str` or `List[str]`): pip style definition, e.g.,  "tokenizers==0.9.4", "tqdm>=4.27", "numpy"
+        requirements (`str` or `List[str]`): pip style definition, e.g.,  "tokenizers==0.9.4", "tqdm>=4.27", "numpy"
         func_name (`str`, *optional*): what suggestion to print in case of requirements not being met
 
     Example:
@@ -80,21 +80,21 @@ def require_dependencies(requirement: Union[str, List[str]], func_name: Optional
     ```
     """
 
-    if isinstance(requirement, list):
-        if len(requirement) == 0:
+    if isinstance(requirements, list):
+        if len(requirements) == 0:
             return
-        require_dependencies(requirement[0], func_name=func_name)
-        return require_dependencies(requirement[1:], func_name=func_name)
+        require_dependencies(requirements[0], func_name=func_name)
+        return require_dependencies(requirements[1:], func_name=func_name)
 
     # non-versioned check
-    if re.match(r"^[\w_\-\d]+$", requirement):
-        package, op, want_version = requirement, None, None
+    if re.match(r"^[\w_\-\d]+$", requirements):
+        package, op, want_version = requirements, None, None
     else:
-        match = re.findall(r"^([^!=<>\s]+)([\s!=<>]{1,2}.+)", requirement)
+        match = re.findall(r"^([^!=<>\s]+)([\s!=<>]{1,2}.+)", requirements)
         if not match:
             raise ValueError(
                 "requirement needs to be in the pip package format, .e.g., package_a==1.23, or package_b>=1.23, but"
-                f" got {requirement!r}."
+                f" got {requirements!r}."
             )
         package, want_full = match[0]
         want_range = want_full.split(",")  # there could be multiple requirements
@@ -104,18 +104,18 @@ def require_dependencies(requirement: Union[str, List[str]], func_name: Optional
             if not match:
                 raise ValueError(
                     "requirement needs to be in the pip package format, .e.g., package_a==1.23, or package_b>=1.23,"
-                    f" but got {requirement!r}."
+                    f" but got {requirements!r}."
                 )
             op, want_version = match[0]
             wanted[op] = want_version
             if op not in ops:
-                raise ValueError(f"{requirement}: need one of {list(ops.keys())}, but got {op!r}.")
+                raise ValueError(f"{requirements}: need one of {list(ops.keys())}, but got {op!r}.")
 
     # special case
     if package == "python":
         got_version = ".".join([str(x) for x in sys.version_info[:3]])
         for op, want_version in wanted.items():
-            _compare_versions(op, got_version, want_version, requirement, package, func_name=func_name)
+            _compare_versions(op, got_version, want_version, requirements, package, func_name=func_name)
         return
 
     # check if any version is installed
@@ -124,26 +124,26 @@ def require_dependencies(requirement: Union[str, List[str]], func_name: Optional
     except importlib_metadata.PackageNotFoundError:
         raise ModuleNotFoundError(
             f"'{package}' must be installed{f' to use `{func_name}`' if func_name else ''}! You can"
-            f" install '{package}' with this command: `pip install {requirement}`"
+            f" install '{package}' with this command: `pip install {requirements}`"
         )
 
     # check that the right version is installed if version number or a range was provided
     if want_version is not None:
         for op, want_version in wanted.items():
-            _compare_versions(op, got_version, want_version, requirement, package, func_name=func_name)
+            _compare_versions(op, got_version, want_version, requirements, package, func_name=func_name)
 
 
 _P = ParamSpec("_P")
 _R = TypeVar("_R")
 
 
-def requires_version(requirement: Union[str, List[str]]) -> Callable[[Callable[_P, _R]], Callable[_P, _R]]:
+def requires_dependencies(requirements: Union[str, List[str]]) -> Callable[[Callable[_P, _R]], Callable[_P, _R]]:
     """Decorator variant of `require_version`.
     Perform a runtime check of the dependency versions, using the exact same syntax used by pip.
     The installed module version comes from the *site-packages* dir via *importlib_metadata*.
 
     Args:
-        requirement (`str`): pip style definition, e.g.,  "tokenizers==0.9.4", "tqdm>=4.27", "numpy"
+        requirements (`str`): pip style definition, e.g.,  "tokenizers==0.9.4", "tqdm>=4.27", "numpy"
 
     Example:
     ```python
@@ -156,7 +156,7 @@ def requires_version(requirement: Union[str, List[str]]) -> Callable[[Callable[_
     def decorator(func: Callable[_P, _R]) -> Callable[_P, _R]:
         @functools.wraps(func)
         def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _R:
-            require_version(requirement, func.__name__)
+            require_dependencies(requirements, func.__name__)
             return func(*args, **kwargs)
 
         return wrapper
@@ -164,4 +164,4 @@ def requires_version(requirement: Union[str, List[str]]) -> Callable[[Callable[_
     return decorator
 
 
-__all__ = ["requires_version", "require_version"]
+__all__ = ["requires_dependencies", "require_dependencies"]
