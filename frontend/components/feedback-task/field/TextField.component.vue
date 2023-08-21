b38089464d35368cb65e272edd54cf8ba451f755
leire <leire@recogn.ai>
2023-08-21 17:31:11 +0200

    make record transition more explicit

--- frontend/components/feedback-task/field/TextField.component.vue
+++ frontend/components/feedback-task/field/TextField.component.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="text_field_component">
-    <div class="title-area --body2">
-      <span v-text="title" />
-      <BaseActionTooltip tooltip="Copied" tooltip-position="left">
-        <BaseButton
-          title="Copy to clipboard"
-          class="text_field_component__copy-button"
-          @click.prevent="$copyToClipboard(fieldText)"
-        >
-          <svgicon color="#acacac" name="copy" width="18" height="18" />
-        </BaseButton>
-      </BaseActionTooltip>
-    </div>
-    <transition name="fade" v-if="fieldText" appear mode="out-in">
-      <div class="content-area --body1" :key="fieldText">
+  <transition name="fade" v-if="fieldText" appear mode="out-in">
+    <div class="text_field_component" :key="fieldText">
+      <div class="title-area --body2">
+        <span v-text="title" />
+        <BaseActionTooltip tooltip="Copied" tooltip-position="left">
+          <BaseButton
+            title="Copy to clipboard"
+            class="text_field_component__copy-button"
+            @click.prevent="$copyToClipboard(fieldText)"
+          >
+            <svgicon color="#acacac" name="copy" width="18" height="18" />
+          </BaseButton>
+        </BaseActionTooltip>
+      </div>
+      <div class="content-area --body1">
         <div v-if="!useMarkdown" v-text="fieldText" />
         <RenderMarkdownBaseComponent v-else :markdown="fieldText" />
       </div>
-    </transition>
-  </div>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -67,7 +67,7 @@ export default {
 }
 .fade-enter-active,
 .fade-leave-active {
-  transition: all 0.2s;
+  transition: all 0.25s;
 }
 .fade-enter-from,
 .fade-leave-to {
