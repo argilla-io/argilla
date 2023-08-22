3636bcd5a5a1ae733b1cc6b1449967ab60bf9159
keithCuniah <keithcuniah@gmail.com>
2023-08-22 12:27:46 +0200

    :white_check_mark: remove `skipped` test

--- frontend/components/commons/forms/questions-form/fields/shared-components/question-header/questionHeader.component.spec.js
+++ frontend/components/commons/forms/questions-form/fields/shared-components/question-header/questionHeader.component.spec.js
@@ -42,34 +42,6 @@ describe("QuestionHeaderComponent", () => {
       false
     );
   });
-  it.skip("render the optional-field directive if the question is not required", async () => {
-    await wrapper.setProps({ isRequired: true });
-    const getAllByText = (wrapper, text) => {
-      //  Get all elements with the given text.
-      return wrapper.findAll("*").filter((node) => node.text() === text);
-    };
-
-    const getByText = (wrapper, text) => {
-      // Get the first element that has the given text.
-
-      const results = getAllByText(wrapper, text);
-      if (results.length === 0) {
-        throw new Error(
-          `getByText() found no element with the text: "${text}".`
-        );
-      }
-      return results.at(0);
-    };
-
-    // FIXME - test that the question text have ' (optional)' at the end
-    // console.log(wrapper.find("span").element.innerHTML);
-
-    // expect(wrapper.find(" (optional)")" (optional)".exists()).toBe(true);
-    // expect(wrapper.vm.showAsOptional).toBe(false); // by default question is optional
-
-    expect(getByText(wrapper, "This is a question to ask").exists()).toBe(true);
-    // console.log(getByText(wrapper, " (optional)"));
-  });
   it("render the BaseIconWithBadge component if there is a tooltipMessage", async () => {
     await wrapper.setProps({ tooltipMessage: "The tooltip message to show" });
 
