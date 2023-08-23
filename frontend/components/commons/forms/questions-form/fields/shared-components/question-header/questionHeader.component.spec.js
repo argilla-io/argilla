128315d33598c8934f6c1dc99c9660944d9adb34
Keith Cuniah <88380932+keithCuniah@users.noreply.github.com>
2023-08-23 11:26:22 +0200

    :sparkles: FEAT: signal required questions instead optional (#3608)
    Show required question with a `red asterisk` instead of showing optional
    widget
    
    See #3394 
    
    **Type of change**
    
    - New feature
    
    **How Has This Been Tested**
    
    Only feedback task => we should see a `red asterisk` at the end of the
    title from a required question instead of showing `(optional)` for
    optional question.
    
    Note : 
    All e2e images have to be regenerated since this affect all `feedback
    task` datasets
    
    ---------
    
    Co-authored-by: Damián Pumar <damianpumar@gmail.com>

--- frontend/components/commons/forms/questions-form/fields/shared-components/question-header/questionHeader.component.spec.js
+++ frontend/components/commons/forms/questions-form/fields/shared-components/question-header/questionHeader.component.spec.js
@@ -5,8 +5,8 @@ let wrapper = null;
 const options = {
   stubs: ["BaseIconWithBadge"],
   directives: {
-    "optional-field"() {
-      // this directive is used to show '(optional)' at the end of a question optional
+    "required-field"() {
+      // this directive is used to show a red asterisk at the end of a required question
     },
     tooltip() {
       // this directive is used to show a tooltip
@@ -41,34 +41,6 @@ describe("QuestionHeaderComponent", () => {
     expect(wrapper.findComponent({ name: "BaseIconWithBadge" }).exists()).toBe(
       false
     );
-  });
-  it.skip("render the optional-field directive if the question is not required", async () => {
-    await wrapper.setProps({ isRequired: true });
-    const getAllByText = (wrapper, text) => {
-      //  Get all elements with the given text.
-      return wrapper.findAll("*").filter((node) => node.text() === text);
-    };
-
-    const getByText = (wrapper, text) => {
-      // Get the first element that has the given text.
-
-      const results = getAllByText(wrapper, text);
-      if (results.length === 0) {
-        throw new Error(
-          `getByText() found no element with the text: "${text}".`
-        );
-      }
-      return results.at(0);
-    };
-
-    // FIXME - test that the question text have ' (optional)' at the end
-    // console.log(wrapper.find("span").element.innerHTML);
-
-    // expect(wrapper.find(" (optional)")" (optional)".exists()).toBe(true);
-    // expect(wrapper.vm.showAsOptional).toBe(false); // by default question is optional
-
-    expect(getByText(wrapper, "This is a question to ask").exists()).toBe(true);
-    // console.log(getByText(wrapper, " (optional)"));
   });
   it("render the BaseIconWithBadge component if there is a tooltipMessage", async () => {
     await wrapper.setProps({ tooltipMessage: "The tooltip message to show" });
