e756d3c776caff18e3cbd38ec2d25e6dac01a5b0
leire <leire@recogn.ai>
2023-08-23 16:21:13 +0200

    move logic from annotation to questions view model

--- frontend/components/commons/forms/questions-form/useQuestionsFormViewModel.ts
+++ frontend/components/commons/forms/questions-form/useQuestionsFormViewModel.ts
@@ -1,13 +1,16 @@
 import { useResolve } from "ts-injecty";
+import { ref } from "vue-demi";
 import { Record } from "~/v1/domain/entities/record/Record";
 import { ClearRecordUseCase } from "~/v1/domain/usecases/clear-record-use-case";
 import { DiscardRecordUseCase } from "~/v1/domain/usecases/discard-record-use-case";
 import { SubmitRecordUseCase } from "~/v1/domain/usecases/submit-record-use-case";
+import { SaveDraftRecord } from "~/v1/domain/usecases/save-draft-use-case";
 
 export const useQuestionFormViewModel = () => {
   const discardUseCase = useResolve(DiscardRecordUseCase);
   const submitUseCase = useResolve(SubmitRecordUseCase);
   const clearUseCase = useResolve(ClearRecordUseCase);
+  const saveDraftUseCase = useResolve(SaveDraftRecord);
 
   const discard = async (record: Record) => {
     await discardUseCase.execute(record);
@@ -21,5 +24,26 @@ export const useQuestionFormViewModel = () => {
     await clearUseCase.execute(record);
   };
 
-  return { clear, submit, discard };
+  const draftSaving = ref(false);
+  const onSaveDraft = async (record: Record) => {
+    if (!record.hasAnyQuestionAnswered) return;
+    draftSaving.value = true;
+    try {
+      await saveDraftUseCase.execute(record);
+    } catch {
+    } finally {
+      draftSaving.value = false;
+    }
+  };
+
+  let timer = null;
+  const saveDraft = (record: Record) => {
+    if (timer) clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      onSaveDraft(record);
+    }, 2000);
+  };
+
+  return { clear, submit, discard, saveDraft, draftSaving };
 };
--- frontend/pages/dataset/_id/annotation-mode/useAnnotationModeViewModel.ts
+++ frontend/pages/dataset/_id/annotation-mode/useAnnotationModeViewModel.ts
@@ -1,17 +1,12 @@
-import { getCurrentInstance, onBeforeMount } from "vue-demi";
-import { useResolve } from "ts-injecty";
+import { onBeforeMount } from "vue-demi";
 import { useDatasetViewModel } from "../useDatasetViewModel";
 import {
   UpdateMetricsEventHandler,
   useEvents,
 } from "@/v1/infrastructure/events";
-import { SaveDraftRecord } from "~/v1/domain/usecases/save-draft-use-case";
-import { Record } from "~/v1/domain/entities/record/Record";
 
 export const useAnnotationModeViewModel = () => {
   const datasetViewModel = useDatasetViewModel();
-  const saveDraftUseCase = useResolve(SaveDraftRecord);
-  const instance = getCurrentInstance();
 
   onBeforeMount(() => {
     useEvents(() => {
@@ -21,27 +16,5 @@ export const useAnnotationModeViewModel = () => {
     datasetViewModel.loadDataset();
   });
 
-  const onSaveDraft = async (record: Record) => {
-    if (!record.hasAnyQuestionAnswered) return;
-
-    instance.proxy.$root.$emit("record-saving", true);
-
-    try {
-      await saveDraftUseCase.execute(record);
-    } catch {
-    } finally {
-      instance.proxy.$root.$emit("record-saving", false);
-    }
-  };
-
-  let timer = null;
-  const saveDraft = (record: Record) => {
-    if (timer) clearTimeout(timer);
-
-    timer = setTimeout(() => {
-      onSaveDraft(record);
-    }, 2000);
-  };
-
-  return { ...datasetViewModel, saveDraft };
+  return { ...datasetViewModel };
 };
