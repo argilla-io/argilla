ed22edfa3b836467383fb35f50b83c77beee0d2c
Eshwar Hebbur Shivakumar <45142059+eshwarhs@users.noreply.github.com>
2023-08-23 10:14:15 -0700

    Resolve PR comments

--- src/argilla/tasks/logout/__main__.py
+++ src/argilla/tasks/logout/__main__.py
@@ -12,44 +12,20 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import Optional
 
 import typer
 
 app = typer.Typer(invoke_without_command=True)
 
 
-@app.callback(help="Logout from an Argilla Server")
-def logout(
-    api_url: str = typer.Option(..., help="The URL of the Argilla Server to logout from"),
-    api_key: str = typer.Option(..., help="The API key for logging out from the Argilla Server", prompt="API Key"),
-    workspace: Optional[str] = typer.Option(
-        None, help="The default workspace over which the operations will be performed"
-    ),
-    extra_headers: Optional[str] = typer.Option(
-        None, help="A JSON string with extra headers to be sent in the requests to the Argilla Server"
-    ),
-):
-    import json
+@app.callback(help="Logout from an Argilla Server.")
+def logout():
+    from argilla.tasks.callback import init_callback
+    from argilla.client.login import ArgillaCredentials
 
-    from argilla.client.logout import logout as logout_func
-
-    try:
-        if extra_headers:
-            headers = json.loads(extra_headers)
-        else:
-            headers = {}
-        logout_func(api_url=api_url, api_key=api_key, workspace=workspace, extra_headers=headers)
-        typer.echo(f"Logged out successfully from '{api_url}' Argilla server!")
-    except json.JSONDecodeError as e:
-        typer.echo("The provided extra headers are not a valid JSON string.")
-        raise typer.Exit(code=1) from e
-    except ValueError as e:
-        typer.echo(
-            f"Could not logout from the '{api_url}' Argilla server. Please check the provided credentials and try again."
-        )
-        raise typer.Exit(code=1) from e
-
+    init_callback()
+    ArgillaCredentials.remove()
+    typer.echo("Logged out successfully from Argilla server!")
 
 if __name__ == "__main__":
     app()
