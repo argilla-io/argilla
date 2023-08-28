0832e7dbb6c494c2be04dbd72f4a7658b9eedb54
Damián Pumar <damianpumar@gmail.com>
2023-08-28 17:43:30 +0200

    🐛 Fix pagination

--- frontend/v1/domain/entities/record/Records.ts
+++ frontend/v1/domain/entities/record/Records.ts
@@ -1,10 +1,10 @@
 import { Record } from "./Record";
 
 export class Records {
-  constructor(
-    public readonly records: Record[] = [],
-    public readonly total: number = 0
-  ) {}
+  public readonly records: Record[];
+  constructor(records: Record[] = [], public readonly total: number = 0) {
+    this.records = records.sort((r1, r2) => (r1.page < r2.page ? -1 : 1));
+  }
 
   get hasRecordsToAnnotate() {
     return this.records.length;
@@ -15,20 +15,36 @@ export class Records {
   }
 
   getRecordOn(page: number) {
-    const arrayOffset = page - 1;
-    return this.records.find((record) => record.arrayOffset === arrayOffset);
+    return this.records.find((record) => record.page === page);
   }
 
-  getOffsetToFind(page: number, status: string): number {
-    const offsetToFind = page - 1;
+  getPageToFind(
+    page: number,
+    status: string
+  ): { pageToFind: number; recordsToFetch: number } {
+    if (!this.hasRecordsToAnnotate)
+      return { pageToFind: page, recordsToFetch: 10 };
 
-    if (this.records.length < offsetToFind) return offsetToFind;
+    const changedRecords = this.records.filter(
+      (record) => record.status !== status
+    ).length;
 
     const firstRecord = this.records[0];
-    if (firstRecord?.arrayOffset > offsetToFind) return offsetToFind;
+    const latestRecord = this.records[this.records.length - 1];
 
-    const previousRecords = this.records.slice(0, page);
+    const isMovingToNext = page > latestRecord?.page;
 
-    return previousRecords.filter((record) => record.status === status).length;
+    if (isMovingToNext) {
+      return {
+        pageToFind: latestRecord.page + 1 - changedRecords,
+        recordsToFetch: 10,
+      };
+    } else if (firstRecord.page > page)
+      return {
+        pageToFind: firstRecord.page - 1,
+        recordsToFetch: 1,
+      };
+
+    return { pageToFind: page, recordsToFetch: 10 };
   }
 }
