d40748633d837dcf78b3e9a5720909979f96349a
Alvaro Bartolome <alvaro@argilla.io>
2023-08-29 13:38:40 +0200

    fix: `RemoteFeedbackRecord` to handle `RemoteSuggestionSchema`

--- src/argilla/client/feedback/schemas/records.py
+++ src/argilla/client/feedback/schemas/records.py
@@ -263,40 +263,127 @@ class RemoteFeedbackRecord(FeedbackRecord):
     `RemoteFeedbackDataset` class when fetching records from Argilla.
 
     Args:
-        id: The ID of the record in Argilla. Defaults to None, and is automatically
-            fulfilled internally once the record is pushed to Argilla.
         client: The Argilla client to use to push the record to Argilla. Is shared with
             the `RemoteFeedbackDataset` that created this record.
         name2id: A dictionary that maps the question names to their corresponding IDs.
+        id: The ID of the record in Argilla. Defaults to None, and is automatically
+            fulfilled internally once the record is pushed to Argilla.
+        suggestions: A list of `RemoteSuggestionSchema` that contains the suggestions
+            for the current record in Argilla. Every suggestion is linked to only one
+            question. Defaults to an empty list.
     """
 
     client: httpx.Client
     name2id: Dict[str, UUID]
 
     id: UUID
+    suggestions: Union[Tuple[RemoteSuggestionSchema], List[RemoteSuggestionSchema]] = Field(
+        default_factory=tuple, allow_mutation=False
+    )
+
+    # TODO(alvarobartt): here to be able to use the `allowed_for_roles` decorator
+    @property
+    def _client(self) -> httpx.Client:
+        return self.client
 
     @allowed_for_roles(roles=[UserRole.owner, UserRole.admin])
     def update(
-        self, suggestions: Union[SuggestionSchema, List[SuggestionSchema], Dict[str, Any], List[Dict[str, Any]]]
+        self,
+        suggestions: Union[
+            RemoteSuggestionSchema,
+            List[RemoteSuggestionSchema],
+            SuggestionSchema,
+            List[SuggestionSchema],
+            Dict[str, Any],
+            List[Dict[str, Any]],
+        ],
     ) -> None:
         """Update a `RemoteFeedbackRecord`. Currently just `suggestions` are supported.
 
         Note that this method will update the record in Argilla directly.
 
         Args:
-            suggestions: can be a single `SuggestionSchema`, a list of `SuggestionSchema`,
-                a single dictionary, or a list of dictionaries. If a dictionary is provided,
-                it will be converted to a `SuggestionSchema` internally.
+            suggestions: can be a single `RemoteSuggestionSchema` or `SuggestionSchema`,
+                a list of `RemoteSuggestionSchema` or `SuggestionSchema`, a single
+                dictionary, or a list of dictionaries. If a dictionary is provided,
+                it will be converted to a `RemoteSuggestionSchema` internally.
 
         Raises:
             PermissionError: if the user does not have either `owner` or `admin` role.
         """
-        super().update(suggestions)
-        for suggestion in self.suggestions:
-            suggestion.question_id = self.name2id[suggestion.question_name]
-            datasets_api_v1.set_suggestion(
-                client=self.client, record_id=self.id, **suggestion.dict(exclude_none=True, exclude={"question_name"})
+        if isinstance(suggestions, (dict, SuggestionSchema, RemoteSuggestionSchema)):
+            suggestions = [suggestions]
+
+        existing_suggestions = {suggestion.question_name: suggestion for suggestion in self.suggestions}
+        new_suggestions = {}
+
+        for suggestion in suggestions:
+            if isinstance(suggestion, dict):
+                if "question_id" not in suggestion or not suggestion["question_id"]:
+                    suggestion["question_id"] = self.name2id[suggestion["question_name"]]
+                if "id" in suggestion:
+                    suggestion = RemoteSuggestionSchema(client=self._client, record_id=self.id, **suggestion)
+
+            if isinstance(suggestion, SuggestionSchema):
+                suggestion = SuggestionSchema(**suggestion)
+                if not suggestion.question_id:
+                    suggestion.question_id = self.name2id[suggestion.question_name]
+
+            if suggestion.question_name in new_suggestions:
+                warnings.warn(
+                    f"A suggestion for question `{suggestion.question_name}` has been"
+                    " provided twice in the same update, so the last one will be the one"
+                    " to be kept.",
+                    UserWarning,
+                    stacklevel=1,
+                )
+                new_suggestions.pop(suggestion.question_name, None)
+                new_suggestions[suggestion.question_name] = suggestion
+            elif suggestion.question_name in existing_suggestions:
+                comparable_fields = {"question_name", "type", "score", "value", "agent"}
+                comparable_suggestion = suggestion.dict(include={"question_name", "type", "score", "value", "agent"})
+                if any(
+                    [
+                        comparable_suggestion == suggestion.dict(include=comparable_fields)
+                        for suggestion in existing_suggestions.values()
+                    ]
+                ):
+                    warnings.warn(
+                        f"A suggestion for question `{suggestion.question_name}` has already"
+                        " been provided and the provided suggestion is the same, so it will"
+                        " be ignored.",
+                        UserWarning,
+                        stacklevel=1,
+                    )
+                else:
+                    warnings.warn(
+                        f"A suggestion for question `{suggestion.question_name}` has already"
+                        " been provided but the provided suggestion is different, so it will"
+                        " overwrite the existing one.",
+                        UserWarning,
+                        stacklevel=1,
+                    )
+                    existing_suggestions.pop(suggestion.question_name, None)
+                    new_suggestions[suggestion.question_name] = suggestion
+            else:
+                new_suggestions[suggestion.question_name] = suggestion
+
+        for suggestion in new_suggestions.values():
+            if isinstance(suggestion, SuggestionSchema):
+                exclude = {"question_name"}
+            elif isinstance(suggestion, RemoteSuggestionSchema):
+                exclude = {"client", "id", "record_id", "question_name"}
+            pushed_suggestion = datasets_api_v1.set_suggestion(
+                client=self._client, record_id=self.id, **suggestion.dict(exclude_none=True, exclude=exclude)
             )
+            existing_suggestions[suggestion.question_name] = RemoteSuggestionSchema(
+                client=self._client,
+                record_id=self.id,
+                question_name=suggestion.question_name,
+                **pushed_suggestion.parsed.dict(exclude_none=True),
+            )
+
+        self.__dict__["suggestions"] = tuple(existing_suggestions.values())
 
     def set_suggestions(
         self, suggestions: Union[SuggestionSchema, List[SuggestionSchema], Dict[str, Any], List[Dict[str, Any]]]
