5de2597310edc713592ad8a64849f3d88249217c
Eshwar Hebbur Shivakumar <45142059+eshwarhs@users.noreply.github.com>
2023-08-30 12:06:24 -0400

    Add tests for create workspace command

---
+++ tests/unit/tasks/workspaces/test_create.py
@@ -0,0 +1,45 @@
+#  Copyright 2021-present, the Recognai S.L. team.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from typing import TYPE_CHECKING
+from unittest.mock import ANY
+
+import pytest
+
+if TYPE_CHECKING:
+    from click.testing import CliRunner
+    from pytest_mock import MockerFixture
+    from typer import Typer
+
+from argilla.client.workspaces import Workspace
+
+
+@pytest.mark.usefixtures("login_mock")
+def test_cli_workspaces_create_without_name(cli_runner: "CliRunner", cli: "Typer", mocker: "MockerFixture"):
+    result = cli_runner.invoke(cli, "workspaces create")
+    assert result.exit_code == 2
+
+
+@pytest.mark.usefixtures("login_mock")
+def test_cli_workspaces_create_with_name(cli_runner: "CliRunner", cli: "Typer", mocker: "MockerFixture"):
+    result = cli_runner.invoke(cli, "workspaces create workspace1")
+    assert result.exit_code == 1
+
+
+@pytest.mark.usefixtures("not_logged_mock")
+def test_cli_workspaces_create_needs_login(cli_runner: "CliRunner", cli: "Typer", mocker: "MockerFixture"):
+    result = cli_runner.invoke(cli, "workspaces create")
+
+    assert "You are not logged in. Please run `argilla login` to login to an Argilla server." in result.stdout
+    assert result.exit_code == 1
