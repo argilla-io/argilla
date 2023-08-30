5be947393c3937afcbc864fe4211c70539eef21a
Alvaro Bartolome <alvaro@argilla.io>
2023-08-30 16:10:00 +0200

    feat: add `whoami` command

--- src/argilla/tasks/whoami/__init__.py
+++ src/argilla/tasks/whoami/__init__.py
@@ -12,12 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from .database import app as database_app
-from .datasets import app as datasets_app
-from .login import app as login_app
-from .logout import app as logout_app
-from .server import app as server_app
-from .training import app as training_app
-from .users import app as users_app
-from .whoami import app as whoami_app
-from .workspaces import app as workspaces_app
+from .__main__ import app
+
+if __name__ == "__main__":
+    app()
