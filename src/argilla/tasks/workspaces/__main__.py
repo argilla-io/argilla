7c1b2368b444b7b7a281d37ad51bcb2d8e92acf5
Eshwar Hebbur Shivakumar <eshwar1011235@gmail.com>
2023-09-01 11:58:31 -0400

    feat/cli : add `python -m argilla workspaces create` command (#3676)
    <!-- Thanks for your contribution! As part of our Community Growers
    initiative 🌱, we're donating Justdiggit bunds in your name to reforest
    sub-Saharan Africa. To claim your Community Growers certificate, please
    contact David Berenstein in our Slack community or fill in this form
    https://tally.so/r/n9XrxK once your PR has been merged. -->
    
    # Description
    
    This PR adds a new CLI command `python -m argilla workspaces create`.
    
    Closes #3586 
    
    **Type of change**
    
    - [x] New feature (non-breaking change which adds functionality)
    
    **How Has This Been Tested**
    
    Unit tests have been added to tests/unit/tasks/workspaces directory
    
    - [x] Create workspace without logging in
    - [x] Create workspace without providing workspace name
    - [ ] Create a workspace with the same name
    - [x] Create workspace after logging in and providing workspace name
    
    **Checklist**
    
    - [ ] I added relevant documentation
    - [x] I followed the style guidelines of this project
    - [x] I did a self-review of my code
    - [ ] I made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] I filled out [the contributor form](https://tally.so/r/n9XrxK)
    (see text above)
    - [x] I have added relevant notes to the `CHANGELOG.md` file (See
    https://keepachangelog.com/)
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Alvaro Bartolome <alvarobartt@gmail.com>

--- src/argilla/tasks/workspaces/__main__.py
+++ src/argilla/tasks/workspaces/__main__.py
@@ -16,11 +16,13 @@
 from argilla.tasks.async_typer import AsyncTyper
 from argilla.tasks.callback import init_callback
 
+from .create import create_workspace
 from .list import list_workspaces
 
 app = AsyncTyper(help="Holds CLI commands for workspace management.", no_args_is_help=True, callback=init_callback)
 
 app.command(name="list", help="Lists workspaces of the logged user.")(list_workspaces)
+app.command(name="create", help="Create a workspace")(create_workspace)
 
 
 if __name__ == "__main__":
