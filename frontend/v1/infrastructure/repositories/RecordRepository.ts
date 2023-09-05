47f84700012c3855ecb42127fba3894126bead5b
Damián Pumar <damianpumar@gmail.com>
2023-09-05 17:40:29 +0200

    🐛 Fix merge

--- frontend/v1/infrastructure/repositories/RecordRepository.ts
+++ frontend/v1/infrastructure/repositories/RecordRepository.ts
@@ -212,6 +212,8 @@ export class RecordRepository {
     params.append("limit", howMany.toString());
     params.append("response_status", backendStatus);
 
+    if (backendStatus === "missing") params.append("response_status", "draft");
+
     return params;
   }
 }
