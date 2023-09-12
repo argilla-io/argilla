2338052c3c765f64040da5bc9323ba52f76b1802
gabrielmbmb <gmartinbdev@gmail.com>
2023-09-12 16:05:22 +0200

    feat: update `users delete` unit tests

--- tests/unit/cli/users/test_delete.py
+++ tests/unit/cli/users/test_delete.py
@@ -29,35 +29,35 @@ class TestSuiteDeleteUserCommand:
         user_from_name_mock = mocker.patch("argilla.client.users.User.from_name", return_value=user)
         user_delete_mock = mocker.patch("argilla.client.users.User.delete")
 
-        result = cli_runner.invoke(cli, "users delete unit-test")
+        result = cli_runner.invoke(cli, "users --username unit-test delete")
 
         assert result.exit_code == 0
         assert "User removed" in result.stdout
-        assert "User with username 'unit-test' has been removed!" in result.stdout
+        assert "User with username=unit-test has been removed" in result.stdout
         user_from_name_mock.assert_called_once_with("unit-test")
         user_delete_mock.assert_called_once()
 
-    @pytest.mark.parametrize(
-        "ExceptionType, expected_msg",
-        [
-            (ValueError, "User with username=unit-test doesn't exist"),
-            (RuntimeError, "An unexpected error occurred when trying to remove the user"),
-        ],
-    )
-    def test_delete_user_errors(
-        self,
-        cli_runner: "CliRunner",
-        cli: "Typer",
-        mocker: "MockerFixture",
-        ExceptionType: Type[Exception],
-        expected_msg: str,
+    def test_delete_user_not_found(
+        self, cli_runner: "CliRunner", cli: "Typer", mocker: "MockerFixture", user: "User"
     ) -> None:
-        user_from_name_mock = mocker.patch("argilla.client.users.User.from_name", side_effect=ExceptionType)
-        result = cli_runner.invoke(cli, "users delete unit-test")
+        user_from_name_mock = mocker.patch("argilla.client.users.User.from_name", side_effect=ValueError)
+        result = cli_runner.invoke(cli, "users --username unit-test delete")
 
         assert result.exit_code == 1
-        assert expected_msg in result.stdout
+        assert "User with username=unit-test doesn't exist." in result.stdout
         user_from_name_mock.assert_called_once_with("unit-test")
+
+    def test_delete_user_runtime_error(
+        self, cli_runner: "CliRunner", cli: "Typer", mocker: "MockerFixture", user: "User"
+    ) -> None:
+        user_delete_mock = mocker.patch.object(user, "delete", side_effect=RuntimeError)
+        user_from_name_mock = mocker.patch("argilla.client.users.User.from_name", return_value=user)
+        result = cli_runner.invoke(cli, "users --username unit-test delete")
+
+        assert result.exit_code == 1
+        assert "An unexpected error occurred when trying to remove the user" in result.stdout
+        user_from_name_mock.assert_called_once_with("unit-test")
+        user_delete_mock.assert_called_once()
 
 
 @pytest.mark.usefixtures("not_logged_mock")
