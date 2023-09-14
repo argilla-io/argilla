12fbe05673bd1ff2f03d7cfdcd28c1b7bc493983
Alvaro Bartolome <alvaro@argilla.io>
2023-09-14 15:56:30 +0200

    feat: add `server_version` to `__init__`

--- src/argilla/__init__.py
+++ src/argilla/__init__.py
@@ -66,6 +66,7 @@ if _TYPE_CHECKING:
         TokenClassificationRecord,
     )
     from argilla.client.users import User
+    from argilla.client.utils import server_version
     from argilla.client.workspaces import Workspace
     from argilla.datasets import (
         TextClassificationSettings,
@@ -138,6 +139,7 @@ _import_structure = {
         "read_pandas",
     ],
     "client.users": ["User"],
+    "client.utils": ["server_version"],
     "client.workspaces": ["Workspace"],
     "monitoring.model_monitor": ["monitor"],
     "listeners.listener": [
