7a289913ac41ed7b1097152b9dd0b69152def860
Paco Aranda <francis@argilla.io>
2023-09-14 12:40:04 +0200

    refactor: Use base class for opensearch engine

--- src/argilla/server/search_engine/opensearch.py
+++ src/argilla/server/search_engine/opensearch.py
@@ -13,60 +13,23 @@
 #  limitations under the License.
 
 import dataclasses
-from typing import Any, AsyncGenerator, Dict, Iterable, List, Optional, Union
-from uuid import UUID
+from typing import Any, AsyncGenerator, Dict, List, Optional
 
 from opensearchpy import AsyncOpenSearch, helpers
-from pydantic import BaseModel
-from pydantic.utils import GetterDict
 
-from argilla.server.enums import FieldType, ResponseStatusFilter
 from argilla.server.models import (
-    Dataset,
-    Field,
-    Question,
-    QuestionType,
-    Record,
-    Response,
-    ResponseStatus,
+    VectorSettings,
 )
 from argilla.server.search_engine.base import (
     SearchEngine,
-    SearchResponseItem,
-    SearchResponses,
-    StringQuery,
-    UserResponse,
     UserResponseStatusFilter,
 )
+from argilla.server.search_engine.commons import BaseElasticAndOpenSearchEngine, field_name_for_vector_settings
 from argilla.server.settings import settings
 
 
-class SearchDocumentGetter(GetterDict):
-    def get(self, key: Any, default: Any = None) -> Any:
-        if key == "responses":
-            return {
-                response.user.username: UserResponse(
-                    values={k: v["value"] for k, v in response.values.items()} if response.values else None,
-                    status=response.status,
-                )
-                for response in self._obj.responses
-            }
-        return super().get(key, default)
-
-
-class SearchDocument(BaseModel):
-    id: UUID
-    fields: Dict[str, Any]
-
-    responses: Optional[Dict[str, UserResponse]]
-
-    class Config:
-        orm_mode = True
-        getter_dict = SearchDocumentGetter
-
-
 @dataclasses.dataclass
-class OpenSearchEngine(SearchEngine):
+class OpenSearchEngine(BaseElasticAndOpenSearchEngine):
     config: Dict[str, Any]
 
     es_number_of_shards: int
@@ -75,8 +38,8 @@ class OpenSearchEngine(SearchEngine):
     def __post_init__(self):
         self.client = AsyncOpenSearch(**self.config)
 
-    async def create_index(self, dataset: Dataset):
-        mappings = {
+    def _configure_index_mappings(self, dataset) -> dict:
+        return {
             # See https://www.elastic.co/guide/en/elasticsearch/reference/current/dynamic.html#dynamic-parameters
             "dynamic": "strict",
             "dynamic_templates": self._dynamic_templates_for_question_responses(dataset.questions),
@@ -84,197 +47,77 @@ class OpenSearchEngine(SearchEngine):
                 # See https://www.elastic.co/guide/en/elasticsearch/reference/current/explicit-mapping.html
                 "id": {"type": "keyword"},
                 "responses": {"dynamic": True, "type": "object"},
+                **self._mapping_for_vectors_settings(dataset.vectors_settings),
                 **self._mapping_for_fields(dataset.fields),
             },
         }
 
-        settings = {
+    def _configure_index_settings(self):
+        return {
+            "index.knn": False,
             "number_of_shards": self.es_number_of_shards,
             "number_of_replicas": self.es_number_of_replicas,
         }
 
-        index_name = self._index_name_for_dataset(dataset)
+    def _mapping_for_vector_settings(self, vector_settings: VectorSettings) -> dict:
+        return {
+            field_name_for_vector_settings(vector_settings): {
+                "type": "knn_vector",
+                "dimension": vector_settings.dimensions,
+                "method": {
+                    "name": "hnsw",
+                    "engine": "lucene",  # See https://opensearch.org/blog/Expanding-k-NN-with-Lucene-aNN/
+                    "space_type": "l2",
+                    "parameters": {"m": 2, "ef_construction": 4},
+                },
+            }
+        }
+
+    async def _request_similarity_search(
+        self,
+        index: str,
+        vector_settings: VectorSettings,
+        value: List[float],
+        k: int,
+        user_response_status_filter: Optional[UserResponseStatusFilter] = None,
+    ) -> dict:
+        knn_query = {field_name_for_vector_settings(vector_settings): {"vector": value, "k": k}}
+
+        if user_response_status_filter:
+            # See https://opensearch.org/docs/latest/search-plugins/knn/filter-search-knn/#efficient-k-nn-filtering
+            # Will work from Opensearch >= v2.4
+            knn_query["filter"] = self._response_status_filter_builder(user_response_status_filter)
+
+        body = {"query": {"knn": knn_query}}
+        return await self.client.search(index=index, body=body, _source=False, track_total_hits=True)
+
+    async def _create_index_request(self, index_name: str, mappings: dict, settings: dict) -> None:
         await self.client.indices.create(index=index_name, body=dict(settings=settings, mappings=mappings))
 
-    async def delete_index(self, dataset: Dataset):
-        index_name = self._index_name_for_dataset(dataset)
+    async def _delete_index_request(self, index_name: str):
         await self.client.indices.delete(index_name, ignore=[404], ignore_unavailable=True)
 
-    async def add_records(self, dataset: Dataset, records: Iterable[Record]):
-        index_name = await self._get_index_or_raise(dataset)
+    async def _update_document_request(self, index_name: str, id: str, body: dict):
+        await self.client.update(index=index_name, id=id, body=body)
 
-        bulk_actions = [
-            {
-                "_op_type": "index",  # If document exist, we update source with latest version
-                "_id": record.id,
-                "_index": index_name,
-                **SearchDocument.from_orm(record).dict(),
-            }
-            for record in records
-        ]
+    async def put_index_mapping_request(self, index: str, mappings: dict):
+        await self.client.indices.put_mapping(index=index, body={"properties": mappings})
 
-        await self._bulk_op(bulk_actions)
-
-    async def delete_records(self, dataset: Dataset, records: Iterable[Record]):
-        index_name = await self._get_index_or_raise(dataset)
-
-        bulk_actions = [{"_op_type": "delete", "_id": record.id, "_index": index_name} for record in records]
-
-        await self._bulk_op(bulk_actions)
-
-    async def update_record_response(self, response: Response):
-        record = response.record
-        index_name = await self._get_index_or_raise(record.dataset)
-
-        es_response = UserResponse(
-            values={k: v["value"] for k, v in response.values.items()} if response.values else None,
-            status=response.status,
-        )
-
-        await self.client.update(
-            index=index_name,
-            id=record.id,
-            body={"doc": {"responses": {response.user.username: es_response.dict()}}},
-        )
-
-    async def delete_record_response(self, response: Response):
-        record = response.record
-        index_name = await self._get_index_or_raise(record.dataset)
-
-        await self.client.update(
-            index=index_name,
-            id=record.id,
-            body={"script": f'ctx._source["responses"].remove("{response.user.username}")'},
-        )
-
-    async def search(
-        self,
-        dataset: Dataset,
-        query: Union[StringQuery, str],
-        user_response_status_filter: Optional[UserResponseStatusFilter] = None,
-        offset: int = 0,
-        limit: int = 100,
-    ) -> SearchResponses:
-        # See https://www.elastic.co/guide/en/elasticsearch/reference/current/search-search.html
-
-        if isinstance(query, str):
-            query = StringQuery(q=query)
-
-        text_query = self._text_query_builder(dataset, text=query)
-
-        bool_query = {"must": [text_query]}
-        if user_response_status_filter:
-            bool_query["filter"] = self._response_status_filter_builder(user_response_status_filter)
-
-        body = {"_source": False, "query": {"bool": bool_query}}
-
-        response = await self.client.search(
-            index=self._index_name_for_dataset(dataset),
-            body=body,
-            from_=offset,
-            size=limit,
+    async def _index_search_request(self, index: str, query: dict, size: int, from_: int):
+        return await self.client.search(
+            index=index,
+            body={"query": query},
+            from_=from_,
+            size=size,
             _source=False,
             sort="_score:desc,id:asc",
             track_total_hits=True,
         )
 
-        items = [
-            SearchResponseItem(record_id=UUID(hit["_id"]), score=hit["_score"]) for hit in response["hits"]["hits"]
-        ]
-        total = response["hits"]["total"]["value"]
+    async def _index_exists_request(self, index_name: str) -> bool:
+        return await self.client.indices.exists(index=index_name)
 
-        return SearchResponses(items=items, total=total)
-
-    @staticmethod
-    def _text_query_builder(dataset: Dataset, text: StringQuery) -> dict:
-        if not text.field:
-            # See https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html
-            field_names = [
-                f"fields.{field.name}" for field in dataset.fields if field.settings.get("type") == FieldType.text
-            ]
-            return {"multi_match": {"query": text.q, "type": "cross_fields", "fields": field_names, "operator": "and"}}
-        else:
-            # See https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-match-query.html
-            return {"match": {f"fields.{text.field}": {"query": text.q, "operator": "and"}}}
-
-    def _mapping_for_fields(self, fields: List[Field]):
-        return {f"fields.{field.name}": self._es_mapping_for_field(field) for field in fields}
-
-    def _dynamic_templates_for_question_responses(self, questions: List[Question]) -> List[dict]:
-        # See https://www.elastic.co/guide/en/elasticsearch/reference/current/dynamic-templates.html
-        return [
-            {"status_responses": {"path_match": "responses.*.status", "mapping": {"type": "keyword"}}},
-            *[
-                {
-                    f"{question.name}_responses": {
-                        "path_match": f"responses.*.values.{question.name}",
-                        "mapping": self._field_mapping_for_question(question),
-                    },
-                }
-                for question in questions
-            ],
-        ]
-
-    def _field_mapping_for_question(self, question: Question):
-        settings = question.parsed_settings
-
-        if settings.type == QuestionType.rating:
-            # See https://www.elastic.co/guide/en/elasticsearch/reference/current/number.html
-            return {"type": "integer"}
-        elif settings.type == QuestionType.text:
-            # TODO: Review mapping for label selection. Could make sense to use `keyword` mapping instead.
-            #  See https://www.elastic.co/guide/en/elasticsearch/reference/current/keyword.html
-            #  See https://www.elastic.co/guide/en/elasticsearch/reference/current/text.html
-            return {"type": "text", "index": False}
-        elif settings.type in [QuestionType.label_selection, QuestionType.multi_label_selection]:
-            return {"type": "keyword"}
-        elif settings.type == QuestionType.ranking:
-            return {"type": "nested"}
-        else:
-            raise ValueError(f"ElasticSearch mappings for Question of type {settings.type} cannot be generated")
-
-    @staticmethod
-    def _es_mapping_for_field(field: Field):
-        field_type = field.settings["type"]
-
-        if field_type == FieldType.text:
-            return {"type": "text"}
-        else:
-            raise ValueError(f"ElasticSearch mappings for Field of type {field_type} cannot be generated")
-
-    async def _get_index_or_raise(self, dataset: Dataset):
-        index_name = self._index_name_for_dataset(dataset)
-        if not await self.client.indices.exists(index=index_name):
-            raise ValueError(f"Cannot access to index for dataset {dataset.id}: the specified index does not exist")
-
-        return index_name
-
-    @staticmethod
-    def _index_name_for_dataset(dataset: Dataset):
-        return f"rg.{dataset.id}"
-
-    def _response_status_filter_builder(self, status_filter: UserResponseStatusFilter) -> Optional[Dict[str, Any]]:
-        if not status_filter.statuses:
-            return None
-
-        user_response_field = f"responses.{status_filter.user.username}"
-
-        statuses = [
-            ResponseStatus(status).value for status in status_filter.statuses if status != ResponseStatusFilter.missing
-        ]
-
-        filters = []
-        if ResponseStatusFilter.missing in status_filter.statuses:
-            # See https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-exists-query.html
-            filters.append({"bool": {"must_not": {"exists": {"field": user_response_field}}}})
-
-        if statuses:
-            # See https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-terms-query.html
-            filters.append({"terms": {f"{user_response_field}.status": statuses}})
-
-        return {"bool": {"should": filters, "minimum_should_match": 1}}
-
-    async def _bulk_op(self, actions: List[Dict[str, Any]]):
+    async def _bulk_op_request(self, actions: List[Dict[str, Any]]):
         _, errors = await helpers.async_bulk(client=self.client, actions=actions, raise_on_error=False)
         if errors:
             raise RuntimeError(errors)
