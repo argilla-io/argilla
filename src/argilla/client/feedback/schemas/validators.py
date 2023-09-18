7a2e7c703bf4418600f5f304b14739473ed73263
Alvaro Bartolome <alvaro@argilla.io>
2023-09-18 16:40:32 +0200

    fix(style): add missing type-hint and docstring

--- src/argilla/client/feedback/schemas/validators.py
+++ src/argilla/client/feedback/schemas/validators.py
@@ -12,13 +12,25 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import Any, Dict, Optional
+from typing import TYPE_CHECKING, Any, Dict, Optional
+
+if TYPE_CHECKING:
+    from pydantic import BaseModel
 
 
-def title_must_have_value(cls, v: Optional[str], values: Dict[str, Any]) -> str:
+def title_must_have_value(cls: "BaseModel", v: Optional[str], values: Dict[str, Any]) -> str:
+    """Function to add as a `pydantic` validator to a field.
+
+    This function will be called when the field is being validated, and it will
+    receive the value of the field, and the values of the model being validated.
+
+    If the field `title` is not empty, then it will be returned as is. Otherwise,
+    the value of the field `name` will be capitalized and returned.
+    """
     if not v:
         # If `name` doesn't pass the regex validation, then `values` won't have it
         name = values.get("name")
         if name:
+            # If `name` has a value, then capitalize it and return it as the `title`
             return name.capitalize()
     return v
