375b23e7dadc98330a528c63ecfe0b9bb245fc2f
Alvaro Bartolome <alvaro@argilla.io>
2023-09-19 16:33:41 +0200

    fix: skip `FeedbackDatasetBase` validation in `RemoteFeedbackDatasetBase`

--- src/argilla/client/feedback/dataset/remote/base.py
+++ src/argilla/client/feedback/dataset/remote/base.py
@@ -159,7 +159,9 @@ class RemoteFeedbackDatasetBase(Generic[T], FeedbackDatasetBase):
             TypeError: if `guidelines` is not None and not a string.
             ValueError: if `guidelines` is an empty string.
         """
-        super().__init__(fields=fields, questions=questions, guidelines=guidelines)
+        self._fields = fields
+        self._questions = questions
+        self._guidelines = guidelines
 
         self._client = client  # Required to be able to use `allowed_for_roles` decorator
         self._id = id
