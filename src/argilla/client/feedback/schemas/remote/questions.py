904f3281aa831e988dc00abf5d349a40a644f395
Alvaro Bartolome <alvaro@argilla.io>
2023-09-21 17:31:57 +0200

    fix: `from_api` type-hints

--- src/argilla/client/feedback/schemas/remote/questions.py
+++ src/argilla/client/feedback/schemas/remote/questions.py
@@ -111,7 +111,7 @@ class RemoteMultiLabelQuestion(MultiLabelQuestion, RemoteSchema):
         )
 
     @classmethod
-    def from_api(cls, payload: "FeedbackQuestionModel") -> "RemoteLabelQuestion":
+    def from_api(cls, payload: "FeedbackQuestionModel") -> "RemoteMultiLabelQuestion":
         return RemoteMultiLabelQuestion(
             id=payload.id,
             name=payload.name,
@@ -133,7 +133,7 @@ class RemoteRankingQuestion(RankingQuestion, RemoteSchema):
         )
 
     @classmethod
-    def from_api(cls, payload: "FeedbackQuestionModel") -> "RemoteLabelQuestion":
+    def from_api(cls, payload: "FeedbackQuestionModel") -> "RemoteRankingQuestion":
         return RemoteRankingQuestion(
             id=payload.id,
             name=payload.name,
