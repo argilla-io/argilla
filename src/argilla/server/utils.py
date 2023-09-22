6a155f593ca015292511f4ce0ff0381c71ecb0fd
gabrielmbmb <gmartinbdev@gmail.com>
2023-09-22 16:49:22 +0200

    feat: add `parse_query_param` function for parsing params

--- src/argilla/server/utils.py
+++ src/argilla/server/utils.py
@@ -12,10 +12,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import List
+from collections import defaultdict
+from typing import Any, Callable, Dict, List, Optional, Type, TypeVar, Union
 from uuid import UUID
 
-from fastapi import HTTPException
+from fastapi import HTTPException, Query
+from pydantic import BaseModel
 
 
 def parse_uuids(uuids_str: str) -> List[UUID]:
@@ -23,3 +25,58 @@ def parse_uuids(uuids_str: str) -> List[UUID]:
         return [UUID(uuid_str) for uuid_str in uuids_str.split(",")]
     except ValueError:
         raise HTTPException(status_code=422, detail="Invalid UUID format")
+
+
+T = TypeVar("T", bound=BaseModel)
+
+
+def parse_query_param(
+    name: str, help: str, model: Optional[Type[T]] = None
+) -> Callable[[Optional[List[str]]], Union[Dict[str, Any], T, None]]:
+    """Generates a function that can be used as a FastAPI dependency (`fastapi.Depends`) and that parses the values of
+    a query parameter with the following format:
+
+        `...?param_name=key1&param_name=key2&param_name=key3:value1,value2,value3`
+
+    The values are parsed into a dictionary with the following structure:
+
+        ```python
+        {
+            "keys": ["key1", "key2"],
+            "key3": ["value1", "value2", "value3"]
+        }
+        ```
+
+    In addition, if a `pydantic.BaseModel` is provided, the dictionary is parsed into an instance of that model.
+
+    Args:
+        name: name of the query parameter.
+        help: help text for the query parameter.
+        model: optional `pydantic.BaseModel` to parse the query parameter into. Defaults to `None`.
+
+    Returns:
+        A function that parses the query parameter into a dictionary or a `pydantic.BaseModel` instance.
+    """
+
+    def _parse(
+        param_values: Optional[List[str]] = Query(None, alias=name, description=help)
+    ) -> Union[Dict[str, Any], T, None]:
+        if param_values is None:
+            return None
+
+        parsed_params = defaultdict(list)
+        for value in param_values:
+            parts = value.split(":")
+            if len(parts) == 1:
+                parsed_params["keys"].append(parts[0])
+            else:
+                key = parts[0]
+                values = parts[1].split(",")
+                parsed_params[key] = values
+
+        if model is not None:
+            return model(**parsed_params)
+
+        return parsed_params
+
+    return _parse
