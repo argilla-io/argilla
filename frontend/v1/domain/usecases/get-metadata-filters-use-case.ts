080d809dc254972cfde40e589109b028c11edd94
Damián Pumar <damianpumar@gmail.com>
2023-09-26 17:22:59 +0200

    ✨ Backend connection

--- frontend/v1/domain/usecases/get-metadata-filters-use-case.ts
+++ frontend/v1/domain/usecases/get-metadata-filters-use-case.ts
@@ -1,22 +1,6 @@
-import { metadataMocked } from "../entities/__mocks__/metadata/mock";
 import { Metadata } from "../entities/metadata/Metadata";
 import { MetadataFilter } from "../entities/metadata/MetadataFilter";
-import { useDebounce } from "~/v1/infrastructure/services/useDebounce";
 import { MetadataRepository } from "~/v1/infrastructure/repositories/MetadataRepository";
-
-export class MetadataRepositoryMocked extends MetadataRepository {
-  constructor() {
-    super(null as any);
-  }
-
-  async getMetadataFilters(_: string): Promise<any> {
-    const debounce = useDebounce(400);
-
-    await debounce.wait();
-
-    return metadataMocked;
-  }
-}
 
 export class GetMetadataFiltersUseCase {
   constructor(private readonly metadataRepository: MetadataRepository) {}
