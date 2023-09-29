1de3d93a41d2d11e241b50b9eae35007a2aaf6f2
Alvaro Bartolome <alvaro@argilla.io>
2023-09-29 14:12:35 +0200

    feat: add `generate_pydantic_schema_for_metadata`

--- src/argilla/client/feedback/utils.py
+++ src/argilla/client/feedback/utils.py
@@ -14,11 +14,12 @@
 
 from typing import TYPE_CHECKING, List, Optional, Union
 
-from pydantic import BaseModel, create_model
+from pydantic import BaseModel, Extra, create_model
 
 from argilla.client.api import active_client
 from argilla.client.feedback.constants import FIELD_TYPE_TO_PYTHON_TYPE
-from argilla.client.feedback.schemas.fields import FieldSchema
+from argilla.client.feedback.schemas.enums import MetadataPropertyTypes
+from argilla.client.feedback.schemas.types import AllowedFieldTypes, AllowedMetadataPropertyTypes
 from argilla.client.sdk.v1.datasets import api as datasets_api_v1
 from argilla.client.workspaces import Workspace
 
@@ -28,12 +29,14 @@ if TYPE_CHECKING:
     from argilla.client.sdk.v1.datasets.models import FeedbackDatasetModel
 
 
-def generate_pydantic_schema_for_fields(fields: List[FieldSchema], name: Optional[str] = "FieldsSchema") -> BaseModel:
-    """Generates a `pydantic.BaseModel` schema from a list of `FieldSchema` objects to validate
+def generate_pydantic_schema_for_fields(
+    fields: List[AllowedFieldTypes], name: Optional[str] = "FieldsSchema"
+) -> BaseModel:
+    """Generates a `pydantic.BaseModel` schema from a list of `AllowedFieldTypes` objects to validate
     the fields of a `FeedbackDataset` object before inserting them.
 
     Args:
-        fields: the list of `FieldSchema` objects to generate the schema from.
+        fields: the list of `AllowedFieldTypes` objects to generate the schema from.
         name: the name of the `pydantic.BaseModel` schema to generate. Defaults to "FieldsSchema".
 
     Returns:
@@ -44,8 +47,8 @@ def generate_pydantic_schema_for_fields(fields: List[FieldSchema], name: Optiona
         ValueError: if one of the fields has an unsupported type.
 
     Examples:
-        >>> from argilla.client.feedback.schemas import TextField
-        >>> from argilla.client.feedback.dataset import generate_pydantic_schema_for_fields
+        >>> from argilla.client.feedback.schemas.fields import TextField
+        >>> from argilla.client.feedback.utils import generate_pydantic_schema_for_fields
         >>> fields = [
         ...     TextField(name="text", required=True),
         ...     TextField(name="label", required=True),
@@ -63,6 +66,52 @@ def generate_pydantic_schema_for_fields(fields: List[FieldSchema], name: Optiona
             )
         fields_schema.update({field.name: (FIELD_TYPE_TO_PYTHON_TYPE[field.type], ... if field.required else None)})
     return create_model(name, **fields_schema)
+
+
+def generate_pydantic_schema_for_metadata(
+    metadata_properties: List[AllowedMetadataPropertyTypes], name: Optional[str] = "MetadataSchema"
+) -> BaseModel:
+    """Generates a `pydantic.BaseModel` schema from a list of `AllowedMetadataPropertyTypes` objects to validate
+    the metadata of a `FeedbackDataset` object before inserting them.
+
+    Args:
+        metadata_properties: the list of `AllowedMetadataPropertyTypes` objects to generate the schema from.
+        name: the name of the `pydantic.BaseModel` schema to generate. Defaults to "MetadataSchema".
+
+    Returns:
+        A `pydantic.BaseModel` schema to validate the metadata of a `FeedbackDataset` object before
+        inserting them.
+
+    Raises:
+        ValueError: if one of the metadata properties has an unsupported type.
+
+    Examples:
+        >>> from argilla.client.feedback.schemas.metadata import IntegerMetadataProperty
+        >>> from argilla.client.feedback.utils import generate_pydantic_schema_for_metadata
+        >>> metadata_properties = [
+        ...     IntegerMetadataProperty(name="int-metadata", min=0, max=10),
+        ...     ...,
+        ... ]
+        >>> MetadataSchema = generate_pydantic_schema_for_metadata(metadata_properties)
+        >>> MetadataSchema(int-metadata=5)
+        MetadataSchema(int-metadata=5)
+    """
+    metadata_fields, metadata_validators = {}, {}
+
+    for metadata_property in metadata_properties:
+        if metadata_property.type not in MetadataPropertyTypes:
+            raise ValueError(
+                f"Metadata property {metadata_property.name} has an unsupported type: {metadata_property.type}, for the moment only the"
+                f" following types are supported: {[arg.value for arg in MetadataPropertyTypes]}"
+            )
+        pydantic_field, pydantic_validator = metadata_property._pydantic_field_with_validator
+        metadata_fields.update(pydantic_field)
+        metadata_validators.update(pydantic_validator)
+
+    class MetadataConfig:
+        extra = Extra.ignore
+
+    return create_model(name, **metadata_fields, __validators__=metadata_validators, __config__=MetadataConfig)
 
 
 def feedback_dataset_in_argilla(
