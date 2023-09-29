c801caaf0d3a78d1311dd1b0c1e85d745cc5a8cb
leire <leire@recogn.ai>
2023-09-29 10:14:04 +0200

    refactor filter and sort button

---
+++ frontend/components/feedback-task/header/sort-filter/SortButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <FilterButton
+    button-name="Sort"
+    icon-name="sort"
+    :show-icon="!!activeSortItemsCounter"
+    :is-button-active="isButtonActive"
+  >
+    <BaseBadge
+      class="sort-button__badge"
+      v-if="activeSortItemsCounter"
+      :text="`Sort (${activeSortItemsCounter})`"
+    />
+  </FilterButton>
+</template>
+
+<script>
+import "assets/icons/sort";
+export default {
+  props: {
+    activeSortItems: {
+      type: Array,
+      default: () => [],
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    isButtonActive() {
+      return this.isActive || !!this.activeSortItemsCounter;
+    },
+    activeSortItemsCounter() {
+      return this.activeSortItems.length;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.sort-button {
+  &__badge {
+    max-width: 140px;
+    background: $black-4;
+    border: none;
+    font-weight: 500;
+    @include line-height(16px);
+  }
+}
+</style>
