96acf34dd227490fba492624170526e88d1d4b27
Damián Pumar <damianpumar@gmail.com>
2023-10-06 18:08:01 +0100

    🔥 Feature toggle

--- frontend/components/feedback-task/header/metadata-filter/useMetadataFilterViewModel.ts
+++ frontend/components/feedback-task/header/metadata-filter/useMetadataFilterViewModel.ts
@@ -2,6 +2,7 @@ import { useRoute } from "@nuxtjs/composition-api";
 import { ref } from "vue-demi";
 import { Metadata } from "~/v1/domain/entities/metadata/Metadata";
 import { MetadataFilterList } from "~/v1/domain/entities/metadata/MetadataFilter";
+import { useFeatureToggle } from "~/v1/infrastructure/services";
 import { useDebounce } from "~/v1/infrastructure/services/useDebounce";
 
 export const useMetadataFilterViewModel = ({
@@ -9,8 +10,11 @@ export const useMetadataFilterViewModel = ({
 }: {
   metadata: Metadata[];
 }) => {
+  const { getValue } = useFeatureToggle();
   const router = useRoute();
-  const debounce = useDebounce(1000);
+  const debounce = useDebounce(
+    getValue("metadata-filter-delay", "integer") ?? 1000
+  );
   const metadataFilters = ref<MetadataFilterList>(
     new MetadataFilterList(metadata)
   );
--- frontend/components/feedback-task/header/sort-filter/useSortRecords.ts
+++ frontend/components/feedback-task/header/sort-filter/useSortRecords.ts
@@ -2,10 +2,13 @@ import { useRoute } from "@nuxtjs/composition-api";
 import { ref } from "vue-demi";
 import { Metadata } from "~/v1/domain/entities/metadata/Metadata";
 import { MetadataSortList } from "~/v1/domain/entities/metadata/MetadataSort";
+import { useFeatureToggle } from "~/v1/infrastructure/services";
 import { useDebounce } from "~/v1/infrastructure/services/useDebounce";
 
 export const useSortRecords = ({ metadata }: { metadata: Metadata[] }) => {
-  const debounce = useDebounce(1500);
+  const { getValue } = useFeatureToggle();
+
+  const debounce = useDebounce(getValue("sort-delay", "integer") ?? 1500);
   const router = useRoute();
   const metadataSort = ref<MetadataSortList>(new MetadataSortList(metadata));
 
