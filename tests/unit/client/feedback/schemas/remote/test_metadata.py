66c89647529689c0186d080eda1a7d6b40cc39ac
Alvaro Bartolome <alvaro@argilla.io>
2023-10-10 13:22:58 +0200

    test(integration): `Remote{Terms,Integer,Float}MetadataProperty`

---
+++ tests/unit/client/feedback/schemas/remote/test_metadata.py
@@ -0,0 +1,241 @@
+#  Copyright 2021-present, the Recognai S.L. team.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from datetime import datetime
+from typing import Any, Dict
+from uuid import uuid4
+
+import pytest
+from argilla.client.feedback.schemas.enums import MetadataPropertyTypes
+from argilla.client.feedback.schemas.metadata import (
+    FloatMetadataProperty,
+    IntegerMetadataProperty,
+    TermsMetadataProperty,
+)
+from argilla.client.feedback.schemas.remote.metadata import (
+    RemoteFloatMetadataProperty,
+    RemoteIntegerMetadataProperty,
+    RemoteTermsMetadataProperty,
+)
+from argilla.client.sdk.v1.datasets.models import FeedbackMetadataPropertyModel
+
+
+@pytest.mark.parametrize(
+    "schema_kwargs, server_payload",
+    [
+        (
+            {"name": "a"},
+            {"name": "a", "description": None, "settings": {"type": "terms"}},
+        ),
+        (
+            {"name": "a", "description": "b"},
+            {"name": "a", "description": "b", "settings": {"type": "terms"}},
+        ),
+        (
+            {"name": "a", "values": ["a"]},
+            {"name": "a", "description": None, "settings": {"type": "terms", "values": ["a"]}},
+        ),
+        (
+            {"name": "a", "values": ["a", "b", "c"]},
+            {"name": "a", "description": None, "settings": {"type": "terms", "values": ["a", "b", "c"]}},
+        ),
+    ],
+)
+def test_remote_terms_metadata_property(schema_kwargs: Dict[str, Any], server_payload: Dict[str, Any]) -> None:
+    text_field = RemoteTermsMetadataProperty(**schema_kwargs)
+    assert text_field.type == MetadataPropertyTypes.terms
+    assert text_field.server_settings == server_payload["settings"]
+    assert text_field.to_server_payload() == server_payload
+
+    local_text_field = text_field.to_local()
+    assert isinstance(local_text_field, TermsMetadataProperty)
+    assert local_text_field.type == MetadataPropertyTypes.terms
+    assert local_text_field.server_settings == server_payload["settings"]
+    assert local_text_field.to_server_payload() == server_payload
+
+
+@pytest.mark.parametrize(
+    "payload",
+    [
+        FeedbackMetadataPropertyModel(
+            id=uuid4(),
+            name="a",
+            description="A",
+            settings={"type": "terms"},
+            inserted_at=datetime.now(),
+            updated_at=datetime.now(),
+        ),
+        FeedbackMetadataPropertyModel(
+            id=uuid4(),
+            name="b",
+            description=None,
+            settings={"type": "terms", "values": ["a", "b", "c"]},
+            inserted_at=datetime.now(),
+            updated_at=datetime.now(),
+        ),
+    ],
+)
+def test_remote_terms_metadata_property_from_api(payload: FeedbackMetadataPropertyModel) -> None:
+    text_field = RemoteTermsMetadataProperty.from_api(payload)
+    assert text_field.type == MetadataPropertyTypes.terms
+    assert text_field.server_settings == payload.settings
+    assert text_field.to_server_payload() == payload.dict(exclude={"id", "inserted_at", "updated_at"})
+
+
+@pytest.mark.parametrize(
+    "schema_kwargs, server_payload",
+    [
+        (
+            {"name": "a"},
+            {"name": "a", "description": None, "settings": {"type": "integer"}},
+        ),
+        (
+            {"name": "a", "description": "b"},
+            {"name": "a", "description": "b", "settings": {"type": "integer"}},
+        ),
+        (
+            {"name": "a", "min": 0},
+            {"name": "a", "description": None, "settings": {"type": "integer", "min": 0}},
+        ),
+        (
+            {"name": "a", "max": 10},
+            {"name": "a", "description": None, "settings": {"type": "integer", "max": 10}},
+        ),
+        (
+            {"name": "a", "min": 0, "max": 10},
+            {"name": "a", "description": None, "settings": {"type": "integer", "min": 0, "max": 10}},
+        ),
+    ],
+)
+def test_remote_integer_metadata_property(schema_kwargs: Dict[str, Any], server_payload: Dict[str, Any]) -> None:
+    text_field = RemoteIntegerMetadataProperty(**schema_kwargs)
+    assert text_field.type == MetadataPropertyTypes.integer
+    assert text_field.server_settings == server_payload["settings"]
+    assert text_field.to_server_payload() == server_payload
+
+    local_text_field = text_field.to_local()
+    assert isinstance(local_text_field, IntegerMetadataProperty)
+    assert local_text_field.type == MetadataPropertyTypes.integer
+    assert local_text_field.server_settings == server_payload["settings"]
+    assert local_text_field.to_server_payload() == server_payload
+
+
+@pytest.mark.parametrize(
+    "payload",
+    [
+        FeedbackMetadataPropertyModel(
+            id=uuid4(),
+            name="a",
+            description="A",
+            settings={"type": "integer"},
+            inserted_at=datetime.now(),
+            updated_at=datetime.now(),
+        ),
+        FeedbackMetadataPropertyModel(
+            id=uuid4(),
+            name="b",
+            description=None,
+            settings={"type": "integer", "min": 0},
+            inserted_at=datetime.now(),
+            updated_at=datetime.now(),
+        ),
+        FeedbackMetadataPropertyModel(
+            id=uuid4(),
+            name="c",
+            description=None,
+            settings={"type": "integer", "min": 0, "max": 10},
+            inserted_at=datetime.now(),
+            updated_at=datetime.now(),
+        ),
+    ],
+)
+def test_remote_integer_metadata_property_from_api(payload: FeedbackMetadataPropertyModel) -> None:
+    text_field = RemoteIntegerMetadataProperty.from_api(payload)
+    assert text_field.type == MetadataPropertyTypes.integer
+    assert text_field.server_settings == payload.settings
+    assert text_field.to_server_payload() == payload.dict(exclude={"id", "inserted_at", "updated_at"})
+
+
+@pytest.mark.parametrize(
+    "schema_kwargs, server_payload",
+    [
+        (
+            {"name": "a"},
+            {"name": "a", "description": None, "settings": {"type": "float"}},
+        ),
+        (
+            {"name": "a", "description": "b"},
+            {"name": "a", "description": "b", "settings": {"type": "float"}},
+        ),
+        (
+            {"name": "a", "min": 0.0},
+            {"name": "a", "description": None, "settings": {"type": "float", "min": 0.0}},
+        ),
+        (
+            {"name": "a", "max": 10.0},
+            {"name": "a", "description": None, "settings": {"type": "float", "max": 10.0}},
+        ),
+        (
+            {"name": "a", "min": 0.0, "max": 10.0},
+            {"name": "a", "description": None, "settings": {"type": "float", "min": 0.0, "max": 10.0}},
+        ),
+    ],
+)
+def test_remote_float_metadata_property(schema_kwargs: Dict[str, Any], server_payload: Dict[str, Any]) -> None:
+    text_field = RemoteFloatMetadataProperty(**schema_kwargs)
+    assert text_field.type == MetadataPropertyTypes.float
+    assert text_field.server_settings == server_payload["settings"]
+    assert text_field.to_server_payload() == server_payload
+
+    local_text_field = text_field.to_local()
+    assert isinstance(local_text_field, FloatMetadataProperty)
+    assert local_text_field.type == MetadataPropertyTypes.float
+    assert local_text_field.server_settings == server_payload["settings"]
+    assert local_text_field.to_server_payload() == server_payload
+
+
+@pytest.mark.parametrize(
+    "payload",
+    [
+        FeedbackMetadataPropertyModel(
+            id=uuid4(),
+            name="a",
+            description="A",
+            settings={"type": "float"},
+            inserted_at=datetime.now(),
+            updated_at=datetime.now(),
+        ),
+        FeedbackMetadataPropertyModel(
+            id=uuid4(),
+            name="b",
+            description=None,
+            settings={"type": "float", "min": 0.0},
+            inserted_at=datetime.now(),
+            updated_at=datetime.now(),
+        ),
+        FeedbackMetadataPropertyModel(
+            id=uuid4(),
+            name="c",
+            description=None,
+            settings={"type": "float", "min": 0.0, "max": 10.0},
+            inserted_at=datetime.now(),
+            updated_at=datetime.now(),
+        ),
+    ],
+)
+def test_remote_float_metadata_property_from_api(payload: FeedbackMetadataPropertyModel) -> None:
+    text_field = RemoteFloatMetadataProperty.from_api(payload)
+    assert text_field.type == MetadataPropertyTypes.float
+    assert text_field.server_settings == payload.settings
+    assert text_field.to_server_payload() == payload.dict(exclude={"id", "inserted_at", "updated_at"})
