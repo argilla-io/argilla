aacc0f0416cf0afac7d913b78641d3677b745d28
Damián Pumar <damianpumar@gmail.com>
2023-10-16 11:13:50 +0200

    🚀 Performance up

--- frontend/pages/dataset/_id/annotation-mode/index.vue
+++ frontend/pages/dataset/_id/annotation-mode/index.vue
@@ -1,50 +1,52 @@
 <template>
-  <BaseLoading v-if="isLoadingDataset" />
-  <HeaderAndTopAndOneColumn v-else>
-    <template v-slot:header>
-      <HeaderFeedbackTaskComponent
-        :key="datasetId"
-        :datasetId="datasetId"
-        :breadcrumbs="breadcrumbs"
-        :showTrainButton="true"
-        @on-click-train="showTrainModal(true)"
-      />
-      <BaseModal
-        :modal-custom="true"
-        :prevent-body-scroll="true"
-        modal-class="modal-auto"
-        modal-position="modal-top-center"
-        :modal-visible="visibleTrainModal"
-        allow-close
-        @close-modal="showTrainModal(false)"
-      >
-        <DatasetTrainComponent
-          datasetTask="FeedbackTask"
-          :datasetName="dataset.name"
-          :workspaceName="dataset.workspace"
+  <div>
+    <BaseLoading v-if="isLoadingDataset" />
+    <HeaderAndTopAndOneColumn>
+      <template v-slot:header>
+        <HeaderFeedbackTaskComponent
+          :key="datasetId"
+          :datasetId="datasetId"
+          :breadcrumbs="breadcrumbs"
+          :showTrainButton="true"
+          @on-click-train="showTrainModal(true)"
         />
-      </BaseModal>
-    </template>
-    <template v-slot:sidebar-right>
-      <SidebarFeedbackTaskComponent
-        :datasetId="datasetId"
-        @refresh="loadDataset()"
-      />
-    </template>
-    <template v-slot:top>
-      <section class="dataset__top-area">
-        <DatasetFiltersComponent :recordCriteria="recordCriteria" />
-      </section>
-    </template>
-    <template v-slot:center>
-      <RecordFeedbackTaskAndQuestionnaireContent
-        :recordCriteria="recordCriteria"
-      />
-    </template>
-    <template v-slot:footer>
-      <PaginationFeedbackTaskComponent :recordCriteria="recordCriteria" />
-    </template>
-  </HeaderAndTopAndOneColumn>
+        <BaseModal
+          :modal-custom="true"
+          :prevent-body-scroll="true"
+          modal-class="modal-auto"
+          modal-position="modal-top-center"
+          :modal-visible="visibleTrainModal"
+          allow-close
+          @close-modal="showTrainModal(false)"
+        >
+          <DatasetTrainComponent
+            datasetTask="FeedbackTask"
+            :datasetName="dataset.name"
+            :workspaceName="dataset.workspace"
+          />
+        </BaseModal>
+      </template>
+      <template v-slot:sidebar-right>
+        <SidebarFeedbackTaskComponent
+          :datasetId="datasetId"
+          @refresh="loadDataset()"
+        />
+      </template>
+      <template v-slot:top>
+        <section class="dataset__top-area">
+          <DatasetFiltersComponent :recordCriteria="recordCriteria" />
+        </section>
+      </template>
+      <template v-slot:center>
+        <RecordFeedbackTaskAndQuestionnaireContent
+          :recordCriteria="recordCriteria"
+        />
+      </template>
+      <template v-slot:footer>
+        <PaginationFeedbackTaskComponent :recordCriteria="recordCriteria" />
+      </template>
+    </HeaderAndTopAndOneColumn>
+  </div>
 </template>
 
 <script>
