477d13c58d87e853fce5ee0b8e85531a21ab4072
Alvaro Bartolome <alvaro@argilla.io>
2023-10-17 15:52:15 +0200

    fix(style): clear imports and remove unused

--- src/argilla/client/feedback/dataset/base.py
+++ src/argilla/client/feedback/dataset/base.py
@@ -19,13 +19,9 @@ from typing import TYPE_CHECKING, Any, Dict, Generic, Iterable, List, Literal, O
 from pydantic import ValidationError
 
 from argilla.client.feedback.integrations.huggingface import HuggingFaceDatasetMixin
-from argilla.client.feedback.schemas import (
-    FeedbackRecord,
-    FieldSchema,
-    SortBy,
-)
 from argilla.client.feedback.schemas.enums import ResponseStatusFilter
 from argilla.client.feedback.schemas.metadata import MetadataFilters
+from argilla.client.feedback.schemas.records import FeedbackRecord, SortBy
 from argilla.client.feedback.schemas.types import AllowedFieldTypes, AllowedMetadataPropertyTypes, AllowedQuestionTypes
 from argilla.client.feedback.training.schemas import (
     TrainingTaskForChatCompletion,
@@ -40,7 +36,6 @@ from argilla.client.feedback.training.schemas import (
 )
 from argilla.client.feedback.utils import generate_pydantic_schema_for_fields, generate_pydantic_schema_for_metadata
 from argilla.client.models import Framework
-from argilla.client.sdk.v1.datasets.models import FeedbackResponseStatusFilter
 from argilla.utils.dependency import require_dependencies, requires_dependencies
 
 if TYPE_CHECKING:
