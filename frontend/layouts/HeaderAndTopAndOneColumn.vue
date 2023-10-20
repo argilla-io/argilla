10650d44c5605d6efeabbad8c4d8929509546007
leire <leire@recogn.ai>
2023-10-20 15:24:42 +0200

    update styles for layout

--- frontend/layouts/HeaderAndTopAndOneColumn.vue
+++ frontend/layouts/HeaderAndTopAndOneColumn.vue
@@ -6,10 +6,10 @@
     <div class="sidebar-area">
       <slot name="sidebar-right">here is the sidebar content left</slot>
     </div>
-    <div class="top-area">
+    <div class="top-area container">
       <slot name="top">here is the top content</slot>
     </div>
-    <div class="center-area">
+    <div class="center-area container">
       <slot name="center">here is the center content</slot>
     </div>
     <div class="footer-area">
@@ -46,7 +46,7 @@ export default {
 $gap-width: $base-space * 2;
 .layout {
   display: grid;
-  grid-template-columns: $gap-width 1fr $gap-width $sidebarMenuWidth;
+  grid-template-columns: 1fr $sidebarMenuWidth;
   grid-template-rows: auto auto minmax(0, 1fr) $base-space * 2 auto;
   grid-column-gap: 0px;
   grid-row-gap: 0px;
@@ -61,25 +61,27 @@ $gap-width: $base-space * 2;
 }
 
 .header-area {
-  grid-area: 1 / 1 / 2 / 6;
-}
-.empty-content-left {
-  grid-area: 2 / 1 / 4 / 2;
+  grid-area: 1 / 1 / 2 / 3;
 }
 .footer-area {
-  grid-area: 5 / 1 / 5 / 4;
+  grid-area: 5 / 1 / 5 / 2;
 }
 .sidebar-area {
-  grid-area: 2 / 4 / 5 / 5;
-}
-.empty-content-right {
-  grid-area: 2 / 3 / 4 / 4;
+  grid-area: 2 / 2 / 5 / 3;
 }
 .top-area {
-  grid-area: 2 / 2 / 3 / 3;
+  grid-area: 2 / 1 / 3 / 2;
 }
 .center-area {
-  grid-area: 3 / 2 / 4 / 3;
+  grid-area: 3 / 1 / 4 / 2;
   min-width: 0;
+}
+.container {
+  max-width: min(calc(100% - $base-space * 8), 1700px);
+  width: 100%;
+  margin: 0 auto;
+  @include media("<desktopLarge") {
+    max-width: min(calc(100% - $base-space * 4), 1700px);
+  }
 }
 </style>
