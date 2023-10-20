d2922abb9d4f45c46b8ef70573c2531701701fa4
Paco Aranda <francis@argilla.io>
2023-10-20 11:10:01 +0200

    refactor: Define validate_* helpers

--- src/argilla/client/feedback/dataset/helpers.py
+++ src/argilla/client/feedback/dataset/helpers.py
@@ -14,8 +14,77 @@
 
 import typing
 
+from argilla.client.feedback.schemas.types import AllowedFieldTypes, AllowedMetadataPropertyTypes, AllowedQuestionTypes
+
 if typing.TYPE_CHECKING:
     from argilla.client.feedback.dataset.base import FeedbackDatasetBase
+
+
+def validate_fields(fields: typing.Union[AllowedFieldTypes, typing.List[AllowedFieldTypes]]):
+    """Validates that the fields used in the filters are valid."""
+    if not isinstance(fields, list):
+        raise TypeError(f"Expected `fields` to be a list, got {type(fields)} instead.")
+
+    any_required = False
+    unique_names = set()
+    for field in fields:
+        if not isinstance(field, AllowedFieldTypes):
+            raise TypeError(
+                f"Expected `fields` to be a list of `{AllowedFieldTypes.__name__}`, got {type(field)} instead."
+            )
+        if field.name in unique_names:
+            raise ValueError(f"Expected `fields` to have unique names, got {field.name} twice instead.")
+        unique_names.add(field.name)
+        if not any_required and field.required:
+            any_required = True
+
+    if not any_required:
+        raise ValueError("At least one field in `fields` must be required (`required=True`).")
+
+
+def validate_questions(questions: typing.Union[AllowedQuestionTypes, typing.List[AllowedQuestionTypes]]) -> None:
+    """Validates that the questions used in the filters are valid."""
+    if not isinstance(questions, list):
+        raise TypeError(f"Expected `questions` to be a list, got {type(questions)} instead.")
+
+    any_required = False
+    unique_names = set()
+    for question in questions:
+        if not isinstance(question, AllowedQuestionTypes.__args__):
+            raise TypeError(
+                "Expected `questions` to be a list of"
+                f" `{'`, `'.join([arg.__name__ for arg in AllowedQuestionTypes.__args__])}` got a"
+                f" question in the list with type {type(question)} instead."
+            )
+        if question.name in unique_names:
+            raise ValueError(f"Expected `questions` to have unique names, got {question.name} twice instead.")
+        unique_names.add(question.name)
+        if not any_required and question.required:
+            any_required = True
+
+    if not any_required:
+        raise ValueError("At least one question in `questions` must be required (`required=True`).")
+
+
+def validate_metadata_properties(metadata_properties: typing.List[AllowedMetadataPropertyTypes]) -> None:
+    """Validates that the metadata properties used in the filters are valid."""
+
+    if not metadata_properties:
+        return
+
+    unique_names = set()
+    for metadata_property in metadata_properties:
+        if not isinstance(metadata_property, AllowedMetadataPropertyTypes.__args__):
+            raise TypeError(
+                f"Expected `metadata_properties` to be a list of"
+                f" `{'`, `'.join([arg.__name__ for arg in AllowedMetadataPropertyTypes.__args__])}` got a"
+                f" metadata property in the list with type type {type(metadata_property)} instead."
+            )
+        if metadata_property.name in unique_names:
+            raise ValueError(
+                f"Expected `metadata_properties` to have unique names, got '{metadata_property.name}' twice instead."
+            )
+        unique_names.add(metadata_property.name)
 
 
 def validate_metadata_names(dataset: "FeedbackDatasetBase", names: typing.List[str]) -> None:
