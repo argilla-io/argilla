0a47edfb64fdd08dacb5576d1c7d675c470e159f
agustin <agustin.piqueres@gmail.com>
2023-10-25 14:13:02 +0200

    feat: add tests for frameworks that don't implement the functionality

--- tests/integration/client/feedback/training/test_openai.py
+++ tests/integration/client/feedback/training/test_openai.py
@@ -17,6 +17,7 @@ import re
 import argilla as rg
 import pytest
 from argilla.client.feedback.training.schemas import TrainingTaskForChatCompletionFormat
+from argilla.feedback import ArgillaTrainer, FeedbackDataset, TrainingTask
 
 
 def test_training_task_for_chat_completion(mocked_openai):
@@ -78,3 +79,38 @@ def test_training_task_for_chat_completion(mocked_openai):
         framework="openai",
     )
     trainer.train("mock")
+
+
+def test_push_to_huggingface(mocked_openai, mocked_is_on_huggingface):
+    dataset = FeedbackDataset.from_huggingface("argilla/customer_assistant")
+    # adapation from LlamaIndex's TEXT_QA_PROMPT_TMPL_MSGS[1].content
+    user_message_prompt = """Context information is below.
+    ---------------------
+    {context_str}
+    ---------------------
+    Given the context information and not prior knowledge but keeping your Argilla Cloud assistant style, answer the query.
+    Query: {query_str}
+    Answer:
+    """
+    # adapation from LlamaIndex's TEXT_QA_SYSTEM_PROMPT
+    system_prompt = """You are an expert customer service assistant for the Argilla Cloud product that is trusted around the world."""
+
+    def formatting_func(sample: dict):
+        from uuid import uuid4
+
+        if sample["response"]:
+            chat = str(uuid4())
+            user_message = user_message_prompt.format(context_str=sample["context"], query_str=sample["user-message"])
+            return [
+                (chat, "0", "system", system_prompt),
+                (chat, "1", "user", user_message),
+                (chat, "2", "assistant", sample["response"][0]["value"]),
+            ]
+        else:
+            return None
+
+    task = TrainingTask.for_chat_completion(formatting_func=formatting_func)
+    trainer = ArgillaTrainer(dataset=dataset, task=task, framework="openai")
+    repo_id = "test/model"
+    with pytest.raises(NotImplementedError, match="This method is not implemented for `ArgillaOpenAITrainer`."):
+        trainer.push_to_huggingface(repo_id, generate_card=True)
--- tests/integration/client/feedback/training/test_sentence_transformers.py
+++ tests/integration/client/feedback/training/test_sentence_transformers.py
@@ -326,86 +326,88 @@ def test_push_to_huggingface(
     feedback_dataset_records: List[FeedbackRecord],
     mocked_is_on_huggingface,
 ) -> None:
-    raise NotImplementedError(
-        "This framework is not implemented yet. Cross-Encoder moodels don't implement the functionality"
-        " for pushing a model to huggingface, and SentenceTransformer models have the functionality"
-        " but is outdated and doesn't work with the current versions of 'huggingface-hub'."
-        " The present test is let here for the future, when we either implement the functionality"
-        " in 'argilla', or to 'sentence-transformers'."
-    )
-    # The token will be grabbed internally, but fail and warn soon if the user has no token available
-    token = HfFolder.get_token()
-    if token is None:
-        raise ValueError("No token available, please set it with the following env var name: 'HUGGING_FACE_HUB_TOKEN'")
+    with pytest.raises(NotImplementedError, match="sentence-transformers doesn't implement this functionality yet."):
+        # This framework is not implemented yet. Cross-Encoder models don't implement the functionality
+        # for pushing a model to huggingface, and SentenceTransformer models have the functionality
+        # but is outdated and doesn't work with the current versions of 'huggingface-hub'.
+        # The present test is let here for the future, when we either implement the functionality
+        # in 'argilla', or to 'sentence-transformers'.
 
-    hf_api = HfApi()
+        # The token will be grabbed internally, but fail and warn soon if the user has no token available
+        token = HfFolder.get_token()
+        if token is None:
+            raise ValueError(
+                "No token available, please set it with the following env var name: 'HUGGING_FACE_HUB_TOKEN'"
+            )
 
-    dataset = FeedbackDataset(
-        guidelines=feedback_dataset_guidelines,
-        fields=feedback_dataset_fields,
-        questions=feedback_dataset_questions,
-    )
-    dataset.add_records(records=feedback_dataset_records * 2)
+        hf_api = HfApi()
 
-    def formatting_func(sample):
-        labels = [
-            annotation["value"]
-            for annotation in sample["question-3"]
-            if annotation["status"] == "submitted" and annotation["value"] is not None
-        ]
-        if labels:
-            # Three cases for the tests: None, one tuple and yielding multiple tuples
-            if labels[0] == "a":
-                return None
-            elif labels[0] == "b":
-                return {"sentence-1": sample["text"], "sentence-2": sample["text"], "label": 1}
-            elif labels[0] == "c":
-                return [
-                    {"sentence-1": sample["text"], "sentence-2": sample["text"], "label": 1},
-                    {"sentence-1": sample["text"], "sentence-2": sample["text"], "label": 0},
-                ]
+        dataset = FeedbackDataset(
+            guidelines=feedback_dataset_guidelines,
+            fields=feedback_dataset_fields,
+            questions=feedback_dataset_questions,
+        )
+        dataset.add_records(records=feedback_dataset_records * 2)
 
-    task = TrainingTask.for_sentence_similarity(formatting_func=formatting_func)
+        def formatting_func(sample):
+            labels = [
+                annotation["value"]
+                for annotation in sample["question-3"]
+                if annotation["status"] == "submitted" and annotation["value"] is not None
+            ]
+            if labels:
+                # Three cases for the tests: None, one tuple and yielding multiple tuples
+                if labels[0] == "a":
+                    return None
+                elif labels[0] == "b":
+                    return {"sentence-1": sample["text"], "sentence-2": sample["text"], "label": 1}
+                elif labels[0] == "c":
+                    return [
+                        {"sentence-1": sample["text"], "sentence-2": sample["text"], "label": 1},
+                        {"sentence-1": sample["text"], "sentence-2": sample["text"], "label": 0},
+                    ]
 
-    model = "all-MiniLM-L6-v2"
+        task = TrainingTask.for_sentence_similarity(formatting_func=formatting_func)
 
-    trainer = ArgillaTrainer(dataset=dataset, task=task, framework=__FRAMEWORK__, model=model)
+        model = "all-MiniLM-L6-v2"
 
-    trainer.update_config(max_steps=1)
+        trainer = ArgillaTrainer(dataset=dataset, task=task, framework=__FRAMEWORK__, model=model)
 
-    # NOTE: This is just to test locally, we need a better solution for the CI.
-    username = "plaguss"
-    model_name = "test_model"
-    repo_id = f"{username}/{model_name}"
-    # Filename to check on huggingface
-    filename = "config.json"
+        trainer.update_config(max_steps=1)
 
-    train_with_cleanup(trainer, __OUTPUT_DIR__)
+        # NOTE: This is just to test locally, we need a better solution for the CI.
+        username = "plaguss"
+        model_name = "test_model"
+        repo_id = f"{username}/{model_name}"
+        # Filename to check on huggingface
+        filename = "config.json"
 
-    trainer.push_to_huggingface(repo_id, generate_card=True)
+        train_with_cleanup(trainer, __OUTPUT_DIR__)
 
-    # Check the repo is created, the same check done at:
-    # https://github.com/huggingface/huggingface_hub/blob/v0.18.0.rc0/tests/test_hubmixin.py#L154
-    model_info = hf_api.model_info(repo_id)
-    assert model_info.modelId == repo_id
+        trainer.push_to_huggingface(repo_id, generate_card=True)
 
-    tmp_config_path = hf_hub_download(
-        repo_id=repo_id,
-        filename=filename,
-        use_auth_token=token,
-    )
+        # Check the repo is created, the same check done at:
+        # https://github.com/huggingface/huggingface_hub/blob/v0.18.0.rc0/tests/test_hubmixin.py#L154
+        model_info = hf_api.model_info(repo_id)
+        assert model_info.modelId == repo_id
 
-    with open(tmp_config_path) as f:
-        conf = json.load(f)
-        assert isinstance(conf, dict)
-        assert len(conf) > 0
+        tmp_config_path = hf_hub_download(
+            repo_id=repo_id,
+            filename=filename,
+            use_auth_token=token,
+        )
 
-    # No need to test this file, if the download succeeds its working
-    tmp_readme_path = hf_hub_download(
-        repo_id=repo_id,
-        filename="README.md",
-        use_auth_token=token,
-    )
+        with open(tmp_config_path) as f:
+            conf = json.load(f)
+            assert isinstance(conf, dict)
+            assert len(conf) > 0
 
-    # Delete repo
-    hf_api.delete_repo(repo_id=repo_id)
+        # No need to test this file, if the download succeeds its working
+        tmp_readme_path = hf_hub_download(
+            repo_id=repo_id,
+            filename="README.md",
+            use_auth_token=token,
+        )
+
+        # Delete repo
+        hf_api.delete_repo(repo_id=repo_id)
