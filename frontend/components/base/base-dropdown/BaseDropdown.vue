a17dc0087d5f0a90ab0db59ce6d1a524abd136d5
leire <leire@recogn.ai>
2023-10-27 14:01:19 +0200

    include boundary option to base dropdown

--- frontend/components/base/base-dropdown/BaseDropdown.vue
+++ frontend/components/base/base-dropdown/BaseDropdown.vue
@@ -16,12 +16,22 @@
   -->
 
 <template>
-  <div v-click-outside="onClickOutside" class="dropdown">
+  <div v-click-outside="onClickOutside" ref="dropdown" class="dropdown">
     <div class="dropdown__header" @click="onClick">
       <slot name="dropdown-header" />
     </div>
     <transition appear name="fade">
-      <div v-if="visible" class="dropdown__content">
+      <div
+        v-if="visible"
+        class="dropdown__content"
+        :style="
+          isViewportBoundary && {
+            top: `${dropdownTop}px`,
+            left: `${dropdownLeft}px`,
+            position: 'fixed',
+          }
+        "
+      >
         <slot name="dropdown-content" />
       </div>
     </transition>
@@ -35,8 +45,28 @@ export default {
   },
   props: {
     visible: {
-      default: false,
       type: Boolean,
+      default: false,
+    },
+    boundary: {
+      type: String,
+      default: "parent",
+      validator: (value) => ["parent", "viewport"].includes(value),
+    },
+    gap: {
+      type: Number,
+      default: 8,
+    },
+  },
+  data() {
+    return {
+      dropdownTop: null,
+      dropdownLeft: null,
+    };
+  },
+  computed: {
+    isViewportBoundary() {
+      return this.boundary === "viewport";
     },
   },
   methods: {
@@ -45,7 +75,29 @@ export default {
     },
     onClick() {
       this.$emit("visibility", !this.visible);
+      if (this.isViewportBoundary) {
+        this.setViewportPosition();
+      }
     },
+    setViewportPosition() {
+      return this.$nextTick(() => {
+        const { top, left, height } =
+          this.$refs.dropdown?.getBoundingClientRect();
+        this.dropdownTop = top + height + this.gap;
+        this.dropdownLeft = left;
+      });
+    },
+  },
+  created() {
+    if (this.isViewportBoundary) {
+      this.setViewportPosition();
+      window.addEventListener("resize", this.setViewportPosition);
+    }
+  },
+  beforeDestroy() {
+    if (this.isViewportBoundary) {
+      window.removeEventListener("resize", this.setViewportPosition);
+    }
   },
 };
 </script>
@@ -71,8 +123,8 @@ export default {
   }
   &__content {
     position: absolute;
-    top: calc(100% + 10px);
-    right: 0;
+    top: calc(100% + 8px);
+    left: 0;
     z-index: 3;
     transform: translate(0);
     box-shadow: $shadow;
