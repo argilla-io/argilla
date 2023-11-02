d4aa9d6f9a2f5a22a26bccf74a856f267b23df56
Paco Aranda <francis@argilla.io>
2023-11-02 16:56:54 +0100

    tests: Adding more tests

---
+++ tests/integration/client/feedback/dataset/remote/test_find_similar.py
@@ -0,0 +1,117 @@
+import pytest
+import argilla as rg
+
+from argilla import User, Workspace
+from argilla.feedback import (
+    FeedbackDataset,
+    FeedbackRecord,
+    TextField,
+    TextQuestion,
+    VectorSettings,
+    ResponseSchema,
+)
+
+
+@pytest.fixture
+def feedback_dataset() -> FeedbackDataset:
+    return FeedbackDataset(fields=[TextField(name="text")], questions=[TextQuestion(name="question")])
+
+
+class TestSuiteFindSimilar:
+    def tests_find_similar_records_using_value(self, owner: User, feedback_dataset: FeedbackDataset):
+        rg.init(api_key=owner.api_key)
+        workspace = Workspace.create(name="test")
+
+        feedback_dataset.add_vector_settings(VectorSettings(name="vector", dimensions=2))
+
+        feedback_dataset.add_records(
+            [
+                FeedbackRecord(external_id="0", fields={"text": "hello"}, vectors={"vector": [1, 1]}),
+                FeedbackRecord(external_id="1", fields={"text": "hello"}, vectors={"vector": [2, 2]}),
+                FeedbackRecord(external_id="2", fields={"text": "hello"}, vectors={"vector": [4, 4]}),
+            ]
+        )
+
+        remote = feedback_dataset.push_to_argilla("test_find_similar_records", workspace=workspace)
+
+        records_with_scores = remote.find_similar_records(vector_name="vector", value=[1, 1], max_results=2)
+        assert len(records_with_scores) == 2
+
+        record, score = records_with_scores[0]
+        assert record.external_id == "0"
+        assert score == 1.0
+
+        record, score = records_with_scores[1]
+        assert record.external_id == "1"
+        assert score < 1.0
+
+    def test_find_similar_records_using_record(self, owner: User, feedback_dataset: FeedbackDataset):
+        rg.init(api_key=owner.api_key)
+        workspace = Workspace.create(name="test")
+
+        feedback_dataset.add_vector_settings(VectorSettings(name="vector", dimensions=2))
+
+        feedback_dataset.add_records(
+            [
+                FeedbackRecord(external_id="0", fields={"text": "hello"}, vectors={"vector": [1, 1]}),
+                FeedbackRecord(external_id="1", fields={"text": "hello"}, vectors={"vector": [2, 2]}),
+                FeedbackRecord(external_id="2", fields={"text": "hello"}, vectors={"vector": [4, 4]}),
+            ]
+        )
+
+        remote = feedback_dataset.push_to_argilla("test_find_similar_records", workspace=workspace)
+
+        reference_record = remote[0]
+
+        records_with_scores = remote.find_similar_records(vector_name="vector", record=reference_record, max_results=2)
+        assert len(records_with_scores) == 2
+
+        record, score = records_with_scores[0]
+        assert record.external_id == "1"
+        assert score < 1.0
+
+        record, score = records_with_scores[1]
+        assert record.external_id == "2"
+        assert score < 1.0
+
+    def test_find_similar_combining_filters(self, owner: User, feedback_dataset: FeedbackDataset):
+        rg.init(api_key=owner.api_key)
+        workspace = Workspace.create(name="test")
+
+        feedback_dataset.add_vector_settings(VectorSettings(name="vector", dimensions=2))
+
+        feedback_dataset.add_records(
+            [
+                FeedbackRecord(external_id="0", fields={"text": "hello"}, vectors={"vector": [1, 1]}),
+                FeedbackRecord(
+                    external_id="1",
+                    fields={"text": "hello"},
+                    vectors={"vector": [2, 2]},
+                    responses=[ResponseSchema(status="discarded")],
+                ),
+                FeedbackRecord(
+                    external_id="2",
+                    fields={"text": "hello"},
+                    vectors={"vector": [4, 4]},
+                    responses=[ResponseSchema(status="submitted", values={"question": {"value": "answer"}})],
+                ),
+            ]
+        )
+
+        remote = feedback_dataset.push_to_argilla("test_find_similar_records", workspace=workspace)
+        only_submitted_and_discarded_records = remote.filter_by(response_status=["submitted", "discarded"])
+
+        records_with_scores = only_submitted_and_discarded_records.find_similar_records(
+            vector_name="vector",
+            value=[1, 1],
+            max_results=3,
+        )
+        assert len(records_with_scores) == 2
+
+        record, score = records_with_scores[0]
+        assert record.external_id == "1"
+        assert score < 1.0
+
+        record, score = records_with_scores[1]
+        assert record.external_id == "2"
+        assert score < 1.0
--- tests/unit/client/sdk/v1/test_datasets.py
+++ tests/unit/client/sdk/v1/test_datasets.py
@@ -16,13 +16,13 @@ from datetime import datetime
 from uuid import UUID, uuid4
 
 import httpx
+import pytest
+
 from argilla.client.sdk.v1.datasets import api
 from argilla.client.sdk.v1.datasets.models import (
     FeedbackItemModel,
     FeedbackRecordSearchModel,
     FeedbackRecordsSearchModel,
-    FeedbackRecordsSearchQuery,
-    FeedbackRecordsSearchTextQuery,
     FeedbackRecordsSearchVectorQuery,
 )
 
@@ -63,12 +63,10 @@ def test_search_records(mock_httpx_client: httpx.Client) -> None:
     mock_httpx_client.post.return_value = httpx.Response(status_code=200, json=json_response)
 
     dataset_id = uuid4()
-    query = FeedbackRecordsSearchQuery(
-        text=FeedbackRecordsSearchTextQuery(q="text query", field="field_name"),
-        vector=FeedbackRecordsSearchVectorQuery(name="vector_name", value=[1.0, 2.0, 3.0]),
-    )
 
-    response = api.search_records(client=mock_httpx_client, id=dataset_id, query=query)
+    vector_query = FeedbackRecordsSearchVectorQuery(name="vector_name", value=[1.0, 2.0, 3.0])
+
+    response = api.search_records(client=mock_httpx_client, id=dataset_id, vector_query=vector_query)
 
     assert response.status_code == 200
     assert response.parsed == FeedbackRecordsSearchModel(
@@ -104,10 +102,107 @@ def test_search_records(mock_httpx_client: httpx.Client) -> None:
     )
 
     mock_httpx_client.post.assert_called_once_with(
-        url=f"/api/me/datasets/{dataset_id}/records/search",
+        url=f"/api/v1/me/datasets/{dataset_id}/records/search",
         params={
             "include": ["responses", "suggestions"],
             "limit": 50,
         },
-        json={"query": query},
+        json={"query": {"vector": {"name": "vector_name", "value": [1.0, 2.0, 3.0]}}},
     )
+
+
+def test_search_records_with_record_id(mock_httpx_client: httpx.Client):
+    json_response = {
+        "items": [
+            {
+                "record": {
+                    "id": str(uuid4()),
+                    "fields": {},
+                    "metadata": None,
+                    "external_id": None,
+                    "responses": [],
+                    "suggestions": [],
+                    "inserted_at": datetime.utcnow().isoformat(),
+                    "updated_at": datetime.utcnow().isoformat(),
+                },
+                "query_score": 1.0,
+            },
+            {
+                "record": {
+                    "id": str(uuid4()),
+                    "fields": {},
+                    "metadata": None,
+                    "external_id": None,
+                    "responses": [],
+                    "suggestions": [],
+                    "inserted_at": datetime.utcnow().isoformat(),
+                    "updated_at": datetime.utcnow().isoformat(),
+                },
+                "query_score": 0.5,
+            },
+        ],
+        "total": 2,
+    }
+
+    mock_httpx_client.post.return_value = httpx.Response(status_code=200, json=json_response)
+
+    dataset_id = uuid4()
+    expected_record_id = uuid4()
+
+    vector_query = FeedbackRecordsSearchVectorQuery(name="vector_name", record_id=expected_record_id)
+
+    response = api.search_records(
+        client=mock_httpx_client,
+        id=dataset_id,
+        vector_query=vector_query,
+    )
+
+    assert response.status_code == 200
+    assert response.parsed == FeedbackRecordsSearchModel(
+        items=[
+            FeedbackRecordSearchModel(
+                record=FeedbackItemModel(
+                    id=UUID(json_response["items"][0]["record"]["id"]),
+                    fields={},
+                    metadata=None,
+                    external_id=None,
+                    responses=[],
+                    suggestions=[],
+                    inserted_at=datetime.fromisoformat(json_response["items"][0]["record"]["inserted_at"]),
+                    updated_at=datetime.fromisoformat(json_response["items"][0]["record"]["updated_at"]),
+                ),
+                query_score=1.0,
+            ),
+            FeedbackRecordSearchModel(
+                record=FeedbackItemModel(
+                    id=UUID(json_response["items"][1]["record"]["id"]),
+                    fields={},
+                    metadata=None,
+                    external_id=None,
+                    responses=[],
+                    suggestions=[],
+                    inserted_at=datetime.fromisoformat(json_response["items"][1]["record"]["inserted_at"]),
+                    updated_at=datetime.fromisoformat(json_response["items"][1]["record"]["updated_at"]),
+                ),
+                query_score=0.5,
+            ),
+        ],
+        total=2,
+    )
+
+    mock_httpx_client.post.assert_called_once_with(
+        url=f"/api/v1/me/datasets/{dataset_id}/records/search",
+        params={
+            "include": ["responses", "suggestions"],
+            "limit": 50,
+        },
+        json={"query": {"vector": {"name": "vector_name", "record_id": str(expected_record_id)}}},
+    )
+
+
+def test_search_records_with_invalid_query(mock_httpx_client: httpx.Client):
+    dataset_id = uuid4()
+
+    with pytest.raises(ValueError, match="Either 'record_id' or 'value' must be provided"):
+        vector_query = FeedbackRecordsSearchVectorQuery(name="vector_name", value=[1.0, 2.0, 3.0], record_id=uuid4())
+        api.search_records(client=mock_httpx_client, id=dataset_id, vector_query=vector_query)
