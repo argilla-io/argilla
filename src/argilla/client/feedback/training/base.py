ed9b3661a7caf4038d42105913c4a39a0d3fc132
agustin <agustin.piqueres@gmail.com>
2023-11-02 17:11:48 +0100

    feat: add filter_by argument to ArgillaTrainer

--- src/argilla/client/feedback/training/base.py
+++ src/argilla/client/feedback/training/base.py
@@ -19,6 +19,7 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
 
+from argilla.client.feedback.dataset.remote.dataset import RemoteFeedbackDataset
 from argilla.client.feedback.schemas.records import FeedbackRecord
 from argilla.client.feedback.training.schemas import TrainingTaskForTextClassification, TrainingTaskTypes
 from argilla.client.models import Framework, TextClassificationRecord
@@ -32,6 +33,7 @@ if TYPE_CHECKING:
 
     from argilla.client.feedback.dataset import FeedbackDataset
     from argilla.client.feedback.integrations.huggingface.model_card import ArgillaModelCard, FrameworkCardData
+    from argilla.client.feedback.schemas.enums import ResponseStatusFilter
 
 
 class ArgillaTrainer(ArgillaTrainerV1):
@@ -46,6 +48,7 @@ class ArgillaTrainer(ArgillaTrainerV1):
         train_size: Optional[float] = None,
         seed: Optional[int] = None,
         gpu_id: Optional[int] = -1,
+        filter_by: Optional[Dict[str, Union["ResponseStatusFilter", List["ResponseStatusFilter"]]]] = None,
         framework_kwargs: Optional[dict] = {},
     ) -> None:
         """
@@ -69,10 +72,18 @@ class ArgillaTrainer(ArgillaTrainerV1):
             gpu_id: the GPU ID to use when training a SpaCy model. Defaults to -1, which means that the CPU
                 will be used by default. GPU IDs start in 0, which stands for the default GPU in the system,
                 if available.
+            filter_by: A dict with key the field to filter by, and values the filters to apply. Currently only
+                defined for `response_status` filters. Can be one of: draft, pending, submitted, and discarded.
+                Defaults to `None` (no filter is applied).
             framework_kwargs: arguments for the framework's trainer. A special key (model_card_kwargs) is reserved
                 for the arguments that can be passed to the model card.
             **load_kwargs: arguments for the rg.load() function.
         """
+        if filter_by:
+            if not isinstance(dataset, RemoteFeedbackDataset):
+                raise ValueError("`filter_by` is only supported for `RemoteFeedbackDataset`.")
+            dataset = dataset.filter_by(**filter_by).pull()
+
         self._dataset = dataset
         self._train_size = train_size
         self._task = task
