9db7b6bcaf4c596129e8de584e5e04e137fe1a15
leire <leire@recogn.ai>
2023-11-07 15:59:32 +0100

    update condition to show filter button

--- frontend/components/feedback-task/header/DatasetFilters.component.vue
+++ frontend/components/feedback-task/header/DatasetFilters.component.vue
@@ -79,7 +79,7 @@ export default {
       );
     },
     isAnyAvailableFilter() {
-      return !!this.datasetMetadata.length;
+      return !!this.datasetMetadata.length || !!this.datasetQuestions.length;
     },
     isAnyFilterActive() {
       return this.recordCriteria.isFilteredByMetadata;
