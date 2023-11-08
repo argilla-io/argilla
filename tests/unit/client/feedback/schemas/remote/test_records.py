2bd6f1acaba13e08b5d371d7ad7b476a7340a4a3
Paco Aranda <francis@argilla.io>
2023-11-08 09:59:49 +0100

    chore: Apply alignment with develop branch

--- tests/unit/client/feedback/schemas/remote/test_records.py
+++ tests/unit/client/feedback/schemas/remote/test_records.py
@@ -345,7 +345,6 @@ def test_remote_feedback_record_schema_from_api(payload: FeedbackItemModel) -> N
             "client": ...,
             "responses": {"__all__": {"id", "client"}},
             "suggestions": ...,
-            "vectors": ...,
             "inserted_at": ...,
             "updated_at": ...,
         }
