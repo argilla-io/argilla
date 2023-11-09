ed70c4060eba8b4b583280430fd13c108bee9616
agustin <agustin.piqueres@gmail.com>
2023-11-09 18:18:41 +0100

    feat: create agreement metrics module

---
+++ src/argilla/client/feedback/metrics/agreement_metrics.py
@@ -0,0 +1,197 @@
+#  Copyright 2021-present, the Recognai S.L. team.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+"""This module contains metrics to gather information related to inter-Annotator agreement. """
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any, Hashable, List, Tuple, Union
+
+# TODO(plaguss): hide this dependencies behind requires_dependencies
+from nltk.metrics.agreement import AnnotationTask as NLTKAnnotationTask
+from nltk.metrics.distance import binary_distance, edit_distance, interval_distance
+from pydantic import BaseModel
+
+from argilla.client.feedback.schemas import (
+    LabelQuestion,
+    MultiLabelQuestion,
+    RankingQuestion,
+    RatingQuestion,
+    TextQuestion,
+)
+from argilla.client.feedback.schemas.enums import ResponseStatusFilter
+from argilla.utils.dependency import require_dependencies
+
+if TYPE_CHECKING:
+    from argilla.client.feedback.dataset import FeedbackDataset
+    from argilla.client.feedback.dataset.remote.dataset import RemoteFeedbackDataset
+
+
+# Expected format for the nltk's AnnotationTask
+FormattedResponses = List[Tuple[Any, Hashable, Hashable]]
+
+
+class AgreementMetricResult(BaseModel):
+    metric_name: str
+    result: float
+
+
+def prepare_dataset_for_annotation_task(
+    dataset: Union["FeedbackDataset", "RemoteFeedbackDataset"], question_name: str
+) -> FormattedResponses:
+    """Helper function to prepare the dataset for the nltk's AnnotationTask.
+
+    The AnnotationTask class from nltk expects the data to be formatted as a list
+    of tuples, each containing the annotator id, the task id and the label.
+
+    The AnnotationTask is supposed to deal with sets and hashable objects, but
+    there are errors transforming the data to sets and using the MASI distance function.
+    For the moment what we do with those type of questions is create a string
+    with all the values, and use the edit distance function.
+
+    Note:
+        We could potentially extend the functionality to a more than a question name. The
+        requirement would be that all the questions are of the same type, as that would
+        determine the type of distance function to use.
+
+    Args:
+        dataset: FeedbackDataset to compute the metrics.
+        question_name: Name of the question for which we want to analyse the agreement.
+
+    Returns:
+        formatted_responses: The responses formatted as a list of tuples of (user_id, question_id, value).
+    """
+    question_type = type(dataset.question_by_name(question_name))
+    supported_question_types = list(QUESTION_TO_DISTANCE.keys())
+    if question_type not in supported_question_types:
+        raise NotImplementedError(
+            f"Question '{question_name}' is of type '{question_type}', the supported question types are: {supported_question_types}."
+        )
+
+    dataset = dataset.filter_by(response_status=ResponseStatusFilter.submitted.value)
+
+    hf_dataset = dataset.format_as("datasets")
+
+    formatted_responses: FormattedResponses = []
+
+    for responses_ in hf_dataset[question_name]:
+        for response in responses_:
+            # We do this check here because local datasets don't implement the filter_by method.
+            if response["status"] != ResponseStatusFilter.submitted.value:
+                continue
+            user_id = response["user_id"]
+
+            if user_id is None:
+                raise NotImplementedError(
+                    "In order to use this functionality the records need to be assigned to a user."
+                )
+
+            value = response["value"]
+            if question_type == RankingQuestion:
+                value = "".join(value["value"])
+            elif question_type == MultiLabelQuestion:
+                value = "".join(value)
+
+            formatted_responses.append((user_id, question_name, value))
+
+    return formatted_responses
+
+
+QUESTION_TO_DISTANCE = {
+    LabelQuestion: binary_distance,
+    MultiLabelQuestion: edit_distance,
+    RatingQuestion: interval_distance,
+    RankingQuestion: edit_distance,
+}
+
+
+class AgreementMetric:
+    def __init__(self, dataset: "FeedbackDataset", question_name: str) -> None:
+        self._dataset = dataset
+        self._question_name = question_name
+        self._question_type = type(self._dataset.question_by_name(question_name))
+        if self._question_type == TextQuestion:
+            raise NotImplementedError(f"No metrics are defined currently for {self._question_type.__name__}")
+        self._allowed_metrics = METRICS_PER_QUESTION[self._question_type]
+
+    def compute(self, metric_names: Union[str, List[str]], **kwargs) -> List[AgreementMetricResult]:
+        if isinstance(metric_names, str):
+            metric_names = [metric_names]
+
+        if any([metric not in self._allowed_metrics for metric in metric_names]):
+            raise ValueError(
+                f"Metrics allowed for question {self._question_name}: {list(self._allowed_metrics.keys())}"
+            )
+
+        metric_classes = [(metric_name, self._allowed_metrics[metric_name]) for metric_name in metric_names]
+
+        dataset = prepare_dataset_for_annotation_task(self._dataset, self._question_name)
+
+        distance_function = kwargs.get("distance_function")
+        if not distance_function:
+            distance_function = QUESTION_TO_DISTANCE[self._question_type]
+
+        metrics = []
+        for metric_name, metric_cls in metric_classes:
+            metric = metric_cls(annotated_dataset=dataset, distance_function=distance_function)
+            result = metric.compute(**kwargs)
+            metrics.append(AgreementMetricResult(metric_name=metric_name, result=result))
+
+        return metrics
+
+
+class AnnotationTaskMetricBase(ABC):
+    def __init__(self, annotated_dataset=None, distance_function=None) -> None:
+        self._dataset = annotated_dataset
+        self._distance_function = distance_function
+
+    def compute(self, **kwargs):
+        data = self._pre_process(self._dataset)
+        return self._compute(data, **kwargs)
+
+    def _pre_process(self, data, **kwargs) -> Any:
+        return data
+
+    @abstractmethod
+    def _compute(self, data, **kwargs):
+        pass
+
+
+class NLTKAnnotationTaskMetric(NLTKAnnotationTask, AnnotationTaskMetricBase):
+    def __init__(self, annotated_dataset=None, distance_function=binary_distance) -> None:
+        require_dependencies("nltk")
+        AnnotationTaskMetricBase.__init__(
+            self, annotated_dataset=annotated_dataset, distance_function=distance_function
+        )
+        super().__init__(data=annotated_dataset, distance=distance_function)
+
+
+class KrippendorfAlpha(NLTKAnnotationTaskMetric):
+    def _compute(self, dataset, **kwargs):
+        return self.alpha()
+
+
+METRICS_PER_QUESTION = {
+    LabelQuestion: {
+        "alpha": KrippendorfAlpha,
+    },
+    MultiLabelQuestion: {
+        "alpha": KrippendorfAlpha,
+    },
+    RatingQuestion: {
+        "alpha": KrippendorfAlpha,
+    },
+    RankingQuestion: {
+        "alpha": KrippendorfAlpha,
+    },
+}
---
+++ tests/integration/client/feedback/metrics/test_agreement_metrics.py
@@ -0,0 +1,121 @@
+#  Copyright 2021-present, the Recognai S.L. team.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import uuid
+from typing import TYPE_CHECKING, List, Union
+
+import pytest
+from argilla.client.feedback.dataset import FeedbackDataset
+from argilla.client.feedback.metrics.agreement_metrics import (
+    AgreementMetric,
+    AgreementMetricResult,
+    prepare_dataset_for_annotation_task,
+)
+from argilla.client.feedback.schemas import FeedbackRecord
+
+if TYPE_CHECKING:
+    from argilla.client.feedback.schemas.types import AllowedFieldTypes, AllowedQuestionTypes
+
+
+@pytest.mark.parametrize(
+    "question, num_items",
+    [
+        ("question-1", None),
+        ("question-2", 12),
+        ("question-3", 12),
+        ("question-4", 12),
+        ("question-5", 12),
+    ],
+)
+@pytest.mark.usefixtures(
+    "feedback_dataset_guidelines",
+    "feedback_dataset_fields",
+    "feedback_dataset_questions",
+    "feedback_dataset_records_with_paired_suggestions",
+)
+def test_prepare_dataset_for_annotation_task(
+    feedback_dataset_guidelines: str,
+    feedback_dataset_fields: List["AllowedFieldTypes"],
+    feedback_dataset_questions: List["AllowedQuestionTypes"],
+    feedback_dataset_records_with_paired_suggestions: List[FeedbackRecord],
+    question: str,
+    num_items: int,
+):
+    dataset = FeedbackDataset(
+        guidelines=feedback_dataset_guidelines,
+        fields=feedback_dataset_fields,
+        questions=feedback_dataset_questions,
+    )
+    dataset.add_records(records=feedback_dataset_records_with_paired_suggestions)
+
+    if question in ("question-1",):
+        with pytest.raises(NotImplementedError, match=r"^Question '"):
+            prepare_dataset_for_annotation_task(dataset, question)
+    else:
+        formatted_dataset = prepare_dataset_for_annotation_task(dataset, question)
+        assert isinstance(formatted_dataset, list)
+        assert len(formatted_dataset) == num_items
+        item = formatted_dataset[0]
+        assert isinstance(item, tuple)
+        assert isinstance(item[0], str)
+        assert item[0].startswith("00000000-")  # beginning of our uuid for tests
+        assert isinstance(item[1], str)
+        assert item[1].startswith("question-")
+        assert isinstance(item[2], (int, str))
+
+
+@pytest.mark.parametrize(
+    "question, metric_names",
+    [
+        # TextQuestion
+        ("question-1", None),
+        # RatingQuestion
+        ("question-2", "alpha"),
+        # LabelQuestion
+        ("question-3", "alpha"),
+        # MultiLabelQuestion
+        ("question-4", "alpha"),
+        # RankingQuestion
+        ("question-5", "alpha"),
+    ],
+)
+@pytest.mark.usefixtures(
+    "feedback_dataset_guidelines",
+    "feedback_dataset_fields",
+    "feedback_dataset_questions",
+    "feedback_dataset_records_with_paired_suggestions",
+)
+def test_agreement_metrics(
+    feedback_dataset_guidelines: str,
+    feedback_dataset_fields: List["AllowedFieldTypes"],
+    feedback_dataset_questions: List["AllowedQuestionTypes"],
+    feedback_dataset_records_with_paired_suggestions: List[FeedbackRecord],
+    question: str,
+    metric_names: Union[str, List[str]],
+):
+    dataset = FeedbackDataset(
+        guidelines=feedback_dataset_guidelines,
+        fields=feedback_dataset_fields,
+        questions=feedback_dataset_questions,
+    )
+    dataset.add_records(records=feedback_dataset_records_with_paired_suggestions)
+
+    if question in ("question-1",):
+        with pytest.raises(NotImplementedError, match=r"^No metrics are defined currently for"):
+            AgreementMetric(dataset=dataset, question_name=question)
+    else:
+        metric = AgreementMetric(dataset=dataset, question_name=question)
+        metrics_report = metric.compute(metric_names)
+        assert isinstance(metrics_report, list)
+        assert all([isinstance(m, AgreementMetricResult) for m in metrics_report])
