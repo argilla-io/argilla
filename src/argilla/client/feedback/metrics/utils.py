8245df78c0ba4430514dc2e5c7d0b9d9c9e46fee
agustin <agustin.piqueres@gmail.com>
2023-11-09 18:17:31 +0100

    feat: create utils module for common functions

---
+++ src/argilla/client/feedback/metrics/utils.py
@@ -0,0 +1,81 @@
+#  Copyright 2021-present, the Recognai S.L. team.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from collections import defaultdict
+from typing import TYPE_CHECKING, Dict, List, Tuple, Union
+
+from argilla.client.feedback.schemas import RankingQuestion
+from argilla.client.feedback.schemas.enums import ResponseStatusFilter
+
+if TYPE_CHECKING:
+    from argilla.client.feedback.dataset import FeedbackDataset
+    from argilla.client.feedback.dataset.remote.dataset import RemoteFeedbackDataset
+
+
+# Type aliases
+Responses = List[Union[float, int, str]]
+Suggestions = Responses
+
+
+def get_responses_per_user(
+    dataset: Union["FeedbackDataset", "RemoteFeedbackDataset"],
+    question_name: str,
+    response_status: str = ResponseStatusFilter.submitted.value,
+) -> Tuple[Dict[int, Responses], Suggestions]:
+    """Extract the responses per user and the suggestions from a FeedbackDataset.
+
+    Helper function for the metrics module where we want to compare the responses
+    in relation to the suggestions offered.
+
+    Args:
+        dataset: FeedbackDataset or RemoteFeedbackDataset.
+        question_name: The name of the question to filter from the dataset.
+        response_status: The responses status for the responses. Defaults to "submitted".
+
+    Raises:
+        NotImplementedError:
+            When no user_id is given. We need that information to compute the metrics.
+
+    Returns:
+        Tuple containing the responses per user as a dict, with keys the user id and values the responses,
+        and the suggestions.
+    """
+    dataset = dataset.filter_by(response_status=response_status)
+
+    hf_dataset = dataset.format_as("datasets")
+    question_type = type(dataset.question_by_name(question_name))
+    responses_per_user = defaultdict(list)
+
+    for responses_ in hf_dataset[question_name]:
+        for response in responses_:
+            # We do this check here because local datasets don't implement the filter_by method.
+            if response["status"] != response_status:
+                continue
+            user_id = response["user_id"]
+            if user_id is None:
+                raise NotImplementedError(
+                    "In order to use this functionality the records need to be assigned to a user."
+                )
+            if question_type == RankingQuestion:
+                value = response["value"]["value"]
+            else:
+                value = response["value"]
+
+            responses_per_user[user_id].append(value)
+
+    suggestions = hf_dataset[f"{question_name}-suggestion"]
+    if question_type == RankingQuestion:
+        suggestions = [suggestion["value"] for suggestion in suggestions]
+
+    return responses_per_user, suggestions
---
+++ tests/integration/client/feedback/metrics/test_utils.py
@@ -0,0 +1,69 @@
+#  Copyright 2021-present, the Recognai S.L. team.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from typing import TYPE_CHECKING, List
+
+import pytest
+from argilla.client.feedback.dataset import FeedbackDataset
+from argilla.client.feedback.metrics.utils import get_responses_per_user
+from argilla.client.feedback.schemas import FeedbackRecord
+
+if TYPE_CHECKING:
+    from argilla.client.feedback.schemas.types import AllowedFieldTypes, AllowedQuestionTypes
+
+
+@pytest.mark.parametrize(
+    "question, status, num_responses",
+    [
+        ("question-1", "submitted", 4),
+        ("question-2", "submitted", 4),
+        ("question-3", "submitted", 4),
+        ("question-4", "submitted", 4),
+        ("question-5", "submitted", 4),
+        ("question-1", "draft", 0),
+    ],
+)
+@pytest.mark.usefixtures(
+    "feedback_dataset_guidelines",
+    "feedback_dataset_fields",
+    "feedback_dataset_questions",
+    "feedback_dataset_records_with_paired_suggestions",
+)
+def test_responses_per_user(
+    feedback_dataset_guidelines: str,
+    feedback_dataset_fields: List["AllowedFieldTypes"],
+    feedback_dataset_questions: List["AllowedQuestionTypes"],
+    feedback_dataset_records_with_paired_suggestions: List[FeedbackRecord],
+    question: str,
+    status: str,
+    num_responses: int,
+):
+    dataset = FeedbackDataset(
+        guidelines=feedback_dataset_guidelines,
+        fields=feedback_dataset_fields,
+        questions=feedback_dataset_questions,
+    )
+    dataset.add_records(records=feedback_dataset_records_with_paired_suggestions)
+
+    responses_per_user, suggestions = get_responses_per_user(dataset, question, response_status=status)
+    if status != "submitted":
+        num_users = 0
+        assert len(responses_per_user) == num_users
+    else:
+        num_users = 3
+        assert len(responses_per_user) == num_users
+        assert isinstance(responses_per_user, dict)
+        responses = responses_per_user[list(responses_per_user.keys())[0]]
+        assert len(suggestions) == len(responses)
+        assert len(responses) == num_responses
