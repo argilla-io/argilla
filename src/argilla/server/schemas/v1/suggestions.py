7bb7b6d39cf10ef0d1c5e41bc2b4ecd11d5c3d27
Paco Aranda <francis@argilla.io>
2023-11-10 12:29:52 +0100

    chore: Adding a TODO

--- src/argilla/server/schemas/v1/suggestions.py
+++ src/argilla/server/schemas/v1/suggestions.py
@@ -23,6 +23,7 @@ from argilla.server.models import SuggestionType
 class BaseSuggestion(BaseModel):
     question_id: UUID
     type: Optional[SuggestionType]
+    # TODO: score must be a float between 0 and 1
     score: Optional[float]
     value: Any
     agent: Optional[str]
