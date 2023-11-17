a4d3057cceb3a14d48fe6871c75c729addd76299
Agustin Piqueres <agus@Agustins-MacBook-Pro.local>
2023-11-17 16:43:50 +0100

    test: add tests for unified annotator metrics

--- tests/integration/client/feedback/metrics/test_annotator_metrics.py
+++ tests/integration/client/feedback/metrics/test_annotator_metrics.py
@@ -17,9 +17,26 @@ from typing import TYPE_CHECKING, List, Union
 
 import pytest
 from argilla.client.feedback.dataset import FeedbackDataset
-from argilla.client.feedback.metrics.annotator_metrics import AnnotatorMetric
+from argilla.client.feedback.metrics.annotator_metrics import AnnotatorMetric, UnifiedAnnotatorMetric
 from argilla.client.feedback.metrics.base import AnnotatorMetricResult
-from argilla.client.feedback.schemas import FeedbackRecord
+from argilla.client.feedback.schemas import (
+    FeedbackRecord,
+    LabelQuestion,
+    MultiLabelQuestion,
+    RankingQuestion,
+    RatingQuestion,
+)
+from argilla.client.feedback.unification import (
+    LabelQuestionStrategy,
+    LabelQuestionUnification,
+    MultiLabelQuestionStrategy,
+    MultiLabelQuestionUnification,
+    RankingQuestionStrategy,
+    RankingQuestionUnification,
+    RatingQuestionStrategy,
+    RatingQuestionUnification,
+    UnifiedValueSchema,
+)
 
 if TYPE_CHECKING:
     from argilla.client.feedback.schemas.types import AllowedFieldTypes, AllowedQuestionTypes
@@ -80,6 +97,65 @@ def test_annotator_metric(
             metric_names = [metric_names]
 
         assert all([result.metric_name == name for result, name in zip(metric_results, metric_names)])
+
+
+@pytest.mark.parametrize(
+    "question, metric_names, strategy_name",
+    [
+        # TextQuestion
+        ("question-1", None, None),
+        # RatingQuestion
+        ("question-2", "accuracy", "majority"),
+        ("question-2", ["accuracy", "f1-score", "precision", "recall", "confusion-matrix", "spearman-r"], "majority"),
+        # LabelQuestion
+        ("question-3", ["accuracy", "f1-score", "precision", "recall", "confusion-matrix", "pearson-r"], "majority"),
+        # MultiLabelQuestion
+        ("question-4", None, None),
+        # RankingQuestion
+        ("question-5", None, None),
+    ],
+)
+@pytest.mark.usefixtures(
+    "feedback_dataset_guidelines",
+    "feedback_dataset_fields",
+    "feedback_dataset_questions",
+    "feedback_dataset_records_with_paired_suggestions",
+)
+def test_annotator_metrics_unified(
+    feedback_dataset_guidelines: str,
+    feedback_dataset_fields: List["AllowedFieldTypes"],
+    feedback_dataset_questions: List["AllowedQuestionTypes"],
+    feedback_dataset_records_with_paired_suggestions: List[FeedbackRecord],
+    question: str,
+    metric_names: Union[str, List[str]],
+    strategy_name: str,
+):
+    if not strategy_name:
+        return
+    dataset = FeedbackDataset(
+        guidelines=feedback_dataset_guidelines,
+        fields=feedback_dataset_fields,
+        questions=feedback_dataset_questions,
+    )
+    dataset.add_records(records=feedback_dataset_records_with_paired_suggestions)
+
+    unified_dataset = dataset.unify_responses(question, strategy_name)
+
+    if question in ("question-1", "question-4", "question-5"):
+        with pytest.raises(NotImplementedError):
+            UnifiedAnnotatorMetric(unified_dataset, question)
+    else:
+        metric = UnifiedAnnotatorMetric(unified_dataset, question)
+        metrics_report = metric.compute(metric_names)
+
+        if isinstance(metric_names, list):
+            assert isinstance(metrics_report, list)
+        else:
+            assert isinstance(metrics_report, AnnotatorMetricResult)
+            metrics_report = [metrics_report]
+            metric_names = [metric_names]
+
+        assert all([result.metric_name == name for result, name in zip(metrics_report, metric_names)])
 
 
 @pytest.mark.parametrize(
