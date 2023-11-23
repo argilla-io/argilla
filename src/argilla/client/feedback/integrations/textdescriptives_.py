d200749f326c2c5fade3a4000d7fbb0dd0b6e483
m-newhauser <mary.newhauser@gmail.com>
2023-11-23 10:27:15 +0100

    Add unit tests and other minor changes

--- src/argilla/client/feedback/integrations/textdescriptives_.py
+++ src/argilla/client/feedback/integrations/textdescriptives_.py
@@ -13,11 +13,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 import re
+import logging
 from typing import Dict, List, Optional, Tuple, Union
 
 import pandas as pd
 import textdescriptives as td
-from tqdm import tqdm
+from rich.progress import Progress
 
 from argilla.client.feedback.dataset.local.dataset import FeedbackDataset
 from argilla.client.feedback.dataset.remote.dataset import RemoteFeedbackDataset
@@ -30,6 +31,10 @@ from argilla.client.feedback.schemas.records import FeedbackRecord
 from argilla.client.feedback.schemas.remote.records import RemoteFeedbackRecord
 
 
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.setLevel(logging.INFO)
+
+
 class TextDescriptivesExtractor:
     """This class extracts a number of basic text descriptives from FeedbackDataset
     records using the TextDescriptives library and adds them as record metadata."""
@@ -40,7 +45,7 @@ class TextDescriptivesExtractor:
         metrics: Optional[List[str]] = None,
         fields: Optional[List[str]] = None,
         visible_for_annotators: bool = True,
-        progress: bool = True,
+        show_progress: bool = True,
     ):
         """
         Initialize a new TextDescriptivesExtractor object.
@@ -50,13 +55,13 @@ class TextDescriptivesExtractor:
             metrics (Optional[List[str]]): A list of metrics to extract. If None, all metrics will be extracted.
             fields (Optional[List[str]]): A list of field names to extract metrics from. If None, all fields will be used.
             visible_for_annotators (bool): Whether the extracted metrics should be visible to annotators.
-            progress (bool): Whether to show a progress bar when extracting metrics.
+            show_progress (bool): Whether to show a progress bar when extracting metrics.
         """
         self.model = model
         self.metrics = metrics
         self.fields = fields
         self.visible_for_annotators = visible_for_annotators
-        self.progress = progress
+        self.show_progress = show_progress
         self.__basic_metrics = [
             "n_tokens",
             "n_unique_tokens",
@@ -89,13 +94,16 @@ class TextDescriptivesExtractor:
         if not field_text:
             return None
         field_metrics = td.extract_metrics(text=field_text, lang=self.model, metrics=self.metrics)
-        # Drop text column and any column containing NaNs
-        field_metrics = field_metrics.dropna(axis=1, how="any").drop("text", axis=1)
+        # Drop text column
+        field_metrics = field_metrics.drop("text", axis=1)
+        # Select all column names that contain ONLY NaNs
+        nan_columns = field_metrics.columns[field_metrics.isnull().all()].tolist()
+        if nan_columns:
+            _LOGGER.warning(f"The following columns contain only NaN values: {nan_columns}")
         # If basic metrics is None, use all basic metrics
         if basic_metrics is None and self.metrics is None:
             basic_metrics = self.__basic_metrics
             field_metrics = field_metrics.loc[:, basic_metrics]
-
         # Concatenate field name with the metric name
         field_metrics.columns = [f"{field}_{metric}" for metric in field_metrics.columns]
         return field_metrics
@@ -124,8 +132,10 @@ class TextDescriptivesExtractor:
         # If there is only one field, return the metrics for that field directly
         if len(field_metrics) == 1:
             return list(field_metrics.values())[0]
-        # If there are multiple fields, combine metrics for each field into a single dataframe
-        final_metrics = pd.concat(field_metrics, axis=1, keys=field_metrics.keys())
+        else:
+            # If there are multiple fields, combine metrics for each field into a single dataframe
+            final_metrics = pd.concat(field_metrics, axis=1, keys=field_metrics.keys())
+            final_metrics.columns = final_metrics.columns.droplevel(0)
         return final_metrics
 
     def _cast_to_python_types(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -151,6 +161,18 @@ class TextDescriptivesExtractor:
         df[float_cols] = df[float_cols].astype(float).round(2)
         return df
 
+    def _clean_column_name(self, col_name: str) -> str:
+        """
+        Clean the column name of a dataframe to fit a specific regex pattern.
+        Args:
+            col_name (str): A column name.
+        Returns:
+            str: A column name that fits the regex pattern.
+        """
+        col_name = col_name.lower()  # Convert to lowercase
+        col_name = re.sub(r"[^a-z0-9_]", "_", col_name)  # Replace non-alphanumeric characters with underscores
+        return col_name
+
     def _create_metadata_properties(self, df: pd.DataFrame) -> List:
         """
         Generate metadata properties based on dataframe columns and data types.
@@ -167,7 +189,10 @@ class TextDescriptivesExtractor:
             title = name.replace("_", " ").title()
             if dtype in ["object", "bool"]:
                 prop = TermsMetadataProperty(
-                    name=name, title=title, visible_for_annotators=self.visible_for_annotators, values=["True", "False"]
+                    name=name,
+                    title=title,
+                    visible_for_annotators=self.visible_for_annotators,
+                    values=df[col].unique().tolist(),
                 )
             elif dtype == "int64":
                 prop = IntegerMetadataProperty(
@@ -176,9 +201,10 @@ class TextDescriptivesExtractor:
             elif dtype == "float64":
                 prop = FloatMetadataProperty(name=name, title=title, visible_for_annotators=self.visible_for_annotators)
             else:
-                print(f"Unhandled data type for column {col}: {dtype}")
-                continue
-            properties.append(prop)
+                _LOGGER.warning(f"Unhandled data type for column {col}: {dtype}")
+                prop = None
+            if prop is not None:
+                properties.append(prop)
         return properties
 
     def _add_text_descriptives_to_metadata(
@@ -195,24 +221,58 @@ class TextDescriptivesExtractor:
         Returns:
             List[Union[FeedbackRecord, RemoteFeedbackRecord]]: A list of FeedbackDataset or RemoteFeedbackDataset records with extracted metrics added as metadata.
         """
-        # Loop through the records and add the metrics to the metadata
         modified_records = []
-        # Make tqdm optional
-        for record, metrics in tqdm(
-            zip(records, df.to_dict("records")),
-            total=len(records),
-            disable=not self.progress,
-            desc="Getting text descriptives...",
-        ):
-            record.metadata.update(metrics)
-            modified_records.append(record)
+        with Progress() as progress_bar:
+            task = progress_bar.add_task(
+                "Adding text descriptives to metadata...", total=len(records), visible=self.show_progress
+            )
+            for record, metrics in zip(records, df.to_dict("records")):
+                filtered_metrics = {key: value for key, value in metrics.items() if not pd.isna(value)}
+                record.metadata.update(filtered_metrics)
+                modified_records.append(record)
+                progress_bar.update(task, advance=1)
         return modified_records
+
+    def update_records(
+        self, records: List[Union[FeedbackRecord, RemoteFeedbackRecord]]
+    ) -> List[Union[FeedbackRecord, RemoteFeedbackRecord]]:
+        """
+        Extract text descriptives metrics from a list of FeedbackDataset or RemoteFeedbackDataset records,
+        add them as metadata to the records and return the updated records.
+
+        Args:
+            records (List[Union[FeedbackRecord, RemoteFeedbackRecord]]): A list of FeedbackDataset or RemoteFeedbackDataset records.
+
+        Returns:
+            List[Union[FeedbackRecord, RemoteFeedbackRecord]]: A list of FeedbackDataset or RemoteFeedbackDataset records with text descriptives metrics added as metadata.
+
+        >>> import argilla as rg
+        >>> records = [rg.FeedbackRecord(fields={"text": "This is a test."})]
+        >>> tde = rg.TextDescriptivesExtractor()
+        >>> updated_records = tde.update_records(records)
+        """
+        # Extract text descriptives metrics from records
+        extracted_metrics = self._extract_metrics_for_all_fields(records)
+        # If the dataframe doesn't contain any columns, return the original records and log a warning
+        if extracted_metrics.shape[1] == 0:
+            _LOGGER.warning(
+                "No text descriptives metrics were extracted. This could be because the metrics contained NaNs."
+            )
+            return records
+        else:
+            # Cast integer and boolean columns to Python native types
+            extracted_metrics = self._cast_to_python_types(extracted_metrics)
+            # Clean column names
+            extracted_metrics.columns = [self._clean_column_name(col) for col in extracted_metrics.columns]
+            # Add the metrics to the metadata of the records
+            modified_records = self._add_text_descriptives_to_metadata(records, extracted_metrics)
+            return modified_records
 
     def update_dataset(
         self, dataset: Union[FeedbackDataset, RemoteFeedbackDataset]
     ) -> Union[FeedbackDataset, RemoteFeedbackDataset]:
         """
-        Extract text descriptives metrics from a records in a FeedbackDataset
+        Extract text descriptives metrics from records in a FeedbackDataset
         or RemoteFeedbackDataset, add them as metadata to the records and
         return the updated dataset.
 
@@ -221,6 +281,13 @@ class TextDescriptivesExtractor:
 
         Returns:
             Union[FeedbackDataset, RemoteFeedbackDataset]: A FeedbackDataset or RemoteFeedbackDataset with text descriptives metrics added as metadata.
+
+        >>> import argilla as rg
+        >>> rg.init(...)
+        >>> dataset = rg.FeedbackDataset.from_argilla(name="my-dataset")
+        >>> tde = rg.TextDescriptivesExtractor()
+        >>> updated_dataset = tde.update_dataset(dataset)
+
         """
         if isinstance(dataset, FeedbackDataset):
             records = dataset.records
@@ -228,12 +295,14 @@ class TextDescriptivesExtractor:
             records = dataset.pull().records
         else:
             raise ValueError(
-                f"Provided `dataset` is of `type={type(dataset)}` while only `type=FeedbackDataset` or `type=RemoteFeedbackDataset` are allowed."
+                f"Provided object is of `type={type(dataset)}` while only `type=FeedbackDataset` or `type=RemoteFeedbackDataset` are allowed."
             )
         # Extract text descriptives metrics from records
         extracted_metrics = self._extract_metrics_for_all_fields(records)
         # Cast integer and boolean columns to Python native types
         extracted_metrics = self._cast_to_python_types(extracted_metrics)
+        # Clean column names
+        extracted_metrics.columns = [self._clean_column_name(col) for col in extracted_metrics.columns]
         # Create metadata properties based on dataframe columns and data types
         metadata_properties = self._create_metadata_properties(extracted_metrics)
         # Add each metadata property iteratively to the dataset
