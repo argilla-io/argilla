0c0c255ad30653c63d155364829937e79183640e
plaguss <agus@Agustins-MacBook-Pro.local>
2023-11-27 14:41:40 +0100

    feat: update parameters from code review

--- scripts/end2end_examples.py
+++ scripts/end2end_examples.py
@@ -29,11 +29,9 @@ from typing import Dict, Optional
 
 import papermill
 import typer
+from argilla._constants import DEFAULT_API_KEY
 
-EXAMPLES_FOLDER = (
-    Path(__file__).parent.parent / "docs/_source/tutorials_and_integrations/tutorials/feedback/end2end_examples"
-)
-OUTPUT_FOLDER = EXAMPLES_FOLDER / "output_notebooks"
+ROOT_FOLDER = Path(__file__).parent.parent
 
 
 @dataclass
@@ -44,22 +42,25 @@ class ExampleNotebook:
     parameters: Dict = field(default_factory=dict)
 
     def __post_init__(self):
-        self.src_filename = EXAMPLES_FOLDER / self.src_filename
+        self.src_filename = Path(self.src_filename)
         assert self.src_filename.exists(), f"File {self.src_filename} does not exist"
         self.sort_index = int(self.src_filename.stem.split("-")[-1])
-        dst_folder = OUTPUT_FOLDER
+        self.dst_filename = Path(self.dst_filename)
+        dst_folder = self.dst_filename.parent
         if not dst_folder.exists():
             dst_folder.mkdir(exist_ok=True)
-        self.dst_filename = dst_folder / self.dst_filename
 
     def run(self):
         try:
             papermill.execute_notebook(
-                str(self.src_filename), str(self.dst_filename), parameters=self.parameters, kernel_name="python3"
+                str(self.src_filename),
+                str(self.dst_filename),
+                parameters=self.parameters,
+                kernel_name="python3",
             )
-            print(f"✅  {self.src_filename.stem}")
+            print(f"✅  {self.src_filename}")
         except Exception as e:
-            print(f"❌  {self.src_filename.stem}")
+            print(f"❌  {self.src_filename}")
             raise e from None
 
     def clean(self):
@@ -80,7 +81,12 @@ def get_huggingface_token() -> Optional[str]:
     raise ValueError("No token found. Please set HF_HUB_ACCESS_TOKEN environment variable.")
 
 
-def main(api_url: Optional[str] = None, api_key: Optional[str] = None, hf_token: Optional[str] = None) -> None:
+def main(
+    api_url: Optional[str] = "http://localhost:6900",
+    api_key: Optional[str] = DEFAULT_API_KEY,
+    hf_token: Optional[str] = None,
+    examples_folder: Optional[str] = "docs/_source/tutorials_and_integrations/tutorials/feedback/end2end_examples",
+) -> None:
     """
     Run the end2end example notebooks. If no arguments are passed, it
     will try to get the api_key and the hf_token from the environment variables.
@@ -90,17 +96,22 @@ def main(api_url: Optional[str] = None, api_key: Optional[str] = None, hf_token:
 
     notebook_parameters = {
         "hf_token": hf_token,
+        "api_url": api_url,
+        "api_key": api_key,
     }
 
     # Name of the output notebook that will be removed after running the examples
     output_notebook = "output_notebook.ipynb"
 
+    examples_folder = ROOT_FOLDER / examples_folder
+    output_folder = examples_folder.parent / "end2end_outputs"
+
     examples = []
-    for filename in EXAMPLES_FOLDER.glob("*.ipynb"):
+    for filename in examples_folder.glob("*.ipynb"):
         examples.append(
             ExampleNotebook(
-                src_filename=Path(filename).name,
-                dst_filename=output_notebook,
+                src_filename=filename,
+                dst_filename=output_folder / output_notebook,
                 parameters=notebook_parameters,
             )
         )
@@ -110,9 +121,9 @@ def main(api_url: Optional[str] = None, api_key: Optional[str] = None, hf_token:
         example.run()
         example.clean()
 
-    if OUTPUT_FOLDER.exists():
-        OUTPUT_FOLDER.rmdir()
-        print(f"Removed output folder: {OUTPUT_FOLDER.name}")
+    if output_folder.exists():
+        output_folder.rmdir()
+        print(f"Removed output folder: {output_folder.name}")
 
 
 if __name__ == "__main__":
