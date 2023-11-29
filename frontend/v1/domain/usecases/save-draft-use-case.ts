e5ef966f28db867961f54ac71b9c37b2fbffadb1
leire <leire@recogn.ai>
2023-11-29 12:53:28 +0100

    reverse commit

--- frontend/v1/domain/usecases/save-draft-use-case.ts
+++ frontend/v1/domain/usecases/save-draft-use-case.ts
@@ -1,19 +1,12 @@
-import { IEventDispatcher } from "@codescouts/events";
-import { RecordResponseUpdatedEvent } from "../events/RecordResponseUpdatedEvent";
 import { Record } from "../entities/record/Record";
 import { RecordRepository } from "@/v1/infrastructure/repositories";
 
 export class SaveDraftRecord {
-  constructor(
-    private readonly recordRepository: RecordRepository,
-    private readonly eventDispatcher: IEventDispatcher
-  ) {}
+  constructor(private readonly recordRepository: RecordRepository) {}
 
   async execute(record: Record) {
     const response = await this.recordRepository.saveDraft(record);
 
     record.submit(response);
-
-    this.eventDispatcher.dispatch(new RecordResponseUpdatedEvent(record));
   }
 }
