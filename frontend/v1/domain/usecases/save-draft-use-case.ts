1909f83802328bffe232b42a41457880fad9729d
leire <leire@recogn.ai>
2023-11-29 16:07:08 +0100

    update draft counter

--- frontend/v1/domain/usecases/save-draft-use-case.ts
+++ frontend/v1/domain/usecases/save-draft-use-case.ts
@@ -1,12 +1,20 @@
+import { IEventDispatcher } from "@codescouts/events";
+import { RecordResponseUpdatedEvent } from "../events/RecordResponseUpdatedEvent";
 import { Record } from "../entities/record/Record";
 import { RecordRepository } from "@/v1/infrastructure/repositories";
 
 export class SaveDraftRecord {
-  constructor(private readonly recordRepository: RecordRepository) {}
+  constructor(
+    private readonly recordRepository: RecordRepository,
+    private readonly eventDispatcher: IEventDispatcher
+  ) {}
 
   async execute(record: Record) {
     const response = await this.recordRepository.saveDraft(record);
 
+    if (record.answer) {
+      this.eventDispatcher.dispatch(new RecordResponseUpdatedEvent(record));
+    }
     record.submit(response);
   }
 }
