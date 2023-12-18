3bde0b92b71cf36c38981827a2e50d8da66f1ec0
Damián Pumar <damianpumar@gmail.com>
2023-12-18 23:59:42 +0100

    🔥 Refactor code removing some unnecessary pieces of code

--- frontend/v1/domain/services/IRecordStorage.ts
+++ frontend/v1/domain/services/IRecordStorage.ts
@@ -1,7 +1,6 @@
 import { Records } from "../entities/record/Records";
 
 export interface IRecordStorage {
-  append(records: Records);
-  replace(records: Records);
+  save(records: Records);
   get(): Records;
 }
