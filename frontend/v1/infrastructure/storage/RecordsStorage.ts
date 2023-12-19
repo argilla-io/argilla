4cfd3174f7208c88dc7366e9877de0cf0432263d
Damián Pumar <damianpumar@gmail.com>
2023-12-19 11:06:27 +0100

    ⚰️ Remove dead code

--- frontend/v1/infrastructure/storage/RecordsStorage.ts
+++ frontend/v1/infrastructure/storage/RecordsStorage.ts
@@ -4,24 +4,4 @@ import { Records } from "@/v1/domain/entities/record/Records";
 
 const useStoreForRecords = useStoreFor<Records, IRecordStorage>(Records);
 
-export const useRecords = () => {
-  const state = useStoreForRecords();
-
-  const append = (newRecords: Records) => {
-    const records = state.get();
-
-    records.append(newRecords);
-
-    state.save(records);
-  };
-
-  const replace = (newRecords: Records) => {
-    state.save(newRecords);
-  };
-
-  return {
-    ...state,
-    append,
-    replace,
-  };
-};
+export const useRecords = () => useStoreForRecords();
