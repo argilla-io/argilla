b512e48d45c92547b0590c55cc65c6b537344f31
leire <leire@recogn.ai>
2023-12-22 10:39:24 +0100

    include bulk / focus mode icons

---
+++ frontend/assets/icons/bulk-mode.js
@@ -0,0 +1,27 @@
+/*
+ * coding=utf-8
+ * Copyright 2021-present, the Recognai S.L. team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'bulk-mode': {
+    width: 20,
+    height: 16,
+    viewBox: '0 0 20 16',
+    data: '<path pid="0" fill-rule="evenodd" clip-rule="evenodd" d="M0 2.716a.833.833 0 111.667 0v.215a.833.833 0 11-1.667 0v-.215zM4.275 3.765a.941.941 0 110-1.882h14.784a.941.941 0 010 1.882H4.275zM0 6.48a.833.833 0 011.667 0v.216a.833.833 0 01-1.667 0V6.48zM4.275 7.53a.941.941 0 110-1.882h14.784a.941.941 0 010 1.882H4.275zM0 10.245a.833.833 0 011.667 0v.216a.833.833 0 01-1.667 0v-.216zM4.275 11.294a.941.941 0 110-1.882h14.784a.941.941 0 010 1.882H4.275zM0 14.01a.833.833 0 011.667 0v.216a.833.833 0 01-1.667 0v-.216zM4.275 15.059a.941.941 0 110-1.882h14.784a.941.941 0 010 1.882H4.275z" _fill="#000" fill-opacity=".54"/>'
+  }
+})
