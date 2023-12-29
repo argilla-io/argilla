ac1a247d039829423d7252c32b9f12a1de567f16
leire <leire@recogn.ai>
2023-12-29 09:32:19 +0100

    update styles for record fields header

--- frontend/components/feedback-task/container/fields/Record.vue
+++ frontend/components/feedback-task/container/fields/Record.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="record__wrapper" v-if="!!record" :key="`${record.id}_fields`">
-    <div class="record">
+    <div
+      class="record"
+      :class="Array.isArray(selectedRecords) ? 'record--bulk' : 'record--focus'"
+    >
+      <RecordFieldsHeader
+        class="record__fixed-header"
+        :record="record"
+        :recordCriteria="recordCriteria"
+        :datasetVectors="datasetVectors"
+        :selectedRecords="selectedRecords"
+        @on-select-record="onSelectedRecord"
+      />
       <div class="record__content">
-        <RecordFieldsHeader
-          :record="record"
-          :recordCriteria="recordCriteria"
-          :datasetVectors="datasetVectors"
-          :selectedRecords="selectedRecords"
-          @on-select-record="onSelectedRecord"
-        />
         <RecordFields :fields="record.fields" />
       </div>
     </div>
@@ -52,6 +56,9 @@ export default {
   background: palette(white);
   border: 1px solid palette(grey, 600);
   border-radius: $border-radius-m;
+  &--focus {
+    overflow: auto;
+  }
   &__wrapper {
     flex: 1;
     display: flex;
@@ -62,13 +69,15 @@ export default {
     min-height: 0;
   }
   &__content {
-    height: 100%;
     display: flex;
     flex-direction: column;
-    overflow: auto;
     gap: $base-space * 2;
     padding: $base-space * 2;
     border-radius: $border-radius-m;
+    .record--bulk & {
+      height: 100%;
+      overflow: auto;
+    }
   }
   &__fixed-header {
     padding: $base-space * 2;
