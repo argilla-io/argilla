5d3653158d0238fad54fb23c0ec7a845c6d7e06b
PIYUSH KUMAR GHOSH <60840567+Piyush-Kumar-Ghosh@users.noreply.github.com>
2024-01-06 08:36:38 +0000

    Feature: #4118 Added ResponseStatusFilter enum in __init__

--- src/argilla/client/feedback/schemas/__init__.py
+++ src/argilla/client/feedback/schemas/__init__.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from argilla.client.feedback.schemas.enums import FieldTypes, QuestionTypes, RecordSortField, SortOrder
+from argilla.client.feedback.schemas.enums import FieldTypes, QuestionTypes, RecordSortField, SortOrder, ResponseStatusFilter
 from argilla.client.feedback.schemas.fields import FieldSchema, TextField
 from argilla.client.feedback.schemas.metadata import (
     FloatMetadataFilter,
@@ -64,5 +64,6 @@ __all__ = [
     "SortOrder",
     "SortBy",
     "RecordSortField",
+    "ResponseStatusFilter",
     "VectorSettings",
 ]
