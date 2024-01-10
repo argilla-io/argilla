3e15d2c06222f1d7175a2715a03a3ca0f82f4e42
Paco Aranda <francis@argilla.io>
2024-01-10 16:44:29 +0100

    chore: Apply naming suggestions

--- src/argilla/server/constants.py
+++ src/argilla/server/constants.py
@@ -22,10 +22,9 @@ DEFAULT_API_KEY = "argilla.apikey"
 DEFAULT_MAX_KEYWORD_LENGTH = 128
 DEFAULT_TELEMETRY_KEY = "C6FkcaoCbt78rACAgvyBxGBcMB3dM3nn"
 
-
 # The metadata field name prefix defined for protected (non-searchable) values
 PROTECTED_METADATA_FIELD_PREFIX = "_"
 
 ES_INDEX_REGEX_PATTERN = r"^(?!-|_)[a-z0-9-_]+$"
 
-_JS_MAX_SAFE_INTEGER = 9007199254740991
+JS_MAX_SAFE_INTEGER = 9007199254740991
--- src/argilla/server/daos/models/records.py
+++ src/argilla/server/daos/models/records.py
@@ -20,7 +20,7 @@ from typing import Any, Dict, Generic, List, Optional, TypeVar, Union
 
 from argilla import _messages
 from argilla.server.commons.models import PredictionStatus, TaskStatus, TaskType
-from argilla.server.constants import _JS_MAX_SAFE_INTEGER, PROTECTED_METADATA_FIELD_PREFIX
+from argilla.server.constants import JS_MAX_SAFE_INTEGER, PROTECTED_METADATA_FIELD_PREFIX
 from argilla.server.daos.backend.search.model import BaseRecordsQuery, SortConfig
 from argilla.server.helpers import flatten_dict
 from argilla.server.pydantic_v1 import BaseModel, Field, conint, constr, root_validator, validator
@@ -127,7 +127,7 @@ class BaseRecordInDB(GenericModel, Generic[AnnotationDB]):
             )
             warnings.warn(message, DeprecationWarning)
             # See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
-            if v > _JS_MAX_SAFE_INTEGER:
+            if v > JS_MAX_SAFE_INTEGER:
                 message = (
                     "You've provided a big integer value. Use a string instead, otherwise you may experience some "
                     "problems using the UI. See "
