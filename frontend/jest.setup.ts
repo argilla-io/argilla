5d388b6fbd753e9c5fa2810c2b2071acd2fab978
Damián Pumar <damianpumar@gmail.com>
2024-01-24 16:31:10 +0100

    ✨ Improve translation

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,13 +1,14 @@
 import Vue from "vue";
 import { Nuxt, Builder } from "nuxt";
 import SvgIcon from "vue-svgicon";
+import { config } from "@vue/test-utils";
 import nuxtConfig from "./nuxt.config";
+
 Vue.use(SvgIcon);
 Vue.directive("click-outside", {});
 Vue.config.devtools = false;
 Vue.config.productionTip = false;
 
-// these boolean switches turn off the build for all but the store
 const resetConfig = {
   loading: false,
   loadingIndicator: false,
@@ -37,24 +38,26 @@ const resetConfig = {
   },
 };
 
-// we take our nuxt config, lay the resets on top of it,
-// and lastly we apply the non-boolean overrides
-const config = Object.assign({}, nuxtConfig, resetConfig, {
+const overrideConfig = {
+  ...nuxtConfig,
+  ...resetConfig,
   mode: "spa",
   srcDir: nuxtConfig.srcDir,
   ignore: ["**/components/**/*", "**/layouts/**/*", "**/pages/**/*"],
-});
+};
 
 const buildNuxt = async () => {
-  const nuxt = new Nuxt(config);
+  const nuxt = new Nuxt(overrideConfig);
   await new Builder(nuxt).build();
   return nuxt;
+};
+
+config.mocks = {
+  $t: (key) => `#${key}#`,
 };
 
 module.exports = async () => {
   const nuxt = await buildNuxt();
 
-  // we surface this path as an env var now
-  // so we can import the store dynamically later on
   process.env.buildDir = nuxt.options.buildDir;
 };
