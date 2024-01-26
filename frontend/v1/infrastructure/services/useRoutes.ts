4c427582d39cc372dc0830dd0fe9255180e0ddaa
Damián Pumar <damianpumar@gmail.com>
2024-01-26 13:52:49 +0100

    ✨ Add new method on route

--- frontend/v1/infrastructure/services/useRoutes.ts
+++ frontend/v1/infrastructure/services/useRoutes.ts
@@ -111,6 +111,10 @@ export const useRoutes = () => {
     return decodeURIComponent(value) as T;
   };
 
+  const getParams = () => {
+    return route.value.params;
+  };
+
   const go = (where: string) => {
     router.push(where);
   };
@@ -128,6 +132,7 @@ export const useRoutes = () => {
     getDatasetLink,
     setQueryParams,
     getQueryParams,
+    getParams,
     getPreviousRoute,
     previousRouteMatchWith,
     watchBrowserNavigation: (callBack: () => void) => {
