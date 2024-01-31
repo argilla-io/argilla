82e1bf534f41d58eab1247d063fd401027d410e5
leiyre <leire@argilla.io>
2024-01-31 11:31:45 +0100

    Update sign in claim copy (#4553)

--- frontend/translation/en.js
+++ frontend/translation/en.js
@@ -116,7 +116,7 @@ export default {
   },
   login: {
     title: "Sign in",
-    claim: "Work on data together,</br>make models better",
+    claim: "Work on data together.</br>Make models better.",
     support:
       "To get support from the community, join us on <a href='{link}' target='_blank'>Slack</a>",
     quickstart:
