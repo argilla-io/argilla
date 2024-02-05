b2c4a66acbda4e62bdea177527264b7c6e451397
leire <leire@recogn.ai>
2024-02-05 14:34:29 +0100

    update tooltip styles and update position when window is resized

--- frontend/plugins/directives/tooltip.directive.js
+++ frontend/plugins/directives/tooltip.directive.js
@@ -21,7 +21,7 @@ Vue.directive("tooltip", {
       backgroundColor,
       borderColor,
       color,
-      width = 208,
+      width = 240,
       tooltipPosition = TOOLTIP_DIRECTION.BOTTOM,
     } = binding.value;
 
@@ -128,6 +128,16 @@ Vue.directive("tooltip", {
       );
     };
 
+    element.resize = function () {
+      elementOffset = initElementOffset(element);
+      tooltip = initTooltipPosition(
+        tooltip,
+        tooltipPosition,
+        elementOffset,
+        width
+      );
+    };
+
     // NOTE - init all eventListeners
     initEventsListener(element, closeIcon);
 
@@ -177,6 +187,7 @@ const initEventsListener = (element, closeIcon) => {
       "scroll",
       element.scrollInParent
     );
+    window.addEventListener("resize", element.resize);
   }
 };
 
@@ -197,6 +208,7 @@ const destroyEventsListener = (element) => {
     "scroll",
     element.scrollInParent
   );
+  window.removeEventListener("resize", element.resize);
 };
 
 const initTooltipStyle = (
@@ -212,7 +224,7 @@ const initTooltipStyle = (
   tooltip.style.zIndex = "99999";
   tooltip.style.backgroundColor = backgroundColor;
   tooltip.style.borderRadius = "5px";
-  tooltip.style.padding = "16px";
+  tooltip.style.padding = "8px";
   tooltip.style.boxShadow = "0 8px 20px 0 rgba(0,0,0,.2)";
   tooltip.style.transition = "opacity 0.3s ease 0.2s";
   tooltip.style.border = `2px ${borderColor} solid`;
@@ -224,7 +236,7 @@ const initTooltipHeaderStyle = (tooltipHeader) => {
   tooltipHeader.style.justifyContent = "flex-end";
   tooltipHeader.style.marginBottom = "8px";
   tooltipHeader.innerHTML =
-    '<svg width="20" height="20" viewBox="0 0 41 40" fill="none" xmlns="http://www.w3.org/2000/svg" style="cursor: pointer; width: 14px; height: 14px;"><path d="M8.9225 5.58721C8.13956 4.80426 6.87015 4.80426 6.08721 5.58721C5.30426 6.37015 5.30426 7.63956 6.08721 8.4225L17.6647 20L6.08733 31.5774C5.30438 32.3603 5.30438 33.6297 6.08733 34.4127C6.87027 35.1956 8.13968 35.1956 8.92262 34.4127L20.5 22.8353L32.0774 34.4127C32.8603 35.1956 34.1297 35.1956 34.9127 34.4127C35.6956 33.6297 35.6956 32.3603 34.9127 31.5774L23.3353 20L34.9128 8.4225C35.6957 7.63956 35.6957 6.37015 34.9128 5.58721C34.1298 4.80426 32.8604 4.80426 32.0775 5.58721L20.5 17.1647L8.9225 5.58721Z" fill="#9a9a9a"/></svg>';
+    '<svg width="12" height="12" viewBox="0 0 41 40" fill="none" xmlns="http://www.w3.org/2000/svg" style="cursor: pointer"><path d="M8.9225 5.58721C8.13956 4.80426 6.87015 4.80426 6.08721 5.58721C5.30426 6.37015 5.30426 7.63956 6.08721 8.4225L17.6647 20L6.08733 31.5774C5.30438 32.3603 5.30438 33.6297 6.08733 34.4127C6.87027 35.1956 8.13968 35.1956 8.92262 34.4127L20.5 22.8353L32.0774 34.4127C32.8603 35.1956 34.1297 35.1956 34.9127 34.4127C35.6956 33.6297 35.6956 32.3603 34.9127 31.5774L23.3353 20L34.9128 8.4225C35.6957 7.63956 35.6957 6.37015 34.9128 5.58721C34.1298 4.80426 32.8604 4.80426 32.0775 5.58721L20.5 17.1647L8.9225 5.58721Z" fill="#9a9a9a"/></svg>';
   return tooltipHeader;
 };
 
