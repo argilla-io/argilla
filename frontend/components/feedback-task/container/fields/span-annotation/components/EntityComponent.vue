e41c5ccef3dd9f0213b9e28a182321353a3b6461
leire <leire@recogn.ai>
2024-02-27 15:24:32 +0100

    split EntityComponent and allow to use the keyboard

--- frontend/components/feedback-task/container/fields/span-annotation/components/EntityComponent.vue
+++ frontend/components/feedback-task/container/fields/span-annotation/components/EntityComponent.vue
@@ -3,45 +3,14 @@
     <div @click="toggleDropdown()" class="span-entity" v-if="!visibleDropdown">
       {{ selectedEntity.name }}
     </div>
-    <div v-else class="span-entity__dropdown" v-click-outside="hideDropdown">
-      <div class="span-entity__dropdown__header">
-        <EntityBadge
-          class="span-entity__badge--active"
-          :color="selectedEntity.color"
-          :text="selectedEntity.name"
-          @on-clear="removeSelectedOption(selectedEntity)"
-        ></EntityBadge>
-        <input
-          ref="search"
-          class="span-entity__input"
-          type="text"
-          autocomplete="off"
-          autofocus
-          v-model="searchText"
-          @keydown.arrow-up.stop=""
-          @keydown.arrow-down.stop=""
-          @keydown.arrow-right.stop=""
-          @keydown.arrow-left.stop=""
-          @keydown.delete.exact.stop=""
-          @keydown.enter.exact.stop=""
-          @keydown.backspace.exact.stop=""
-        />
-      </div>
-      <ul class="span-entity__dropdown__content">
-        <li
-          class="span-entity__dropdown__item"
-          v-for="entity in filteredEntities"
-          :key="entity.id"
-          @click="selectEntity(entity)"
-        >
-          <EntityBadge
-            class="span-entity__badge"
-            :color="entity.color"
-            :text="entity.name"
-          ></EntityBadge>
-        </li>
-      </ul>
-    </div>
+    <EntityComponentDropdown
+      v-if="visibleDropdown"
+      :selectedEntity="selectedEntity"
+      :entities="entities"
+      @on-replace-entity="selectEntity"
+      @on-remove-entity="removeSelectedOption"
+      v-click-outside="hideDropdown"
+    />
   </div>
 </template>
 
@@ -64,20 +33,11 @@ export default {
   data() {
     return {
       visibleDropdown: false,
-      searchText: "",
     };
   },
   computed: {
     selectedEntity() {
       return this.entities.find((e) => e.id === this.entity.id);
-    },
-    filteredEntities() {
-      return this.availableEntities.filter((entity) =>
-        entity.name.toLowerCase().includes(this.searchText.toLowerCase())
-      );
-    },
-    availableEntities() {
-      return this.entities.filter((entity) => entity.id !== this.entity.id);
     },
     entities() {
       return this.spanQuestion.answer.entities;
@@ -99,11 +59,6 @@ export default {
       this.visibleDropdown = false;
     },
   },
-  mounted() {
-    this.$nextTick(() => {
-      this.$refs.search?.focus();
-    });
-  },
 };
 </script>
 
@@ -114,54 +69,6 @@ export default {
   font-family: "Roboto Condensed", sans-serif;
   margin-left: 2px;
   cursor: pointer;
-  &__dropdown {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    border-radius: $border-radius;
-    background-color: palette(white);
-    box-shadow: $shadow;
-    z-index: 100;
-    margin-top: 5px;
-    &__header {
-      display: flex;
-      gap: $base-space;
-      background: $black-4;
-      padding: calc($base-space / 2);
-    }
-    &__content {
-      padding: calc($base-space / 2);
-      margin: 0;
-      list-style: none;
-      overflow: auto;
-      max-height: 200px;
-    }
-    &__item {
-      display: flex;
-      padding: calc($base-space / 4);
-      border-radius: 4px;
-      cursor: pointer;
-      &:hover {
-        background-color: $black-4;
-      }
-    }
-  }
-  &__badge.badge {
-    @include font-size(12px);
-  }
-  &__input {
-    height: $base-space * 2;
-    width: 100px;
-    background: transparent;
-    border: none;
-    @include font-size(12px);
-    &:focus-visible {
-      outline: 0;
-    }
-    @include input-placeholder {
-      color: $black-37;
-    }
-  }
 }
 .highlight__entity {
   display: block;
---
+++ frontend/components/feedback-task/container/fields/span-annotation/components/EntityComponentDropdown.vue
@@ -0,0 +1,175 @@
+<template>
+  <div
+    class="span-entity__dropdown"
+    @keyup.enter="includePreselectedOption"
+    @keyup.up="preselectPreviousOption"
+    @keyup.down="preselectNextOption"
+  >
+    <div class="span-entity__dropdown__header">
+      <EntityBadge
+        class="span-entity__badge--active"
+        :color="selectedEntity.color"
+        :text="selectedEntity.name"
+        @on-clear="removeSelectedOption(selectedEntity)"
+      ></EntityBadge>
+      <input
+        ref="search"
+        class="span-entity__input"
+        type="text"
+        autocomplete="off"
+        autofocus
+        v-model="searchText"
+        @keydown.arrow-up.stop=""
+        @keydown.arrow-down.stop=""
+        @keydown.arrow-right.stop=""
+        @keydown.arrow-left.stop=""
+        @keydown.delete.exact.stop=""
+        @keydown.enter.exact.stop=""
+        @keydown.backspace.exact.stop=""
+      />
+    </div>
+    <ul class="span-entity__dropdown__content">
+      <li>
+        <BaseButton
+          v-for="(entity, index) in filteredEntities"
+          :key="entity.id"
+          class="span-entity__dropdown__item"
+          :class="{ '--preselected': preSelectionIndex === index }"
+          @click="selectEntity(entity)"
+          @mouseover.native="preSelectionIndex = index"
+        >
+          <EntityBadge
+            class="span-entity__badge"
+            :color="entity.color"
+            :text="entity.name"
+          ></EntityBadge>
+        </BaseButton>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EntityComponent",
+  props: {
+    selectedEntity: {
+      type: Object,
+      required: true,
+    },
+    entities: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      searchText: "",
+      preSelectionIndex: 0,
+    };
+  },
+  computed: {
+    filteredEntities() {
+      return this.availableEntities.filter((entity) =>
+        entity.name.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+    },
+    availableEntities() {
+      return this.entities.filter(
+        (entity) => entity.id !== this.selectedEntity.id
+      );
+    },
+    optionsLength() {
+      return this.filteredEntities.length;
+    },
+  },
+  methods: {
+    selectEntity(entity) {
+      this.$emit("on-replace-entity", entity);
+    },
+    removeSelectedOption() {
+      this.$emit("on-remove-entity");
+    },
+    includePreselectedOption() {
+      if (!this.filteredEntities.length) return;
+      this.selectEntity(this.filteredEntities[this.preSelectionIndex]);
+      this.preSelectionIndex = 0;
+    },
+    preselectNextOption() {
+      this.preSelectionIndex === this.optionsLength - 1
+        ? (this.preSelectionIndex = 0)
+        : this.preSelectionIndex++;
+    },
+    preselectPreviousOption() {
+      this.preSelectionIndex === 0
+        ? (this.preSelectionIndex = this.optionsLength - 1)
+        : this.preSelectionIndex--;
+    },
+  },
+  watch: {
+    searchText() {
+      this.preSelectionIndex = 0;
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$refs.search?.focus();
+    });
+    this.preselectedEntity = this.filteredEntities[0];
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap");
+.span-entity {
+  &__dropdown {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    border-radius: $border-radius;
+    background-color: palette(white);
+    box-shadow: $shadow;
+    z-index: 100;
+    margin-top: 5px;
+    &__header {
+      display: flex;
+      gap: $base-space;
+      background: $black-4;
+      padding: calc($base-space / 2);
+    }
+    &__content {
+      padding: calc($base-space / 2);
+      margin: 0;
+      list-style: none;
+      overflow: auto;
+      max-height: 200px;
+    }
+    &__item.button {
+      display: flex;
+      width: 100%;
+      padding: calc($base-space / 4);
+      border-radius: 4px;
+      &.--preselected {
+        background-color: $black-4;
+      }
+    }
+  }
+  &__badge.badge {
+    @include font-size(12px);
+  }
+  &__input {
+    height: $base-space * 2;
+    width: 100px;
+    background: transparent;
+    border: none;
+    @include font-size(12px);
+    &:focus-visible {
+      outline: 0;
+    }
+    @include input-placeholder {
+      color: $black-37;
+    }
+  }
+}
+</style>
