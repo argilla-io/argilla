74c70f116c6ebc5fba9cd77fd73e66f4a73b9fb8
Paco Aranda <francis@argilla.io>
2024-03-05 17:39:47 +0100

    adapt tests

--- tests/integration/client/conftest.py
+++ tests/integration/client/conftest.py
@@ -25,7 +25,7 @@ from argilla.client.feedback.schemas import (
     LabelQuestion,
     MultiLabelQuestion,
     RankingQuestion,
-    RatingQuestion,
+    RankingValueSchema, RatingQuestion,
     TextField,
     TextQuestion,
 )
@@ -481,7 +481,7 @@ def feedback_dataset_records() -> List[FeedbackRecord]:
                         "question-2": {"value": 2},
                         "question-3": {"value": "b"},
                         "question-4": {"value": ["b", "c"]},
-                        "question-5": {"value": [{"rank": 1, "value": "a"}, {"rank": 2, "value": "b"}]},
+                        "question-5": {"value": [RankingValueSchema(rank=1, value="a"), RankingValueSchema(rank= 2, value="b")]},
                     },
                     "status": "submitted",
                 }
@@ -517,7 +517,7 @@ def feedback_dataset_records() -> List[FeedbackRecord]:
                 },
                 {
                     "question_name": "question-5",
-                    "value": [{"rank": 1, "value": "a"}, {"rank": 2, "value": "b"}],
+                    "value": [RankingValueSchema(rank=1, value="a"), RankingValueSchema(rank= 2, value="b")],
                     "type": "human",
                     "score": 0.0,
                     "agent": "agent-1",
