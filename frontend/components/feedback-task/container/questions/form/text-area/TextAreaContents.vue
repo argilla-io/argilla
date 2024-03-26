ac6c400543b2c067bdb682b5e803b5616a581821
Damián Pumar <damianpumar@gmail.com>
2024-03-26 09:37:51 +0100

    ✨ Fix lint

--- frontend/components/feedback-task/container/questions/form/text-area/TextAreaContents.vue
+++ frontend/components/feedback-task/container/questions/form/text-area/TextAreaContents.vue
@@ -107,8 +107,10 @@ export default {
           /[\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC]/g
         ) || []
       ).length;
+
       const ltr_count = (
         this.question.answer.value.match(
+          // eslint-disable-next-line no-misleading-character-class
           /[A-Za-z\u00C0-\u00C0\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF]/g
         ) || []
       ).length;
