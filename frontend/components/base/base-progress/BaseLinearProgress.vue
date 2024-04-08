cb5368adbbdbf9fae0586786af7684961f83e761
leire <leire@recogn.ai>
2024-04-08 17:12:09 +0200

    update styles

--- frontend/components/base/base-progress/BaseLinearProgress.vue
+++ frontend/components/base/base-progress/BaseLinearProgress.vue
@@ -1,27 +1,42 @@
 <template>
   <transition appear :duration="500" name="animate-progress">
     <div class="progress__wrapper">
-      <div
-        v-for="{ value, name, color, tooltip } in filteredProgressRanges"
-        :key="name"
-        role="progressbar"
-        :class="[
-          'progress__range',
-          showTooltip ? 'progress__range--with-tooltip' : null,
-        ]"
-        :style="{
-          width: `${getPercentage(value)}%`,
-        }"
-      >
-        <div class="progress__bar" :style="{ backgroundColor: color }"></div>
-        <div v-if="showTooltip" class="progress__tooltip">
-          <span
-            class="progress__tooltip__percent-info"
-            v-text="`${name}: ${getPercentage(value)}%`"
-          />
-          {{ tooltip }}
+      <div class="progress__container">
+        <div
+          v-for="range in filteredProgressRanges"
+          :key="range.name"
+          role="progressbar"
+          :class="[
+            'progress__range',
+            showTooltip ? 'progress__range--with-tooltip' : null,
+          ]"
+          :style="{
+            width: `${getPercentage(range.value)}%`,
+          }"
+          @mouseenter="hoveredRange = range"
+          @mouseleave="hoveredRange = null"
+        >
+          <div
+            class="progress__bar"
+            :style="{ backgroundColor: range.color }"
+          ></div>
         </div>
       </div>
+      <template v-if="showTooltip && !!hoveredRange">
+        <span
+          class="progress__tooltip__triangle"
+          :style="{ left: `${getTrianglePosition(hoveredRange)}%` }"
+        />
+        <div class="progress__tooltip">
+          <span
+            class="progress__tooltip__percent-info"
+            v-text="
+              `${hoveredRange.name}: ${getPercentage(hoveredRange.value)}%`
+            "
+          />
+          {{ hoveredRange.tooltip }}
+        </div>
+      </template>
     </div>
   </transition>
 </template>
@@ -42,6 +57,11 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      hoveredRange: null,
+    };
+  },
   computed: {
     filteredProgressRanges() {
       return this.progressRanges.filter((range) => range.value > 0);
@@ -51,13 +71,24 @@ export default {
     getPercentage(value) {
       return ((value / this.progressMax) * 100).toFixed();
     },
+    getTrianglePosition(range) {
+      if (!range) return;
+
+      return this.getPercentage(
+        range.value / 2 + this.getPreviousRangesPercent(range)
+      );
+    },
+    getPreviousRangesPercent(range) {
+      return this.filteredProgressRanges
+        .slice(0, this.filteredProgressRanges.indexOf(range))
+        .reduce((acc, r) => acc + r.value, 0);
+    },
   },
 };
 </script>
 
 <styles lang="scss" scoped>
 $progressHeight: 12px;
-$progressBackgroundColor: palette(grey, 600);
 $tooltipBackgroundColor: palette(grey, 600);
 $tooltipTriangleSize: 5px;
 $borderRadius: 10px;
@@ -66,17 +97,20 @@ $borderRadius: 10px;
   $this: &;
   &__wrapper {
     position: relative;
+  }
+  &__container {
+    position: relative;
     display: flex;
+    width: 100%;
     height: $progressHeight;
-    background: $progressBackgroundColor;
     border-radius: $borderRadius;
-    padding-left: 4px;
+    overflow: hidden;
   }
   &__bar {
     position: relative;
     height: 100%;
-    margin: 0 -4px;
     border-radius: $borderRadius;
+    margin: 0 -4px;
     box-shadow: 0 0 0 1px palette(white);
     z-index: 1;
     &:after {
@@ -85,7 +119,6 @@ $borderRadius: 10px;
     }
   }
   &__tooltip {
-    opacity: 0;
     position: absolute;
     display: flex;
     justify-content: space-between;
@@ -104,29 +137,19 @@ $borderRadius: 10px;
     &__percent-info {
       font-weight: 600;
     }
+    &__triangle {
+      opacity: 1;
+      position: absolute;
+      bottom: calc(100% - #{$tooltipTriangleSize} + 2px);
+      left: 50%;
+      transform: translateX(-50%);
+      border-width: $tooltipTriangleSize;
+      border-style: solid;
+      border-color: $tooltipBackgroundColor transparent transparent transparent;
+      transition: opacity 0.2s 0.2s;
+    }
   }
   &__range {
-    &--with-tooltip {
-      &:hover {
-        #{$this}__tooltip {
-          opacity: 1;
-        }
-        #{$this}__bar {
-          &:after {
-            opacity: 1;
-            position: absolute;
-            bottom: calc(100% - #{$tooltipTriangleSize} + 2px);
-            left: 50%;
-            transform: translateX(-50%);
-            border-width: $tooltipTriangleSize;
-            border-style: solid;
-            border-color: $tooltipBackgroundColor transparent transparent
-              transparent;
-            transition: opacity 0.2s 0.2s;
-          }
-        }
-      }
-    }
     &:last-of-type:not(:first-of-type) {
       #{$this}__bar {
         margin: 0;
