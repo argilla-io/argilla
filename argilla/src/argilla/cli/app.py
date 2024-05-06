af7264d6fcd437df314ec51efe058f8a1ed248fa
Francisco Aranda <francis@argilla.io>
2024-05-06 22:32:08 +0200

    revert change

--- argilla/src/argilla/cli/app.py
+++ argilla/src/argilla/cli/app.py
@@ -31,8 +31,6 @@ warnings.simplefilter("ignore", UserWarning)
 
 app = ArgillaTyper(help="Argilla CLI", no_args_is_help=True)
 
-# Force running the ci build pipeline
-
 
 @app.error_handler(PermissionError)
 def handler_permission_error(e: PermissionError) -> None:
