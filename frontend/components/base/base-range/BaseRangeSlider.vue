e674e3213534dae2ebc7dca8d8ed9bdfad177e5f
Leire <leire@argilla.io>
2024-05-08 10:05:15 +0200

    fix: UI QA improvements and small fixes (#4790)
    UI QA improvements and small fixes for release 28

--- frontend/components/base/base-range/BaseRangeSlider.vue
+++ frontend/components/base/base-range/BaseRangeSlider.vue
@@ -2,7 +2,6 @@
   <div>
     <label :for="id"><slot /></label>
     <div class="range__wrapper">
-      <span class="range__legend" v-text="min" />
       <div class="range">
         <span class="range__progress-value" ref="progress" v-text="range" />
         <input
@@ -15,7 +14,10 @@
           v-model.number="range"
         />
       </div>
-      <span class="range__legend" v-text="max" />
+      <div class="range__legends">
+        <span v-text="min" />
+        <span v-text="max" />
+      </div>
     </div>
   </div>
 </template>
@@ -65,7 +67,7 @@ export default {
       this.$refs.progress.style.left = `calc(${this.progress}% + (${
         8 - this.progress * 0.15
       }px))`;
-      this.$refs.slider.style.background = `linear-gradient(to right, #3e5cc9 ${this.progress}%, #ccc ${this.progress}%)`;
+      this.$refs.slider.style.background = `linear-gradient(to right, #3e5cc9 ${this.progress}%, rgba(0, 0, 0, 0.2) ${this.progress}%)`;
     },
   },
   created() {
@@ -78,6 +80,7 @@ export default {
 <style lang="scss" scoped>
 $slider-color: $primary-color;
 $slider-light-color: rgba(0, 26, 255, 0.1);
+$slider-thumb-size: 16px;
 label {
   width: fit-content;
   height: 14px;
@@ -90,11 +93,14 @@ label {
   min-width: 240px;
   &__wrapper {
     display: flex;
-    gap: $base-space;
+    flex-direction: column;
     align-items: center;
     margin-top: $base-space;
   }
-  &__legend {
+  &__legends {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
     color: $black-37;
     @include font-size(12px);
   }
@@ -135,8 +141,8 @@ label {
 input[type="range"]::-webkit-slider-thumb {
   -webkit-appearance: none;
   appearance: none;
-  height: 15px;
-  width: 15px;
+  height: $slider-thumb-size;
+  width: $slider-thumb-size;
   background-color: $slider-color;
   border-radius: 50%;
   border: none;
@@ -144,8 +150,8 @@ input[type="range"]::-webkit-slider-thumb {
 }
 
 input[type="range"]::-moz-range-thumb {
-  height: 15px;
-  width: 15px;
+  height: $slider-thumb-size;
+  width: $slider-thumb-size;
   background-color: $slider-color;
   border-radius: 50%;
   border: none;
