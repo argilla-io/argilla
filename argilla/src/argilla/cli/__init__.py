1f552b5a3eabec0a715134283b8bf654f5b0f9a4
Francisco Aranda <francis@argilla.io>
2024-05-13 16:08:18 +0200

    ci: moving argilla project (#4816)
    <!-- Thanks for your contribution! As part of our Community Growers
    initiative 🌱, we're donating Justdiggit bunds in your name to reforest
    sub-Saharan Africa. To claim your Community Growers certificate, please
    contact David Berenstein in our Slack community or fill in this form
    https://tally.so/r/n9XrxK once your PR has been merged. -->
    
    # Description
    
    Moving the argilla project to `argilla` folder. 
    
    
    
    **Type of change**
    
    (Please delete options that are not relevant. Remember to title the PR
    according to the type of change)
    
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Refactor (change restructuring the codebase without changing
    functionality)
    - [ ] Improvement (change adding some improvement to an existing
    functionality)
    
    **How Has This Been Tested**
    
    (Please describe the tests that you ran to verify your changes. And
    ideally, reference `tests`)
    
    - [ ] Test A
    - [ ] Test B
    
    **Checklist**
    
    - [ ] I added relevant documentation
    - [ ] I followed the style guidelines of this project
    - [ ] I did a self-review of my code
    - [ ] I made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I filled out [the contributor form](https://tally.so/r/n9XrxK)
    (see text above)
    - [ ] I have added relevant notes to the `CHANGELOG.md` file (See
    https://keepachangelog.com/)

--- argilla/src/argilla/cli/__init__.py
+++ argilla/src/argilla/cli/__init__.py
@@ -11,7 +11,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-#
 
 from .datasets import app as datasets_app
 from .info import app as info_app
