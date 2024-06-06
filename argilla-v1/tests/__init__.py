6237f964a080e3e0f39c6c94bf685df19efb1ec4
Francisco Aranda <francis@argilla.io>
2024-06-06 16:26:46 +0200

    chore: Move argilla project to argilla-v1

--- argilla-v1/tests/__init__.py
+++ argilla-v1/tests/__init__.py
@@ -1,3 +1,4 @@
+#  coding=utf-8
 #  Copyright 2021-present, the Recognai S.L. team.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,3 +12,15 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+# The following configuration tries to limit use of numpy threading and
+# possible problems with fault segmentation
+# For more info, please visit https://gist.github.com/EricCousineau-TRI/8a2d1550f5fa4be4fed87d55a522dbf2
+import os
+
+os.environ.update(
+    OMP_NUM_THREADS="1",
+    OPENBLAS_NUM_THREADS="1",
+    NUMEXPR_NUM_THREADS="1",
+    MKL_NUM_THREADS="1",
+)
