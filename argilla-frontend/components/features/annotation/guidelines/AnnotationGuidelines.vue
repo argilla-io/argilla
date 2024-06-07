d44a3752c273bfa14bf4c86c4a10a21e5bd56522
leire <leire@recogn.ai>
2024-06-07 16:07:19 +0200

    remove sidebar components and restructurecomponents in folders

---
+++ argilla-frontend/components/features/annotation/guidelines/AnnotationGuidelines.vue
@@ -0,0 +1,18 @@
+<template>
+  <RenderMarkdownBaseComponent
+    class="--body1 description__text"
+    :markdown="guidelines"
+  />
+</template>
+
+<script>
+export default {
+  name: "SettingsInfo",
+  props: {
+    guidelines: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
