9b5d2db4c0bc9d5f2391ed6b43cc2a2cbe0e9dae
Francisco Aranda <francis@argilla.io>
2024-07-08 14:57:36 +0200

    feat: Add logic to fetch records by id an load conditional relationships

--- argilla-server/src/argilla_server/repositories/records.py
+++ argilla-server/src/argilla_server/repositories/records.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import Union, List, Tuple, Sequence
+from typing import Union, List, Tuple, Sequence, Optional
 from uuid import UUID
 
 from fastapi import Depends
@@ -21,7 +21,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload, contains_eager
 
 from argilla_server.database import get_async_db
-from argilla_server.models import Record, VectorSettings, Vector
+from argilla_server.models import Record, VectorSettings, Vector, Response
 
 
 class RecordsRepository:
@@ -41,11 +41,42 @@ class RecordsRepository:
         with_vectors: Union[bool, List[str]] = False,
     ) -> Tuple[Sequence[Record], int]:
         query = select(Record).filter_by(dataset_id=dataset_id)
+        query = await self._configure_record_load_relationships(dataset_id, query, with_suggestions, with_vectors)
 
         if with_responses:
             query = query.options(selectinload(Record.responses))
+
+        records = (await self.db.scalars(query.offset(offset).limit(limit).order_by(Record.inserted_at))).unique().all()
+        total = await self.db.scalar(select(func.count(Record.id)).filter_by(dataset_id=dataset_id))
+
+        return records, total
+
+    async def list_by_dataset_id_and_ids(
+        self,
+        dataset_id: UUID,
+        ids: List[UUID],
+        user_id: Optional[UUID] = None,
+        with_responses: bool = False,
+        with_suggestions: bool = False,
+        with_vectors: Union[bool, List[str]] = False,
+    ):
+        query = select(Record).filter_by(dataset_id=dataset_id).filter(Record.id.in_(ids))
+        query = await self._configure_record_load_relationships(dataset_id, query, with_suggestions, with_vectors)
+
+        if with_responses:
+            if user_id:
+                query = query.outerjoin(
+                    Response, and_(Response.record_id == Record.id, Response.user_id == user_id)
+                ).options(contains_eager(Record.responses))
+            else:
+                query = query.options(selectinload(Record.responses))
+
+        return (await self.db.scalars(query)).unique().all()
+
+    async def _configure_record_load_relationships(self, dataset_id, query, with_suggestions, with_vectors):
         if with_suggestions:
             query = query.options(selectinload(Record.suggestions))
+
         if with_vectors is True:
             query = query.options(selectinload(Record.vectors))
         elif isinstance(with_vectors, list):
@@ -56,8 +87,4 @@ class RecordsRepository:
                 Vector, and_(Vector.record_id == Record.id, Vector.vector_settings_id.in_(subquery))
             ).options(contains_eager(Record.vectors))
 
-        records = (await self.db.scalars(query.offset(offset).limit(limit).order_by(Record.inserted_at))).unique().all()
-
-        total = await self.db.scalar(select(func.count(Record.id)).filter_by(dataset_id=dataset_id))
-
-        return records, total
+        return query
