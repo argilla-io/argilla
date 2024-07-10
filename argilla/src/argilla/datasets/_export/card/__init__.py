b68f692a6994159bebfc4051d9b0b77bec94dae0
Ben Burtenshaw <benjamin.burtenshaw@huggingface.co>
2024-07-10 13:22:20 +0200

    chore: use full imports in card module

--- argilla/src/argilla/datasets/_export/card/__init__.py
+++ argilla/src/argilla/datasets/_export/card/__init__.py
@@ -14,7 +14,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from ._dataset_card import ArgillaDatasetCard
-from ._parser import size_categories_parser
+from argilla.datasets._export.card._dataset_card import ArgillaDatasetCard
+from argilla.datasets._export.card._parser import size_categories_parser
 
 __all__ = ["ArgillaDatasetCard", "size_categories_parser"]
