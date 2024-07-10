eadc5ea09d0b1ca8289a565f3181da21a776e293
davidberenstein1957 <david.m.berenstein@gmail.com>
2024-07-10 13:49:36 +0200

    Add updated workflow gen_popular_issues

--- argilla/docs/scripts/gen_popular_issues.py
+++ argilla/docs/scripts/gen_popular_issues.py
@@ -79,53 +79,57 @@ def fetch_data_from_github(repository, auth_token):
 with mkdocs_gen_files.open(DATA_PATH, "w") as f:
     df = fetch_data_from_github(REPOSITORY, GITHUB_ACCESS_TOKEN)
 
-    if len(df):
-        open_issues = df.loc[df["State"] == "open"]
-        engagement_df = (
-            open_issues[["URL", "Issue", "Reactions", "Comments"]]
-            .sort_values(by=["Reactions", "Comments"], ascending=False)
-            .head(10)
-            .reset_index()
-        )
+    df["Milestone"] = df["Milestone"].astype(str).fillna("")
+    planned_issues = df[
+        ((df["Milestone"].str.startswith("v2")) & (df["State"] == "open"))
+        | ((df["Milestone"].str.startswith("2")) & (df["State"] == "open"))
+    ]
+    open_issues = df.loc[df["State"] == "open"]
+    engagement_df = (
+        open_issues[["URL", "Issue", "Reactions", "Comments"]]
+        .sort_values(by=["Reactions", "Comments"], ascending=False)
+        .head(10)
+        .reset_index()
+    )
 
-        community_issues = df[df["Author association"] != "MEMBER"]
-        community_issues_df = (
-            community_issues[["URL", "Issue", "Created at", "Author", "State"]]
-            .sort_values(by=["Created at"], ascending=False)
-            .head(10)
-            .reset_index()
-        )
+    community_issues = df[df["Author association"] != "MEMBER"]
+    community_issues_df = (
+        community_issues[["URL", "Issue", "Created at", "Author", "State"]]
+        .sort_values(by=["Created at"], ascending=False)
+        .head(10)
+        .reset_index()
+    )
 
-        planned_issues = df[
-            ((df["Milestone"].str.startswith("v2")) & (df["State"] == "open"))
-            | ((df["Milestone"].str.startswith("2")) & (df["State"] == "open"))
-        ]
-        planned_issues_df = (
-            planned_issues[["URL", "Issue", "Created at", "Milestone", "State"]]
-            .sort_values(by=["Milestone"], ascending=True)
-            .head(10)
-            .reset_index()
-        )
+    planned_issues = df[
+        ((df["Milestone"].str.startswith("v2")) & (df["State"] == "open"))
+        | ((df["Milestone"].str.startswith("2")) & (df["State"] == "open"))
+    ]
+    planned_issues_df = (
+        planned_issues[["URL", "Issue", "Created at", "Milestone", "State"]]
+        .sort_values(by=["Milestone"], ascending=True)
+        .head(10)
+        .reset_index()
+    )
 
-        f.write('=== "Most engaging open issues"\n\n')
-        f.write("    | Rank | Issue | Reactions | Comments |\n")
-        f.write("    |------|-------|:---------:|:--------:|\n")
-        for ix, row in engagement_df.iterrows():
-            f.write(f"    | {ix+1} | [{row['Issue']}]({row['URL']}) | 👍 {row['Reactions']} | 💬 {row['Comments']} |\n")
+    f.write('=== "Most engaging open issues"\n\n')
+    f.write("    | Rank | Issue | Reactions | Comments |\n")
+    f.write("    |------|-------|:---------:|:--------:|\n")
+    for ix, row in engagement_df.iterrows():
+        f.write(f"    | {ix+1} | [{row['Issue']}]({row['URL']}) | 👍 {row['Reactions']} | 💬 {row['Comments']} |\n")
 
-        f.write('\n=== "Latest issues open by the community"\n\n')
-        f.write("    | Rank | Issue | Author |\n")
-        f.write("    |------|-------|:------:|\n")
-        for ix, row in community_issues_df.iterrows():
-            state = "🟢" if row["State"] == "open" else "🟣"
-            f.write(f"    | {ix+1} | {state} [{row['Issue']}]({row['URL']}) | by **{row['Author']}** |\n")
+    f.write('\n=== "Latest issues open by the community"\n\n')
+    f.write("    | Rank | Issue | Author |\n")
+    f.write("    |------|-------|:------:|\n")
+    for ix, row in community_issues_df.iterrows():
+        state = "🟢" if row["State"] == "open" else "🟣"
+        f.write(f"    | {ix+1} | {state} [{row['Issue']}]({row['URL']}) | by **{row['Author']}** |\n")
 
-        f.write('\n=== "Planned issues for upcoming releases"\n\n')
-        f.write("    | Rank | Issue | Milestone |\n")
-        f.write("    |------|-------|:------:|\n")
-        for ix, row in planned_issues_df.iterrows():
-            state = "🟢" if row["State"] == "open" else "🟣"
-            f.write(f"    | {ix+1} | {state} [{row['Issue']}]({row['URL']}) |  **{row['Milestone']}** |\n")
+    f.write('\n=== "Planned issues for upcoming releases"\n\n')
+    f.write("    | Rank | Issue | Milestone |\n")
+    f.write("    |------|-------|:------:|\n")
+    for ix, row in planned_issues_df.iterrows():
+        state = "🟢" if row["State"] == "open" else "🟣"
+        f.write(f"    | {ix+1} | {state} [{row['Issue']}]({row['URL']}) |  **{row['Milestone']}** |\n")
 
-        today = datetime.today().date()
-        f.write(f"\nLast update: {today}\n")
+    today = datetime.today().date()
+    f.write(f"\nLast update: {today}\n")
