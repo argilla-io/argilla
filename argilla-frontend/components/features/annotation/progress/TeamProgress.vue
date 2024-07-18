20ae66341c6069894d987a8c3e75ef3d1e2d7079
Leire <leire@argilla.io>
2024-07-18 16:53:04 +0200

    🔀 Update UI for distribution task (#5219)
    - [x] Update progress bar styles
    - [x] Show two decimals in the progress bar of the dataset list
    - [x] Remove donut chart and replace with small cards
    - [x] Replace my progress bar with team progress bar
    - [x] Show submitted info when panel is collapsed
    
    ---------
    
    Co-authored-by: Damián Pumar <damianpumar@gmail.com>
    Co-authored-by: David Berenstein <david.m.berenstein@gmail.com>

---
+++ argilla-frontend/components/features/annotation/progress/TeamProgress.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="team-progress">
+    <BaseLinearProgress
+      class="team-progress__bar"
+      :progress-ranges="progressRanges"
+      :progress-max="progress.total"
+    />
+    <span class="team-progress__percent"
+      >{{ progress.percentage.completed }}%</span
+    >
+  </div>
+</template>
+
+<script>
+import { useTeamProgressViewModel } from "./useTeamProgressViewModel";
+
+export default {
+  props: {
+    datasetId: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    progressRanges() {
+      return [
+        {
+          id: "completed",
+          name: "completed",
+          color: "linear-gradient(90deg, #6A6A6C 0%, #252626 100%)",
+          value: this.progress.completed,
+        },
+        {
+          id: "pending",
+          name: "progress",
+          color: "linear-gradient(white)",
+          value: this.progress.pending,
+        },
+      ];
+    },
+  },
+  setup(props) {
+    return useTeamProgressViewModel(props);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.team-progress {
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: $base-space * 2;
+  width: 100%;
+  max-width: 250px;
+  color: $black-54;
+  @include font-size(12px);
+  &__bar {
+    width: 100%;
+    max-width: 160px;
+  }
+  &__data {
+    font-weight: 500;
+  }
+  &__percent {
+    font-weight: lighter;
+  }
+}
+</style>
