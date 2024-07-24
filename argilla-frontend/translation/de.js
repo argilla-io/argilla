9f56d20ffe9c0b270aebeede4392136f9100bf4d
Paco Aranda <frascuchon@gmail.com>
2024-07-24 11:43:32 +0200

    chore: Remove all the quickstart-related logic (#5288)
    # Description
    <!-- Please include a summary of the changes and the related issue.
    Please also include relevant motivation and context. List any
    dependencies that are required for this change. -->
    
    This PR removes all logic related to quickstart/non-quickstart
    deployment. This is not needed anymore.
    
    **Type of change**
    <!-- Please delete options that are not relevant. Remember to title the
    PR according to the type of change -->
    
    - Refactor (change restructuring the codebase without changing
    functionality)
    
    **How Has This Been Tested**
    <!-- Please add some reference about how your feature has been tested.
    -->
    
    **Checklist**
    <!-- Please go over the list and make sure you've taken everything into
    account -->
    
    - I added relevant documentation
    - I followed the style guidelines of this project
    - I did a self-review of my code
    - I made corresponding changes to the documentation
    - I confirm My changes generate no new warnings
    - I have added tests that prove my fix is effective or that my feature
    works
    - I have added relevant notes to the CHANGELOG.md file (See
    https://keepachangelog.com/)

--- argilla-frontend/translation/de.js
+++ argilla-frontend/translation/de.js
@@ -178,8 +178,6 @@ export default {
     hide: "Ausblenden",
     passwordDescription: "Gebe Sie ihr Passwort ein",
     claim: "Gemeinsames Arbeiten an Daten.</br>Verbessern Sie Ihre Modelle.",
-    quickstart:
-      "Sie verwenden die Quickstart-Version von Argilla. Überprüfen Sie <a href='{link}' target='_blank'>diesen Leitfaden</a> um mehr über Nutzung und Konfigurationsoptionen zu erfahren.",
     hf: {
       title: "Willkommen bei {space}",
       subtitle:
