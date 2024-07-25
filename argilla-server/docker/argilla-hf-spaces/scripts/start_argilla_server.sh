07a73124034b91d49faa90ace2659cbce7b3ea15
Francisco Aranda <francis@argilla.io>
2024-07-25 18:05:43 +0200

    chore: Moving out space env var assignation

--- argilla-server/docker/argilla-hf-spaces/scripts/start_argilla_server.sh
+++ argilla-server/docker/argilla-hf-spaces/scripts/start_argilla_server.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-# Preset oauth env vars based on injected space variables.
-# See https://huggingface.co/docs/hub/en/spaces-oauth#create-an-oauth-app
-export OAUTH2_HUGGINGFACE_CLIENT_ID=$OAUTH_CLIENT_ID
-export OAUTH2_HUGGINGFACE_CLIENT_SECRET=$OAUTH_CLIENT_SECRET
-export OAUTH2_HUGGINGFACE_SCOPE=$OAUTH_SCOPES
-
 echo "Running database migrations"
 python -m argilla_server database migrate
 
