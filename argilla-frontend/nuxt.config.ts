ea7405b4b99d0fc7a8d43df19afa6d07adf3c122
Leire <leire@argilla.io>
2024-07-30 11:33:33 +0200

    UI - update docs links (#5321)

--- argilla-frontend/nuxt.config.ts
+++ argilla-frontend/nuxt.config.ts
@@ -203,15 +203,9 @@ const config: NuxtConfig = {
 
   publicRuntimeConfig: {
     clientVersion: pkg.version,
-    documentationSite: "https://docs.v1.argilla.io/",
-    documentationSiteSemanticSearch:
-      "https://docs.v1.argilla.io/en/latest/reference/webapp/features.html#semantic-search",
-    documentationSiteLabelScheme:
-      "https://docs.v1.argilla.io/en/latest/guides/log_load_and_prepare_data.html#define-a-labeling-schema",
-    documentationSiteQueryDatasets:
-      "https://docs.v1.argilla.io/en/latest/guides/query_datasets.html",
+    documentationSite: "https://docs.argilla.io/",
     documentationPersistentStorage:
-      "https://docs.v1.argilla.io/en/latest/getting_started/installation/deployments/huggingface-spaces.html#setting-up-persistent-storage",
+      "https://docs.argilla.io/latest/getting_started/how-to-configure-argilla-on-huggingface/#persistent-storage",
   },
 };
 export default config;
