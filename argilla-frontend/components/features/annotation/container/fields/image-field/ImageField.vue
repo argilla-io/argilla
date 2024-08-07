9b576295237608584f100dab85dd1d31c02e1683
Leire Aguirre <leire@argilla.io>
2024-08-07 12:19:34 +0200

    include spinner

--- argilla-frontend/components/features/annotation/container/fields/image-field/ImageField.vue
+++ argilla-frontend/components/features/annotation/container/fields/image-field/ImageField.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="image_field" :key="content">
     <span class="image_field_title" v-text="title" />
-    <img v-if="!hasError" :src="content" @error="handleError()" />
+    <template v-if="!hasError">
+      <BaseSpinner v-if="!isLoaded" />
+      <img
+        v-show="isLoaded"
+        :src="content"
+        @error="handleError()"
+        @load="onLoad()"
+      />
+    </template>
     <div v-else class="image_field_placeholder">
       <img src="images/img-placeholder.svg" />
       <p v-text="$t('couldNotLoadImage')" />
@@ -28,11 +36,15 @@ export default {
   data() {
     return {
       hasError: false,
+      isLoaded: false,
     };
   },
   methods: {
     handleError() {
       this.hasError = true;
+    },
+    onLoad() {
+      this.isLoaded = true;
     },
   },
 };
