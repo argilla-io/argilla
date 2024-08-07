63217b5bbf1a487317f9b55052f6548944f21dc7
Ben Burtenshaw <benjamin.burtenshaw@huggingface.co>
2024-08-07 12:31:06 +0200

    chore: expand error message

--- argilla/src/argilla/records/_mapping/_mapper.py
+++ argilla/src/argilla/records/_mapping/_mapper.py
@@ -95,9 +95,10 @@ class IngestedRecordMapper:
 
         if data and not (record_id or suggestions or responses or fields or metadata or vectors):
             raise RecordsIngestionError(
-                message=f"""Record has no identifiable keys. If keys in source dataset do not match the names in `dataset.settings`, you should use a `mapping` with the `dataset.records.log` method.
-                Available keys: {self.mapping.keys()}.
-                Unkown keys: {unknown_keys}. """
+                message=f"""Record has no known keys from `Dataset.settings` or `mapping`. 
+                If keys in source dataset do not match the names in `Dataset.settings`, you should use the `mapping` parameter of the `dataset.records.log` method.
+                Available keys in `Dataset.settings` and `mapping`: {self.mapping.keys()}.
+                Unkown keys in Record: {unknown_keys}. """
             )
 
         return Record(
