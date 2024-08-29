3abbaa2fff48c5238d3830393ebdaadac5879687
burtenshaw <ben@argilla.io>
2024-08-29 14:36:18 +0200

    feat: move image casting into record fields (#5436)
    # Description
    <!-- Please include a summary of the changes and the related issue.
    Please also include relevant motivation and context. List any
    dependencies that are required for this change. -->
    
    This PR experiments with cast images within the `Record` class to
    abstract away data uri. This supports this flow:
    
    ```python
    image_paths = [f"{my_image_dir}/{fn}" for fn in os.listdir(my_image_dir)]
    
    records = [
        rg.Record(
            fields={
                "image": image_path,
            },
        )
        for image_path in image_paths
    ]
    
    
    dataset.records.log(records)
    ```
    
    **Type of change**
    <!-- Please delete options that are not relevant. Remember to title the
    PR according to the type of change -->
    
    - Bug fix (non-breaking change which fixes an issue)
    - New feature (non-breaking change which adds functionality)
    - Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - Refactor (change restructuring the codebase without changing
    functionality)
    - Improvement (change adding some improvement to an existing
    functionality)
    - Documentation update
    
    **How Has This Been Tested**
    <!-- Please add some reference about how your feature has been tested.
    -->
    
    **Checklist**
    <!-- Please go over the list and make sure you've taken everything into
    account -->
    
    - I added relevant documentation
    - I followed the style guidelines of this project
    - I did a self-review of my code
    - I made corresponding changes to the documentation
    - I confirm My changes generate no new warnings
    - I have added tests that prove my fix is effective or that my feature
    works
    - I have added relevant notes to the CHANGELOG.md file (See
    https://keepachangelog.com/)
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- argilla/tests/unit/test_media.py
+++ argilla/tests/unit/test_media.py
@@ -11,20 +11,76 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from tempfile import NamedTemporaryFile
 
 import pytest
 from PIL import Image
-from argilla.media import pil_to_data_uri
+from argilla._helpers._media import cast_image, pil_to_data_uri, uncast_image
 
 
-def test_pil_to_data_uri_with_valid_image():
+@pytest.fixture
+def pil_image():
     image = Image.new("RGB", (100, 100), color="red")
-    data_uri = pil_to_data_uri(image)
-    assert isinstance(data_uri, str)
-    assert data_uri.startswith("data:image/png;base64,")
+    return image
 
 
-def test_pil_to_data_uri_with_invalid_image():
-    image = "not an image"
+@pytest.fixture
+def data_uri_image(pil_image):
+    data_uri = pil_to_data_uri(pil_image)
+    return data_uri
+
+
+@pytest.fixture
+def path_to_image(pil_image):
+    with NamedTemporaryFile(suffix=".jpg") as f:
+        pil_image.save(f.name)
+        yield f.name
+
+
+def test_cast_image_with_pil_image(pil_image):
+    result = cast_image(pil_image)
+    uncasted = uncast_image(result)
+
+    assert isinstance(result, str)
+    assert result.startswith("data:image")
+    assert "base64" in result
+
+    assert isinstance(uncasted, Image.Image)
+    assert uncasted.size == pil_image.size
+    assert uncasted.mode == pil_image.mode
+    assert uncasted.getcolors() == pil_image.getcolors()
+
+
+def test_cast_image_with_file_path(path_to_image):
+    result = cast_image(path_to_image)
+    uncasted = uncast_image(result)
+    pil_image = Image.open(path_to_image)
+
+    assert isinstance(result, str)
+    assert result.startswith("data:image")
+    assert "base64" in result
+
+    assert isinstance(uncasted, Image.Image)
+    assert uncasted.size == pil_image.size
+    assert uncasted.mode == pil_image.mode
+    assert uncasted.getcolors() == pil_image.getcolors()
+
+
+def test_cast_image_with_data_uri(data_uri_image):
+    result = cast_image(data_uri_image)
+    uncasted = uncast_image(result)
+
+    assert result == data_uri_image
+    assert isinstance(uncasted, Image.Image)
+
+
+def test_cast_image_with_invalid_input():
+    invalid_input = 123
     with pytest.raises(ValueError):
-        pil_to_data_uri(image)
+        cast_image(invalid_input)
+
+
+def test_uncast_image_with_url():
+    image_url = "https://example.com/image.jpg"
+    result = uncast_image(image_url)
+    assert result == image_url
