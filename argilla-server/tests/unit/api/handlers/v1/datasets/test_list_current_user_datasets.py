0c8eddc0bd800de0dbc4bfaa2321176b0002b12e
Francisco Aranda <francis@argilla.io>
2024-09-17 10:02:21 +0200

    Apply suggestions to test

--- argilla-server/tests/unit/api/handlers/v1/datasets/test_list_current_user_datasets.py
+++ argilla-server/tests/unit/api/handlers/v1/datasets/test_list_current_user_datasets.py
@@ -11,16 +11,32 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import uuid
 
 import pytest
 from httpx import AsyncClient
+from pydantic.schema import timedelta
 
 from argilla_server.enums import DatasetStatus
 from tests.factories import DatasetFactory
 
 
 @pytest.mark.asyncio
-class TestListUserDatasets:
+class TestListCurrentUserDatasets:
+    async def test_list_user_datasets_sorted_by_insterted_at(self, async_client: AsyncClient, owner_auth_header: dict):
+        dataset = await DatasetFactory.create()
+        another_dataset = await DatasetFactory.create(inserted_at=dataset.inserted_at + timedelta(seconds=10))
+
+        response = await async_client.get("/api/v1/me/datasets", headers=owner_auth_header)
+
+        assert response.status_code == 200
+        response_json = response.json()
+
+        datasets = response_json["items"]
+        assert len(datasets) == 2
+        assert datasets[0]["id"] == str(dataset.id)
+        assert datasets[1]["id"] == str(another_dataset.id)
+
     async def test_list_user_datasets_by_name(self, async_client: AsyncClient, owner_auth_header: dict):
         dataset = await DatasetFactory.create(name="dataset-a")
         await DatasetFactory.create(name="dataset-b")
@@ -43,6 +59,23 @@ class TestListUserDatasets:
         response = await async_client.get("/api/v1/me/datasets", params={"status": "draft"}, headers=owner_auth_header)
 
         assert response.status_code == 200
+        response_json = response.json()
+
+        datasets = response_json["items"]
+        assert len(datasets) == 1
+        assert datasets[0]["id"] == str(dataset.id)
+
+    async def test_list_users_datasets_by_workspace_id(self, async_client: AsyncClient, owner_auth_header: dict):
+        dataset = await DatasetFactory.create()
+        await DatasetFactory.create()
+
+        response = await async_client.get(
+            "/api/v1/me/datasets",
+            params={"workspace_id": str(dataset.workspace_id)},
+            headers=owner_auth_header,
+        )
+
+        assert response.status_code == 200, response.json()
         response_json = response.json()
 
         datasets = response_json["items"]
