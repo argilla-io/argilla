f1ae834de5142ec44d7e36b6c1d4ccbe97e0f314
davidberenstein1957 <david.m.berenstein@gmail.com>
2024-09-30 09:45:36 +0200

    feat: Add better token user flow

--- argilla/src/argilla/_helpers/_deploy.py
+++ argilla/src/argilla/_helpers/_deploy.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import time
 import uuid
 from typing import TYPE_CHECKING, Optional, Union
@@ -26,79 +25,43 @@ from argilla._helpers._log import LoggingMixin
 if TYPE_CHECKING:
     from huggingface_hub.hf_api import RepoUrl, SpaceHardware, SpaceStorage  # noqa
 
+    from argilla.client import Argilla
+
 _SLEEP_TIME = 10
 _FROM_REPO_ID = "argilla/argilla-template-space"
 
 
 class SpacesDeploymentMixin(LoggingMixin):
-    def _acquire_hf_token(self, token: Union[str, None]) -> str:
-        """Obtain the Hugging Face authentication token to deploy a space and authenticate."""
-        if token is None:
-            token = get_token()
-        if token is None:
-            token = os.getenv("HF_TOKEN")
-        if token is None:
-            if self._is_interactive():
-                notebook_login()
-            else:
-                login()
-            token = get_token()
-        return token
-
-    def _check_if_running(runtime: SpaceRuntime):
-        """Check the current stage of the space runtime. Simplified to return True when being build."""
-        if runtime.stage in ["RUNNING"]:
-            return False
-        elif runtime.stage in [
-            "RUNNING_APP_STARTING",
-            "RUNNING_BUILDING",
-            "BUILDING",
-            "PAUSED",
-            "STOPPED",
-            "APP_STARTING",
-        ]:
-            return True
-        else:
-            raise ValueError(f"Space configuration is wrong and in state: {runtime.stage}")
-
-    def _check_if_runtime_can_be_build(runtime: SpaceRuntime):
-        """Check the current stage of the space runtime. Simplified to return True when being build."""
-        if runtime.stage in ["RUNNING", "RUNNING_APP_STARTING", "RUNNING_BUILDING", "BUILDING", "APP_STARTING"]:
-            return False
-        elif runtime.stage in ["PAUSED", "STOPPED"]:
-            return True
-        else:
-            raise ValueError(f"Space configuration is wrong and in state: {runtime.stage}")
-
     @classmethod
     def deploy_on_spaces(
         cls,
+        username: Optional[Union[str, None]] = None,
+        password: Optional[Union[str, None]] = "12345678",
+        api_key: Optional[Union[str, None]] = None,
         repo_name: Optional[str] = "argilla",
         org_name: Optional[str] = None,
         token: Optional[str] = None,
         space_storage: Optional[Union[str, "SpaceStorage", None]] = None,
         space_hardware: Optional[Union[str, "SpaceHardware"]] = "cpu-basic",
         private: Optional[Union[bool, None]] = False,
-        username: Optional[Union[str, None]] = "admin",
-        password: Optional[Union[str, None]] = "12345678",
-        api_key: Optional[Union[str, None]] = None,
         overwrite: Optional[Union[bool, None]] = False,
-    ) -> "RepoUrl":
+    ) -> "Argilla":
         """
         Deploys Argilla on Hugging Face Spaces.
         For a full guide check our docs.
 
         Args:
+            username (Optional[Union[str, None]]): The username of the admin user.
+                Defaults to None and is set to the current user of the Hugging Face Hub token.
+            password (Optional[Union[str, None]]): The password of the admin user. Defaults to "12345678".
+            api_key (Optional[Union[str, None]]): The API key of the admin user. Defaults to None.
+                When None, a random API key will be generated using a `uuid.uuid4().hex`.
             repo_name (Optional[str]): The ID of the repository where Argilla will be deployed. Defaults to "argilla".
             org_name (Optional[str]): The name of the organization where Argilla will be deployed. Defaults to None.
             token (Optional[Union[str, SpaceStorage, None]]): The Hugging Face authentication token. Defaults to None.
             space_storage (Optional[Union[str, SpaceStorage, None]]): The persistant storage size for the space. Defaults to None without persistant storage.
             space_hardware (Optional[Union[str, SpaceStorage, None]]): The hardware configuration for the space. Defaults to "cpu-basic" with downtime after 48 hours of inactivity.
             private (Optional[Union[bool, None]]): Whether the space should be private. Defaults to False.
-            username (Optional[Union[str, None]]): The username of the admin user. Defaults to "admin".
-            password (Optional[Union[str, None]]): The password of the admin user. Defaults to "12345678".
-            api_key (Optional[Union[str, None]]): The API key of the admin user. Defaults to None.
-                When None, a random API key will be generated using a `uuid.uuid4().hex`.
             overwrite (Optional[Union[bool, None]]): Whether to overwrite the existing space. Defaults to False.
 
         Returns:
@@ -108,14 +71,15 @@ class SpacesDeploymentMixin(LoggingMixin):
             import argilla as rg
 
             client = rg.Argilla.deploy_on_spaces(
+                username="admin",
+                password="12345678",
+                api_key=None,
                 repo_name="my-space",
                 org_name="my-org",
                 token="hf_ABC123",
                 space_storage="10GB",
                 space_hardware="cpu-basic",
                 private=True,
-                username="admin",
-                password="12345678",
                 overwrite=False,
             )
         """
@@ -123,7 +87,9 @@ class SpacesDeploymentMixin(LoggingMixin):
         api = HfApi(token=token)
 
         # Get the org name from the repo name or default to the current user
-        org_name = org_name or api.whoami(token=token)["name"]
+        token_username = api.whoami(token=token)["name"]
+        username = username or token_username
+        org_name = org_name or token_username
         repo_id = f"{org_name}/{repo_name}"
 
         # Define the secrets for the space
@@ -179,6 +145,43 @@ class SpacesDeploymentMixin(LoggingMixin):
             headers["Authorization"] = f"Bearer {token}"
 
         return cls(api_url=api_url, api_key=api_key, headers=headers)
+
+    def _acquire_hf_token(self, token: Union[str, None]) -> str:
+        """Obtain the Hugging Face authentication token to deploy a space and authenticate."""
+        if token is None:
+            token = get_token()
+        if token is None:
+            if self._is_interactive():
+                notebook_login()
+            else:
+                login()
+            token = get_token()
+        return token
+
+    def _check_if_running(runtime: SpaceRuntime):
+        """Check the current stage of the space runtime. Simplified to return True when being build."""
+        if runtime.stage in ["RUNNING"]:
+            return False
+        elif runtime.stage in [
+            "RUNNING_APP_STARTING",
+            "RUNNING_BUILDING",
+            "BUILDING",
+            "PAUSED",
+            "STOPPED",
+            "APP_STARTING",
+        ]:
+            return True
+        else:
+            raise ValueError(f"Space configuration is wrong and in state: {runtime.stage}")
+
+    def _check_if_runtime_can_be_build(runtime: SpaceRuntime):
+        """Check the current stage of the space runtime. Simplified to return True when being build."""
+        if runtime.stage in ["RUNNING", "RUNNING_APP_STARTING", "RUNNING_BUILDING", "BUILDING", "APP_STARTING"]:
+            return False
+        elif runtime.stage in ["PAUSED", "STOPPED"]:
+            return True
+        else:
+            raise ValueError(f"Space configuration is wrong and in state: {runtime.stage}")
 
     def __repr__(self) -> str:
         if is_notebook() or is_google_colab():
