ccfb37c40da50c812d90a3f4eed13c21d5eb0616
davidberenstein1957 <david.m.berenstein@gmail.com>
2024-10-02 11:04:01 +0200

    feat: only use DEFAULT_USERNAME if no USERNAME is provided

--- argilla-server/docker/argilla-hf-spaces/scripts/start.sh
+++ argilla-server/docker/argilla-hf-spaces/scripts/start.sh
@@ -8,9 +8,9 @@ export OAUTH2_HUGGINGFACE_CLIENT_ID=$OAUTH_CLIENT_ID
 export OAUTH2_HUGGINGFACE_CLIENT_SECRET=$OAUTH_CLIENT_SECRET
 export OAUTH2_HUGGINGFACE_SCOPE=$OAUTH_SCOPES
 
-# Set the space creator name as username if no provided.
+# Set the space creator name as username if no name is provided, if the user is not found, use the provided space author name
 # See https://huggingface.co/docs/hub/en/spaces-overview#helper-environment-variables for more details
-USERNAME=$(curl -s https://huggingface.co/api/users/${SPACES_CREATOR_USER_ID}/overview | jq -r '.user' || echo "$SPACE_AUTHOR_NAME")
-export USERNAME
+DEFAULT_USERNAME=$(curl -s https://huggingface.co/api/users/${SPACES_CREATOR_USER_ID}/overview | jq -r '.user' || echo "$SPACE_AUTHOR_NAME")
+export USERNAME="${USERNAME:-$DEFAULT_USERNAME}"
 
 honcho start
