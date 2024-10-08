8b51b9c8c822e49401289d649b7ed5cb833b6887
Leire Aguirre <leire@argilla.io>
2024-10-08 11:47:22 +0200

    fix Mtetadata creation class

--- argilla-frontend/v1/domain/entities/hub/MetadataCreation.ts
+++ argilla-frontend/v1/domain/entities/hub/MetadataCreation.ts
@@ -1,9 +1,9 @@
 export type MetadataTypes = "int32" | "int64" | "float32" | "float64";
 export class MetadataCreation {
+  public title: string;
   public constructor(
     public readonly name: string,
-    public readonly type: MetadataTypes,
-    public title: string
+    public readonly type: MetadataTypes
   ) {
     this.title = this.name;
   }
