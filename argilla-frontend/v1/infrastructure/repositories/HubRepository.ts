8b4a3141f34acb92aeca0c9036a2fa3539f32829
Francisco Aranda <francis@argilla.io>
2024-10-23 15:16:13 +0200

    chore: API Call to fetch split stats

--- argilla-frontend/v1/infrastructure/repositories/HubRepository.ts
+++ argilla-frontend/v1/infrastructure/repositories/HubRepository.ts
@@ -24,6 +24,55 @@ export class HubRepository {
     }
   }
 
+  async getColumnsDistributions(
+    repoId: string,
+    subset: string,
+    split: string
+  ): Promise<any> {
+    // Example call https://datasets-server.huggingface.co/statistics?dataset=derek-thomas/ScienceQA&config=default&split=train
+    const { data } = await this.axios.get(
+      "https://datasets-server.huggingface.co/statistics",
+      {
+        params: {
+          dataset: encodeURIComponent(repoId),
+          config: subset,
+          split,
+        },
+      }
+    );
+
+    const distributions = [];
+    for (const columnStats of data.statistics) {
+      const columnDistribution = {
+        name: columnStats.column_name,
+        type: columnStats.column_type,
+        values: null,
+      };
+
+      const stats = columnStats.column_statistics;
+
+      switch (columnDistribution.type) {
+        // Helpful for ratings
+        case "int":
+          columnDistribution.values = { min: stats.min, max: stats.max };
+          break;
+        // Helpful for label questions or terms metadata
+        case "string_label":
+          columnDistribution.values = [];
+          for (const key of stats.frequencies) {
+            columnDistribution.values.push(key);
+          }
+          break;
+        default:
+          // For other cases, the values will be null
+          break;
+      }
+      distributions.push(columnDistribution);
+    }
+
+    return distributions;
+  }
+
   async getFirstRecord(dataset: DatasetCreation): Promise<any> {
     try {
       const { repoId, selectedSubset } = dataset;
