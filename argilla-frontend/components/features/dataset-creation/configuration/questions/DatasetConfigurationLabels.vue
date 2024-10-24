0b666990436ecba59cde9f25cc19c0da970f8187
Leire Aguirre <leire@argilla.io>
2024-10-24 10:30:56 +0200

    update card error

--- argilla-frontend/components/features/dataset-creation/configuration/questions/DatasetConfigurationLabels.vue
+++ argilla-frontend/components/features/dataset-creation/configuration/questions/DatasetConfigurationLabels.vue
@@ -95,6 +95,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$error-color: hsl(3, 100%, 69%);
 .dataset-config-label {
   &__input-container {
     width: 100%;
@@ -122,8 +123,8 @@ export default {
   &__label {
     color: var(--fg-secondary);
     @include font-size(12px);
-    &--error {
-      color: var(--fg-error);
+    &.--error {
+      color: $error-color;
     }
   }
 }
