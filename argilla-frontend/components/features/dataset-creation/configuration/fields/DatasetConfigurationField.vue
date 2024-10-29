4b8551f738b95d99683792a99b5d815e03acb153
Leire Aguirre <leire@argilla.io>
2024-10-29 08:31:11 +0100

    remove required visualization for fields

--- argilla-frontend/components/features/dataset-creation/configuration/fields/DatasetConfigurationField.vue
+++ argilla-frontend/components/features/dataset-creation/configuration/fields/DatasetConfigurationField.vue
@@ -5,14 +5,6 @@
     :available-types="availableTypes"
     @is-focused="$emit('is-focused', $event)"
   >
-    <BaseCheckbox
-      class="config-card__required"
-      v-if="!hasNoMapping"
-      :value="field.required"
-      @input="field.required = !field.required"
-    >
-      {{ $t("datasetCreation.requiredField") }}</BaseCheckbox
-    >
   </DatasetConfigurationCard>
 </template>
 
@@ -39,19 +31,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.config-card__required {
-  display: flex;
-  gap: $base-space;
-  margin-top: $base-space;
-  @include font-size(12px);
-  flex-flow: row-reverse;
-  justify-content: flex-end;
-  &:deep(.checkbox__container) {
-    margin: 0;
-    border-color: var(--bg-opacity-20);
-    background: var(--bg-accent-grey-1);
-  }
-}
-</style>
