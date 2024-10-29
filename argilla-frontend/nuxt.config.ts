ca60d0bed82d574408f706c9d359c2980d2a8034
Francisco Aranda <francis@argilla.io>
2024-10-29 12:09:13 +0100

    chore: Revert me

--- argilla-frontend/nuxt.config.ts
+++ argilla-frontend/nuxt.config.ts
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// Include comment to force buils
 
 import Mode from "frontmatter-markdown-loader/mode";
 import { NuxtConfig } from "@nuxt/types";
