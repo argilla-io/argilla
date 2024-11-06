6a5fcd5d8f754708fc50c387a4d75cdfde512e6c
Francisco Aranda <francis@argilla.io>
2024-11-06 09:52:37 +0100

    fix: Pop instead of get redirectTo value

--- argilla-frontend/pages/oauth/_provider/useOAuthViewModel.ts
+++ argilla-frontend/pages/oauth/_provider/useOAuthViewModel.ts
@@ -2,9 +2,12 @@ import { useFetch, useRoute } from "@nuxtjs/composition-api";
 import { useResolve } from "ts-injecty";
 import { ProviderType } from "~/v1/domain/entities/oauth/OAuthProvider";
 import { OAuthLoginUseCase } from "~/v1/domain/usecases/oauth-login-use-case";
-import { useRoutes, useTranslate } from "~/v1/infrastructure/services";
+import {
+  useRoutes,
+  useTranslate,
+  useLocalStorage,
+} from "~/v1/infrastructure/services";
 import { useNotifications } from "~/v1/infrastructure/services/useNotifications";
-import { useLocalStorage } from "~/v1/infrastructure/services";
 
 export const useOAuthViewModel = () => {
   const { t } = useTranslate();
@@ -12,14 +15,14 @@ export const useOAuthViewModel = () => {
   const routes = useRoute();
   const router = useRoutes();
   const oauthLoginUseCase = useResolve(OAuthLoginUseCase);
-  const { get } = useLocalStorage();
+  const { pop } = useLocalStorage();
 
   useFetch(async () => {
     await tryLogin();
   });
 
   const redirect = () => {
-    const redirect = get("redirectTo");
+    const redirect = pop("redirectTo");
     router.go(redirect || "/");
   };
 
--- argilla-frontend/v1/infrastructure/services/useLocalStorage.ts
+++ argilla-frontend/v1/infrastructure/services/useLocalStorage.ts
@@ -34,8 +34,15 @@ export const useLocalStorage = () => {
     } catch {}
   };
 
+  const pop = (key: Options) => {
+    const value = get(key);
+    set(key, null);
+    return value;
+  };
+
   return {
     get,
     set,
+    pop,
   };
 };
