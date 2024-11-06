5ed25fdda6277125f193d104184e45ddd76909f9
Leire Aguirre <leire@argilla.io>
2024-11-06 12:34:57 +0100

    redirect to error page

--- argilla-frontend/pages/useNewDatasetViewModel.ts
+++ argilla-frontend/pages/useNewDatasetViewModel.ts
@@ -1,15 +1,20 @@
 import { useResolve } from "ts-injecty";
-import { ref, useRoute } from "@nuxtjs/composition-api";
+import { ref, useRoute, useContext } from "@nuxtjs/composition-api";
 import { GetDatasetCreationUseCase } from "~/v1/domain/usecases/get-dataset-creation-use-case";
 
 export const useNewDatasetViewModel = () => {
+  const { error } = useContext();
   const datasetConfig = ref();
   const getDatasetCreationUseCase = useResolve(GetDatasetCreationUseCase);
 
   const getNewDatasetByRepoId = async (repositoryId: string) => {
-    datasetConfig.value = await getDatasetCreationUseCase.execute(
-      decodeURI(repositoryId)
-    );
+    try {
+      datasetConfig.value = await getDatasetCreationUseCase.execute(
+        repositoryId
+      );
+    } catch (e) {
+      error({ statusCode: e.status, message: e.response });
+    }
   };
 
   const getNewDatasetByRepoIdFromUrl = async () => {
