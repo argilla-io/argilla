783c7c09bdddcb91d7caa5d57739f506f44f3c60
Leire <leire@argilla.io>
2024-11-08 12:11:57 +0100

    Dataset Creation UI fixes & Improvements (#5670)
    - [x] Rating values validate min / max
    - [x] Question name format and renaming
    - [x] Minor style adjustments 
    - [x] Show required / not required in the form preview
    - [x] Redirect from /new/repoId to /repoId
    - [x] Redirect to error page when repoId is invalid
    
    ---------
    
    Co-authored-by: Francisco Aranda <francis@argilla.io>
    Co-authored-by: Paco Aranda <frascuchon@gmail.com>
    Co-authored-by: José Francisco Calvo <jose@argilla.io>
    Co-authored-by: José Francisco Calvo <josefranciscocalvo@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ argilla-frontend/pages/new/_id.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+export default {
+  middleware({ route, redirect }) {
+    if (route.params.id) {
+      const newRoute = encodeURIComponent(route.params.id);
+      redirect(newRoute);
+    } else {
+      redirect("/");
+    }
+  },
+};
+</script>
