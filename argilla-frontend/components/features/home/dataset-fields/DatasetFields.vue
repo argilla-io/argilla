a8955515af03d5e0650ecc6bcf114eff185f1587
Leire Aguirre <leire@argilla.io>
2024-11-08 14:14:33 +0100

    get field types

---
+++ argilla-frontend/components/features/home/dataset-fields/DatasetFields.vue
@@ -0,0 +1,29 @@
+<template>
+  <BaseBadgeSkeleton v-if="isFieldsLoading" :quantity="3" />
+  <div class="fields" v-else>
+    <BaseBadge v-for="field in fields" :key="field.id" :text="$t(field.type)" />
+  </div>
+</template>
+<script>
+import { useDatasetFields } from "./useDatasetFields";
+
+export default {
+  name: "DatasetFields",
+  props: {
+    dataset: {
+      type: Object,
+    },
+  },
+  setup(props) {
+    return useDatasetFields(props);
+  },
+};
+</script>
+
+<style scoped>
+.fields {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+}
+</style>
