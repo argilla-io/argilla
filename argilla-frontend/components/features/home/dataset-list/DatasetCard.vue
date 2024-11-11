dde97d82915306ee3d98938bcd6f8ac254af2ec5
Leire Aguirre <leire@argilla.io>
2024-11-11 09:06:29 +0100

    update card

--- argilla-frontend/components/features/home/dataset-list/DatasetCard.vue
+++ argilla-frontend/components/features/home/dataset-list/DatasetCard.vue
@@ -3,38 +3,34 @@
     <div class="dataset-card__header">
       <p class="dataset-card__workspace">{{ dataset.workspaceName }}</p>
       <div class="dataset-card__actions">
+        <BaseButton class="dataset-card__action" @click="$emit('copy-name')"
+          ><svgicon name="copy"
+        /></BaseButton>
+        <BaseButton class="dataset-card__action" @click="$emit('copy-url')"
+          ><svgicon name="link"
+        /></BaseButton>
         <BaseButton
           class="dataset-card__action"
-          icon="copy"
-          @click="$emit('copy-name')"
-          title="Copy name to clipboard"
-        />
-        <BaseButton
-          class="dataset-card__action"
-          icon="link"
-          @click="$emit('copy-url')"
-          title="Copy link to clipboard"
-        />
-        <BaseButton
-          class="dataset-card__action"
-          icon="settings"
           @click="$emit('go-to-settings')"
-          title="Go to dataset settings"
-        />
+          ><svgicon name="settings"
+        /></BaseButton>
       </div>
-      <div class="dataset-card__content">
-        <h1 class="dataset-card__title">{{ dataset.name }}</h1>
-        <DatasetProgress :datasetId="dataset.id" />
-        <DatasetFields :dataset="dataset" />
-      </div>
-      <div class="dataset-card__footer">
-        <p class="dataset-card__date">{{ dataset.updatedAt }}</p>
-      </div>
+    </div>
+    <div class="dataset-card__content">
+      <h1 class="dataset-card__title">{{ dataset.name }}</h1>
+      <DatasetProgress :datasetId="dataset.id" />
+      <DatasetFields :dataset="dataset" />
+    </div>
+    <div class="dataset-card__footer">
+      <p class="dataset-card__date">{{ dataset.updatedAt }}</p>
     </div>
   </div>
 </template>
 
 <script>
+import "assets/icons/copy";
+import "assets/icons/link";
+import "assets/icons/settings";
 export default {
   props: {
     dataset: {
@@ -54,5 +50,25 @@ export default {
   border: 1px solid var(--bg-opacity-10);
   border-radius: $border-radius-m;
   padding: $base-space * 2;
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: $base-space;
+  }
+  &__actions {
+    display: flex;
+    justify-content: flex-end;
+    gap: $base-space;
+  }
+  &__action {
+    &.button {
+      padding: $base-space;
+      color: var(--fg-secondary);
+      &:hover {
+        background: var(--bg-opacity-4);
+      }
+    }
+  }
 }
 </style>
