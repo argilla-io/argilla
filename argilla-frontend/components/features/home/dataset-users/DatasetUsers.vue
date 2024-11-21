f4017ac2c4fbc47a524cba314a0079cdc94d8e1f
Leire Aguirre <leire@argilla.io>
2024-11-21 11:17:45 +0100

    add visibleBadges

--- argilla-frontend/components/features/home/dataset-users/DatasetUsers.vue
+++ argilla-frontend/components/features/home/dataset-users/DatasetUsers.vue
@@ -2,22 +2,22 @@
   <div class="dataset-users__wrapper" @mouseleave="expanded = false">
     <div class="dataset-users">
       <BaseTooltip
-        v-for="user in users.slice(0, 3)"
+        v-for="user in users.slice(0, visibleBadges)"
         :key="user.name"
         :text="user.name"
       >
         <UserBadge class="dataset-users__item" :name="user.name" />
       </BaseTooltip>
     </div>
-    <template v-if="users.length > 3">
+    <template v-if="users.length > visibleBadges">
       <BaseButton
         @mouseenter.native="expanded = true"
         class="dataset-users__button"
-        >+{{ users.length - 3 }}</BaseButton
+        >+{{ users.length - visibleBadges }}</BaseButton
       >
       <div v-if="expanded" class="dataset-users__rest">
         <BaseTooltip
-          v-for="user in users.slice(3, users.length)"
+          v-for="user in users.slice(visibleBadges, users.length)"
           :key="user.name"
           :text="user.name"
         >
@@ -31,6 +31,7 @@
 export default {
   data: () => {
     return {
+      visibleBadges: 3,
       expanded: false,
       users: [
         { name: "John" },
