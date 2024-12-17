b445643cf0f73fb18fe4aa6f0a42581191648021
Damián Pumar <damianpumar@gmail.com>
2024-12-17 09:44:22 +0100

    📝 Improve font loading

--- argilla-frontend/nuxt.config.ts
+++ argilla-frontend/nuxt.config.ts
@@ -53,11 +53,7 @@ const config: NuxtConfig = {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    "~assets/css/fonts.css",
-    "~assets/css/themes.css",
-    "~assets/scss/base/base.scss",
-  ],
+  css: ["~assets/styles.scss"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [{ src: "~/plugins" }],
